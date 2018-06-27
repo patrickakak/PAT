147bb263afd7ee111f5a0d2b5c82352909eceb43
ziwei woo <13806680065@163.com>
2018-06-27 22:40:46 +0800

    Update PostOrderGetHeight.c

--- cn_version/extra/PostOrderGetHeight.c
+++ cn_version/extra/PostOrderGetHeight.c
@@ -7,7 +7,7 @@ int PostOrderGetHeight( BinTree BT )
 		HL = PostOrderGetHeight(BT->Left);	// To get the depth of left subtree
 		HR = PostOrderGetHeight(BT->Right);	// To get the depth of right one
 		MaxH = HL > HR ? HL : HR;	// To take the bigger one
-		return (MaxH + 1);	// Return depth of subtree
+		return (MaxH + 1);	// Return depth of tree
 	} else 
 		return 0;	// Empty subtree is depth of 0
 }
