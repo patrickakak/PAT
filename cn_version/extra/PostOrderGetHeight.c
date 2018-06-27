3ca65ef9f4faa7de8669d21b743503d3f17dd8da
zia <13806680065@163.com>
2018-06-27 22:38:46 +0800

    PostOrderGetHeight.c

---
+++ cn_version/extra/PostOrderGetHeight.c
@@ -0,0 +1,14 @@
+// Post order traversal to get the height of binary tree
+int PostOrderGetHeight( BinTree BT )
+{
+	int HL, HR, MaxH;
+	
+	if (BT) {
+		HL = PostOrderGetHeight(BT->Left);	// To get the depth of left subtree
+		HR = PostOrderGetHeight(BT->Right);	// To get the depth of right one
+		MaxH = HL > HR ? HL : HR;	// To take the bigger one
+		return (MaxH + 1);	// Return depth of subtree
+	} else 
+		return 0;	// Empty subtree is depth of 0
+}
+
