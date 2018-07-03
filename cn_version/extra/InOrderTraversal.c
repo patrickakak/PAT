5fef59de878b090a708598c581b102b4ebe901f9
ziwei woo <13806680065@163.com>
2018-07-03 23:50:42 +0800

    Update InOrderTraversal.c

--- cn_version/extra/InOrderTraversal.c
+++ cn_version/extra/InOrderTraversal.c
@@ -1,4 +1,11 @@
 // Non-recursive binary tree InOrderTraversal
+// Brief algorithm:
+//   1. Encounter a node, push it into the stack and 
+//      go through its left subtree
+//   2. After the left subtree has been traversed, 
+//      pop the node out and access it
+//   3. Then traverse the right subtree of the node 
+//      by its right pointer(InOrderTraversal)
 void InOrderTraversal( BinTree BT )
 {
 	BinTree T = BT;
@@ -12,6 +19,7 @@ void InOrderTraversal( BinTree BT )
 		// If T is not NULL
 		while (T) {		// Go left and push treenodes into stack along the way
 			Push(S, T);
+			// printf("%5d", T->Data); // PreOrderTraversal accessing
 			T = T->Left;
 		}
 		// If stack is not empty
@@ -23,3 +31,26 @@ void InOrderTraversal( BinTree BT )
 	}
 }
 
+// Non-recursive postOrder traversal
+void PostOrderTraversal( BinTree BT )
+{
+	BinTree T = BT;
+	Stack S = CreatStack(MaxSize);
+	Stack S2 = CreatStack(MaxSize);
+
+	// PostOrder: Left -> Right -> Root (Root -> Right -> Left)
+	while (T || !IsEmpty(S)) {
+		while (T) {
+			Push(S, T);
+			// printf("%d", T->Data);
+			Push(S2, T);
+			T = T->Right;
+		}
+		if (!IsEmpty(S)) {
+			T = Pop(S);
+			T = T->Left;
+		}
+	}
+
+	while (!IsEmpty(S2)) { T = Pop(S2); printf("%d", T->Data); }
+}
