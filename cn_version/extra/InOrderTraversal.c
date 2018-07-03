c1a9b59ac8aaea82b53824a8a68e218834dd1830
zia <13806680065@163.com>
2018-07-03 22:35:56 +0800

    Update InOrderTraversal.c

--- cn_version/extra/InOrderTraversal.c
+++ cn_version/extra/InOrderTraversal.c
@@ -3,11 +3,18 @@ void InOrderTraversal( BinTree BT )
 {
 	BinTree T = BT;
 	Stack S = CreateStack(MaxSize);		// Create and initialize stack
+
+	// If T is not NULL or there is(are) element(s) inside the stack, 
+	// go on with the loop, i.e. if T is NULL and stack is empty, 
+	// it ends(or never enter the loop)
+
 	while (T || !IsEmpty(S)) {
+		// If T is not NULL
 		while (T) {		// Go left and push treenodes into stack along the way
 			Push(S, T);
 			T = T->Left;
 		}
+		// If stack is not empty
 		if (!IsEmpty(S)) {
 			T = Pop(S);		// Pop a treenode out
 			printf("%5d", T->Data);		// Access data in treenode
