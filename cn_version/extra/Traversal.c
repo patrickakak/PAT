4039a98bfb7a2dff8f350623385fbfa120de536d
zia <13806680065@163.com>
2018-07-03 22:01:11 +0800

    Add Traversal.c

--- cn_version/extra/Traversal.c
+++ cn_version/extra/Traversal.c
@@ -9,11 +9,29 @@ struct TreeNode {
 // Binary tree preorder traversal
 void PreOrderTraversal( BinTree BT )
 {
-	// If the tree is empty, do nothing
+	// If BT is empty, do nothing
 	if (BT) {
 		printf("%d", BT->Data);	// Visit data in root node
 		PreOrderTraversal(BT->Left);	// PreOrderTraversal recursively
 		PreOrderTraversal(BT->Right);
+	}
+}
+
+void InOrderTraversal( BinTree BT )
+{
+	if (BT) {
+		InOrderTraversal(BT->Left);
+		printf("%d", BT->Data);
+		InOrderTraversal(BT->Right);
+	}
+}
+
+void PostOrderTraversal( BinTree BT )
+{
+	if (BT) {
+		PostOrderTraversal(BT->Left);
+		PostOrderTraversal(BT->Right);
+		printf("%d", BT->Data);
 	}
 }
 
