260a059f42dc9ca28bf9750a1c664bd60708dacb
ziwei woo <13806680065@163.com>
2018-07-07 22:58:15 +0800

    Create Isomorphic_6_7.c

---
+++ en_version/Code_Completion/Isomorphic_6_7.c
@@ -0,0 +1,21 @@
+int Isomorphic(Tree T1, Tree T2)
+{
+	if ((T1 == NULL) && (T2 == NULL)) // Both empty
+		return 1;
+	if (((T1 == NULL) && (T2 != NULL)) || ((T2 == NULL) && (T1 != NULL)))
+		return 0; // One of them is empty
+	if (T1->Element != T2->Element)
+		return 0; // Roots are different
+	if ((T1->Left == NULL) && (T2->Left == NULL))
+		return Isomorphic(T1->Right, T2->Right); // Both have no left subtree
+	if (((T1->Left != NULL) && (T2->Left != NULL))
+			&& (T1->Left->Element == T2->Left->Element))
+		// No need to swap the left and the right
+		return (Isomorphic(T1->Left, T2->Left)
+				&& Isomorphic(T1->Right, T2->Right));
+	// Need to swap the left and the right
+	else
+		return (Isomorphic(T1->Left, T2->Right)
+				&& Isomorphic(T1->Right, T2->Left));
+}
+
