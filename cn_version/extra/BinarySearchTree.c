7a47f178617c9aaccb3f00ddb77026dbbdeffc20
zia <13806680065@163.com>
2018-08-06 21:50:05 +0800

    Update BinarySearchTree.c

--- cn_version/extra/BinarySearchTree.c
+++ cn_version/extra/BinarySearchTree.c
@@ -6,7 +6,7 @@ Position Find(ElementType X, BinTree BST)
 {
 	if (!BST) return NULL;	// Empty tree
 	if (X > BST->Data)
-		return Find(X, BST->Right);		// Continue seeking in right subtree
+		return Find(X, BST->Right);	// Continue seeking in right subtree
 	else if (X < BST->Data)
 		return Find(X, BST->Left);	// Continue seeking in left subtree
 	else	// X == BST->Data
@@ -43,6 +43,46 @@ Position FindMax(BinTree BST)
 	if (BST)
 		while (BST->Right)	// Keep seeking the rightest leaf node
 			BST = BST->Right;
+	return BST;
+}
+
+BinTree Insert(ElementType X, BinTree BST)
+{
+	if (!BST) {		// If empty tree, generate a node
+		BST = malloc(sizeof(struct TreeNode));
+		BST->Data = X;
+		BST->Left = BST->Right = NULL;
+	} else	// Start to find out the position to insert
+		if (X < BST->Data)	// Insert into left subtree recursively
+			BST->Left = Insert(X, BST->Left);
+		else (X > BST->Data)	// ... right subtree recursively
+			BST->Right = Insert(X, BST->Right);
+
+	return BST;
+}
+
+BinTree Delete(ElementType X, BinTree BST)
+{
+	Position Tmp;
+
+	if (!BST) printf("Couldn't find the element\n");
+	else if (X < BST->Data)	 // Left subtree deletion recursively
+		BST->Left = Delete(X, BST->Left);
+	else if (X > BST->Data)		// Right subtree deletion recursively
+		BST->Right = Delete(X, BST->Right);
+	else	// Find the node
+		if (BST->Left && BST->Right) { // The node has two children
+			Tmp = FindMin(BST->Right);	// Find the minimum in right subtree to replace the node
+			BST->Data = Tmp->Data;
+			BST->Right = Delete(BST->Data, BST->Right); // Delete the minimum in the right subtree
+		} else {	// There is only one child or no child at all of this node
+			Tmp = BST;
+			if (!BST->Left)		// There's right child or no child
+				BST = BST->Right;
+			else if (!BST->Right) // There's left child or no child
+				BST = BST->Left;
+			free(Tmp);
+		}
 	return BST;
 }
 
