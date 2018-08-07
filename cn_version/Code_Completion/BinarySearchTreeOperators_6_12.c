5b03c9cfc0210ab10a2df6d28e064446a5b4015d
zia <13806680065@163.com>
2018-08-07 14:09:23 +0800

    Add BinarySearchTreeOperators_6_12.c

---
+++ cn_version/Code_Completion/BinarySearchTreeOperators_6_12.c
@@ -0,0 +1,72 @@
+#define EMPTY NULL
+
+BinTree Insert( BinTree BST, ElementType X )
+{
+	if (!BST) {
+		BST = (BinTree) malloc(sizeof(struct TNode));
+		BST->Data = X;
+		BST->Left = BST->Right = NULL;
+	} else
+		if (X < BST->Data)
+			BST->Left = Insert(BST->Left, X);
+		else if (X > BST->Data)
+			BST->Right = Insert(BST->Right, X);
+
+	return BST;
+}
+
+BinTree Delete( BinTree BST, ElementType X )
+{
+	Position Tmp;
+
+	if (!BST) printf("Not Found\n");
+	else if (X < BST->Data)
+		BST->Left = Delete(BST->Left, X);
+	else if (X > BST->Data)
+		BST->Right = Delete(BST->Right, X);
+	else
+		if (BST->Left && BST->Right) {
+			Tmp = FindMax(BST->Left);
+			BST->Data = Tmp->Data;
+			BST->Left = Delete(BST->Left, BST->Data);
+		} else {
+			Tmp = BST;
+			if (!BST->Left)
+				BST = BST->Right;
+			else if (!BST->Right)
+				BST = BST->Left;
+			free(Tmp);
+		}
+	return BST;
+}
+
+Position Find( BinTree BST, ElementType X )
+{
+	while (BST) {
+		if (X < BST->Data)
+			BST = BST->Left;
+		else if (X > BST->Data)
+			BST = BST->Right;
+		else
+			return BST;
+	}
+	return EMPTY;
+}
+
+Position FindMin( BinTree BST )
+{
+	if (!BST) return EMPTY;
+	if (!BST->Left)
+		return BST;
+	else
+		return FindMin(BST->Left);
+}
+
+Position FindMax( BinTree BST )
+{
+	if (BST)
+		while (BST->Right)
+			BST = BST->Right;
+	return BST;
+}
+
