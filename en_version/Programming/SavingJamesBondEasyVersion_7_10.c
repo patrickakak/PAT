4cbfd312c54b24f21c1c8508bbb4bb5e5cbdf4ee
Zia <13806680065@163.com>
2018-10-04 14:24:04 +0800

    Modify SavingJamesBondEasyVersion_7_10.c

--- en_version/Programming/SavingJamesBondEasyVersion_7_10.c
+++ en_version/Programming/SavingJamesBondEasyVersion_7_10.c
@@ -1,78 +1,102 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-#define R 15		/* Radius of the island circle */
-#define M2B 50		/* Maximum length to the bank */
-#define MAX 100		/* Maximum number of crocdiles */
+#define RoI 15	/* Radius of central island */
+#define D2B 50	/* Distance from coordinate origin to bank */
+#define MaxCrocs 100	/* Maximun number of crocdiles */
 
-typedef struct Coord_st {
+#define YES 1
+#define NO 0
+
+typedef struct coordinate_st Vertex;
+struct coordinate_st {
 	int x, y;
-} Coordinate;
+};
 
-int FirstJump( int step, Coordinate c );
-int Within1Step( int step, Coordinate c1, Coordinate c2 );
-int Jump2Bank( int step, Coordinate c );
-int DFS( int step, Coordinate *croc, int cur, int *visited, int n );
+void Save007(Vertex Crocs[], int N, int step);
+int FirstJump(int step, Vertex V);
+int IsSafe(Vertex V, int step);
+int DFS(int curVert, Vertex Crocs[], int N, int step, bool visited[]);
+int Jump(Vertex cur, Vertex nearby, int step);
 
-int main() 
+int main()
 {
-	int n, i, step, flag = 0, visited[MAX] = {0};	/* flag to indicate done finding path */
-	Coordinate croc[MAX];	/* To store the coordinates of crodiles */
+	int N, step, i;
+	Vertex Crocs[MaxCrocs];
 
-	scanf("%d%d", &n, &step);
+	scanf("%d %d", &N, &step);
 
-	/* Can reach bank directly */
-	if (step >= M2B - R) { printf("Yes\n"); return 0; }
+	if (step+RoI >= D2B) {	/* Can reach bank directly */
+		printf("Yes\n");
+		exit(EXIT_SUCCESS);
+	}
+	for (i = 0; i < N; i++)
+		scanf("%d %d", &Crocs[i].x, &Crocs[i].y);
 
-	for (i = 0; i < n; ++i) scanf("%d%d", &croc[i].x, &croc[i].y);
-
-	for (i = 0; i < n; ++i)
-		/* Traverse all the crocs that can be jumped onto from the island */
-		if (!visited[i] && FirstJump(step, croc[i])) {
-			visited[i] = 1;
-			if (DFS(step, croc, i, visited, n)) {	/* Begin DFS in the current coordinate */
-				printf("Yes\n");
-				flag = 1;
-				break;
-			}
-		}
-	if (!flag) printf("No\n");
-
+	Save007(Crocs, N, step);
 	return 0;
+}
+
+/* List components in the graph */
+void Save007(Vertex Crocs[], int N, int step)
+{
+	int i, answer;
+	bool visited[MaxCrocs];
+
+	memset(visited, false, sizeof(visited));
+	/* Traverse all the crocs that can be jumped onto from the island */
+	for (i = 0; i < N; i++)
+		if (!visited[i] && FirstJump(step, Crocs[i])) {
+			answer = DFS(i, Crocs, N, step, visited);
+			if (answer == YES) break;
+		}
+
+	if (answer == YES) printf("Yes\n");
+	else printf("No\n");
 }
 
 /* Whether James can jump onto a crocdile's head in one step from the island? */
-int FirstJump( int step, Coordinate c )
+int FirstJump(int step, Vertex V)
 {
-	return (R + step) * (R + step) >= (c.x * c.x + c.y * c.y);
+	return (RoI + step)*(RoI + step) >= (V.x * V.x)+(V.y * V.y);
 }
 
-/* Whether there's a change to jump onto another crocdile? */
-int Within1Step( int step, Coordinate c1, Coordinate c2 )
+/* Depth first seaching recursively, return YES if the path is found */
+int DFS(int curVertIndex, Vertex Crocs[], int N, int step, bool visited[])
 {
-	return step * step >= 
-		(c1.x - c2.x) * (c1.x - c2.x) + (c1.y - c2.y) * (c1.y - c2.y);
+	int answer, i;
+	Vertex W;
+
+	visited[curVertIndex] = true;
+	if (IsSafe(Crocs[curVertIndex], step)) answer = YES;
+	else
+		for (i = 0; i < N; i++) {
+			W = Crocs[i];
+			if (!visited[i] && Jump(Crocs[curVertIndex], W, step)) {
+				answer = DFS(i, Crocs, N, step, visited);
+				if (answer == YES) break;
+			}
+		}
+	return answer;
 }
 
 /* If James can jump to bank in current position? */
-int Jump2Bank( int step, Coordinate c )
+int IsSafe(Vertex V, int step)
 {
-	return c.x <= -M2B + step || c.x >= M2B - step 
-		|| c.y >= M2B - step || c.y <= -M2B + step;
+	int AbsX, AbsY;
+
+	AbsX = V.x > 0 ? V.x : -V.x;
+	AbsY = V.y > 0 ? V.y : -V.y;
+
+	return AbsX+step >= D2B || AbsY+step >= D2B;
 }
 
-/* Depth first seaching recursively, return 1 if the path is found */
-int DFS( int step, Coordinate *croc, int cur, int *visited, int n )
+/* Whether there's a change to jump onto another crocdile? */
+int Jump(Vertex cur, Vertex nearby, int step)
 {
-	int i;
-
-	if (Jump2Bank(step, croc[cur])) return 1;
-
-	for (i = 0; i < n; ++i)
-		if (!visited[i] && Within1Step(step, croc[cur], croc[i])) {
-			visited[i] = 1;
-			if (DFS(step, croc, i, visited, n)) return 1;
-		}
-	return 0;
+	return step*step >= (cur.x - nearby.x)*(cur.x - nearby.x) + (
+			cur.y - nearby.y)*(cur.y - nearby.y);
 }
-
 
