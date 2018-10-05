e447e9f19b2777c85db76ab1051380e8cd2c4dc8
Zia <13806680065@163.com>
2018-10-05 09:32:39 +0800

    Modify SingleSourceShortestPathProblem_weighted.c

--- cn_version/extra/SingleSourceShortestPathProblem_weighted.c
+++ cn_version/extra/SingleSourceShortestPathProblem_weighted.c
@@ -1,13 +1,25 @@
 /* Adjacency matrix: weighted signle source shortest path problem */
 
-/* Return the minimum element of array dist (among uncollected vertices) */
+/* Return the minimum element of array dist (among uncollected vertices)
+ *
+ * Critical implementatons:
+ *     a. To get minimun dist value among the uncollected 
+ *     b. Update dist[w] (w is a vertex affected by v)
+ * ----------------------- Method 1 ------------------------------------------
+ *   Scan all the uncollected vertices, in this way, Ta=O(|V|^2), Tb=O(|E|), 
+ *   suit for sparse graph(E = O(|V|^2)). Total T = O(|V|^2 + |E|)
+ *
+ * ----------------------- Method 2 ------------------------------------------
+ *   Also, one can implement function FindMinDist() by using MinHeap, 
+ *   Ta=O(|V|log|V|), Tb=O(|E|log|V|), suit for dense graph, 
+ *   Total T = O((|V|+|E|)log|V|) ~ O(|E|log|V|) */
 Vertex FindMinDist(MGraph Graph, int dist[], bool collected[])
 {
 	Vertex MinV, V;
 	int MinDist = INFINITY;
 
 	for (V = 0; V < Graph->Nv; V++)
-		 /* If V has not been colloected and dist[V] is smaller */
+		/* If V has not been colloected and dist[V] is smaller */
 		if (collected[V] == false && dist[V] < MinDist) {
 			MinDist = dist[V];	/* Update minimum distance */
 			MinV = V;	/* Update corresponding vertex */
@@ -17,6 +29,8 @@ Vertex FindMinDist(MGraph Graph, int dist[], bool collected[])
 	else return ERROR;	/* If no such vertex, return ERROR */
 }
 
+/* Dijkstra's algorithm solves the single-source shortest path problem 
+ * with non-negative edge weight. */
 bool Dijkstra(MGraph Graph, int dist[], int path[], Vertex S)
 {
 	bool collected[MaxVertexNum];
@@ -47,12 +61,11 @@ bool Dijkstra(MGraph Graph, int dist[], int path[], Vertex S)
 					return false;		/* Can't handle it properly, return error */
 				/* If colloecting V can diminish dist[W] */
 				if (dist[V]+Graph->G[V][W] < dist[W]) {
-					dist[W] = dist[V]+Graph->G[V][W];	/* Update dist[W] */
+					dist[W] = dist[V] + Graph->G[V][W];		/* Update dist[W] */
 					path[W] = V;	/* Update path of S to W */
 				}
 			}
 	}
 	return true;	/* End of algorithm, data handled properly */
 }
-
 
