ac1ea8a318fa8b7c512ddb9dd95c6be4fd6db9c1
zia1995 <13806680065@163.com>
2018-10-16 16:06:02 +0800

    Comment on MaximumSubsequenceSum_7_1.c

--- en_version/Programming/MaximumSubsequenceSum_7_1.c
+++ en_version/Programming/MaximumSubsequenceSum_7_1.c
@@ -5,58 +5,59 @@
  * 10 1 4
  */
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MAXK 100000
-struct outcome {
+typedef struct OutNode *PtrToOutNode;
+struct OutNode {
 	int first, last;
 	int MaxSum;
 };
 
-struct outcome MaxSubseqSum4(int A[], int N);
+PtrToOutNode MaxSubseqSum4(int A[], int N);
 
 int main()
 {
 	int K, i, a[MAXK+1];
-	struct outcome res;
+	PtrToOutNode out;
 
 	scanf("%d", &K);
 	for (i = 0; i < K; i++)
 		scanf("%d", &a[i]);
-	res = MaxSubseqSum4(a, K);
+	out = MaxSubseqSum4(a, K);
 
-	if (res.MaxSum >= 0)
-		printf("%d %d %d\n", res.MaxSum, a[res.first], a[res.last]);
+	if (out->MaxSum >= 0)
+		printf("%d %d %d\n", out->MaxSum, a[out->first], a[out->last]);
 	else
 		printf("%d %d %d\n", 0, a[0], a[K-1]);
-	
+	free(out);
 	return 0;
 }
 
-struct outcome MaxSubseqSum4(int A[], int N)
+PtrToOutNode MaxSubseqSum4(int A[], int N)
 {
-	int ThisSum, MaxSum;
-	struct outcome ret;
-	int gap, i;
+	int ThisSum, MaxSum, gap, i;
+	PtrToOutNode out;
 
 	/* Set MaxSum as -1 in case of all K numbers are negative except '0' */
 	ThisSum = 0, MaxSum = -1, gap = 0;
-	ret.first = ret.last = ret.MaxSum = 0;
+	out = (PtrToOutNode) malloc(sizeof(struct OutNode));
+	out->first = out->last = out->MaxSum = 0;
 	for (i = 0; i < N; i++) {
-		ThisSum += A[i];	// Online processing
+		ThisSum += A[i];
 		if (ThisSum > MaxSum) {
 			MaxSum = ThisSum;
 			gap++;
 			/* Find the last member of the maximum subsequence 
 			 * then find the first */
-			ret.last = i;
-			ret.first = ret.last - gap + 1;
-		} else if (ThisSum < 0) {
-			ThisSum = 0;
-			gap = 0;
-		} else	// ThisSum == MaxSum
+			out->last = i;
+			out->first = out->last - gap + 1;
+		} else if (ThisSum < 0)
+			ThisSum = gap = 0;
+		else 	/* ThisSum == MaxSum */
 			gap++;
 	}
-	ret.MaxSum = MaxSum;
-	return ret;
+	out->MaxSum = MaxSum;
+	return out;
 }
 
