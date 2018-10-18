0dad253ab2e791da8d05a22f035d199df5da293f
zia1995 <13806680065@163.com>
2018-10-18 16:39:16 +0800

    Comment on PolynomialAddMult_7_2.c

--- cn_version/Programming/PolynomialAddMult_7_2.c
+++ cn_version/Programming/PolynomialAddMult_7_2.c
@@ -10,14 +10,14 @@ struct Node {
 };
 
 /* Assistant functions */
-int Compare( int a, int b );
-PtrToNode MakeNode( int Coef, int Exp );
-void Attach( int Coef, int Exp, PtrToNode *pRear );
+int Compare(int a, int b);
+PtrToNode MakeNode(int Coef, int Exp);
+void Attach(int Coef, int Exp, PtrToNode *pRear);
 
 /* Main feature functions */
-Polynomial Add( Polynomial P1, Polynomial P2 );
-Polynomial Mult( Polynomial P1, Polynomial P2 );
-void PrintPoly( Polynomial P );
+Polynomial Add(Polynomial P1, Polynomial P2);
+Polynomial Mult(Polynomial P1, Polynomial P2);
+void PrintPoly(Polynomial P);
 Polynomial ReadPoly();
 
 int main()
@@ -34,12 +34,12 @@ int main()
 	return 0;
 }
 
-int Compare( int a, int b )
+int Compare(int a, int b)
 {
 	return a > b ? 1 : a == b ? 0 : -1;
 }
 
-PtrToNode MakeNode( int Coef, int Exp )
+PtrToNode MakeNode(int Coef, int Exp)
 {
 	PtrToNode P;
 	
@@ -50,7 +50,7 @@ PtrToNode MakeNode( int Coef, int Exp )
 	return P;
 }
 
-void Attach( int Coef, int Exp, PtrToNode *pRear )
+void Attach(int Coef, int Exp, PtrToNode *pRear)
 {
 	PtrToNode P;
 
@@ -59,12 +59,12 @@ void Attach( int Coef, int Exp, PtrToNode *pRear )
 	(*pRear) = P;
 }
 
-Polynomial Add( Polynomial P1, Polynomial P2 )
+Polynomial Add(Polynomial P1, Polynomial P2)
 {
 	Polynomial P, Rear, Tmp;
 	int Sum;
 	
-	// Using a dummy header for convenience
+	/* Using a dummy header for convenience */
 	P = (Polynomial) malloc(sizeof(struct Node));
 	P->Link = NULL;
 	Rear = P;
@@ -87,23 +87,21 @@ Polynomial Add( Polynomial P1, Polynomial P2 )
 			break;
 		}
 
-	// Handle the rest of the list which is not empty
+	/* Handle the rest of the list which is not empty */
 	while (P1) { Attach(P1->Coef, P1->Exp, &Rear); P1 = P1->Link; }
 	while (P2) { Attach(P2->Coef, P2->Exp, &Rear); P2 = P2->Link; }
 
-	Tmp = P;
-	P = P->Link;
-	free(Tmp);
+	Tmp = P; P = P->Link; free(Tmp);
 	return P;
 }
 
-Polynomial Mult( Polynomial P1, Polynomial P2 )
+Polynomial Mult(Polynomial P1, Polynomial P2)
 {
 	Polynomial P;
 	PtrToNode Rear, t1, t2, Tmp;
 	int Coefficient, Exponent;
 
-	// If one of the polynomial list is empty
+	/* If one of the polynomial list is empty */
 	if (!P1 || !P2) return NULL;
 
 	P = (Polynomial) malloc(sizeof(struct Node));
@@ -111,14 +109,14 @@ Polynomial Mult( Polynomial P1, Polynomial P2 )
 	Rear = P;
 	t1 = P1;
 	t2 = P2;
-	// Create a polynomial list firstly
+	/* Create a polynomial list firstly */
 	while (t2) {
 		Attach(t1->Coef*t2->Coef, t1->Exp+t2->Exp, &Rear);
 		t2 = t2->Link;
 	}
 	t1 = t1->Link;
 
-	// Add new items into polynomial P created beforehand
+	/* Add new items into polynomial P created beforehand */
 	while (t1) {
 		t2 = P2;
 		Rear = P;
@@ -126,14 +124,16 @@ Polynomial Mult( Polynomial P1, Polynomial P2 )
 			Coefficient = t1->Coef * t2->Coef;
 			Exponent = t1->Exp + t2->Exp;
 
-			// Find the position to insert into
+			/* Find the position to insert into */
 			while (Rear->Link && Rear->Link->Exp > Exponent)
 				Rear = Rear->Link;
 			if (Rear->Link && Rear->Link->Exp == Exponent) {
-				// Coefficient sum is not 0
+				/* Coefficient sum is not 0 */
 				if (Rear->Link->Coef + Coefficient)
 					Rear->Link->Coef += Coefficient;
-				else { Tmp = Rear->Link; Rear->Link = Tmp->Link; free(Tmp); }
+				else {
+					Tmp = Rear->Link; Rear->Link = Tmp->Link; free(Tmp);
+				}
 			} else {
 				Tmp = MakeNode(Coefficient, Exponent);
 				Tmp->Link = Rear->Link;
@@ -144,13 +144,11 @@ Polynomial Mult( Polynomial P1, Polynomial P2 )
 		t1 = t1->Link;
 	}
 
-	Tmp = P;
-	P = P->Link;
-	free(Tmp);
+	Tmp = P; P = P->Link; free(Tmp);
 	return P;
 }
 
-void PrintPoly( Polynomial P )
+void PrintPoly(Polynomial P)
 {
 	PtrToNode T = P;
 	int flag = 0;
@@ -176,11 +174,11 @@ Polynomial ReadPoly()
 	P = (Polynomial) malloc(sizeof(struct Node));
 	P->Link = NULL;
 	Rear = P;
-	while (N--) { scanf("%d %d", &c, &e); Attach(c, e, &Rear); }
-
-	t = P;
-	P = P->Link;
-	free(t);
+	while (N--) {
+		scanf("%d %d", &c, &e);
+		Attach(c, e, &Rear);
+	}
+	t = P; P = P->Link; free(t);
 	return P;
 }
 
