0bcf5da7173ca6b55a0687c0ae2877878a1e74df
zia1995 <13806680065@163.com>
2018-10-23 16:39:51 +0800

    Comment on BinarySearchTree.c

--- cn_version/extra/Tree/BinarySearchTree.c
+++ cn_version/extra/Tree/BinarySearchTree.c
@@ -1,3 +1,4 @@
+/* Operation set of Binary Search Tree (BST) */
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,12 +13,12 @@ struct TreeNode {
 	ElementType Data;
 };
 
-Position Find(ElementType X, BinTree BST);	/* Tail recursion */
-Position IterFind(ElementType X, BinTree BST);	/* Loop way */
+Position Find(BinTree BST, ElementType X);	/* Tail recursion */
+Position IterFind(BinTree BST, ElementType X);	/* Iteration */
 Position FindMin(BinTree BST);
 Position FindMax(BinTree BST);
-BinTree Insert(ElementType X, BinTree BST);
-BinTree Delete(ElementType X, BinTree BST);
+BinTree Insert(BinTree BST, ElementType X);
+BinTree Delete(BinTree BST, ElementType X);
 
 int main()
 {
@@ -26,32 +27,32 @@ int main()
 	ElementType Arr[SIZE] = {4, 5, 2, 1, 7};
 
 	for (BST=NULL, i=0; i < SIZE; i++)
-		BST = Insert(Arr[i], BST);
+		BST = Insert(BST, Arr[i]);
 
 	for (i=SIZE-1; i >= 0; i--)
-		BST = Delete(Arr[i], BST);
+		BST = Delete(BST, Arr[i]);
 
 	return 0;
 }
 
-Position Find(ElementType X, BinTree BST)
+Position Find(BinTree BST, ElementType X) 	/* Tail recursion */
 {
-	if (!BST) return EMPTY;		// Empty Tree
+	if (!BST) return EMPTY;		/* Empty Tree */
 	if (X < BST->Data)
-		return Find(X, BST->Left);	// Continue seeking in right subtree
+		return Find(BST->Left, X); 	/* Go seeking in right subtree */
 	else if (X > BST->Data)
-		return Find(X, BST->Right);	// ... in left subtree
-	else	// X == BST->Data
-		return BST;		// Seeking done, return the position
+		return Find(BST->Right, X); 	/* Go seeking in left subtree */
+	else	/* X equals BST->Data */
+		return BST; 	/* Seeking done, return the position */
 }
 
 /* Tail recursion can always be transformed into loop way */
-Position IterFind(ElementType X, BinTree BST)
+Position IterFind(BinTree BST, ElementType X)	/* Iteration */
 {
 	while (BST) {
-		if (X < BST->Data)	// Keep seeking in right subtree
+		if (X < BST->Data) 	/* Go seeking in right subtree */
 			BST = BST->Left;
-		else if (X > BST->Data)		// ... in left subtree
+		else if (X > BST->Data) 	/* Go seeking in left subtree */
 			BST = BST->Right;
 		else
 			return BST;
@@ -59,57 +60,64 @@ Position IterFind(ElementType X, BinTree BST)
 	return EMPTY;
 }
 
+/* Recursive */
 Position FindMin(BinTree BST)
 {
-	if (!BST) return EMPTY;		// Empty binary search tree, return NULL
+	if (!BST) return EMPTY;
 	if (!BST->Left)
-		return BST;		// Find the leftest leaf node and return it
+		return BST;		/* Find the leftest leaf node and return it */
 	else
-		return FindMin(BST->Left);	// Keep seeking in the left subtree
+		return FindMin(BST->Left); 	/* Keep seeking in the left subtree */
 }
 
+/* No-recursive */
 Position FindMax(BinTree BST)
 {
+	/* Keep seeking the rightest leaf node */
 	if (BST)
-		while (BST->Right)	// Keep seeking the rightest leaf node
-			BST = BST->Right;
+		while (BST->Right) BST = BST->Right;
 	return BST;
 }
 
-BinTree Insert(ElementType X, BinTree BST)
+BinTree Insert(BinTree BST, ElementType X)
 {
-	if (!BST) {		// If empty tree, generate a node
+	if (!BST) { 	/* If empty, it means we've found the place to insert */
 		BST = (BinTree) malloc(sizeof(struct TreeNode));
 		BST->Data = X;
 		BST->Left = BST->Right = NULL;
-	} else	// Start to find out the position to insert
-		if (X < BST->Data)	// Insert into left subtree recursively
-			BST->Left = Insert(X, BST->Left);
-		else if (X > BST->Data)		// ... right subtree recursively
-			BST->Right = Insert(X, BST->Right);
+	} else 	/* Keep finding... */
+		if (X < BST->Data) 	/* Should be the left subtree to insert into */
+			BST->Left = Insert(BST->Left, X);
+		/* Should be the left subtree to insert into */
+		else if (X > BST->Data)
+			BST->Right = Insert(BST->Right, X);
 
 	return BST;
 }
 
-BinTree Delete(ElementType X, BinTree BST)
+BinTree Delete(BinTree BST, ElementType X)
 {
 	Position Tmp;
 
 	if (!BST) printf("Couldn't find the element!\n");
-	else if (X < BST->Data)		// Left subtree deletion recursively
-		BST->Left = Delete(X, BST->Left);
-	else if (X > BST->Data)		// Right subtree deletion recursively
-		BST->Right = Delete(X, BST->Right);
-	else	// Find the node
-		if (BST->Left && BST->Right) {	// The node has two children
-			Tmp = FindMax(BST->Left);	// Find the maximum of left subtree
+	else if (X < BST->Data)		/* Deletion happends in left subtree */
+		BST->Left = Delete(BST->Left, X);
+	else if (X > BST->Data)		/* Deletion happends in left subtree */
+		BST->Right = Delete(BST->Right, X);
+	else	/* Find it */
+		/* Special handling of the node has two children by convert it into 
+		 * a node with only a child or a leave node */
+		if (BST->Left && BST->Right) {
+			/* Or you can FindMin(BST->Right), try not to 
+			 * change the feature of BST anyway */
+			Tmp = FindMax(BST->Left);
 			BST->Data = Tmp->Data;
-			BST->Left = Delete(BST->Data, BST->Left); // Delete the maximum of left subtree
-		} else {	// There is only one child or no child at all of this node
+			BST->Left = Delete(BST->Left, BST->Data);
+		} else {	/* Handling of node with one or no child */
 			Tmp = BST;
-			if (!BST->Left)		// There's right child or no child
+			if (!BST->Left)
 				BST = BST->Right;
-			else if (!BST->Right)	// There's left child or no child
+			else if (!BST->Right)
 				BST = BST->Left;
 			free(Tmp);
 		}
