49fd8b3541ba07178d59eba09627b7f0eebe8c8a
zia1995 <13806680065@163.com>
2018-10-24 17:20:15 +0800

    Comment on IdenticalBST_7_4.c

--- cn_version/Programming/IdenticalBST_7_4.c
+++ cn_version/Programming/IdenticalBST_7_4.c
@@ -1,3 +1,17 @@
+/* Sample Input:
+ * 4 2
+ * 3 1 4 2
+ * 3 4 1 2
+ * 3 2 4 1
+ * 2 1
+ * 2 1
+ * 1 2
+ * 0
+ * Sample Output:
+ * Yes
+ * No
+ * No
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -22,8 +36,7 @@ int main()
 	int N, L, i;
 	Tree T;
 
-	/**
-	 * For every group of data series,
+	/* For every group of data series,
 	 *     1. Readin N and L
 	 *     2. Build a benchmark tree T
 	 *     3. Judge following trees
@@ -31,16 +44,15 @@ int main()
 	scanf("%d", &N);
 	while (N) {
 		scanf("%d", &L);
-		T = MakeTree(N); 	// Make the benchmark tree
+		T = MakeTree(N); 	/* Make the benchmark tree */
 		for (i = 0; i < L; i++) {
 			if (Judge(T, N)) printf("Yes\n");
 			else printf("No\n");
-			ResetT(T);	// Clear out all the flags
+			ResetT(T); 	/* Clear out all the flags */
 		}
 		FreeTree(T);
 		scanf("%d", &N);
 	}
-
 	return 0;
 }
 
@@ -51,10 +63,10 @@ Tree MakeTree(int N)
 	ElementType V;
 
 	scanf("%d", &V);
-	T = NewNode(V); 	// Build a tree with a single node
+	T = NewNode(V); 	/* Build a tree with a single node */
 	for (i = 1; i < N; i++) {
 		scanf("%d", &V);
-		T = Insert(T, V); 	// Insert following nodes into the tree recursively
+		T = Insert(T, V); 	/* Insert following nodes */
 	}
 	return T;
 }
@@ -87,24 +99,26 @@ static int check(Tree T, ElementType V)
 		else if (V > T->v) return check(T->Right, V);
 		else return 0;
 	else
-		if (V == T->v) { 	// If v has just been visited, set flag=1
+		if (V == T->v) { 	/* If v has just been visited, set flag=1 */
 			T->flag = 1;
 			return 1;
-		} else 			// If the last node of v is unvisited, return 0
+		} else 	/* If the last node of v is unvisited, return 0 */
 			return 0;
 }
 
 int Judge(Tree T, int N)
 {
-	int i, flag = 0; // flag=0 to indicate consistency for now
+	int i, flag = 0; 	/* flag=0 to indicate so far so good */
 	ElementType V;
 
 	scanf("%d", &V);
-	if (V != T->v) flag = 1; 	// flag=1 to indicate follwing invalid readin
-	else T->flag = 1; 		// T->flag=1 to indicate we've already check the validity of root
+	if (V != T->v) flag = 1; 	/* flag=1 to indicate follwing invalid readin */
+	/* T->flag=1 to indicate we've already check the validity of root */
+	else T->flag = 1;
 	for (i = 1; i < N; i++) {
 		scanf("%d", &V);
-		if ((!flag) && (!check(T, V))) flag = 1; // If flag=1, don't bother to check
+		/* If flag=1, don't bother to check, read the rest */
+		if ((!flag) && (!check(T, V))) flag = 1;
 	}
 	if (flag) return 0;
 	else return 1;
