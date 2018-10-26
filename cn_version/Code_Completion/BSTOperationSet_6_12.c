280f909e33ccaa4c4b1b0d7058b3f76a7af99f93
zia1995 <13806680065@163.com>
2018-10-26 18:44:18 +0800

    Comment on BSTOperationSet_6_12.c

--- cn_version/Code_Completion/BSTOperationSet_6_12.c
+++ cn_version/Code_Completion/BSTOperationSet_6_12.c
@@ -1,23 +1,3 @@
-/* Sapmle Input:
- * 10
- * 5 8 6 2 4 1 0 10 9 7
- * 5
- * 6 3 10 0 5
- * 5
- * 5 7 0 10 3
- * -----------------------------
- * Sample Output:
- * Preorder: 5 2 1 0 4 8 6 7 10 9
- * 6 is found
- * 3 is not found
- * 10 is found
- * 10 is the largest key
- * 0 is found
- * 0 is the smallest key
- * 5 is found
- * Not Found
- * Inorder: 1 2 4 6 8 9
- */
 #define EMPTY NULL
 
 BinTree Insert(BinTree BST, ElementType X)
@@ -26,12 +6,10 @@ BinTree Insert(BinTree BST, ElementType X)
 		BST = (BinTree) malloc(sizeof(struct TNode));
 		BST->Data = X;
 		BST->Left = BST->Right = NULL;
-	} else
-		if (X < BST->Data)
-			BST->Left = Insert(BST->Left, X);
-		else if (X > BST->Data)
-			BST->Right = Insert(BST->Right, X);
-
+	} else if (X < BST->Data)
+		BST->Left = Insert(BST->Left, X);
+	else if (X > BST->Data)
+		BST->Right = Insert(BST->Right, X);
 	return BST;
 }
 
@@ -44,32 +22,32 @@ BinTree Delete(BinTree BST, ElementType X)
 		BST->Left = Delete(BST->Left, X);
 	else if (X > BST->Data)
 		BST->Right = Delete(BST->Right, X);
-	else
-		if (BST->Left && BST->Right) {
-			Tmp = FindMax(BST->Left);
-			BST->Data = Tmp->Data;
-			BST->Left = Delete(BST->Left, BST->Data);
-		} else {
-			Tmp = BST;
-			if (!BST->Left)
-				BST = BST->Right;
-			else if (!BST->Right)
-				BST = BST->Left;
-			free(Tmp);
-		}
+	/* BST->Data equals to X */
+	else if (BST->Left && BST->Right) {
+		Tmp = FindMax(BST->Left); 	/* Or FindMin(BST->Right) */
+		BST->Data = Tmp->Data;
+		/* Or BST->Right = Delete(BST->Right, BST->Data) */
+		BST->Left = Delete(BST->Left, BST->Data);
+	} else {
+		Tmp = BST;
+		if (!BST->Left)
+			BST = BST->Right;
+		else if (!BST->Right)
+			BST = BST->Left;
+		free(Tmp);
+	}
 	return BST;
 }
 
 Position Find(BinTree BST, ElementType X)
 {
-	while (BST) {
+	while (BST)
 		if (X < BST->Data)
 			BST = BST->Left;
 		else if (X > BST->Data)
 			BST = BST->Right;
 		else
 			return BST;
-	}
 	return EMPTY;
 }
 
