4eb985ea8941e7a74831f786503142624947a7de
zia1995 <13806680065@163.com>
2018-10-26 21:58:38 +0800

    Comment on CompleteBinarySearchTree_7_7.c

--- en_version/Programming/CompleteBinarySearchTree_7_7.c
+++ en_version/Programming/CompleteBinarySearchTree_7_7.c
@@ -8,31 +8,33 @@
 #include <stdlib.h>
 
 #define MAX 1000
+typedef int ElemType;
 int LevelOrderCBST[MAX], j=0;
 
 int compar(const void *pA, const void *pB);
-void InorderSort(int A[], int R, int N);
+void InorderSort(int Root, int N, ElemType A[]);
 
 int main()
 {
-	int A[MAX], N, i, flag=0;
+	int N, i, flag=0;
+	ElemType A[MAX];
 
 	/***********************************************************
-	 *               6                        A[6]
-	 *             /   \                    /      \
-	 *            3     8                A[3]     A[8]
-	 *           / \   / \               /  \     /   \
-	 *          1   5 7   9           A[1]  A[5]A[7] A[9]
-	 *         / \  /                 / \   /
-	 *        0   2 4              A[0]A[2] A[4]
+	 *               6                        0
+	 *             /   \                    /   \
+	 *            3     8                  1     2
+	 *           / \   / \               /  \   /  \
+	 *          1   5 7   9             3    4 5    6
+	 *         / \  /                  / \   /
+	 *        0   2 4                 7   8 9
 	 ***********************************************************/
 	scanf("%d", &N); 	/* N is positve guranteed */
 	for (i = 0; i < N; i++)
 		scanf("%d", &A[i]);
 
 	/* Inorder traversal of CBST is a incremental sequence */
-	qsort(A, N, sizeof(int), compar);
-	InorderSort(A, 0, N);
+	qsort(A, N, sizeof(ElemType), compar);
+	InorderSort(0, N, A);
 	/* Match levelorderArr with inorder sequence in arr */
 	for (i = 0; i < N; i++) {
 		if (!flag) flag = 1;
@@ -45,7 +47,7 @@ int main()
 
 int compar(const void *pA, const void *pB)
 {
-	return *(int *)pA - *(int *)pB;
+	return *(ElemType *)pA - *(ElemType *)pB;
 }
 
 /* Inorder traversal: extract the data inside array A (which have been 
@@ -53,12 +55,12 @@ int compar(const void *pA, const void *pB)
  *   
  * In complete binary tree (CBST), 2i+1 and 2i+2 is the left and right 
  * child of the i node respectively (array begins with index zero) */
-void InorderSort(int A[], int R, int N)
+void InorderSort(int Root, int N, ElemType A[])
 {
-	if (R < N) {
-		InorderSort(A, 2*R+1, N);
-		LevelOrderCBST[R] = A[j++];
-		InorderSort(A, 2*R+2, N);
+	if (Root < N) {
+		InorderSort(2*Root+1, N, A);
+		LevelOrderCBST[Root] = A[j++];
+		InorderSort(2*Root+2, N, A);
 	}
 }
 
