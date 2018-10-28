c2ba755e86842ebd8c658102b19a44492f0cc213
ziwei woo <13806680065@163.com>
2018-10-28 23:36:49 +0800

    Comment on FileTransfer_7_8.c

--- en_version/Programming/FileTransfer_7_8.c
+++ en_version/Programming/FileTransfer_7_8.c
@@ -1,53 +1,73 @@
+/* Sample Input1:
+ * 5
+ * C 3 2
+ * I 3 2
+ * C 1 5
+ * I 4 5
+ * I 2 4
+ * C 3 5
+ * S
+ * Sample Output1:
+ * no
+ * no
+ * yes
+ * There are 2 components.
+ * -----------------------
+ * Sample Input2:
+ * 5
+ * C 3 2
+ * I 3 2
+ * C 1 5
+ * I 4 5
+ * I 2 4
+ * C 3 5
+ * I 1 3
+ * C 1 5
+ * S
+ * Sample Output2:
+ * no
+ * no
+ * yes
+ * yes
+ * The network is connected.
+ */
 #include <stdio.h>
 
 #define MaxSize 10000
 
-typedef int ElementType;	// Elements are non-negative by default
-typedef int SetName;		// Using the index of root as the name of a set
+typedef int ElementType;	/* Elements are non-negative by default */
+typedef int SetName;		/* Using the index of root as the name of a set */
 typedef ElementType SetType[MaxSize];
 
 SetName Find(SetType S, ElementType X);
-void Union(SetType S, SetName Root1, SetName Root);
-void Initialization(SetType S, int n);
+void Union(SetType S, SetName Root1, SetName Root2);
+void Initialization(SetType S, int N);
 void Input_connection(SetType S);
 void Check_connection(SetType S);
-void Check_network(SetType S, int n);
+void Check_network(SetType S, int N);
 
 int main()
 {
 	SetType S;
-	int n;
+	int N;
 	char in;
 
-	/* INPUT   |  OUTPUT
-	 * -------------------
-	 *  5      |
-	 *  C 3 2  |   no
-	 *  I 3 2  |
-	 *  C 1 5  |   no
-	 *  I 4 5  |
-	 *  I 2 4  |
-	 *  C 3 5  |   yes
-	 *  I 1 3  |
-	 *  C 1 5  |   yes
-	 *  S      |   The network is connected.
-	 */
-	scanf("%d\n", &n);
-	Initialization(S, n);
+	scanf("%d\n", &N);
+	Initialization(S, N);
 	do {
 		scanf("%c", &in);
 		switch (in) {
 		case 'I': Input_connection(S); break;
 		case 'C': Check_connection(S); break;
-		case 'S': Check_network(S, n); break;
+		case 'S': Check_network(S, N); break;
 		}
 	} while (in != 'S');
 	
 	return 0;
 }
 
+/* T = O(N^2) */
 /*
-// T = O(N^2)
 void Find(SetType S[], ElementType X)
 {
 	int i;
@@ -59,21 +79,25 @@ void Find(SetType S[], ElementType X)
 }
 */
 
+/* TSSN implimentation: two simple sometimes naive 
+ * if Union(Find(2), Find(1)), ... Union(Find(n), Find(1)), then T = O(N^2))
+ *
+ * By default, Root1 and Root2 are different root of set respectively */
 /*
-// TSSN implimentation: two simple sometimes naive 
-// if Union(Find(2), Find(1)), ... Union(Find(n), Find(1)), then T = O(N^2))
 void Union(SetType S, SetName Root1, SetName Root2)
 {
-	// By default, Root1 and Root2 are different root of set respectively
 	S[Root2] = Root1;
 }
 */
 
-// Merge trees according to rank, 
-// in worst case, the height of tree will be H = O(logN)
+/* Merge trees according to rank, 
+ * in worst case, the height of tree will be H = O(logN) */
+
 void Union(SetType S, SetName Root1, SetName Root2)
 {
-	// S[Root] = -Elements
+	/* S[Root] = -NumberOfElements, it's better than to use tree height 
+	 * cause we don't need to update the size when we merge two set */
+
 	if (S[Root2] < S[Root1]) {
 		S[Root2] += S[Root1];
 		S[Root1] = Root2;
@@ -83,16 +107,16 @@ void Union(SetType S, SetName Root1, SetName Root2)
 	}
 }
 
+/* Initialize the elements in a set as -1 */
 /*
 SetName Find(SetType S, ElementType X)
 {
-	for (; S[X] >= 0; X = S[X]) ;	// Initialize the elements in a set as -1
+	for (; S[X] >= 0; X = S[X]) ;
 	return X;
 }
 */
 
-/**
- * Path compression (refer to Ackermann Function)
+/* Path compression (refer to Ackermann Function)
  * 
  * k1Ma(M, N) <= T(M, N) <= k2Ma(M, N)
  *
@@ -104,15 +128,16 @@ SetName Find(SetType S, ElementType X)
  */
 SetName Find(SetType S, ElementType X)
 {
-	if (S[X] < 0) return X;		// Find the root of a set
-	// 1. Find the root  2. Turn the root into it's parent node  3. Return the root
+	if (S[X] < 0) return X;		/* Find the root of a set */
+	/* 1. Find root  2. Turn root into it's parent node  3. Return root */
 	else return S[X] = Find(S, S[X]);
 }
 
-void Initialization(SetType S, int n)
+void Initialization(SetType S, int N)
 {
 	int i;
-	for (i = 0; i < n; i++) S[i] = -1;
+	for (i = 0; i < N; i++)
+		S[i] = -1;
 }
 
 void Input_connection(SetType S)
@@ -132,22 +157,21 @@ void Check_connection(SetType S)
 	ElementType u, v;
 	SetName Root1, Root2;
 
-	scanf("%d %d", &u, &v);
-	getchar();	// To get the immediate result
+	scanf("%d %d", &u, &v); getchar();	/* To get the immediate result */
 	Root1 = Find(S, u-1);
 	Root2 = Find(S, v-1);
-	if (Root1 == Root2) printf("yes\n");
-	else printf("no\n");
+	if (Root1 == Root2) puts("yes");
+	else puts("no");
 }
 
-void Check_network(SetType S, int n)
+void Check_network(SetType S, int N)
 {
 	int i, counter = 0;
 	
-	for (i = 0; i < n; i++)
+	for (i = 0; i < N; i++)
 		if (S[i] < 0) counter++;
 
-	if (counter == 1) printf("The network is connected.\n");
+	if (counter == 1) puts("The network is connected.");
 	else printf("There are %d components.\n", counter);
 }
 
