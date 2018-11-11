18e6d1ae07cd0d9f6540453f2678a87f7e8c417d
ziwei woo <13806680065@163.com>
2018-11-11 12:13:54 +0800

    Comment on 7_7_SixDegreesofSeparation.c

--- cn_version/Programming/7_7_SixDegreesofSeparation.c
+++ cn_version/Programming/7_7_SixDegreesofSeparation.c
@@ -1,169 +1,276 @@
+/**
+ * Sample Input:
+ * 10 9
+ * 1 2
+ * 2 3
+ * 3 4
+ * 4 5
+ * 5 6
+ * 6 7
+ * 7 8
+ * 8 9
+ * 9 10
+ * --------------
+ * Sample Output:
+ * 1: 70.00%
+ * 2: 80.00%
+ * 3: 90.00%
+ * 4: 100.00%
+ * 5: 100.00%
+ * 6: 100.00%
+ * 7: 100.00%
+ * 8: 90.00%
+ * 9: 80.00%
+ * 10: 70.00%
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <string.h>
-
-#define Limit 6
-#define MaxVertexNum 10001
-
-typedef int WeightType;
-typedef struct GNode *PtrToGNode;
-struct GNode {
-	int Nv, Ne;
-	WeightType G[MaxVertexNum][MaxVertexNum];
-};
-typedef PtrToGNode MGraph;
 
 typedef int Vertex;
+typedef struct AdjVNode *PtrToAdjVNode;
+struct AdjVNode {
+	Vertex AdjV;
+	PtrToAdjVNode Next;
+};
+
+#define MaxVertexNum 10000
+typedef struct VNode *PtrToVNode;
+typedef struct VNode {
+	PtrToAdjVNode FirstEdge;
+} AdjList[MaxVertexNum];
+
+typedef struct GNode *PtrToGNode;
+struct GNode {
+	int Nv, Ne;		/* Number of vertices and edges */
+	AdjList G;
+};
+typedef PtrToGNode Graph;
+typedef Graph LGraph;
+
 typedef struct ENode *PtrToENode;
 struct ENode {
 	Vertex V1, V2;
-	WeightType Weight;
 };
 typedef PtrToENode Edge;
 
-typedef Vertex ElementType;
+#define ERROR -1
+typedef int Position;
+typedef Vertex ElemType;
 typedef struct QNode *PtrToQNode;
 struct QNode {
-	ElementType Data[MaxVertexNum];
-	int Front, Rear;
+	ElemType *Data;
+	Position Front, Rear;
+	int MaxQSize;
 };
 typedef PtrToQNode Queue;
 
-MGraph CreateGraph(int VertexNum);
-void InsertEdge(MGraph Graph, Edge E);
-MGraph BuildGraph();
-Queue CreatQueue();
-int IsFullQ(Queue Q);
-void Enqueue(Queue Q, ElementType Item);
-int IsEmptyQ(Queue Q);
-ElementType Dequeue(Queue Q);
-void SDS(MGraph Graph);
-int BFS(MGraph Graph, Vertex V, bool visited[]);
+/**
+ * Graph related functions:
+ */
+Graph CreatGraph(int VertexNum);
+void DestroyGraph(Graph graph);
+void InsertEdge(Graph graph, Edge E);
+Graph BuildGraph();
+
+/**
+ * Queue related functions: 
+ */
+Queue CreatQ(int QSize);
+void DestroyQ(Queue Q);
+bool IsFullQ(Queue Q);
+bool IsEmptyQ(Queue Q);
+bool Enqueue(Queue Q, ElemType X);
+ElemType Dequeue(Queue Q);
+
+/**
+ * Core algorithm functions:
+ */
+void InitVisited(int N);
+void SDS(Graph graph);
+
+bool visited[MaxVertexNum];		/* Global array to help BFS algorithm */
 
 int main()
 {
-	MGraph Graph;
+	/**
+	 * In this case: MAXN = 10000, so (N + 2*2E <= N^2 - N),
+	 * we use graph adjacency list implementation instead 
+	 */
+	LGraph graph;
 
-	Graph = BuildGraph();
-
-	SDS(Graph);
+	graph = BuildGraph();
+	SDS(graph);
+	DestroyGraph(graph);
 
 	return 0;
 }
 
-void SDS(MGraph Graph)
+void InitVisited(int N)
 {
-	Vertex V;
-	int count;
-	bool visited[MaxVertexNum];
+	int i;
 
-	for (V = 1; V <= Graph->Nv; V++) {
-		memset(visited+1, false, Graph->Nv);
-		count = BFS(Graph, V, visited);
-		printf("%d: %.2f%%\n", V, 100*(double)count/Graph->Nv);
-	}
+	for (i = 0; i < N; i++)
+		visited[i] = false;
 }
 
-int BFS(MGraph Graph, Vertex V, bool visited[])
+int BFS(Graph graph, Vertex S)
 {
-	Vertex W;
+	/* last and tail to indicate the last vertex of last level 
+	 * and the tail of enqueue vertex respectively */
 	int count, level, last, tail;
+	Vertex V, W;
 	Queue Q;
+	PtrToAdjVNode T;
 
-	visited[V] = true;
-	count = 1;
-	level = 0;
-	last = V;
-	Q = CreatQueue();
-	Enqueue(Q, V);
+	InitVisited(graph->Nv);
+	Q = CreatQ(MaxVertexNum);
+
+	visited[S] = true;
+	count = 1; level = 0; last = S;
+	Enqueue(Q, S);
+
 	while (!IsEmptyQ(Q)) {
 		V = Dequeue(Q);
-		for (W = 1; W <= Graph->Nv; W++)
-			if (!visited[W] && Graph->G[V][W]) {
+		for (T = graph->G[V].FirstEdge; T; T = T->Next) {
+			W = T->AdjV;
+			if (!visited[W]) {
 				visited[W] = true;
 				Enqueue(Q, W);
 				count++;
 				tail = W;
 			}
-		/* When the last layer handling of BFS is finished */
-		if (V == last) { level++; last = tail; }
-		if (level == Limit) break;
+		}
+		if (V == last) {
+			level++;
+			last = tail;
+		}
+		if (level == 6)
+			break;
 	}
+	DestroyQ(Q);
 	return count;
 }
 
-MGraph CreateGraph(int VertexNum)
+void SDS(Graph graph)
 {
-	Vertex V, W;
-	MGraph Graph;
+	Vertex V;
+	int count;
 
-	Graph = (MGraph) malloc(sizeof(struct GNode));
-	Graph->Nv = VertexNum;
-	Graph->Ne = 0;
-
-	/* Notes: the index of vertex from 1 To (Graph->Nv) by default */
-	for (V = 1; V <= Graph->Nv; V++)
-		for (W = 1; W <= Graph->Nv; W++)
-			Graph->G[V][W] = 0;
-
-	return Graph;
+	for (V = 0; V < graph->Nv; V++) {
+		count = BFS(graph, V);
+		printf("%d: %.2f%%\n", V+1, (double)100*count/graph->Nv);
+	}
 }
 
-void InsertEdge(MGraph Graph, Edge E)
+Graph CreatGraph(int VertexNum)
 {
-	/* Undirected Graphs, need to insert edge<V2, V1> also */
-	Graph->G[E->V1][E->V2] = 1;
-	Graph->G[E->V2][E->V1] = 1;
+	Graph graph;
+	Vertex V;
+
+	graph = (Graph) malloc(sizeof(*graph));
+	graph->Nv = VertexNum;
+	graph->Ne = 0;
+
+	for (V = 0; V < graph->Nv; V++)
+		graph->G[V].FirstEdge = (PtrToAdjVNode)0;
+
+	return graph;
 }
 
-MGraph BuildGraph()
+void DestroyGraph(Graph graph)
 {
-	MGraph Graph;
-	Edge E;
-	int Nv, i;
+	Vertex V;
+	PtrToAdjVNode T, tmp;
 
-	scanf("%d", &Nv);
-	Graph = CreateGraph(Nv);
-	scanf("%d", &Graph->Ne);
-	if (Graph->Ne) {
-		E = (Edge) malloc(sizeof(struct ENode));
-		for (i = 0; i < Graph->Ne; i++) {
-			scanf("%d %d", &E->V1, &E->V2);
-			InsertEdge(Graph, E);
+	for (V = 0; V < graph->Nv; V++) {
+		T = graph->G[V].FirstEdge;
+		while (T) {
+			tmp = T->Next;
+			free(T);
+			T = tmp;
 		}
 	}
-	return Graph;
+	free(graph);
 }
 
-Queue CreatQueue()
+void InsertEdge(Graph graph, Edge E)
 {
-	Queue Q = (Queue) malloc(sizeof(struct QNode));
+	PtrToAdjVNode NewNode;
+
+	NewNode = (PtrToAdjVNode) malloc(sizeof(*NewNode));
+	NewNode->AdjV = E->V2;
+	NewNode->Next = graph->G[E->V1].FirstEdge;
+	graph->G[E->V1].FirstEdge = NewNode;
+
+	/* Undirected Graphs, need to insert edge<V2, V1> also */
+
+	NewNode = (PtrToAdjVNode) malloc(sizeof(*NewNode));
+	NewNode->AdjV = E->V1;
+	NewNode->Next = graph->G[E->V2].FirstEdge;
+	graph->G[E->V2].FirstEdge = NewNode;
+}
+
+Graph BuildGraph()
+{
+	Graph graph;
+	int Nv, i;
+	Edge E;
+
+	scanf("%d", &Nv);	/* 1<Nv<=10000 */
+	graph = CreatGraph(Nv);
+	scanf("%d", &graph->Ne);	/* Ne<=33*N */
+	if (graph->Ne) {
+		E = (Edge) malloc(sizeof(*E));
+		for (i = 0; i < graph->Ne; i++) {
+			scanf("%d %d", &E->V1, &E->V2);
+			E->V1--; E->V2--;
+			InsertEdge(graph, E);
+		}
+		free(E);
+	}
+	return graph;
+}
+
+Queue CreatQ(int QSize)
+{
+	Queue Q = (Queue) malloc(sizeof(*Q));
+	Q->Data = (ElemType *) malloc(QSize * sizeof(ElemType));
 	Q->Front = Q->Rear = 0;
+	Q->MaxQSize = QSize;
 	return Q;
 }
 
-int IsFullQ(Queue Q)
+void DestroyQ(Queue Q)
 {
-	return (Q->Rear + 1) % MaxVertexNum == Q->Front;
+	free(Q->Data);
+	free(Q);
 }
 
-void Enqueue(Queue Q, ElementType Item)
+bool IsFullQ(Queue Q)
 {
-	if (IsFullQ(Q)) { printf("Queue full!\n"); return; }
-	Q->Rear = (Q->Rear + 1) % MaxVertexNum;
-	Q->Data[Q->Rear] = Item;
+	return (Q->Rear+1)%Q->MaxQSize == Q->Front;
 }
 
-int IsEmptyQ(Queue Q)
+bool IsEmptyQ(Queue Q)
 {
 	return Q->Front == Q->Rear;
 }
 
-ElementType Dequeue(Queue Q)
+bool Enqueue(Queue Q, ElemType X)
 {
-	if (IsEmptyQ(Q)) { printf("Queue empty!\n"); return -1; }
-	Q->Front = (Q->Front + 1) % MaxVertexNum;
+	if (IsFullQ(Q))
+		return false;
+	Q->Rear = (Q->Rear+1)%Q->MaxQSize;
+	Q->Data[Q->Rear] = X;
+	return true;
+}
+
+ElemType Dequeue(Queue Q)
+{
+	if (IsEmptyQ(Q))
+		return ERROR;
+	Q->Front = (Q->Front+1)%Q->MaxQSize;
 	return Q->Data[Q->Front];
 }
 
