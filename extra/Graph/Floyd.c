09958cea2a56ea7766418a0c9a9c9a6880275a9d
ziwei woo <13806680065@163.com>
2018-11-11 21:41:33 +0800

    Comment on Floyd.c

--- extra/Graph/Floyd.c
+++ extra/Graph/Floyd.c
@@ -1,27 +1,25 @@
 /**
  * Adjacency matrix: All pairs shortest paths problem (APSP)
- ***********************************************************************************
+ *****************************************************************************
  * [Sparse Graph algorithm]:
- * -------- Method 1 (same as V size the single source shortest path problem) ------
+ * ---- Method 1 (same as V size the single source shortest path problem) ----
  *   One can implement function FindMinDist() by using MinHeap, 
  * Ta=O(|V|^2 * log|V|), Tb=O(|V||E|log|V|), Total T = O(|V|*(|V|+|E|)log|V|) 
  *
- * ------------------------- Method 2  ---------------------------------------------
- *   Scan all the uncollected vertices, in this way, Ta=O(|V|^3), Tb=O(|E||V|), 
- * Total T = Ta + Tb = O(|V|^3 + |E||V|)
- ***********************************************************************************
+ * ------------------------- Method 2  ---------------------------------------
+ *   Scan all the uncollected vertices, in this way, 
+ * Ta=O(|V|^3), Tb=O(|E||V|), Total T = Ta + Tb = O(|V|^3 + |E||V|)
+ *****************************************************************************
  * [Dense Graph algorithms] (i.e. E = O(|V|^2)):
- * ------------------------ Method 3 (exactly T=O(|V|^3)) --------------------------
+ * ------------------------ Method 3 (exactly T=O(|V|^3)) --------------------
  *   Floyd algorithm with elegancy, 
  * Initiaization: for (each v in G), set D[v][v] = 0, D[i][j] = INFINITY 
- **********************************************************************************
  */
 bool Floyd(MGraph Graph, WeightType D[][MaxNv], Vertex path[][MaxNv])
 {
-	Vertex i, j, k;
+	Vertex i, j, k;		/* k: the time of matrix transform */
 
-	/* Initialization */
-	for (i = 0; i < Graph->Nv; i++)
+	for (i = 0; i < Graph->Nv; i++)		/* Initialization */
 		for (j = 0; j < Graph->Nv; j++) {
 			D[i][j] = Graph->G[i][j];
 			path[i][j] = -1;
