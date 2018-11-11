3a7e6a7154b77235074acb5e1325ceb33db3db1a
ziwei woo <13806680065@163.com>
2018-11-11 20:37:33 +0800

    Update Floyd.c

--- extra/Graph/Floyd.c
+++ extra/Graph/Floyd.c
@@ -1,20 +1,23 @@
 /**
  * Adjacency matrix: All pairs shortest paths problem (APSP)
  ***********************************************************************************
+ *
  * [Sparse Graph algorithm]:
  * -------- Method 1 (same as V size the single source shortest path problem) ------
  *   One can implement function FindMinDist() by using MinHeap, 
  * Ta=O(|V|^2 * log|V|), Tb=O(|V||E|log|V|), Total T = O(|V|*(|V|+|E|)log|V|) 
  *
- ***********************************************************************************
- * [Dense Graph algorithms] (i.e. E = O(|V|^2)):
- * -------- Method 2 (same as V size the single source shortest path problem) ------
+ * ------------------------- Method 2  ---------------------------------------------
  *   Scan all the uncollected vertices, in this way, Ta=O(|V|^3), Tb=O(|E||V|), 
  * Total T = Ta + Tb = O(|V|^3 + |E||V|)
  *
+ ***********************************************************************************
+ *
+ * [Dense Graph algorithms] (i.e. E = O(|V|^2)):
  * ------------------------ Method 3 (exactly T=O(|V|^3)) --------------------------
  *   Floyd algorithm with elegancy, 
  * Initiaization: for (each v in G), set D[v][v] = 0, D[i][j] = INFINITY 
+ *
  **********************************************************************************
  */
 bool Floyd(MGraph Graph, WeightType D[][MaxNv], Vertex path[][MaxNv])
