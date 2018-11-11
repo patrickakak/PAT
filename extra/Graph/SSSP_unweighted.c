321b87f6dc480d2244565c50cd85967ab5cb064a
ziwei woo <13806680065@163.com>
2018-11-11 22:08:43 +0800

    Comment on SSSP_unweighted.c

--- extra/Graph/SSSP_unweighted.c
+++ extra/Graph/SSSP_unweighted.c
@@ -1,9 +1,27 @@
 /**
- * Adjacency list: unweighted single-source shortest path problem (SSSP) 
- * (quite resembles breadth first search algorithm)
- */
+ * Unweighted single-source shortest path problem (SSSP)
+ * (quite resembles breadth first search algorithm), T = O(|V|+|E|)
+ *
+ * Psudocode for SSSP problem:
+ * Initialize array dist[] and path[] as -1 firstly 
+ * */
+void Unweighted(Vertex S)
+{
+	Enqueue(S, Q);
+	while (!IsEmpty(Q)) {
+		V = Dequeue(Q);
+		for (W: each adjacent node of V)
+			if (dist[W] == -1) {
+				dist[W] = dist[V]+1;
+				path[W] = V;
+				Enqueue(W, Q);
+			}
+	}
+}
 
-/* Initialize array dist[] and path[] as -1 firstly */
+/**
+ * Adjacency list implementation:
+ */
 void Unweighted(LGraph Graph, int dist[], int path[], Vertex S)
 {
 	Queue Q;
@@ -16,11 +34,34 @@ void Unweighted(LGraph Graph, int dist[], int path[], Vertex S)
 	while (!IsEmpty(Q)) {
 		V = Dequeue(Q);
 		for (W = Graph->G[V].FirstEdge; W; W = W->Next)
-			if (dist[W->AdjV] == -1) {	/* If W->AdjV has not been visited */
+			/* If the adjacent vertex has not been visited */
+			if (dist[W->AdjV] == -1) {
 				/* Update the distance from W->AdjV to S */
 				dist[W->AdjV] = dist[V] + 1;
-				path[W->AdjV] = V;	/* Record V in path of S to W->AdjV */
+				path[W->AdjV] = V;	/* Record V in the path of S to W->AdjV */
 				Enqueue(Q, W->AdjV);
+			}
+	}
+}
+
+/**
+ * Adjacency matrix implementation:
+ */
+void Unweighted(MGraph Graph, int dist[], int path[], Vertex S)
+{
+	Queue Q;
+	Vertex V;
+
+	Q = CreateQueue(Graph->Nv);
+	dist[S] = 0;	/* Initialize source */
+	Enqueue(Q, S);
+	while (!IsEmpty(Q)) {
+		V = Dequeue(Q);
+		for (W = 0; W < Graph->Nv; W++)
+			if (dist[W] == -1) {
+				dist[W] = dist[V] + 1;
+				path[W] = V;
+				Enqueue(Q, W);
 			}
 	}
 }
