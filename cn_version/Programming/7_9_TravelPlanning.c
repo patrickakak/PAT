d3d7522d6ae4c7f46e6aaeffff6c32feb7b9ab5c
ziwei woo <13806680065@163.com>
2018-11-13 21:12:28 +0800

    Update 7_9_TravelPlanning.c

--- cn_version/Programming/7_9_TravelPlanning.c
+++ cn_version/Programming/7_9_TravelPlanning.c
@@ -1,3 +1,14 @@
+/**
+ * Sample Input:
+ * 4 5 0 3
+ * 0 1 1 20
+ * 1 3 2 30
+ * 0 3 4 10
+ * 0 2 2 20
+ * 2 3 1 20
+ * Sample Output:
+ * 3 40
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
