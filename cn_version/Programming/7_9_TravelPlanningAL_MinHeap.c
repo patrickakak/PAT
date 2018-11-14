5f04378c8bfc7838d7a2712e6e5615339ab8cacc
ziwei woo <13806680065@163.com>
2018-11-14 21:20:36 +0800

    Update 7_9_TravelPlanningAL_MinHeap.c

--- cn_version/Programming/7_9_TravelPlanningAL_MinHeap.c
+++ cn_version/Programming/7_9_TravelPlanningAL_MinHeap.c
@@ -1,3 +1,14 @@
+/**
+ * Sample Input:
+ * 4 5 0 3
+ * 0 1 1 20
+ * 1 3 2 30
+ * 0 3 4 10
+ * 0 2 2 20
+ * 2 3 1 20
+ * Sample Output:
+ * 3 40
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -32,8 +43,10 @@ struct GNode {
 };
 typedef PtrToGNode LGraph;
 
-#define MinData -1
+/* The nuber of vertices in MinHeap can be larger than MAXN 
+ * because we insert v regardless of old v may is in there */
 #define MaxHeapSize MAXN*MAXN
+#define MinData -1
 typedef struct {
 	Vertex v;
 	int dist;
@@ -48,14 +61,13 @@ struct HNode {
 typedef PtrToHNode Heap;
 typedef Heap MinHeap;
 
-// Graph related functions:
+/**
+ * Assistant functions: 
+ */
 LGraph CreatGraph(int VertexNum);
 void InsertEdge(LGraph LG, Edge E);
-LGraph BuildGraph(int Nv, int Ne);
+LGraph BuildGraph(Vertex *pS, Vertex *pD);
 void DestroyGraph(LGraph LG);
-// void InitPathAndDist(int N);
-
-// Heap related functions:
 Heap CreatHeap();
 void DestroyHeap(Heap H);
 Heap BuildHeap();
@@ -66,33 +78,31 @@ void PercUp(MinHeap H, int p);
 void PercDown(MinHeap H, int p);
 ElemType DeleteMin(MinHeap H);
 
-// Core functions:
-void TraverlingPlan(LGraph LG);
-void Dijkstra(LGraph LG, Vertex S);
-
-// Global variables:
-int N, M, S, D; // 2<=N<=500
-LGraph LG;	// Solution guaranteed
-bool collected[MAXN];
-int dist[MAXN];
-Vertex path[MAXN];
-int cost[MAXN];
+/**
+ * Core functions: 
+ */
+void TravelPlan(LGraph LG, Vertex S, Vertex D);
+void Dijkstra(LGraph LG, int dist[], int cost[], Vertex S);
 
 int main()
 {
-	freopen("data.txt", "r", stdin);
-	scanf("%d %d %d %d", &N, &M, &S, &D);
+	LGraph LG;	/* Solution guaranteed */
+	Vertex S, D;
 
-	LG = BuildGraph(N, M);
-	TraverlingPlan(LG);
+	LG = BuildGraph(&S, &D);
+
+	TravelPlan(LG, S, D);
+	
 	DestroyGraph(LG);
 
 	return 0;
 }
 
-void TraverlingPlan(LGraph LG)
+void TravelPlan(LGraph LG, Vertex S, Vertex D)
 {
-	Dijkstra(LG, S);
+	int cost[MAXN], dist[MAXN];
+
+	Dijkstra(LG, dist, cost, S);
 	printf("%d %d\n", dist[D], cost[D]);
 }
 
@@ -104,6 +114,7 @@ Heap CreatHeap()
 	H->Elems[0].v = -1;
 	H->Elems[0].dist = MinData;
 	H->Size = 0;
+
 	return H;
 }
 
@@ -160,12 +171,11 @@ bool IsFullH(Heap H)
 
 void PercDown(MinHeap H, int p)
 {
-	ElemType Tmp = H->Elems[p];
 	int Parent, Child;
+	ElemType Tmp = H->Elems[p];
 
 	for (Parent = p; 2*Parent <= H->Size; Parent = Child) {
 		Child = 2 * Parent;
-		// Find the child with smaller dist
 		if (Child != H->Size && H->Elems[Child+1].dist < H->Elems[Child].dist)
 			Child++;
 		if (Tmp.dist <= H->Elems[Child].dist)
@@ -176,100 +186,53 @@ void PercDown(MinHeap H, int p)
 	H->Elems[Parent] = Tmp;
 }
 
-/*
-   Heap BuildHeap()
-   {
-   MinHeap H;
-   int i;
-
-   H = CreatHeap(LG->Nv);
-   for (i = 0; i < LG->Nv; i++) {
-   H->Elems[i+1].v = i;
-   H->Elems[i+1].dist = dist[i];
-   H->Size++;
-   }
-   for (i = H->Size/2; i > 0; i--)
-   PercDown(H, i);
-   return H;
-   }
-   */
-
-void Dijkstra(LGraph LG, Vertex S)
+void Dijkstra(LGraph LG, int dist[], int cost[], Vertex S)
 {
+	bool collected[MAXN];
 	Vertex V;
 	PtrToAdjVNode W;
 	MinHeap H;
 	Pair pair;
 
-	// Initialize path[] and dist[]
+	/* Initialize path[], dist[], cost[] and collected[] */
 	for (V = 0; V < LG->Nv; V++) {
-		path[V] = -1;
-		dist[V] = INFINITY;
-		cost[V] = INFINITY;
+		dist[V] = cost[V] = INFINITY;
 		collected[V] = false;
 	}
-	// Initialize dist[S]=0 and adjacent vertices of S
-	dist[S] = 0;
-	cost[S] = 0;
-	/*
-	   collected[S] = true;
-	   for (T = LG->G[S].FirstEdge; T; T = T->Next) {
-	   dist[T->AdjV] = T->Weight.dist;
-	   path[T->AdjV] = S;
-	   }
-	   */
-	// Build a MinHeap (dist[] as key)
-	// H = BuildHeap();
-	H = CreatHeap();
-	pair.v = S;
-	pair.dist = dist[S];
+	/* Initialize dist[S]=0 and insert it into MinHeap */
+	H = CreatHeap();	/* Create a MinHeap (dist[] as key) */
+	dist[S] = cost[S] = 0;
+	pair.v = S; pair.dist = dist[S];
 	InsertHeap(H, pair);
-	Pair kk;
+
 	while (!IsEmptyH(H)) {
-		// V = DeleteMin(H).v;
-		kk = DeleteMin(H);
-		// printf("v=%d, dist=%d\n", kk.v, kk.dist);
-		V = kk.v;
+		V = DeleteMin(H).v;
+		/**
+		 * Distance is used as first item  of pair as first 
+		 * item is by default used to compare two pairs 
+		 */
 		if (collected[V])
 			continue;
-		// printf("V=%d\n", V);
 		collected[V] = true;
-		/*
-		// ----------------------
-		Vertex v;
-		printf("dist:");
-		for (v=0; v<LG->Nv; v++)
-		if (dist[v] == INFINITY) printf("  ∞");
-		else printf(" %2d", dist[v]);
-		putchar('\n');
-		printf("path:");
-		for (v=0; v<LG->Nv; v++)
-		printf(" %2d", path[v]);
-		putchar('\n');
-		printf("cost:");
-		for (v=0; v<LG->Nv; v++)
-		if (cost[v] == INFINITY) printf("  ∞");
-		else printf(" %2d", cost[v]);
-		putchar('\n');
-		// ----------------------
-		*/
+
 		for (W = LG->G[V].FirstEdge; W; W = W->Next)
 			if (!collected[W->AdjV]) {
-				if (dist[W->AdjV] > dist[V]+W->Weight.dist) {
-					dist[W->AdjV] = dist[V]+W->Weight.dist;
-					cost[W->AdjV] = cost[V]+W->Weight.cost;
-					// Update dist[W->AdjV] in MinHeap and re-arrange heap
-					pair.v = W->AdjV;
-					pair.dist = dist[W->AdjV];
+				if (dist[W->AdjV] > dist[V] + W->Weight.dist) {
+					/**
+					 * Update cost[w] and dist[w] in MinHeap heap, because of 
+					 * we insert a pair insted of a vertex, we can insert v 
+					 * into the MinHeap (Even if v is already there 
+					 */
+					dist[W->AdjV] = dist[V] + W->Weight.dist;
+					cost[W->AdjV] = cost[V] + W->Weight.cost;
+					pair.v = W->AdjV; pair.dist = dist[W->AdjV];
 					InsertHeap(H, pair);
-					path[W->AdjV] = V;
-				} else if (dist[W->AdjV] == dist[V]+W->Weight.dist 
-						&& cost[W->AdjV] > cost[V]+W->Weight.cost) {
-					cost[W->AdjV] = cost[V]+W->Weight.cost;
-					path[W->AdjV] = V;
-				}
+				} else if (dist[W->AdjV] == dist[V] + W->Weight.dist 
+						&& cost[W->AdjV] > cost[V] + W->Weight.cost)
+					cost[W->AdjV] = cost[V] + W->Weight.cost;
 			}
 	}
+	DestroyHeap(H);
 }
 
 LGraph CreatGraph(int VertexNum)
@@ -303,16 +266,19 @@ void InsertEdge(LGraph LG, Edge E)
 	LG->G[E->V2].FirstEdge = NewNode;
 }
 
-LGraph BuildGraph(int Nv, int Ne)
+LGraph BuildGraph(Vertex *pS, Vertex *pD)
 {
 	LGraph LG;
 	Edge E;
-	int i;
+	int Nv, i;
 
+	scanf("%d", &Nv);
 	LG = CreatGraph(Nv);
-	if (Ne) {
+	scanf("%d", &LG->Ne);
+	scanf("%d %d", pS, pD);
+	if (LG->Ne) {
 		E = (Edge) malloc(sizeof(*E));
-		for (i = 0; i < Ne; i++) {
+		for (i = 0; i < LG->Ne; i++) {
 			scanf("%d %d %d %d", &E->V1, &E->V2, 
 					&E->Weight.dist, &E->Weight.cost);
 			InsertEdge(LG, E);
