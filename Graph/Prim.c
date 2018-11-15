90f8ee23f01aefa0edf6f1ec6d9cbaeda8159d0c
ziwei woo <13806680065@163.com>
2018-11-15 13:46:15 +0800

    Update Prim.c

--- Graph/Prim.c
+++ Graph/Prim.c
@@ -55,9 +55,10 @@ int Prim(MGraph Graph, LGraph MST)
 		V = FindMinDist(Graph, dist);
 		if (V == ERROR)
 			break;	/* V doesn't exist */
-
-		/* Recruit V and insert E<parent[V], V> into MST, (Don't bother 
-		 * to consider whether there's a cycle when inserting an edge) */
+		/**
+		 * Recruit V and insert E<parent[V], V> into MST, (Don't bother to 
+		 * consider whether there's a cycle formed when inserting an edge) 
+		 */
 		E->V1 = parent[V];
 		E->V2 = V;
 		E->Weight = dist[V];
