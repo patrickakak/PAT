855fa723236c99d7587aedcd218e0d6df00402b4
ziwei woo <13806680065@163.com>
2018-11-17 16:22:12 +0800

    Create SelectionSort.c

---
+++ Sort/SelectionSort.c
@@ -0,0 +1,18 @@
+/**
+ * Selection sort:
+ * Wrost: Tw = O(N^2) when using simple traverse search
+ * can be optimised by using minheap technique(Tw=N*logN)
+ */
+void SelectionSort(ElementType A[], int N)
+{
+	int i, MinPosition;
+
+	for (i = 0; i < N; i++) {
+		/* Find the index of minimum element between A[i] and A[N-1] 
+		 * (which is a unsorted set) */
+		MinPosition = ScanForMin(A, i, N-1);
+		/* Add the element to the rear of sorted set */
+		Swap(A[i], A[MinPosition]);
+	}
+}
+
