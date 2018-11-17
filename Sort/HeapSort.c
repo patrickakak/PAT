2c5bfcd1bc7e9b0693a7cd0c7e028cd59a50a080
ziwei woo <13806680065@163.com>
2018-11-17 17:01:22 +0800

    Update HeapSort.c

--- Sort/HeapSort.c
+++ Sort/HeapSort.c
@@ -18,7 +18,7 @@ void PercDown(ElementType A[], int p, int N)
 	ElementType X;
 
 	X = A[p];	/* Fetch the value from root */
-	for (Parent = p; (Parent*2+1) < N; Parent=Child) {
+	for (Parent = p; (Parent*2+1) < N; Parent = Child) {
 		Child = 2*Parent + 1;
 		if ((Child != N-1) && (A[Child] < A[Child+1]) )
 			Child++;	/* Child points to the larger one */
