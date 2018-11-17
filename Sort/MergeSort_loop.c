2b62809dc903ca3f2ec448a19377a06ec8fcbe1f
ziwei woo <13806680065@163.com>
2018-11-17 21:31:52 +0800

    Update MergeSort_loop.c

--- Sort/MergeSort_loop.c
+++ Sort/MergeSort_loop.c
@@ -1,26 +1,12 @@
 /**
  * Merge sort (stable):
  * Worst: Tw = O(NlogN), usually be used as a way of external sort
- *
- * function: merge the adjacent two subsequences
- * length: equals to the length of the current ordered subsequence 
  */
-void Merge_pass(ElementType A[], ElementType TmpA[], int N, int length)
-{
-	int i, j;
-
-	for (i = 0; i <= N-2*length; i += 2*length)
-		Merge1(A, TmpA, i, i+length, i+2*length-1);
-	if (i+length < N)	/* Merge the last 2 sub-sequences */
-		Merge1(A, TmpA, i, i+length, N-1);
-	else	/* There's only 1 sub-sequence left */
-		for (j = i; j < N; j++) TmpA[j] = A[j];
-}
 
 /* Don't need to copy ordered sequence from TmpA[] to A[] */
 void Merge1(ElementType A[], ElementType TmpA[], int L, int R, int RightEnd)
 {
-	int LeftEnd, Tmp, i;
+	int LeftEnd, Tmp;
 
 	LeftEnd = R - 1;	/* The end position of left part */
 	Tmp = L;	/* The starting position of ordered sequence */
@@ -35,6 +21,22 @@ void Merge1(ElementType A[], ElementType TmpA[], int L, int R, int RightEnd)
 		TmpA[Tmp++] = A[L++];	/* Copy the leftover of left part */
 	while (R <= RightEnd)
 		TmpA[Tmp++] = A[R++];	/* Copy the leftover of right part */
+}
+
+/**
+ * function: merge the adjacent two subsequences
+ * length: equals to the length of the current ordered subsequence 
+ */
+void Merge_pass(ElementType A[], ElementType TmpA[], int N, int length)
+{
+	int i, j;
+
+	for (i = 0; i <= N-2*length; i += 2*length)
+		Merge1(A, TmpA, i, i+length, i+2*length-1);
+	if (i+length < N)	/* Merge the last 2 sub-sequences */
+		Merge1(A, TmpA, i, i+length, N-1);
+	else	/* There's only 1 sub-sequence left */
+		for (j = i; j < N; j++) TmpA[j] = A[j];
 }
 
 void Merge_Sort(ElementType A[], int N)
