f1658f5930d3145cb2333e008c0beb0a40ca3f7f
ziwei woo <13806680065@163.com>
2018-11-19 11:59:28 +0800

    Update MostSignificantDigit.c

--- Sort/MostSignificantDigit.c
+++ Sort/MostSignificantDigit.c
@@ -37,9 +37,10 @@ void MSD(ElementType A[], int L, int R, int D)
 	PtrToNode tmp, p, List = NULL; 
 	if (D == 0) return;		/* Termination condition */
 
-	for (i = 0; i < Radix; i++)		/* Initialize every bucket as empty list */
+	for (i = 0; i < Radix; i++)		/* Init every bucket as an empty list */
 		B[i].head = B[i].tail = NULL;
-	for (i = L; i <= R; i++) {	/* Store the original sequence into List in reversed order */
+	/* Store the original sequence into List in reversed order */
+	for (i = L; i <= R; i++) {
 		tmp = (PtrToNode) malloc(sizeof(struct Node));
 		tmp->key = A[i];
 		tmp->next = List;
@@ -48,18 +49,19 @@ void MSD(ElementType A[], int L, int R, int D)
 	/* Distribution process */
 	p = List;
 	while (p) {
-		Di = GetDigit(p->key, D);	/* Get the current digit of the current element */
-		/* Remove from List */
-		tmp = p; p = p->next;
-		/* Insert into the bucket of number B[Di] */
-		if (B[Di].head == NULL) B[Di].tail = tmp;
+		/* Get the current digit of the current element */
+		Di = GetDigit(p->key, D);
+		tmp = p; p = p->next;	/* Remove from List */
+		if (B[Di].head == NULL)
+			B[Di].tail = tmp;	/* Insert into the bucket of number B[Di] */
 		tmp->next = B[Di].head;
 		B[Di].head = tmp;
 	}
 	/* Collecting process */
 	i = j = L;	/* index i, j to mark the left and right boundary of A[] */
 	for (Di = 0; Di < Radix; Di++)	/* For every bucket */
-		if (B[Di].head) {	/* Pour elements in non-empty bucket into A[] recursively */
+		/* Pour elements in non-empty bucket into A[] recursively */
+		if (B[Di].head) {
 			p = B[Di].head;
 			while (p) {
 				tmp = p;
