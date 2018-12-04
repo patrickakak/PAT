41faccb00d1ed372128cc4604490ff874c2f6c28
ziwei woo <13806680065@163.com>
2018-12-04 21:52:50 +0800

    Update 6_7_Isomorphic.c

--- Assignments/en_version/Code_Completion/6_7_Isomorphic.c
+++ Assignments/en_version/Code_Completion/6_7_Isomorphic.c
@@ -1,3 +1,19 @@
+/**
+ *        A          A              A          A
+ *       / \        / \            / \        / \
+ *      B   C      C   B          B   C      B   C
+ *     / \  /     /   / \        / \  /     /   / \
+ *    D  E  G    G   E   D      D  E  G    G   D   E
+ *      /  /      \ /             /   /     \  /
+ *     F   H      H F            F    H     H F
+ *          Figure 1                  Figure 2
+ * -------------------------------------------------
+ * Sample Output1 (for the trees shown in Figure 1):
+ * 1
+ * -------------------------------------------------
+ * Sample Output2 (for the trees shown in Figure 2):
+ * 0
+ */
 int Isomorphic(Tree T1, Tree T2)
 {
 	if ((T1 == NULL) && (T2 == NULL))	/* Both empty */
@@ -6,14 +22,16 @@ int Isomorphic(Tree T1, Tree T2)
 		return 0;	/* One of them is empty */
 	if (T1->Element != T2->Element)
 		return 0;	/* Roots are different */
+	/* Both have no left subtree */
 	if ((T1->Left == NULL) && (T2->Left == NULL))
-		return Isomorphic(T1->Right, T2->Right);	/* Both have no left subtree */
+		return Isomorphic(T1->Right, T2->Right);
+	/* No need to swap the left and the right */
 	if (((T1->Left != NULL) && (T2->Left != NULL))
-			&& (T1->Left->Element == T2->Left->Element))	/* No need to swap the left and the right */
+			&& (T1->Left->Element == T2->Left->Element))
 		return (Isomorphic(T1->Left, T2->Left)
 				&& Isomorphic(T1->Right, T2->Right));
 	else	/* Need to swap the left and the right */
-		return (Isomorphic(T1->Left, T2->Right)
+		return (Isomorphic(T1->Left, T2->Right) 
 				&& Isomorphic(T1->Right, T2->Left));
 }
 
