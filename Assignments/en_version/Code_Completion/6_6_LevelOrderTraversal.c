0435cb654e83c11cf2b107be16609ab17a4ad0c7
ziwei woo <13806680065@163.com>
2018-12-04 21:41:37 +0800

    Update 6_6_LevelOrderTraversal.c

--- Assignments/en_version/Code_Completion/6_6_LevelOrderTraversal.c
+++ Assignments/en_version/Code_Completion/6_6_LevelOrderTraversal.c
@@ -1,5 +1,16 @@
-/* Your function will be put here */
-void Level_order( Tree T, void (*visit)(Tree ThisNode) )
+/**
+ *       3
+ *      / \
+ *     5   6
+ *    / \   \
+ *   1   8  10
+ *      /
+ *     9
+ * ---------------------------
+ * Sample Output:
+ * Level-order: 3 5 6 1 8 10 9
+ */
+void Level_order(Tree T, void (*visit)(Tree ThisNode))
 {
 	Tree TArr[32];
 	int Front = 0, Rear = 0;
@@ -7,8 +18,10 @@ void Level_order( Tree T, void (*visit)(Tree ThisNode) )
 
 	TArr[Rear++] = T;
 	while (Front < Rear) {
-		if (TArr[Front]->Left) TArr[Rear++] = TArr[Front]->Left;
-		if (TArr[Front]->Right) TArr[Rear++] = TArr[Front]->Right;
+		if (TArr[Front]->Left)
+			TArr[Rear++] = TArr[Front]->Left;
+		if (TArr[Front]->Right)
+			TArr[Rear++] = TArr[Front]->Right;
 		(*visit)(TArr[Front++]);
 	}
 }
