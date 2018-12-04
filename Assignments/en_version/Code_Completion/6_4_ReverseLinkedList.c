2b32057de1b4f76dc41b7abe92e3d28487910571
ziwei woo <13806680065@163.com>
2018-12-04 12:44:47 +0800

    Update 6_4_ReverseLinkedList.c

--- Assignments/en_version/Code_Completion/6_4_ReverseLinkedList.c
+++ Assignments/en_version/Code_Completion/6_4_ReverseLinkedList.c
@@ -1,15 +1,16 @@
 /* Your function will be put here */
 
-// The function Reverse is supposed to return the reverse linked list of L,
-// with a dummy header. 
+/* The function Reverse is supposed to return the reverse linked list of L,
+ * with a dummy header. 
 
-// A nonrecursive procedure to reverse a singly linked list in O(N) time 
-// using constant extra space
-List Reverse( List L )
+ * A nonrecursive procedure to reverse a singly linked list in O(N) time 
+ * using constant extra space
+ */
+List Reverse(List L)
 {
 	List R, S, T, ReversedL = NULL;
 
-	// Start to reverse the list without header
+	/* Start to reverse the list without head node */
 	T = L->Next;
 	while (T) {
 		R = T->Next;
@@ -17,7 +18,7 @@ List Reverse( List L )
 		ReversedL = T;
 		T = R;
 	}
-	L->Next = ReversedL;	// Add dummy header
+	L->Next = ReversedL;	/* Add dummy head node */
 	return L;
 }
 
