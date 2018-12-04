f0704959aa50ffab910044088d17863cfc0fe3dd
ziwei woo <13806680065@163.com>
2018-12-04 13:51:43 +0800

    Update 6_1_Reverse.c

--- Assignments/cn_version/Code_Completion/6_1_Reverse.c
+++ Assignments/cn_version/Code_Completion/6_1_Reverse.c
@@ -1,9 +1,17 @@
-// PTA--Data Structures and Algorithms (Chinese version)
-// 6-1 Reverse linked list
+/**
+ * 6-1 Reverse linked list 
+ * Sample Input:
+ * 5
+ * 1 3 4 5 2
+ * Sample Output:
+ * 1
+ * 2 5 4 3 1
+ */
 
-// L point to node 1
-// 1->3->4->5->2 ==> 2->5->4->3->1
-List Reverse( List L )
+/* L point to node 1
+ * 1->3->4->5->2 ==> 2->5->4->3->1
+ */
+List Reverse(List L)
 {
 	PtrToNode RemainL = NULL, NewL = NULL;
 
