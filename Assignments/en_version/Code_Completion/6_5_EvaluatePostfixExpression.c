41a84fb1e701e90d4976459cf2f24d91041b363e
ziwei woo <13806680065@163.com>
2018-12-05 11:26:16 +0800

    Update 6_5_EvaluatePostfixExpression.c

--- Assignments/en_version/Code_Completion/6_5_EvaluatePostfixExpression.c
+++ Assignments/en_version/Code_Completion/6_5_EvaluatePostfixExpression.c
@@ -17,7 +17,6 @@
 #include <stdbool.h>
 #include <ctype.h>
 #include <string.h>
-#include <unistd.h>
 #define ERROR NULL
 #define err Infinity
 typedef enum { operand, operator } type;
@@ -60,7 +59,7 @@ bool IsEmptyS(Stack S)
 	return S->Top == -1;
 }
 
-ElementType Pop( Stack S )
+ElementType Pop(Stack S)
 {
 	if (IsEmptyS(S)) return err;
 	return S->Data[S->Top--];
@@ -75,48 +74,41 @@ type GetOperandOrOperator(char *expr, char s[Max_Expr])
         s[i] = *expr;
 	s[i] = '\0';
 
-	if (s[0]=='-' && (isdigit(s[1]) || s[1]=='.'))
+	if (s[0] == '-' && (isdigit(s[1]) || s[1] == '.'))
 		typ = operand;
-	else if (s[0]=='+' && (isdigit(s[1]) || s[1]=='.'))
+	else if (s[0] == '+' && (isdigit(s[1]) || s[1] == '.'))
 		typ = operand;
-	else if (s[0]=='.')
+	else if (s[0] == '.')
 		typ = operand;
 	else if (isdigit(s[0]))
 		typ = operand;
 	else
 		typ = operator;
-
 	return typ;
 }
 
 ElementType EvalPostfix(char *expr)
 {
+	Stack S;
 	char s[Max_Expr];
 	type tp;
-	Stack S = CreateStack();
-	ElementType X, v1, v2, v;
-
+	ElementType v1, v2, v;
+	
+	S = CreateStack();
 	while (*expr) {
 		tp = GetOperandOrOperator(expr, s);
 		expr += strlen(s);
 		if (*expr) expr += 1;
-		if (tp == operand) {
-			X = atof(s);
-			Push(S, X);
-		} else {
+		
+		if (tp == operand)
+			Push(S, atof(s));
+		else {
 			v2 = Pop(S); v1 = Pop(S);
-			if (v2 == err || v1 == err)
-				return Infinity;
+			if (v2 == err || v1 == err) return Infinity;
 			switch (s[0]) {
-			case '+':
-				v = v1 + v2;
-				break;
-			case '-':
-				v = v1 - v2;
-				break;
-			case '*':
-				v = v1 * v2;
-				break;
+			case '+': v = v1 + v2; break;
+			case '-': v = v1 - v2; break;
+			case '*': v = v1 * v2; break;
 			case '/':
 				if (v2 == 0.0f) return Infinity;
 				v = v1 / v2;
