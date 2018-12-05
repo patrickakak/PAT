64198d86641aee84baeec35c4c23b8bc70864020
ziwei woo <13806680065@163.com>
2018-12-05 11:15:34 +0800

    Update 6_5_EvaluatePostfixExpression.c

--- Assignments/en_version/Code_Completion/6_5_EvaluatePostfixExpression.c
+++ Assignments/en_version/Code_Completion/6_5_EvaluatePostfixExpression.c
@@ -14,12 +14,13 @@
  * Sample Output3:
  * ERROR
  */
+#include <stdbool.h>
 #include <ctype.h>
-#include <math.h>
 #include <string.h>
-
+#include <unistd.h>
 #define ERROR NULL
-typedef enum { false, true } bool;
+#define err Infinity
+typedef enum { operand, operator } type;
 typedef struct StackRecord *PtrToStack;
 typedef PtrToStack Stack;
 struct StackRecord {
@@ -34,110 +35,103 @@ Stack CreateStack()
 	S = (Stack) malloc(sizeof(struct StackRecord));
 	if (!S) return ERROR;
 	S->Top = -1;
-	
 	return S;
+}
+
+void DestroyStack(Stack S)
+{
+	free(S);
+}
+
+bool IsFullS(Stack S)
+{
+	return S->Top == Max_Expr-1;
 }
 
 bool Push( Stack S, ElementType X )
 {
-	if (S->Top == Max_Expr-1) return false;
-	
+	if (IsFullS(S)) return false;
+
 	S->Data[++S->Top] = X;
-	
+
 	return true;
+}
+
+bool IsEmptyS(Stack S)
+{
+	return S->Top == -1;
 }
 
 ElementType Pop( Stack S )
 {
-	if (S->Top == -1) return Infinity;
+	if (IsEmptyS(S))
+		return err;
 	return S->Data[S->Top--];
 }
 
-bool IsOperator( int ch )
+type GetOperandOrOperator(char *expr, char s[Max_Expr])
 {
-	if (strchr("+-*/", ch)) return true;
-	return false;
+	int i;
+	type typ;
+
+	for (i = 0; *expr && !isspace(*expr); expr++, i++)
+        s[i] = *expr;
+	s[i] = '\0';
+
+	if (s[0]=='-' && (isdigit(s[1]) || s[1]=='.'))
+		typ = operand;
+	else if (s[0]=='+' && (isdigit(s[1]) || s[1]=='.'))
+		typ = operand;
+	else if (s[0]=='.')
+		typ = operand;
+	else if (isdigit(s[0]))
+		typ = operand;
+	else
+		typ = operator;
+	return typ;
 }
 
-bool IsValidInput( int ch )
+ElementType EvalPostfix(char *expr)
 {
-	if (strchr("+-*/ .0123456789", ch)) return true;
-	return false;
-}
-
-ElementType EvalPostfix( char *expr )
-{
+	char s[Max_Expr];
+	type tp;
+	Stack S = CreateStack();
 	ElementType X, v1, v2, v;
-	char *t = expr, *front = expr;
-	Stack S;
-	int i, flag = 0;
 
-	// If expression string is empty, return Infinity
-	if (expr[0] == '\0') return Infinity;
-
-	if ((expr[0] == '.' || expr[0] == '-') && expr[1] == '\0') return Infinity;
-
-	// If input is only one float number, print it directly
-	for (i = 1; expr[i]; i++)
-		if (expr[i] != '.' && !isdigit(expr[i]))
-			flag = 1;
-	if (flag == 0) return atof(expr);
-
-	S = CreateStack();
-
-	while (*t) {
-		// Check the validity of input characters
-		if (!IsValidInput(*t)) return Infinity;
-
-		// Last input character must be a valid operator
-		if (*(t+1)=='\0' && !IsOperator(*t)) return Infinity;
-
-		switch (*t) {
-		case ' ':
-			// Space as seperator
-			if (sscanf(front, "%lf", &X)) Push(S, X);
-			front = t + 1;
-			break;
-		case '+':
-			if (*(front+1) != '\0' && *(front+1) != ' ') break;
-			v2 = Pop(S);
-			v1 = Pop(S);
-			if (v1 == Infinity || v2 == Infinity) return Infinity;
-			v = v1 + v2;
-			Push(S, v);
-			break;
-		case '-':
-			if (*(front+1) != '\0' && *(front+1) != ' ') break;
-			v2 = Pop(S);
-			v1 = Pop(S);
-			if (v1 == Infinity || v2 == Infinity) return Infinity;
-			v = v1 - v2;
-			Push(S, v);
-			break;
-		case '*':
-			v2 = Pop(S);
-			v1 = Pop(S);
-			if (v1 == Infinity || v2 == Infinity) return Infinity;
-			v = v1 * v2;
-			Push(S, v);
-			break;
-		case '/':
-			v2 = Pop(S);
-			v1 = Pop(S);
-			if (v1 == Infinity || v2 == Infinity || fabs(v2) < 0.000000001)
+	while (*expr) {
+		tp = GetOperandOrOperator(expr, s);
+		expr += strlen(s);
+		if (*expr) expr += 1;
+		if (tp == operand) {
+			X = atof(s);
+			Push(S, X);
+		} else {
+			v2 = Pop(S); v1 = Pop(S);
+			if (v2 == err || v1 == err)
 				return Infinity;
-			v = v1 / v2;
-			Push(S, v);
-			break;
-		case '.':
-			if (*(t+1) != '\0' && !isdigit(*(t+1))) return Infinity;
+			switch (s[0]) {
+			case '+':
+				v = v1 + v2;
+				Push(S, v);
+				break;
+			case '-':
+				v = v1 - v2;
+				Push(S, v);
+				break;
+			case '*':
+				v = v1 * v2;
+				Push(S, v);
+				break;
+			case '/':
+				if (v2 == 0.0f) return Infinity;
+				v = v1 / v2;
+				Push(S, v);
+				break;
+			}
 		}
-		t++;
 	}
-	// Take the last number in stack
-	v = Pop(S);
+	v  = Pop(S);
 	if (S->Top != -1) return Infinity;
-
-	return v;
+	else return v;
 }
 
