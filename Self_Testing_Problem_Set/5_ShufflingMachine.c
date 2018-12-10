1066c5cca3e90131657e3462ed7c5c064efdb4f8
ziwei woo <13806680065@163.com>
2018-12-11 00:10:26 +0800

    Update 5_ShufflingMachine.c

--- Self_Testing_Problem_Set/5_ShufflingMachine.c
+++ Self_Testing_Problem_Set/5_ShufflingMachine.c
@@ -3,62 +3,59 @@
 
 #define MAXK 20
 #define NumCards 54
+#define LEN 4
+
+void InitialStatus(char Strs[][LEN], char Suit[4]);
+void Output(char Seq[][LEN], int N);
 
 int main()
 {
 	int K, i, j;
-	int order[NumCards];
-	int FinalPos[NumCards];
-	char NewOrder[NumCards][5];
-	char Strs[NumCards][5];
-	char color[4] = {'S', 'H', 'C', 'D'};
+	int GivenOrder[NumCards], FinalPos[NumCards];
+	char NewSeq[NumCards][LEN], Strs[NumCards][LEN];
+	char Suit[4] = {'S', 'H', 'C', 'D'};
 
-	int k = 0;
-	for (i = 0, k = 0; i < 4; i++, k+=13) {
-		for (j = 0; j < 13; j++) {
-			sprintf(Strs[j+k], "%c%d", color[i], j+1);
-		}
-	}
-	sprintf(Strs[52], "J1");
-	sprintf(Strs[53], "J2");
-	
-	/*
-	// test
-	for (i = 0; i < NumCards; i++) {
-		printf(" %s", Strs[i]);
-	}
-	printf("\n");
-	// test
-	*/
+	InitialStatus(Strs, Suit);
 
-	// freopen("data.txt", "r", stdin);
 	scanf("%d", &K);
-	for (i = 0; i < NumCards; i++) {
-		scanf("%d", &order[i]);
-	}
+	for (i = 0; i < NumCards; i++)
+		scanf("%d", &GivenOrder[i]);
 
-	for (i = 0; i < NumCards; i++) {
-		for (FinalPos[i]=order[i]-1, j=0; j < K-1; j++) {
-			FinalPos[i] = order[FinalPos[i]]-1;
-		}
-		// printf("NewPos=%d\n", NewPos);
-	}
-	for (i = 0; i < NumCards; i++) {
-		//NewOrder[FinalPos[i]] = order[i];
-		strcpy(NewOrder[FinalPos[i]], Strs[i]);
-	}
+	/* Find final position of each card, K > 0 */
+	for (i = 0; i < NumCards; i++)
+		for (FinalPos[i] = GivenOrder[i]-1, j = 1; j < K; j++)
+			FinalPos[i] = GivenOrder[FinalPos[i]] - 1;
 
-	int flag = 0;
-	for (i = 0; i < NumCards; i++) {
-		if (!flag)
-			flag = 1;
-		else
-			printf(" ");
-		printf("%s", NewOrder[i]);
-	}
+	for (i = 0; i < NumCards; i++)
+		strcpy(NewSeq[FinalPos[i]], Strs[i]);
+
+	Output(NewSeq, NumCards);
 	printf("\n");
 
 	return 0;
 }
 
+void InitialStatus(char Strs[][LEN], char Suit[4])
+{
+	int i, j, k;
+
+	for (i = 0, k = 0; i < 4; i++, k+=13)
+		for (j = 0; j < 13; j++)
+			sprintf(Strs[j+k], "%c%d", Suit[i], j+1);
+	sprintf(Strs[52], "J1");
+	sprintf(Strs[53], "J2");
+}
+
+void Output(char Seq[][LEN], int N)
+{
+	int i, flag;
+
+	for (flag = 0, i = 0; i < N; i++) {
+		if (!flag)
+			flag = 1;
+		else
+			printf(" ");
+		printf("%s", Seq[i]);
+	}
+}
 
