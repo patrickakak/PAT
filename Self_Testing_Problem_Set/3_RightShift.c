afdb3b26d5d3cb1bdf592423b8c2d821c7be3208
ziwei woo <13806680065@163.com>
2018-12-11 14:13:35 +0800

    Update 3_RightShift.c

--- Self_Testing_Problem_Set/3_RightShift.c
+++ Self_Testing_Problem_Set/3_RightShift.c
@@ -13,6 +13,7 @@ typedef int ElementType;
 /* You can get something's chiral dual by go through a mirror change */
 void Mirror(ElementType A[], int L, int R);
 void Swap(ElementType *a, ElementType *b);
+void RightShift(ElementType S[], int N, int M);
 
 int main()
 {
@@ -24,18 +25,7 @@ int main()
 	for (i = 0; i < N; i++)
 		scanf("%d", &A[i]);
 
-	/* Chirality:
-	 *
-	 * Divide N elements into two parts: 0~N-M-1 and N-M~N-1.
-	 *
-	 * Elements in the same part, they go through twice mirror change, so 
-	 * their relative position is unchanged. However, if you see the two 
-	 * parts as two huge elements, both of them have only mirroed once, so 
-	 * the order of those two is reversed.
-	 */
-	Mirror(A, 0, N-M-1);
-	Mirror(A, N-M, N-1);
-	Mirror(A, 0, N-1);
+	RightShift(A, N, M);
 	
 	for (flag = 0, i = 0; i < N; i++) {
 		if (!flag)
@@ -55,11 +45,28 @@ void Swap(int *a, int *b)
 	tmp = *a; *a = *b; *b = tmp;
 }
 
+/* Chirality:
+ *
+ * Divide N elements into two parts: 0~N-M-1 and N-M~N-1.
+ *
+ * Elements in the same part, they go through twice mirror change, so 
+ * their relative position is unchanged. However, if you see the two 
+ * parts as two huge elements, both of them have only mirroed once, so 
+ * the order of those two is reversed.
+ */
 void Mirror(ElementType A[], int L, int R)
 {
 	int i, j;
 
 	for (i = L, j = R; i < j; i++, j--)
 		Swap(&A[i], &A[j]);
+}
+
+/* M: right shift offset (M<N) */
+void RightShift(ElementType S[], int N, int M)
+{
+	Mirror(S, 0, N-M-1);
+	Mirror(S, N-M, N-1);
+	Mirror(S, 0, N-1);
 }
 
