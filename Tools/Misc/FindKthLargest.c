0c124b6d93962f53c0ad0f3e46a267ec710fb96c
ziwei woo <13806680065@163.com>
2018-12-11 14:55:55 +0800

    Update FindKthLargest.c

---
+++ Tools/Misc/FindKthLargest.c
@@ -0,0 +1,40 @@
+/**
+ * Find the Kth largest number in an array S[]
+ */
+
+void Swap(ElementType *X, ElementType *Y)
+{
+	ElementType tmp;
+
+	tmp = *X; *X = *Y; *Y = tmp;
+}
+
+ElementType FindKthLargest(ElementType S[], int K, int Left, int Right)
+{
+	ElementType e = S[Left];
+	int L = Left, R = Right;
+
+	while (1) {
+		while (Left <= Right && e <= S[Left])
+			Left++;
+		while (Left < Right && e > S[Right])
+			Right--;
+		if (Left < Right)
+			Swap(&S[Left], &S[Right]);
+		else
+			break;
+	}
+	Swap(&S[Left-1], &S[L]);
+	if (Left-L-1 >= K)
+		return FindKthLargest(S, K, L, Left-2);
+	else if (Left-L-1 < K-1)
+		return FindKthLargest(S, K-(Left-L-1)-1, Left, R);
+	else
+		return e;
+}
+
+ElementType Median(ElementType S[], int N)
+{
+	return FindKthLargest(S, (N+1)/2, 0, N-1);
+}
+
