7e4451d126d359a118e95353ea688e12d50b5caa
ziweiix <13806680065@163.com>
2019-05-27 16:37:20 +0800

    Update Mirror.c

--- Tools/Misc/Mirror.c
+++ Tools/Misc/Mirror.c
@@ -1,9 +1,9 @@
 /**
  * Chirality:
  *
- * Elements in the same part, they go through twice mirror change, so 
- * their relative position is unchanged. However, if you see the two 
- * parts as two huge elements, both of them have only mirroed once, so 
+ * Elements in the same part, they go through twice mirror change, so
+ * their relative position is unchanged. However, if you see the two
+ * parts as two huge elements, both of them have only mirroed once, so
  * the order of those two is reversed.
  */
 void Swap(ElementType *X, ElementType *Y)
@@ -21,23 +21,23 @@ void Mirror(ElementType S[], int L, int R)
 		Swap(&S[i], &S[j]);
 }
 
-/* M: offset (M<N) 
- * Divide N elements into two parts: 0~N-M-1 and N-M~N-1.
- */
-void RightShift(ElementType S[], int N, int M)
-{
-	Mirror(S, 0, N-M-1);
-	Mirror(S, N-M, N-1);
-	Mirror(S, 0, N-1);
-}
-
-/* M: offset (M<N) 
+/* M: offset (M<N)
  * Divide N elements into two parts: 0~M-1 and M~N-1
  */
 void LeftShift(ElementType S[], int N, int M)
 {
+	if (M >= N) return;
 	Mirror(S, 0, M-1);
 	Mirror(S, M, N-1);
 	Mirror(S, 0, N-1);
 }
 
+/* M: offset (M<N)
+ * Divide N elements into two parts: 0~N-M-1 and N-M~N-1.
+ */
+void RightShift(ElementType S[], int N, int M)
+{
+	if (M >= N) return;
+	
+	LeftShift(S, N, N-M);
+}
