9fc0906ed4fdbb19ed4fcca926e42d2f2ee21053
ziweiix <13806680065@163.com>
2019-06-21 22:24:13 +0800

    Create ThreadBTree.c

---
+++ Toolbox/Tree/ThreadBTree.c
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PRE_THREAD 0
+#define IN_THREAD 1
+#define POST_THREAD 2
+
+typedef struct TBTNode *PtrToTBTNode;
+typedef PtrToTBTNode TBTree;
+struct TBTNode {
+	int data;
+	int ltag, rtag;
+	TBTree lchild, rchild;
+};
+
+TBTree CBtree(int a[], int b[], int L1, int R1, int L2, int R2);
+void inThread(TBTree p, TBTree *pre);
+void preThread(TBTree p, TBTree *pre);
+void postThread(TBTree p, TBTree *pre);
+void creatThreadBTree(TBTree r, int flag);
+void preOrder(TBTree root);
+void inOrder(TBTree root);
+TBTree First(TBTree p);
+TBTree Next(TBTree p);
+void visit(TBTree p);
+
+
+int main()
+{
+	TBTree root;
+	int a[5] = {1, 2, 3, 5, 4};
+	int b[5] = {3, 2, 5, 1, 4};
+
+	root = CBtree(a, b, 0, 4, 0, 4);
+	creatThreadBTree(root, IN_THREAD);
+	inOrder(root);
+	return 0;
+}
+
+TBTree CBtree(int a[], int b[], int L1, int R1, int L2, int R2)
+{
+	int k;
+
+	if (L1 > R1)
+		return NULL;
+	else {
+		TBTree t = (TBTree) malloc(sizeof(struct TBTNode));
+		t->ltag = t->rtag = 0;
+		t->data = a[L1];
+		for (k = L2; k <= R2; ++k)
+			if (a[L1] == b[k])
+				break;
+		t->lchild = CBtree(a, b, L1+1, L1+k-L2, L2, k-1);
+		t->rchild = CBtree(a, b, L1+k-L2+1, R1, k+1, R2);
+		return t;
+	}
+}
+
+void inThread(TBTree p, TBTree *pre)
+{
+	if (p == NULL)
+		return;
+	inThread(p->lchild, pre);
+	if (p->lchild == NULL) {
+		p->lchild = (*pre);
+		p->ltag = 1;
+	}
+	if (*pre != NULL && (*pre)->rchild == NULL) {
+		(*pre)->rchild = p;
+		(*pre)->rtag = 1;
+	}
+	(*pre) = p;
+	inThread(p->rchild, pre);
+}
+
+void postThread(TBTree p, TBTree *pre)
+{
+	if (p == NULL)
+		return;
+	postThread(p->lchild, pre);
+	postThread(p->rchild, pre);
+	if (p->lchild == NULL) {
+		p->lchild = (*pre);
+		p->ltag = 1;
+	}
+	if (*pre != NULL && (*pre)->rchild == NULL) {
+		(*pre)->rchild = p;
+		(*pre)->rtag = 1;
+	}
+	(*pre) = p;
+}
+
+void preThread(TBTree p, TBTree *pre)
+{
+	if (p == NULL)
+		return;
+	if (p->lchild == NULL) {
+		p->lchild = (*pre);
+		p->ltag = 1;
+	}
+	if (*pre != NULL && (*pre)->rchild == NULL) {
+		(*pre)->rchild = p;
+		(*pre)->rtag = 1;
+	}
+	(*pre) = p;
+	if (p->ltag == 0)
+		preThread(p->lchild, pre);
+	if (p->rtag == 0)
+		preThread(p->rchild, pre);
+}
+
+void creatThreadBTree(TBTree r, int flag)
+{
+	TBTree p = NULL;
+	TBTree *pre = &p;
+
+	if (!r) return;
+
+	switch (flag) {
+	case PRE_THREAD:
+		preThread(r, pre);
+		break;
+	case IN_THREAD:
+		inThread(r, pre);
+		break;
+	case POST_THREAD:
+		postThread(r, pre);
+		break;
+	}
+	(*pre)->rchild = NULL;
+	(*pre)->rtag = 1;
+}
+
+void preOrder(TBTree root)
+{
+	if (root == NULL)
+		return;
+	TBTree p = root;
+	while (p != NULL) {
+		while (p->ltag == 0) {
+			visit(p);
+			p = p->lchild;
+		}
+		visit(p);
+		p = p->rchild;
+	}
+}
+
+TBTree First(TBTree p)
+{
+	while (p->ltag == 0)
+		p = p->lchild;
+	return p;
+}
+
+TBTree Next(TBTree p)
+{
+	if (p->rtag == 0)
+		return First(p->rchild);
+	else
+		return p->rchild;
+}
+
+void visit(TBTree p)
+{
+	printf("%d ", p->data);
+}
+
+void inOrder(TBTree root)
+{
+	TBTree p;
+
+	for (p = First(root); p != NULL; p = Next(p))
+		visit(p);
+}
+
