d99bbe5d53546db39661c875a683f2e9fcbb81dc
ziweiix <13806680065@163.com>
2019-07-03 21:30:20 +0800

    Update B_Tree.c

--- Toolbox/Tree/B_Tree.c
+++ Toolbox/Tree/B_Tree.c
@@ -7,8 +7,8 @@
 typedef int keyType;
 typedef struct node *ptrToNode;
 struct node {
-    int n; /* n < M No. of keys in node will always less than order of B tree */
-    keyType keys[M-1]; /*array of keys*/
-    struct node *p[M]; /* (n+1 pointers will be in use) */
+	int n; 	/* n < M No. of keys in node will always less than order of B tree */
+	keyType keys[M-1]; 	/*array of keys*/
+	struct node *p[M]; 	/* (n+1 pointers will be in use) */
 } *root = NULL;
 
