cbb9a52d06b3e1a098443dddfcf63e5f465b5fbc
ziweiix <13806680065@163.com>
2019-07-04 10:16:19 +0800

    Update B_Tree.c

--- Toolbox/Tree/B_Tree.c
+++ Toolbox/Tree/B_Tree.c
@@ -99,3 +99,155 @@ enum KeyStatus ins( struct node *ptr, int key, int *upKey, struct node **newnode
 	return InsertIt;
 }
 
+void search(int key)
+{
+	int pos, i, n;
+	struct node *ptr = root;
+	
+	printf("Search path:\n");
+	while (ptr) {
+		n = ptr->n;
+		for (i = 0; i < ptr->n; i++)
+			printf(" %d", ptr->keys[i]);
+		printf("\n");
+		pos = searchPos(key, ptr->keys, n);
+		if (pos < n && key == ptr->keys[pos]) {
+			printf("Key %d found in position %d of last dispalyed node\n", key, i);
+			return;
+		}
+		ptr = ptr->p[pos];
+	}
+	printf("Key %d is not available\n", key);
+}
+
+int searchPos(int key, int *key_arr, int n)
+{
+	int pos = 0;
+	while (pos < n && key > key_arr[pos])
+		pos++;
+	return pos;
+}
+
+void DelNode(int key)
+{
+	struct node *uproot;
+	enum KeyStatus value;
+	
+	value = del(root, key);
+	switch (value) {
+	case SearchFailure:
+		printf("Key %d is not available\n", key);
+		break;
+	case LessKeys:
+		uproot = root;
+		root = root->p[0];
+		free(uproot);
+		break;
+	}
+}
+
+enum KeyStatus del(struct node *ptr, int key)
+{
+	int pos, i, pivot, n, min;
+	int *key_arr;
+	enum KeyStatus value;
+	struct node **p, *lptr, *rptr;
+
+	if (ptr == NULL)
+		return SearchFailure;
+	/* Assigns values of node */
+	n = ptr->n;
+	key_arr = ptr->keys;
+	p = ptr->p;
+	min = (M - 1)/2; 	/* Minimum number of keys */
+
+	// Search for key to delete
+	pos = searchPos(key, key_arr, n);
+	// p is a leaf
+	if (p[0] == NULL) {
+		if (pos == n || key < key_arr[pos])
+			return SearchFailure;
+		/* Shift keys and pointers left */
+		for (i = pos+1; i < n; i++) {
+			key_arr[i-1] = key_arr[i];
+			p[i] = p[i+1];
+		}
+		return --ptr->n >= (ptr==root ? 1 : min) ? Success : LessKeys;
+	}
+
+	// if found key but p is not a leaf
+	if (pos < n && key == key_arr[pos]) {
+		struct node *qp = p[pos], *qp1;
+		int nkey;
+		while (1) {
+			nkey = qp->n;
+			qp1 = qp->p[nkey];
+			if (qp1 == NULL)
+				break;
+			qp = qp1;
+		}
+		key_arr[pos] = qp->keys[nkey-1];
+		qp->keys[nkey - 1] = key;
+	}
+	value = del(p[pos], key);
+	if (value != LessKeys)
+		return value;
+
+	if (pos > 0 && p[pos-1]->n > min) {
+		pivot = pos - 1; 	/* pivot for left and right node */
+		lptr = p[pivot];
+		rptr = p[pos];
+		/* Assigns values for right node */
+		rptr->p[rptr->n + 1] = rptr->p[rptr->n];
+		for (i = rptr->n; i > 0; i--) {
+			rptr->keys[i] = rptr->keys[i-1];
+			rptr->p[i] = rptr->p[i-1];
+		}
+		rptr->n++;
+		rptr->keys[0] = key_arr[pivot];
+		rptr->p[0] = lptr->p[lptr->n];
+		key_arr[pivot] = lptr->keys[--lptr->n];
+		return Success;
+	}
+	// if (posn > min)
+	if (pos < n && p[pos + 1]->n > min) {
+		pivot = pos; 	/* pivot for left and right node */
+		lptr = p[pivot];
+		rptr = p[pivot+1];
+		/* Assigns values for left node */
+		lptr->keys[lptr->n] = key_arr[pivot];
+		lptr->p[lptr->n + 1] = rptr->p[0];
+		key_arr[pivot] = rptr->keys[0];
+		lptr->n++;
+		rptr->n--;
+		for (i = 0; i < rptr->n; i++) {
+			rptr->keys[i] = rptr->keys[i+1];
+			rptr->p[i] = rptr->p[i+1];
+		}
+		rptr->p[rptr->n] = rptr->p[rptr->n + 1];
+		return Success;
+	}
+
+	if (pos == n)
+		pivot = pos-1;
+	else
+		pivot = pos;
+
+	lptr = p[pivot];
+	rptr = p[pivot+1];
+	/* merge right node with left node */
+	lptr->keys[lptr->n] = key_arr[pivot];
+	lptr->p[lptr->n + 1] = rptr->p[0];
+	for (i = 0; i < rptr->n; i++) {
+		lptr->keys[lptr->n + 1 + i] = rptr->keys[i];
+		lptr->p[lptr->n + 2 + i] = rptr->p[i+1];
+	}
+	lptr->n = lptr->n + rptr->n +1;
+	free(rptr); /*Remove right node*/
+	for (i = pos+1; i < n; i++) {
+		key_arr[i-1] = key_arr[i];
+		p[i] = p[i+1];
+	}
+	return --ptr->n >= (ptr == root ? 1 : min) ? Success : LessKeys;
+}
+
