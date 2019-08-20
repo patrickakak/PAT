f812c33fac7919f6b03e56ac94a30d7f346d8cd2
chopchop <13806680065@163.com>
2019-08-20 14:40:29 +0800

    Create SPFA.cpp

---
+++ Toolbox/Graph/SPFA.cpp
@@ -0,0 +1,44 @@
+/**
+ * Shortest Path Faster Algorithm
+ * T = O(kE)
+ */
+
+struct Node {
+	int v, dis;
+};
+
+vector<Node> Adj[MAXV];
+int n, d[MAXV], num[MAXV];
+bool inq[MAXV];
+
+bool SPFA(int s)
+{
+	memset(inq, false, sizeof(inq));
+	memset(num, 0, sizeof(num));
+	fill(d, d+MAXV, INF);
+	queue<int> Q;
+	Q.push(s);
+	inq[s] = true;
+	num[s]++;
+	d[s] = 0;
+	while (!Q.empty()) {
+		int u = Q.front();
+		Q.pop();
+		inq[u] = false;
+		for (int j = 0; j < (int)Adj[u].size(); j++) {
+			int v = Adj[u][j].v;
+			int dis = Adj[u][j].dis;
+			if (d[u] + dis < d[v]) {
+				d[v] = d[u] + dis;
+				if (!inq[v]) {
+					Q.push(v);
+					inq[v] = true;
+					num[v]++;
+					if (num[v] >= n) return false;
+				}
+			}
+		}
+	}
+	return true;
+}
+
