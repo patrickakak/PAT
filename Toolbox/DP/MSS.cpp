71c3013b6744331107d5a80647a495845fdb5b0f
ziwei woo <13806680065@163.com>
2019-08-20 17:11:04 +0800

    Add MSS.cpp

---
+++ Toolbox/DP/MSS.cpp
@@ -0,0 +1,34 @@
+/**
+ * Maximum Subsequence Sum
+ */
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+const int maxn = 10010;
+int A[maxn], dp[maxn];
+
+int main()
+{
+	// 6
+	// -2 11 -4 13 -5 -2
+	int n;
+
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+		scanf("%d", &A[i]);
+
+	dp[0] = A[0];
+	for (int i = 1; i < n; i++)
+		dp[i] = max(A[i], dp[i-1]+A[i]);
+
+	int k = 0;
+	for (int i = 1; i < n; i++)
+		if (dp[i] > dp[k])
+			k = i;
+	printf("%d\n", dp[k]);
+
+	return 0;
+}
+
+
