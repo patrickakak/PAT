0569eb5500a43efa0230abd5a5f3070f71a06421
chopchop <13806680065@163.com>
2019-08-21 17:44:49 +0800

    Update LCS.cpp

--- Toolbox/DP/LCS.cpp
+++ Toolbox/DP/LCS.cpp
@@ -18,19 +18,25 @@ int main()
 	// adminsorry
 	gets(A+1);
 	gets(B+1);
+	
 	int lenA = strlen(A+1);
 	int lenB = strlen(B+1);
+	
 	for (int i = 0; i <= lenA; i++)
 		dp[i][0] = 0;
 	for (int j = 0; j <= lenB; j++)
 		dp[0][j] = 0;
+	
+	//             / dp[i][j-1]+1, A[i]==B[j]
+	// dp[i][j] = 
+	//             \ max{ dp[i-1][j], dp[i][j-1], A[i]!=B[j] }
 	for (int i = 0; i <= lenA; i++)
 		for (int j = 1; j <= lenB; j++)
 			if (A[i] == B[j])
 				dp[i][j] = dp[i-1][j-1] + 1;
 			else
 				dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-
+	// 6
 	printf("%d\n", dp[lenA][lenB]);
 	return 0;
 }
