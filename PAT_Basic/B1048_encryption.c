06d09267be21a4df3e866eb689d44920efe35091
chopchop <13806680065@163.com>
2019-08-23 19:45:03 +0800

    Update B1048_encryption.c

--- PAT_Basic/B1048_encryption.c
+++ PAT_Basic/B1048_encryption.c
@@ -13,22 +13,23 @@ char N2C[13] = {'0','1','2','3','4','5','6','7','8','9','J','Q','K'};
 int main()
 {
 	// freopen("test.txt", "r", stdin);
+	
 	char A[105], B[105], stack[105], sum;
-	int len_a, len_b, i, j, c, count=1, top=-1;
+	int len_a, len_b, i, j, c, count = 1, top = -1;
 
 	scanf("%s %s", A, B);
 
 	len_a = strlen(A); len_b = strlen(B);
 	i = len_a - 1; j = len_b - 1;
 	while (i >= 0 && j >= 0) {
-		if (count%2==1) {
+		if (count%2 == 1) {
 			c = (A[i]-'0') + (B[j]-'0');
 			sum = N2C[c%13];
 		} else {
 			c = B[j] - A[i];
 			if (c < 0)
 				c += 10;
-			sum = c+'0';
+			sum = c + '0';
 		}
 		stack[++top] = sum;
 		count++;
