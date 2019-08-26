8a0fc3570926102e66ff84727277b22627cf1466
ziwei woo <13806680065@163.com>
2019-08-26 16:12:29 +0800

    Add MergeSort_Iterative.cpp

--- Toolbox/Sort/MergeSort_Iterative.cpp
+++ Toolbox/Sort/MergeSort_Iterative.cpp
@@ -68,6 +68,7 @@ void Merge_Sort(ElementType A[], int N)
 	}
 }
 
+// Or ...
 #define maxn 100000
 void merge(int A[], int L1, int R1, int L2, int R2)
 {
@@ -89,9 +90,19 @@ void mergeSort(int A[], int n)
 	for (int step = 2; step/2 < n; step *= 2)
 		for (int i = 0; i < n; i += step) {
 			int mid = i + step/2 - 1;
-			if (mid + 1 <= n)
-				merge(A, i, mid, mid+1, min(i+step-1, n));
+			if (mid + 1 < n)
+				merge(A, i, mid, mid+1, min(i+step-1, n-1));
 		}
+}
+
+// Favorable
+void mergeSort(int A[], int n)
+{
+	for (int step = 2; step/2 < n; step *= 2)
+		for (int i = 0; i < n; i += step)
+			sort(A+i, A+min(i+step, n));
+		// Do something
+	}
 }
 
 
