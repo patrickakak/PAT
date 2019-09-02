8aa34d3cb31db2e826e7b023013e78330a51d06f
chopchop <13806680065@163.com>
2019-09-02 16:59:07 +0800

    Update A1101_QuickSort.cpp

--- PAT_Advanced/A1101_QuickSort.cpp
+++ PAT_Advanced/A1101_QuickSort.cpp
@@ -24,12 +24,14 @@ int main()
 	scanf("%d", &n);
 	for (int i = 0; i < n; i++)
 		scanf("%d", &a[i]);
+	
 	leftMax[0] = 0;
 	for (int i = 1; i < n; i++)
 		leftMax[i] = max(leftMax[i-1], a[i-1]);
 	rightMin[n-1] = INF;
 	for (int i = n-2; i >= 0; i--)
 		rightMin[i] = min(rightMin[i+1], a[i+1]);
+	
 	for (int i = 0; i < n; i++)
 		if (leftMax[i] < a[i] && rightMin[i] > a[i])
 			ans[num++] = a[i];
