33cee3209c481a59d949ab169fbd5a1ae01e53b9
ziwei woo <13806680065@163.com>
2019-09-03 10:26:57 +0800

    Update Eratosthenes.cpp

--- Toolbox/Misc/Eratosthenes.cpp
+++ Toolbox/Misc/Eratosthenes.cpp
@@ -19,6 +19,26 @@ void Find_Prime()
 		}
 }
 
+/*
+bool isPrime(int n)
+{
+	if (n <= 1) return false;
+	int sqr = (int) sqrt(1.0 * n);
+	for (int i = 2; i <= sqr; i++)
+		if (n % i == 0) return false;
+	return true;
+}
+
+void Find_Prime()
+{
+	for (int i = 1; i < maxn; i++)
+		if (isPrime(i)) {
+			prime[num++] = i;
+			p[i] = true;
+		}
+}
+*/
+
 int main()
 {
 	Find_Prime();
