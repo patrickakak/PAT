0b2004c6fbbeec14188bb1ee39e67589b7ce554b
ziwei woo <13806680065@163.com>
2019-09-03 10:50:26 +0800

    Update Eratosthenes.cpp

--- Toolbox/Misc/Eratosthenes.cpp
+++ Toolbox/Misc/Eratosthenes.cpp
@@ -21,24 +21,39 @@ void Find_Prime()
 		}
 }
 
-/*
-bool isPrime(int n)
+// Euler sieve method: T = O(n)
+bool vis[maxn] = {0};
+void EulerSieve()
 {
-	if (n <= 1) return false;
-	int sqr = (int) sqrt(1.0 * n);
-	for (int i = 2; i <= sqr; i++)
-		if (n % i == 0) return false;
-	return true;
+	for (int i = 2; i < maxn; i++) {
+		if (!vis[i])
+			prime[num++] = i;
+		for (int j = 0; j < num && i*prime[j] < maxn; j++) {
+			vis[i * prime[j]] = 1;
+			if (i % prime[j] == 0)
+				break;
+		}
+	}
 }
+
+/*
+   bool isPrime(int n)
+   {
+   if (n <= 1) return false;
+   int sqr = (int) sqrt(1.0 * n);
+   for (int i = 2; i <= sqr; i++)
+   if (n % i == 0) return false;
+   return true;
+   }
 
 // T = O(n * sqrt(n)), works fine if maxn < 10^5
 void Find_Prime()
 {
-	for (int i = 1; i < maxn; i++)
-		if (isPrime(i)) {
-			prime[num++] = i;
-			p[i] = true;
-		}
+for (int i = 1; i < maxn; i++)
+if (isPrime(i)) {
+prime[num++] = i;
+p[i] = true;
+}
 }
 */
 
