8f12bb986f1bfaef7bf1af68213ef6ffcfd6ad36
ziwei woo <13806680065@163.com>
2019-09-05 11:11:19 +0800

    Update A1051_PopSequence.cpp

--- PAT_Advanced/A1051_PopSequence.cpp
+++ PAT_Advanced/A1051_PopSequence.cpp
@@ -6,7 +6,7 @@
  * 7 6 5 4 3 2 1
  * 5 6 4 3 7 2 1
  * 1 7 6 5 4 3 2
- * --------------
+ * -------------
  * Sample output:
  * YES
  * NO
@@ -18,25 +18,25 @@
 #include <stack>
 using namespace std;
 
-const int maxn = 1010;
+#define maxn 1010
 int arr[maxn];
 stack<int> s;
 
 int main()
 {
 	// freopen("tst.txt", "r", stdin);
-	int m, n, k;	// m: max capacity
+	int m, n, k;
 
 	scanf("%d%d%d", &m, &n, &k);
-	while (k--) {
-		while (!s.empty())
+	for (int i=0; i<k; i++) {
+		while (!s.empty())	// clear stack once again
 			s.pop();
-		for (int i = 1; i <= n; i++)
-			scanf("%d", &arr[i]);
-		int current = 1;
-		bool flag = true;
-		for (int i = 1; i <= n; i++) {
-			s.push(i);
+		for (int j = 0; j < n; j++)
+			scanf("%d", &arr[j]);
+		bool flag = true;	// To indicate validity of the sequence
+		int current = 0;	// point to next element in to-be-checked sequence
+		for (int j = 1; j <= n; j++) {
+			s.push(j);
 			if ((int)s.size() > m) {
 				flag = false;
 				break;
