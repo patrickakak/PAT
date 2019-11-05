f7d49ff4d8ef8d5eb0ea257065f57cb4a3d85d84
chopchop <13806680065@163.com>
2019-11-05 23:43:24 +0800

    Update MergeSort_Iterative.cpp

--- Toolbox/Sort/MergeSort_Iterative.cpp
+++ Toolbox/Sort/MergeSort_Iterative.cpp
@@ -75,10 +75,8 @@ void merge(int A[], int L1, int R1, int L2, int R2)
 	int i = L1, j = L2;
 	int temp[maxn], index =0;
 	while (i <= R1 && j <= R2)
-		if (A[i] <= A[j])
-			temp[index++] = A[i++];
-		else
-			temp[index++] = A[j++];
+		if (A[i] <= A[j]) temp[index++] = A[i++];
+		else temp[index++] = A[j++];
 	while (i <= R1) temp[index++] = A[i++];
 	while (j <= R2) temp[index++] = A[j++];
 	for (int i = 0; i < index; i++)
