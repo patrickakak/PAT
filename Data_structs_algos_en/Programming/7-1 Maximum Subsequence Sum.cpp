cc6ae064b8e704d96254abb8ec346082505a3f6e
Vuzzway <13806680065@163.com>
2019-11-28 14:24:52 +0800

    Update and rename 7-1 Maximum Subsequence Sum.c to 7-1 Maximum Subsequence Sum.cpp

--- Data_structs_algos_en/Programming/7-1 Maximum Subsequence Sum.cpp
+++ Data_structs_algos_en/Programming/7-1 Maximum Subsequence Sum.cpp
@@ -1,11 +1,6 @@
-/* Sample Input:
- * 10
- * -10 1 2 3 4 -5 -23 3 7 -21
- * Sample Output:
- * 10 1 4
- */
-#include <stdio.h>
-#include <stdlib.h>
+
+#include <cstdio>
+#include <cstdlib>
 
 #define MAXK 100000
 typedef struct OutNode *PtrToOutNode;
@@ -36,24 +31,19 @@ int main()
 
 PtrToOutNode MaxSubseqSum4(int A[], int N)
 {
-	/* Set MaxSum as -1 in case of all K numbers are negative except '0' */
-	int ThisSum=0, MaxSum=-1, Diff=0, i;
-	PtrToOutNode out = (PtrToOutNode) malloc(sizeof(struct OutNode));
-
+	int ThisSum=0, MaxSum=-1, Diff=0;
+	PtrToOutNode out = new struct OutNode;
 	out->first = out->last = out->MaxSum = 0;
-	for (i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++) {
 		ThisSum += A[i];
 		if (ThisSum > MaxSum) {
 			MaxSum = ThisSum;
-			/* When ThisSum>MaxSum, the last member of max subsequence must 
-			 * be i (which is the current processed node), then find the first
-			 * element according to the difference between them */
 			out->last = i;
 			out->first = out->last - Diff;
 			Diff++;
 		} else if (ThisSum < 0) {
 			ThisSum = 0, Diff = 0;
-		} else 	/* ThisSum equals to MaxSum, difference+1 */
+		} else
 			Diff++;
 	}
 	out->MaxSum = MaxSum;
