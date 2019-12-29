0361352a58d2ba3ab351917df3d3489937147d94
Vuzzway <13806680065@163.com>
2019-12-29 10:34:35 +0800

    Create 7-29 删除字符串中的子串.cpp

---
+++ 基础编程题目集/编程题/7-29 删除字符串中的子串.cpp
@@ -0,0 +1,23 @@
+
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  //freopen("tst.txt", "r", stdin);
+  string s1, s2;
+  getline(cin, s1);
+  getline(cin, s2);
+  int len2=s2.length();
+  while (1) {
+    int p=s1.find(s2);
+    if (p!=-1) {
+      s1.erase(p, len2);
+    } else break;
+  }
+  cout << s1;
+
+  return 0;
+}
+
