5ae1eef8dbddc50aebf6cd214fc5f47aaa2ff8d9
Vuzzway <13806680065@163.com>
2019-12-29 10:22:21 +0800

    Create 6-9 统计个位数字.cpp

---
+++ 基础编程题目集/函数题/6-9 统计个位数字.cpp
@@ -0,0 +1,15 @@
+int Count_Digit ( const int N, const int D )
+{
+	int temp, count;
+
+	temp = N < 0 ? -N : N;
+	if (N == 0 && D == 0)  return 1;
+	count = 0;
+	while (temp) {
+		if (temp%10 == D)
+			count++;
+		temp /= 10;
+	}
+	return count;
+}
+
