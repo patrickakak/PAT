6b457d944f96f44b4426c05761b5f98519c32a68
Vuzzway <13806680065@163.com>
2019-12-29 10:24:49 +0800

    Create 7-2 然后是几点.cpp

---
+++ 基础编程题目集/编程题/7-2 然后是几点.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+int main()
+{
+  // freopen("tst.txt", "r", stdin);
+  int a, b;
+  scanf("%d%d", &a, &b);
+  int hh=a/100, mm=a%100;
+  int mins=hh*60+mm;
+  int c=mins+b;
+  printf("%d%02d", c/60, c%60);
+
+  return 0;
+}
+
