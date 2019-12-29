3e7b8e10ddb43e9104ad15e17350c5c362bb608d
Vuzzway <13806680065@163.com>
2019-12-29 10:27:43 +0800

    Create 7-10 计算工资.cpp

---
+++ 基础编程题目集/编程题/7-10 计算工资.cpp
@@ -0,0 +1,24 @@
+
+#include <cstdio>
+
+int main()
+{
+  //freopen("tst.txt", "r", stdin);
+  int year, hour;
+  scanf("%d%d", &year, &hour);
+  int h1, h2;
+  if (hour<=40) {
+    h1=hour;
+    h2=0;
+  } else {
+    h1=40;
+    h2=hour-40;
+  }
+  if (year<5) {
+    printf("%.2f", 30.0*h1+30.0*1.5*h2);
+  } else {
+    printf("%.2f", 50.0*h1+50.0*1.5*h2);
+  }
+
+  return 0;
+}
