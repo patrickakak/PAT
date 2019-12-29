efbf7eaf0ece99b51db848e2c3f43a715d229f05
Vuzzway <13806680065@163.com>
2019-12-29 10:23:19 +0800

    Create 6-11 求自定类型元素序列的中位数.cpp

---
+++ 基础编程题目集/函数题/6-11 求自定类型元素序列的中位数.cpp
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+/*--------------- Simple integer sort --------------------*/
+int compare(const void *a, const void *b)
+{
+    ElementType r = *(ElementType*)a - *(ElementType*)b;
+    if (r < 0) return -1;
+    else if (r > 0) return 1;
+    else return 0;
+}
+
+ElementType Median( ElementType A[], int N )
+{
+    qsort(A, N, sizeof(ElementType), compare);
+    return A[N/2];
+}
