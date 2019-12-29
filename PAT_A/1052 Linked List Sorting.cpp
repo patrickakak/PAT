83f6280089b6be4553572f917b51f307450fc3d4
Vuzzway <13806680065@163.com>
2019-12-29 15:45:42 +0800

    Update 1052 Linked List Sorting.cpp

--- PAT_A/1052 Linked List Sorting.cpp
+++ PAT_A/1052 Linked List Sorting.cpp
@@ -1,72 +1,51 @@
-/**
- * Sample input:
- * 5 00001
- * 11111 100 -1
- * 00001 0 22222
- * 33333 100000 11111
- * 12345 -1 33333
- * 22222 1000 12345
- * ----------------
- * Sample output:
- * 5 12345
- * 12345 -1 00001
- * 00001 0 11111
- * 11111 100 22222
- * 22222 1000 33333
- * 33333 100000 -1
- */
+
+#include <iostream>
 #include <cstdio>
 #include <algorithm>
 using namespace std;
 
-const int maxn = 100005;
-struct Node {
-	int address, data, next;
-	bool flag;
-} node[maxn];
+const int maxn=100005;
+struct node {
+	int addr, key, next;
+	int flag;
+} nodes[maxn];
 
-bool cmp(Node a, Node b)
+bool cmp(node a, node b)
 {
-	if (a.flag == false || b.flag == false)
-		return a.flag > b.flag;
-	else
-		return a.data < b.data;
+	if (a.flag!=b.flag) return a.flag>b.flag;
+	else return a.key<b.key;
 }
 
 int main()
 {
 	// freopen("tst.txt", "r", stdin);
-	for (int i = 0; i < maxn; i++)
-		node[i].flag = false;
-
-	int n, begin, address;
-	scanf("%d%d", &n, &begin);
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &address);
-		scanf("%d%d", &node[address].data, &node[address].next);
-		node[address].address = address;
+	int n, fir, addr, key, next;
+	scanf("%d %d", &n, &fir);
+	for (int i=0; i<n; i++) {
+		scanf("%d%d%d", &addr, &key, &next);
+		nodes[addr].addr=addr;
+		nodes[addr].key=key;
+		nodes[addr].next=next;
 	}
-	int count = 0, p = begin;
-	while (p != -1) {
-		node[p].flag = true;
-		count++;
-		p = node[p].next;
+	int cnt=0, p=fir;
+	while (p!=-1) {
+		nodes[p].flag++;
+		p=nodes[p].next;
+		cnt++;
 	}
-	if (count == 0)
+	if (cnt==0) {
 		printf("0 -1");
-	else {
-		sort(node, node+maxn, cmp);
-		printf("%d %05d\n", count, node[0].address);
-		for (int i = 0; i < count; i++)
-			if (i != count-1)
-				printf("%05d %d %05d\n", node[i].address, 
-                                         node[i].data,
-                                         node[i+1].address);
-			else
-				printf("%05d %d -1\n", node[i].address, node[i].data);
+		return 0;
+	}
+	sort(nodes, nodes+maxn, cmp);
+	printf("%d %05d\n", cnt, nodes[0].addr);
+	for (int i=0; i<cnt; i++) {
+		printf("%05d %d ", nodes[i].addr, nodes[i].key);
+		if (i < cnt-1)
+			printf("%05d\n", nodes[i+1].addr);
+		else printf("-1");
 	}
 
 	return 0;
 }
-
 
