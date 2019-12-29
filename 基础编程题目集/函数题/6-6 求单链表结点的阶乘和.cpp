d255af7fd9ccc231c842219bb041d083c16bbb50
Vuzzway <13806680065@163.com>
2019-12-29 10:21:06 +0800

    Create 6-6 求单链表结点的阶乘和.cpp

---
+++ 基础编程题目集/函数题/6-6 求单链表结点的阶乘和.cpp
@@ -0,0 +1,16 @@
+int FactorialSum(List L)
+{
+	List p = L;
+	int total = 0, member, i;
+	
+	while (p) {
+		member = 1;
+		for (i = p->Data; i > 0; i--)
+			member *= i;
+		total += member;
+		p = p->Next;
+	}
+	
+	return total;
+}
+
