2f20c761cba9cb009a5ad4c7acee2d832201b444
Vuzzway <13806680065@163.com>
2019-12-29 17:28:03 +0800

    Update 1051 Pop Sequence.cpp

--- PAT_A/1051 Pop Sequence.cpp
+++ PAT_A/1051 Pop Sequence.cpp
@@ -9,29 +9,26 @@ stack<int> st;
 
 int main()
 {
-	//freopen("tst.txt", "r", stdin);
+	// freopen("tst.txt", "r", stdin);
 	int m, n, k;
 	scanf("%d%d%d", &m, &n, &k);
 	for (int i=0; i<k; i++) {
 		while (!st.empty()) st.pop();
 		for (int j=0; j<n; j++) {
-			scanf("%d", out_seq+j);
+			scanf("%d", &out_seq[j]);
 		}
 		int index=0;
 		for (int j=1; j<=n; j++) {
 			st.push(j);
-			if (st.size()>m) {
-				break;
-			}
+			if ((int)st.size() > m) break;
 			while (!st.empty() && st.top()==out_seq[index]) {
-				st.pop();
 				index++;
+				st.pop();
 			}
 		}
 		if (st.empty()) printf("YES\n");
 		else printf("NO\n");
 	}
-
 	return 0;
 }
 
