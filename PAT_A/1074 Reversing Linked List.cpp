0af1a5891b197eba78fc1e4e3b8a55e3e6a194f6
Vuzzway <13806680065@163.com>
2019-12-30 12:03:39 +0800

    Update 1074 Reversing Linked List.cpp

--- PAT_A/1074 Reversing Linked List.cpp
+++ PAT_A/1074 Reversing Linked List.cpp
@@ -1,82 +1,56 @@
-/**
- * Sample input:
- * 00100 6 4
- * 00000 4 99999
- * 00100 1 12309
- * 68237 6 -1
- * 33218 3 00000
- * 99999 5 68237
- * 12309 2 33218
- * -------------
- * Sample output:
- * 00000 4 33218
- * 33218 3 12309
- * 12309 2 00100
- * 00100 1 99999
- * 99999 5 68237
- * 68237 6 -1
- */
+
 #include <cstdio>
 #include <algorithm>
 using namespace std;
 
-#define maxn 100100
-struct Node {
+const int maxn=100010;
+struct node {
 	int addr, data, next;
-	int order;
-} node[maxn];
+	int order, flag;
+} nodes[maxn];
 
-bool cmp(Node a, Node b)
+bool cmp(node a, node b)
 {
-	return a.order < b.order;
-}
-
-void init()
-{
-	for (int i = 0; i < maxn; i++)
-		node[i].order = maxn;
+	if (a.flag!=b.flag) return a.flag>b.flag;
+	else return a.order<b.order;
 }
 
 int main()
 {
 	// freopen("tst.txt", "r", stdin);
-	int begin, n, K, addr;
-
-	init();
-	scanf("%d%d%d", &begin, &n, &K);
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &addr);
-		scanf("%d%d", &node[addr].data, &node[addr].next);
-		node[addr].addr = addr;
+	int fir, n, k;
+	scanf("%d%d%d", &fir, &n, &k);
+	int addr, data, next;
+	for (int i=0; i<n; i++) {
+		scanf("%d%d%d", &addr, &data, &next);
+		nodes[addr].addr=addr;
+		nodes[addr].data=data;
+		nodes[addr].next=next;
 	}
-	int p = begin, count = 0;
-	while (p != -1) {
-		node[p].order = count++;
-		p = node[p].next;
+	int cnt=0;
+	for (int p=fir; p!=-1; p=nodes[p].next) {
+		nodes[p].order=cnt++;
+		nodes[p].flag=1;
 	}
-	sort(node, node + maxn, cmp);
-	n = count;
-	for (int i = 0; i < n/K; i++) {
-		for (int j = (i+1)*K - 1; j > i*K; j--)
-			printf("%05d %d %05d\n", node[j].addr, node[j].data, node[j-1].addr);
-		printf("%05d %d ", node[i*K].addr, node[i*K].data);
-		if (i < n/K-1)
-			printf("%05d\n", node[(i+2)*K-1].addr);
+	sort(nodes, nodes+maxn, cmp);
+	n=cnt;
+	for (int i=0; i<n/k; i++) {
+		for (int j=(i+1)*k-1; j>i*k; j--) {
+			printf("%05d %d %05d\n", nodes[j].addr, nodes[j].data, nodes[j-1].addr);
+		}
+		printf("%05d %d ", nodes[i*k].addr, nodes[i*k].data);
+		if (i < n/k-1) printf("%05d\n", nodes[(i+2)*k-1].addr);
 		else {
-			if (n % K == 0)
-				printf("-1\n");
+			if (n%k==0) printf("-1\n");
 			else {
-				printf("%05d\n", node[(i+1)*K].addr);
-				for (int i = n/K * K; i < n; i++) {
-					printf("%05d %d ", node[i].addr, node[i].data);
-					if (i < n-1) printf("%05d\n", node[i+1].addr);
+				printf("%05d\n", nodes[(i+1)*k].addr);
+				for (int j=n/k*k; j<n; j++) {
+					printf("%05d %d ", nodes[j].addr, nodes[j].data);
+					if (j<n-1) printf("%05d\n", nodes[j+1].addr);
 					else printf("-1\n");
 				}
 			}
 		}
 	}
-
 	return 0;
 }
-
-
