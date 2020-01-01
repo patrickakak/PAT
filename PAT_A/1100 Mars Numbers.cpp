0cff0d883c25bbabf993554524e50dba0c7cc4b0
Vuzzway <13806680065@163.com>
2020-01-01 15:50:27 +0800

    Update 1100 Mars Numbers.cpp

--- PAT_A/1100 Mars Numbers.cpp
+++ PAT_A/1100 Mars Numbers.cpp
@@ -1,64 +1,48 @@
-/**
- * Sample input:
- * 4
- * 29
- * 5
- * elo nov
- * tam
- * ------------
- * Sample output:
- * hel mar
- * may
- * 115
- * 13
- */
-#include <cstdio>
-#include <map> 	// red-black tree
+
 #include <iostream>
+#include <map>
+#include <cstdio>
+#include <cstdlib>
+#include <cctype>
 #include <string>
 using namespace std;
 
-// 0~12 in Mars number system, unit/ten digit
-string unit[13] = {"tret", "jan", "feb", "mar", "apr", "may", 
-                   "jun", "jly", "aug", "sep", "oct", "nov", "dec"};
-string ten[13] = {"tret", "tam", "hel", "maa", "huh", "tou", 
-                  "kes", "hei", "elo", "syy", "lok", "mer", "jou"};
-string num2Str[170];
-map<string, int> str2Num;
+string ge[15] = {"tret","jan","feb","mar","apr","may","jun","jly","aug","sep","oct","nov","dec"};
+string shi[15] = {"tret","tam","hel","maa","huh","tou","kes","hei","elo","syy","lok","mer","jou"};
+string num2str[170];
+map<string, int> mp;
 
-void init()
+string getMarsNum(int n)
 {
-	for (int i = 0; i < 13; i++) {
-		num2Str[i] = unit[i];
-		str2Num[unit[i]] = i;
-		num2Str[i*13] = ten[i];
-		str2Num[ten[i]] = i * 13;
+	string ret;
+	int q = n/13, r = n%13;
+	if (q != 0) ret += shi[q];
+	if (q != 0 && r == 0) ;
+	else if (q == 0) ret += ge[r];
+	else {
+		ret += " ";
+		ret += ge[r];
 	}
-	for (int i = 1; i < 13; i++)
-		for (int j = 1; j < 13; j++) {
-			string str = ten[i] + " " + unit[j];
-			num2Str[i*13 + j] = str;
-			str2Num[str] = i*13 + j;
-		}
+	return ret;
 }
 
 int main()
 {
 	// freopen("tst.txt", "r", stdin);
-	int T;
+	int n;
+	string tmp;
 
-	init(); 				// print table
-	scanf("%d%*c", &T); 	// * to skip '\n'
-	while (T--) {
-		string str;
-		getline(cin, str);
-		if (str[0] >= '0' && str[0] <= '9') {
-			int num = 0;
-			for (int i = 0; i < (int)str.length(); i++)
-				num = num*10 + (str[i]-'0');
-			cout << num2Str[num] << endl;
-		} else
-			cout << str2Num[str] << endl;
+	for (int i = 0; i < 169; i++) {
+		tmp = getMarsNum(i);
+		num2str[i] = tmp;
+		mp.insert(make_pair(tmp, i));
+	}
+	cin >> n;
+	getchar();
+	for (int i = 0; i < n; i++) {
+		getline(cin, tmp);
+		if (isalpha(tmp[0])) cout << mp[tmp] << endl;
+		else cout << num2str[atoi(tmp.c_str())] << endl;
 	}
 
 	return 0;
