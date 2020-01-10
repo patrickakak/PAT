7fdb16b30f573d359cf3ea3687662987caf3fdbb
Vuzzway <13806680065@163.com>
2020-01-10 17:49:30 +0800

    Update 1011 A+B 和 C.cpp

--- PAT_B/1011 A+B 和 C.cpp
+++ PAT_B/1011 A+B 和 C.cpp
@@ -1,34 +1,13 @@
-/**
- * Sample Input:
- * 4
- * 1 2 3
- * 2 3 4
- * 2147483647 0 2147483646
- * 0 -2147483648 -2147483647
- * -------------------------
- * Sample Output:
- * Case #1: false
- * Case #2: true
- * Case #3: true
- * Case #4: false
- */
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main()
 {
-	// freopen("test.txt", "r", stdin);
-	int T, i;
-	long long A, B, C;
-
-	scanf("%d", &T);
-	for (i = 0; i < T; i++) {
-		scanf("%lld%lld%lld", &A, &B, &C);
-		if (A+B > C)
-			printf("Case #%d: true\n", i+1);
-		else
-			printf("Case #%d: false\n", i+1);
+	long long int t, a, b, c;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> a >> b >> c;
+		cout << "Case #" << i+1 << ": " << (a + b > c ? "true" : "false") << endl;
 	}
-
 	return 0;
 }
-
