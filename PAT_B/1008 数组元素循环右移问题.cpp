5565e81bd441c209288112dad48844130e2c7f02
Vuzzway <13806680065@163.com>
2020-01-10 15:48:14 +0800

    Update 1008 数组元素循环右移问题.cpp

--- PAT_B/1008 数组元素循环右移问题.cpp
+++ PAT_B/1008 数组元素循环右移问题.cpp
@@ -1,41 +1,21 @@
-/**
- * Sample Input:
- * 6 2
- * 1 2 3 4 5 6
- * ------------
- * Sample Ouput:
- * 5 6 1 2 3 4
- */
-#include <stdio.h>
+#include <iostream>
 #include <algorithm>
 using namespace std;
 
-#define MaxL 100
-int A[MaxL];
-
-void shiftRight(int A[], int N, int M)
-{
-	reverse(A, A+N-M);
-	reverse(A+N-M, A+N);
-	reverse(A, A+N);
-}
-
 int main()
 {
-	// freopen("tst.txt", "r", stdin);
-	int N, M, i;
-
-	scanf("%d%d", &N, &M);
-	for (i = 0; i < N; i++)
-		scanf("%d", &A[i]);
-
-	shiftRight(A, N, M%N);
-
-	for (i = 0; i < N-1; i++)
-		printf("%d ", A[i]);
-	printf("%d\n", A[i]);
-
+	int n, m, a[101];
+	cin >> n >> m;
+	m %= n;
+	for (int i = 0; i < n; i++) cin >> a[i];
+	if (m != 0) {
+		reverse(begin(a), begin(a) + n - m);
+		reverse(begin(a) + n - m, begin(a) + n);
+		reverse(begin(a), begin(a) + n);
+	}
+	for (int i = 0; i < n; i++) {
+		cout << a[i];
+		if (i != n-1) cout << ' ';
+	}
 	return 0;
 }
-
-
