17f2daf5a5af917274f786fa2411f1e75762e50a
Vuzzway <13806680065@163.com>
2020-01-10 18:41:23 +0800

    Update 1012 数字分类.cpp

--- PAT_B/1012 数字分类.cpp
+++ PAT_B/1012 数字分类.cpp
@@ -1,61 +1,35 @@
-/**
- * Sample Input1:
- * 13 1 2 3 4 5 6 7 8 9 10 20 16 18
- * Sample Output1:
- * 30 11 2 9.7 9
- * --------------------
- * Sample Input2:
- * 8 1 2 4 5 6 7 9 16
- * Sample Output2:
- * N 11 2 N 9
- */
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+using namespace std;
 
 int main()
 {
-	// freopen("test.txt", "r", stdin);
-	int N, i, A1 = 0, A2 = 0, A3 = 0, A4 = 0, A5 = 0;
-	int tmp, flag = 1, flag2 = 0, cnt = 0, arrow;
-
-	scanf("%d", &N);
-	for (i = 0; i < N; i++) {
-		scanf("%d", &tmp);
-		arrow = tmp % 5;
-		switch (arrow) {
-		case 0:
-			if (tmp%2 == 0)
-				A1 += tmp;
-			break;
-		case 1:
-			flag2 = 1; 	// in case n1-n2+...=0
-			if (flag == 1) {
-				A2 += tmp;
-				flag = 0;
-			} else {
-				A2 -= tmp;
-				flag = 1;
-			}
-			break;
-		case 2:
-			A3++;
-			break;
-		case 3:
-			A4 += tmp;
-			cnt++;
-			break;
-		case 4:
-			if (A5 < tmp)
-				A5 = tmp;
-			break;
+	int n, num, A1 = 0, A2 = 0, A4 = 0, A5 = 0;
+	vector<int> v[5];
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		v[num%5].push_back(num);
+	}
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < v[i].size(); j++) {
+			if (i == 0 && v[i][j]%2 == 0) A1 += v[i][j];
+			if (i == 1 && j%2 == 0) A2 += v[i][j];
+			if (i == 1 && j%2 == 1) A2 -= v[i][j];
+			if (i == 3) A4 += v[i][j];
+			if (i == 4 && A5 < v[i][j]) A5 = v[i][j];
 		}
 	}
-
-	if (A1 == 0) printf("N "); else printf("%d ", A1);
-	if (A2 == 0 && flag2 == 0) printf("N "); else printf("%d ", A2);
-	if (A3 == 0) printf("N "); else printf("%d ", A3);
-	if (A4 == 0) printf("N "); else printf("%.1lf ", (double)A4/cnt);
-	if (A5 == 0) printf("N"); else printf("%d", A5);
-
+	for (int i = 0; i < 5; i++) {
+		if (i != 0) cout << ' ';
+		if ((i == 0 && A1 == 0) || (i != 0 && v[i].size() == 0)) {
+			cout << 'N'; continue;
+		}
+		if (i == 0) cout << A1;
+		if (i == 1) cout << A2;
+		if (i == 2) cout << v[2].size();
+		if (i == 3) printf("%.1f", 1.0*A4 / v[3].size());
+		if (i == 4) cout << A5;
+	}
 	return 0;
 }
-
