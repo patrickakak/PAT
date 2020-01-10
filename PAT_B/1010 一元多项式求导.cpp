24da937ded0729036b83d14df51ed459538f40c2
Vuzzway <13806680065@163.com>
2020-01-10 16:22:19 +0800

    Update 1010 一元多项式求导.cpp

--- PAT_B/1010 一元多项式求导.cpp
+++ PAT_B/1010 一元多项式求导.cpp
@@ -1,32 +1,16 @@
-/**
- * Sample Input:
- * 3 4 -5 2 6 1 -2 0
- * Sample Output:
- * 12 3 -10 1 6 0
- */
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main()
 {
-	// freopen("test.txt", "r", stdin);
-	int A, B, flag = 1, count = 0;
-
-	while (scanf("%d%d", &A, &B) != EOF) {
-		count++;
-		if (B == 0) {
-			if (count == 1)
-				printf("0 0");
-			break;
-		} else {
-			if (flag) {
-				printf("%d %d", A*B, B-1);
-				flag = 0;
-			} else
-				printf(" %d %d", A*B, B-1);
+	int c, e, flag = 0;
+	while (cin >> c >> e) {
+		if (e != 0) {
+			if (flag) cout << ' ';
+			cout << c * e << ' ' << e - 1;
+			flag = 1;
 		}
 	}
-
+	if (flag == 0) cout << "0 0";
 	return 0;
 }
-
-
