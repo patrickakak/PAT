2021d88bb1aad602b312bd13560b89c1f58a5463
Vuzzway <13806680065@163.com>
2020-01-10 20:44:54 +0800

    Update 1013 数素数.cpp

--- PAT_B/1013 数素数.cpp
+++ PAT_B/1013 数素数.cpp
@@ -1,45 +1,29 @@
-/**
- * Sample input:
- * 5 27
- * ------------
- * Sample output:
- * 11 13 17 19 23 29 31 37 41 43
- * 47 53 59 61 67 71 73 79 83 89
- * 97 101 103
- */
-#include <cstdio>
+#include <iostream>
+#include <vector>
+using namespace std;
 
-const int maxn = 1000001;
+const int maxn = 110000;
 int prime[maxn], num = 0;
-bool p[maxn] = {0};
-
-void Find_Prime(int n)
+bool isPrime(int n)
 {
-	for (int i = 2; i < maxn; i++)
-		if (p[i] == false) {
-			prime[num++] = i;
-			if (num >= n)
-				break;
-			for (int j = i+i; j < maxn; j += i)
-				p[j] = true;
-		}
+	if (n <= 1) return false;
+	for (int i = 2; i*i <= n; i++)
+		if (n%i == 0) return false;
+	return true;
 }
 
 int main()
 {
-	// freopen("tst.txt", "r", stdin);
-	int m, n, count = 0;
-
-	scanf("%d%d", &m, &n);
-	Find_Prime(n);
-	for (int i = m; i <= n; i++) {
-		printf("%d", prime[i-1]);
-		count++;
-		if (count % 10 != 0 && i < n)
-			printf(" ");
-		else
-			printf("\n");
+	int m, n, cnt = 0;
+	cin >> m >> n;
+	for (int i = 2; i < maxn; i++)
+		if (isPrime(i))
+			prime[num++] = i;
+	for (int i = m-1; i < n; i++) {
+		cnt++;
+		cout << prime[i];
+		if (cnt%10 != 0 && i != n-1) cout << ' ';
+		else cout << endl;
 	}
 	return 0;
 }
-
