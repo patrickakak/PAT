e3817bcd20670e81e5382d799a66157221f9cc04
Vuzzway <13806680065@163.com>
2020-01-11 00:52:23 +0800

    Update 1013 数素数.cpp

--- PAT_B/1013 数素数.cpp
+++ PAT_B/1013 数素数.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-bool isPrime(int n)
-{
+bool isPrime(int n) {
 	for (int i = 2; i*i <= n; i++)
 		if (n % i == 0) return false;
 	return true;
 }
-int main()
-{
+int main() {
 	int m, n, cnt = 0;
 	cin >> m >> n;
 	vector<int> v;
