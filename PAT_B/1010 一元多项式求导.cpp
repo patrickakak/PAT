ab344e8b82f09a2ad987283fbcfd7586d78ac027
Vuzzway <13806680065@163.com>
2020-01-10 16:22:55 +0800

    Update 1010 一元多项式求导.cpp

--- PAT_B/1010 一元多项式求导.cpp
+++ PAT_B/1010 一元多项式求导.cpp
@@ -4,13 +4,12 @@ using namespace std;
 int main()
 {
 	int c, e, flag = 0;
-	while (cin >> c >> e) {
+	while (cin >> c >> e)
 		if (e != 0) {
 			if (flag) cout << ' ';
 			cout << c * e << ' ' << e - 1;
 			flag = 1;
 		}
-	}
 	if (flag == 0) cout << "0 0";
 	return 0;
 }
