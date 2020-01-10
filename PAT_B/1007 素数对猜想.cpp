f8e924a52ad33fbea6652fef0f1783344008fd61
Vuzzway <13806680065@163.com>
2020-01-11 00:43:41 +0800

    Update 1007 素数对猜想.cpp

--- PAT_B/1007 素数对猜想.cpp
+++ PAT_B/1007 素数对猜想.cpp
@@ -4,7 +4,6 @@ const int maxn = 100010;
 int prime[maxn], num = 0;
 bool isPrime(int n)
 {
-	if (n <= 1) return false;
 	for (int i = 2; i*i <= n; i++)
 		if (n%i == 0) return false;
 	return true;
@@ -16,7 +15,7 @@ int main()
 	for (int i = 0; i <= n; i++)
 		if (isPrime(i)) prime[num++] = i;
 	for (int i = 1; i < num; i++)
-		if (prime[i]-prime[i-1] == 2) cnt++;
+		if (prime[i] - prime[i-1] == 2) cnt++;
 	cout << cnt;
 	return 0;
 }
