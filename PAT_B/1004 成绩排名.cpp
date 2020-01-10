a92deb3d158aa115f70ef7b0ebdb97d211317ec6
Vuzzway <13806680065@163.com>
2020-01-10 13:59:39 +0800

    Update 1004 成绩排名.cpp

--- PAT_B/1004 成绩排名.cpp
+++ PAT_B/1004 成绩排名.cpp
@@ -1,49 +1,26 @@
-/**
- * Sample input:
- * 3
- * Joe Math990112 89
- * Mike CS991301 100
- * Mary EE990830 95
- * -----------------
- * Sample output:
- * Mike CS991301
- * Joe Math990112
- */
 #include <cstdio>
-
-#define max_n 1000
-#define MaxL 16
-
-typedef struct {
-	char name[MaxL];
-	char id[MaxL];
-	int score;
-} Stu[max_n];
+#include <iostream>
+using namespace std;
 
 int main()
 {
-	// freopen("test.txt", "r", stdin);
-	Stu stu;
-	int N, i, max_i, min_i, max_score, min_score;
-
-	scanf("%d", &N);
-	for (i = 0; i < N; i++)
-		scanf("%s %s %d", stu[i].name, stu[i].id, &stu[i].score);
-
-	max_score = -1; min_score = 101;
-	for (i = 0; i < N; i++) {
-		if (max_score < stu[i].score) {
-			max_i = i;
-			max_score = stu[i].score;
+	// freopen("tst.txt", "r", stdin);
+	int n, score, max = -1, min = 101;
+	string name, id, maxname, minname, maxid, minid;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> name >> id >> score;
+		if (score > max) {
+			max = score;
+			maxname = name;
+			maxid = id;
 		}
-		if (min_score > stu[i].score) {
-			min_i = i;
-			min_score = stu[i].score;
+		if (score < min) {
+			min = score;
+			minname = name;
+			minid = id;
 		}
 	}
-	printf("%s %s\n", stu[max_i].name, stu[max_i].id);
-	printf("%s %s\n", stu[min_i].name, stu[min_i].id);
-
+	cout << maxname << ' ' << maxid << endl << minname << ' ' << minid;
 	return 0;
 }
-
