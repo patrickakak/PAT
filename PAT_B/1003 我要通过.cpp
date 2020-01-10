3747a5d375997616be90ac9175681596b7a23b20
Vuzzway <13806680065@163.com>
2020-01-10 12:51:22 +0800

    Update 1003 我要通过.cpp

--- PAT_B/1003 我要通过.cpp
+++ PAT_B/1003 我要通过.cpp
@@ -1,70 +1,24 @@
-/**
- * Sample input:
- * 8
- * PAT
- * PAAT
- * AAPATAA
- * AAPAATAAAA
- * xPATx
- * PT
- * Whatever
- * APAAATAA
- * -------------
- * Sample output:
- * YES
- * YES
- * YES
- * YES
- * NO
- * NO
- * NO
- * NO
- */
 #include <cstdio>
-#include <cstring>
-
+#include <iostream>
+#include <unordered_map>
+using namespace std;
 int main()
 {
 	// freopen("tst.txt", "r", stdin);
-	int n;
-	scanf("%d", &n);
-
-	/* --> PAT ---> APATA ----> AAPATAA ----> ...
-	 *      \          \           \
-	 *      PAAT     APAATAA     AAPAATAAAA
-	 *        \          \           \
-	 *       PAAAT    APAAATAAA    AAPAAATAAAAAA
-	 *          \          \           \
-	 *          ...        ...         ...
-	 */
-	while (n--) {
-		char str[110];
-		scanf("%s", str);
-		int len = strlen(str);
-		int num_p = 0, num_t = 0, other = 0;
-		int loc_p = -1, loc_t = -1;
-		for (int i = 0; i < len; i++)
-			if (str[i] == 'P') {
-				num_p++;
-				loc_p = i;
-			} else if (str[i] == 'T') {
-				num_t++;
-				loc_t = i;
-			} else if (str[i] != 'A')
-				other++;
-		if (num_p != 1 || num_t != 1 || other != 0 || loc_t-loc_p <= 1) {
-			printf("NO\n");
-			continue;
+	int n, p, t;
+	cin >> n;
+	string s;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		unordered_map<char, int> um;
+		for (int j = 0; j < s.length(); j++) {
+			um[s[j]]++;
+			if (s[j] == 'P') p = j;
+			if (s[j] == 'T') t = j;
 		}
-		// x: # of A's in the left side of 'P'
-		// y: # of A's between 'P' and 'T'
-		// z: # of A's in the right side of 'T'
-		int x = loc_p, y = loc_t-loc_p-1, z = len-loc_t-1;
-		if (z - x * (y-1) == x) printf("YES\n");
-		else printf("NO\n");
+		if (um['P']==1 && um['T']==1 && um.size()==3 && (p-t)!=1 && p*(t-p-1)==s.length()-1-t)
+			cout << "YES" << endl;
+		else cout << "NO" << endl;
 	}
-
 	return 0;
 }
-
-
