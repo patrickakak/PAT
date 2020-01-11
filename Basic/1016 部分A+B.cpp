1482bb2b7f50a8cc11ad766300b23ef46d75ae41
Vuzzway <13806680065@163.com>
2020-01-11 10:53:36 +0800

    Update 1016 部分A+B.cpp

--- Basic/1016 部分A+B.cpp
+++ Basic/1016 部分A+B.cpp
@@ -1,39 +1,17 @@
-/**
- * Sample Input1:
- * 3862767 6 13530293 3
- * Sample Output1:
- * 399
- * --------------------
- * Sample Input2:
- * 3862767 1 13530293 8
- * Sample Output2:
- * 0
- */
-#include <stdio.h>
-
-int getP(int A, int Da)
-{
-	int ret = 0;
-	do {
-		if (A % 10 == Da) {
-			ret *= 10;
-			ret += Da;
-		}
-		A /= 10;
-	} while (A);
-	return ret;
-}
-
-int main()
-{
-	// freopen("test.txt", "r", stdin);
-	int A, Da, B, Db, Pa, Pb;
-
-	scanf("%d%d%d%d", &A, &Da, &B, &Db);
-	Pa = getP(A, Da);
-	Pb = getP(B, Db);
-	printf("%d\n", Pa+Pb);
-
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	string a, b, ra, rb;
+	char da, db;
+	cin >> a >> da >> b >> db;
+	for (int i = 0; i < a.length(); i++)
+		if (a[i] == da) ra += da;
+	for (int j = 0; j < b.length(); j++)
+		if (b[j] == db) rb += db;
+	int x = 0, y = 0;
+	if (ra != "") x = stoi(ra);
+	if (rb != "") y = stoi(rb);
+	cout << x + y;
 	return 0;
 }
-
