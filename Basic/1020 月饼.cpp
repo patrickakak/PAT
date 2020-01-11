33e7198cec7028aa7cc097f007623173729a25f7
Vuzzway <13806680065@163.com>
2020-01-11 13:48:09 +0800

    Update 1020 月饼.cpp

--- Basic/1020 月饼.cpp
+++ Basic/1020 月饼.cpp
@@ -1,55 +1,28 @@
-/**
- * Sample input:
- * 3 20
- * 18 15 10
- * 75 72 45
- * -----------
- * Sample output:
- * 94.50
- */
-#include <cstdio>
+#include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
-
 struct mooncake {
-	double store;
-	double sell;
-	double price;
-} cake[1010];
-
-bool cmp(mooncake a, mooncake b)
-{
-	return a.price > b.price;
-}
-
-int main()
-{
-	// freopen("tst.txt", "r", stdin);
-	int n;
-	double D;
-
-	scanf("%d%lf", &n, &D);
-	for (int i = 0; i < n; i++)
-		scanf("%lf", &cake[i].store);
-
-	for (int i = 0; i < n; i++) {
-		scanf("%lf", &cake[i].sell);
-		cake[i].price = cake[i].sell / cake[i].store;
-	}
-	sort(cake, cake + n, cmp);
-
-	double ans = 0;
-	for (int i = 0; i < n; i++)
-		if (cake[i].store <= D) {
-			D -= cake[i].store;
-			ans += cake[i].sell;
+	double amt, per, tot;
+};
+bool cmp(mooncake a, mooncake b) { return a.per > b.per; }
+int main() {
+	int n, d;
+	cin >> n >> d;
+	vector<mooncake> a(n);
+	for (int i = 0; i < n; i++) cin >> a[i].amt;
+	for (int i = 0; i < n; i++) cin >> a[i].tot;
+	for (int i = 0; i < n; i++) a[i].per = a[i].tot / a[i].amt;
+	sort(a.begin(), a.end(), cmp);
+	double res = 0;
+	for (int i = 0; i < a.size(); i++)
+		if (a[i].amt <= d) {
+			d -= a[i].amt;
+			res += a[i].tot;
 		} else {
-			ans += cake[i].price * D;
+			res += a[i].per * d;
 			break;
 		}
-	printf("%.2f\n", ans);
-
+	printf("%.2f", res);
 	return 0;
 }
-
-
