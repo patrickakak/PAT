2c3808a6282b525245ebe5e34c26e4062cc9a223
Vuzzway <13806680065@163.com>
2020-01-11 11:30:48 +0800

    Update 1017 A除以B.cpp

--- Basic/1017 A除以B.cpp
+++ Basic/1017 A除以B.cpp
@@ -1,67 +1,19 @@
-/**
- * Sample input:
- * 123456789050987654321 7
- * -----------------------
- * Sample output:
- * 17636684150141093474 3
- */
-#include <cstdio>
-#include <cstring>
-
-#define maxn 1010
-struct bigNum {
-	int d[maxn];
-	int len;
-	bigNum() {
-		memset(d, 0, sizeof(d));
-		len = 0;
+#include <iostream>
+using namespace std;
+int main() {
+	string a, q;
+	int b, qu, r = 0;
+	cin >> a >> b;
+	for (int i = 0; i < a.length(); i++) {
+		r = r * 10 + a[i] - '0';
+		if (r >= b) {
+			qu = r / b;
+			r %= b;
+			q += qu + '0';
+		} else q.push_back('0');
 	}
-};
-
-bigNum change(char str[])
-{
-	bigNum a;
-	a.len = strlen(str);
-	for (int i = 0; i < a.len; i++)
-		a.d[i] = str[a.len-i-1] - '0';
-	return a;
-}
-
-bigNum divide(bigNum a, int b, int& r)
-{
-	bigNum c;
-	c.len = a.len;
-	for (int i = a.len-1; i >= 0; i--) {
-		r = r * 10 + a.d[i];
-		if (r < b) c.d[i] = 0;
-		else {
-			c.d[i] = r / b;
-			r = r % b;
-		}
-	}
-	while (c.len-1 >= 1 && c.d[c.len-1] == 0)
-		c.len--;
-	return c;
-}
-
-void print(bigNum a)
-{
-	for (int i = a.len-1; i >= 0; i--)
-		printf("%d", a.d[i]);
-}
-
-int main()
-{
-	// freopen("tst.txt", "r", stdin);
-	char str[maxn];
-	int b, r = 0;
-
-	scanf("%s%d", str, &b);
-	bigNum a = change(str);
-	print(divide(a, b, r));
-	printf(" %d", r);
-
+	while (q.length() > 0 && q[0] == '0') q.erase(q.begin());
+	if (q.length() != 0) cout<< q << ' ' << r;
+	else cout << "0 " << r;
 	return 0;
 }
-
-
