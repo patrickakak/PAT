f9a6b9d5a41755eb42b4b44632ec243a8bdb2b07
Vuzzway <13806680065@163.com>
2020-01-11 20:20:34 +0800

    Update 1026 程序运行时间.cpp

--- Basic/1026 程序运行时间.cpp
+++ Basic/1026 程序运行时间.cpp
@@ -2,13 +2,12 @@
 #include <cmath>
 using namespace std;
 int main() {
-	int c1, c2;
-	const int clk_tck = 100;
+	int c1, c2, sec, hh, mm, ss, clk_tck = 100;
 	cin >> c1 >> c2;
-	int sec = round(1.0 * (c2 - c1) / clk_tck);
-	int hh = sec / 3600;
+	sec = round(1.0 * (c2 - c1) / clk_tck);
+	hh = sec / 3600;
 	sec %= 3600;
-	int mm = sec / 60, ss = sec % 60;
+	mm = sec / 60, ss = sec % 60;
 	printf("%02d:%02d:%02d", hh, mm, ss);
 	return 0;
 }
