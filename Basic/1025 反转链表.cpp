9b3adb6baa1242647d536be43a315edb5e4833d3
Vuzzway <13806680065@163.com>
2020-01-11 19:59:12 +0800

    Update 1025 反转链表.cpp

--- Basic/1025 反转链表.cpp
+++ Basic/1025 反转链表.cpp
@@ -17,7 +17,7 @@ int main() {
 	for (int i = 0; i < cnt - cnt%k; i += k)
 		reverse(begin(list) + i, begin(list) + i + k);
 	for (int i = 0; i < cnt - 1; i++)
-		printf("%05d %d %05d\n", list[i], data[list[i]], list[i+1]);
+		printf("%05d %d %05d\n", list[i], data[list[i]], list[i + 1]);
 	printf("%05d %d -1", list[cnt - 1], data[list[cnt - 1]]);
 	return 0;
 }
