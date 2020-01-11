b148f4ab931721e30d14ab3b568b4e6cbc8f6849
Vuzzway <13806680065@163.com>
2020-01-11 11:46:44 +0800

    Update 1017 A除以B.cpp

--- Basic/1017 A除以B.cpp
+++ Basic/1017 A除以B.cpp
@@ -6,10 +6,8 @@ int main() {
 	cin >> a >> b;
 	for (int i = 0; i < a.length(); i++) {
 		r = r * 10 + a[i] - '0';
-		if (r >= b) {
-			q.push_back(r/b + '0');
-			r %= b;
-		} else q.push_back('0');
+		q.push_back(r/b + '0');
+		if (r >= b) r %= b;
 	}
 	while (q.length() > 0 && q[0] == '0') q.erase(q.begin());
 	cout << (q.length() != 0 ? q : "0") << ' ' << r;
