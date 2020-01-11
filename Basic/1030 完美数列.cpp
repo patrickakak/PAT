3d14457a904948cf053feb3a37dcf5d897aaf2c3
Vuzzway <13806680065@163.com>
2020-01-11 22:54:04 +0800

    Update 1030 完美数列.cpp

--- Basic/1030 完美数列.cpp
+++ Basic/1030 完美数列.cpp
@@ -5,19 +5,18 @@ using namespace std;
 int main() {
 	int n;
 	long long p;
-	scanf("%d%lld", &n, &p);
+	cin >> n >> p;
 	vector<int> v(n);
 	for (int i = 0; i < n; i++)
 		cin >> v[i];
 	sort(v.begin(), v.end());
-	int result = 0, temp = 0;
+	int res = 0, cnt = 0;
 	for (int i = 0; i < n; i++)
-		for (int j = i + result; j < n; j++)
+		for (int j = i + res; j < n; j++)
 			if (v[j] <= v[i] * p) {
-				temp = j - i + 1;
-				if (temp > result)
-					result = temp;
+				cnt = j - i + 1;
+				if (cnt > res) res = cnt;
 			} else break;
-	cout << result;
+	cout << res;
 	return 0;
 }
