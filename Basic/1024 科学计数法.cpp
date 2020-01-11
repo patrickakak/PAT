23b60a4e1c6072b799878ad4a15af2addb0f19ba
Vuzzway <13806680065@163.com>
2020-01-11 17:20:57 +0800

    Update 1024 科学计数法.cpp

--- Basic/1024 科学计数法.cpp
+++ Basic/1024 科学计数法.cpp
@@ -1,67 +1,28 @@
-/**
- * Sample input1:
- * +1.23400E-03
- * Sample output1:
- * 0.00123400
- * --------------
- * Sample input2:
- * -1.2E+10
- * Sample output2:
- * -12000000000
- */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define Max_L 10005
-
-int main()
-{
-	// freopen("test.txt", "r", stdin);
-	char sign1, sign2, integer, s[Max_L];
-	int exp, len, i = 0, j = 0;
-
-	scanf("%c%c.%s", &sign1, &integer, s);
-	while (s[i]) {
-		if (s[i] == 'E')
-			break;
-		i++;
-	}
-	s[i++] = '\0';
-	sign2 = s[i++];
-
-	exp = atoi(s + i);
-	if (sign1 == '-')
-		printf("-");
-
-	if (sign2 == '-') {
-		if (exp == 0)
-			printf("%c.%s\n", integer, s);
-		else {
-			printf("0.");
-			for (j = 0; j < exp-1; j++)
-				printf("0");
-			printf("%c%s\n", integer, s);
-		}
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	string s;
+	cin >> s;
+	int p = s.find('E');
+	string m = s.substr(1, p-1);
+	int e = stoi(s.substr(p+1));
+	if (s[0] == '-') cout << '-';
+	if (e < 0) {
+		cout << "0.";
+		for (int i = 0; i < abs(e)-1; i++) cout << '0';
+		for (int i = 0; i < m.length(); i++)
+			if (m[i] != '.') cout << m[i];
 	} else {
-		if (exp == 0)
-			printf("%c.%s\n", integer, s);
+		cout << m[0];
+		int j = 2, cnt = 0;
+		for (; j < m.length() && cnt < e; j++, cnt++) cout << m[j];
+		if (j == m.length())
+			for (int k = 0; k < e-cnt; k++) cout << '0';
 		else {
-			len = strlen(s);
-			if (exp >= len) {	// Need to add decimal point
-				printf("%c%s", integer, s);
-				for (j = 0; j < exp-len; j++)
-					printf("0");
-				printf("\n");
-			} else {
-				printf("%c", integer);
-				for (j = 0; j < exp; j++)
-					printf("%c", s[j]);
-				printf(".");
-				printf("%s", s+j);
-			}
+			cout << '.';
+			for (int k = j; k < m.length(); k++) cout << m[k];
 		}
 	}
 	return 0;
 }
-
