321f586664d633e828da2c17ffbb213b57587d9a
Vuzzway <13806680065@163.com>
2020-01-11 16:01:53 +0800

    Update 1021 个位数统计.cpp

--- Basic/1021 个位数统计.cpp
+++ Basic/1021 个位数统计.cpp
@@ -1,30 +1,13 @@
-/**
- * Sample input:
- * 100311
- * Sample output:
- * 0:2
- * 1:3
- * 3:1
- */
-#include <stdio.h>
-
-#define Max_L 1024
-
-int main()
-{
-	// freopen("test.txt", "r", stdin);
-	char num[Max_L];
-	int i = 0, count[10] = {};
-
-	scanf("%s", num);
-	while (num[i]) {
-		count[num[i]-'0']++;
-		i++;
-	}
-	for (i = 0; i < 10; i++)
-		if (count[i] != 0)
-			printf("%d:%d\n", i, count[i]);
-
+#include <iostream>
+#include <map>
+using namespace std;
+int main() {
+	string s;
+	cin >> s;
+	map<char, int> m;
+	for (int i = 0; i < s.length(); i++)
+		m[s[i]]++;
+	for (auto it = m.begin(); it != m.end(); it++)
+		cout << it->first << ':' << it->second << endl;
 	return 0;
 }
-
