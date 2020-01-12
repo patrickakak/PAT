5e052fcd6d598d90bf0d0d2d3397cb5ab6800cba
Vuzzway <13806680065@163.com>
2020-01-12 20:31:41 +0800

    Update 1035 插入与归并.cpp

--- Basic/1035 插入与归并.cpp
+++ Basic/1035 插入与归并.cpp
@@ -1,99 +1,37 @@
-/**
- * Sample input1:
- * 10
- * 3 1 2 8 7 5 9 4 6 0
- * 1 2 3 7 8 5 9 4 6 0
- * Sample output1:
- * Insertion Sort
- * 1 2 3 5 7 8 9 4 6 0
- * --------------------
- * Sample input2:
- * 10
- * 3 1 2 8 7 5 9 4 0 6
- * 1 3 2 8 5 7 4 9 0 6
- * Sample output2:
- * Merge Sort
- * 1 2 3 8 4 5 7 9 0 6
- */
-#include <cstdio>
+#include <iostream>
 #include <algorithm>
 using namespace std;
-
-const int N = 111;
-int origin[N], tmpOri[N], changed[N];
-int n;
-
-bool isSame(int A[], int B[])
-{
+int main() {
+	int n, a[100], b[100], i, j;
+	cin >> n;
 	for (int i = 0; i < n; i++)
-		if (A[i] != B[i])
-			return false;
-	return true;
-}
-
-void showArray(int A[])
-{
-	for (int i = 0; i < n; i++) {
-		printf("%d", A[i]);
-		if (i < n-1) printf(" ");
-	}
-	printf("\n");
-}
-
-bool insertSort()
-{
-	bool flag = false;
-	for (int i = 1; i < n; i++) {
-		if (i != 1 && isSame(tmpOri, changed))
-			flag = true;
-		int tmp = tmpOri[i], j = i;
-		while (j > 0 && tmpOri[j-1] > tmp) {
-			tmpOri[j] = tmpOri[j-1];
-			j--;
-		}
-		tmpOri[j] = tmp;
-		if (flag == true)
-			return true;
-	}
-	return false;
-}
-
-void mergeSort()
-{
-	bool flag = false;
-	for (int step = 2; step/2 <= n; step *= 2) {
-		if (step != 2 && isSame(tmpOri, changed))
-			flag = true;
-		for (int i = 0; i < n; i += step)
-			sort(tmpOri+i, tmpOri + min(i+step, n));
-		if (flag == true) {
-			showArray(tmpOri);
-			return;
-		}
-	}
-}
-
-int main()
-{
-	// freopen("tst.txt", "r", stdin);
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &origin[i]);
-		tmpOri[i] = origin[i];
-	}
+		cin >> a[i];
 	for (int i = 0; i < n; i++)
-		scanf("%d", &changed[i]);
-	if (insertSort()) {
-		printf("Insertion Sort\n");
-		showArray(tmpOri);
+		cin >> b[i];
+	for (i = 0; i < n - 1 && b[i] <= b[i + 1]; i++);
+	for (j = i + 1; a[j] == b[j] && j < n; j++);
+	if (j == n) {
+		cout << "Insertion Sort" << endl;
+		sort(a, a + i + 2);
 	} else {
-		printf("Merge Sort\n");
-		for (int i = 0; i < n; i++)
-			tmpOri[i] = origin[i];
-		mergeSort();
+		cout << "Merge Sort" << endl;
+		int k = 1, flag = 1;
+		while(flag) {
+			flag = 0;
+			for (i = 0; i < n; i++)
+				if (a[i] != b[i]) {
+					flag = 1;
+					break;
+				}
+			k = k * 2;
+			for (i = 0; i < n / k; i++)
+				sort(a + i * k, a + (i + 1) * k);
+			sort(a + n / k * k, a + n);
+		}
 	}
-
+	for (j = 0; j < n; j++) {
+		if (j != 0) printf(" ");
+		printf("%d", a[j]);
+	}
 	return 0;
 }
-
-
