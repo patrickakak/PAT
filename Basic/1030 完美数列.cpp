8513616163c3a64f0b6ec16263ebdb5e364dec70
Vuzzway <13806680065@163.com>
2020-01-12 10:04:29 +0800

    Update 1030 完美数列.cpp

--- Basic/1030 完美数列.cpp
+++ Basic/1030 完美数列.cpp
@@ -9,13 +9,13 @@ int main() {
 	for (int i = 0; i < n; i++)
 		cin >> v[i];
 	sort(v.begin(), v.end());
-	int res = 0, cnt = 0;
+	int len = 0, cnt = 0;
 	for (int i = 0; i < n; i++)
-		for (int j = i + res; j < n; j++)
+		for (int j = i + len; j < n; j++)
 			if (v[j] <= v[i] * p) {
 				cnt = j - i + 1;
-				if (cnt > res) res = cnt;
+				if (cnt > len) len = cnt;
 			} else break;
-	cout << res;
+	cout << len;
 	return 0;
 }
