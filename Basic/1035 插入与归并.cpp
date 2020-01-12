6433beb42c84e190f3969db023110bc5a060b858
Vuzzway <13806680065@163.com>
2020-01-12 21:07:22 +0800

    Update 1035 插入与归并.cpp

--- Basic/1035 插入与归并.cpp
+++ Basic/1035 插入与归并.cpp
@@ -7,7 +7,7 @@ int main() {
 	for (int i = 0; i < n; i++) cin >> a[i];
 	for (int i = 0; i < n; i++) cin >> b[i];
 	for (i = 0; i < n - 1 && b[i] <= b[i + 1]; i++);
-	for (j = i + 1; a[j] == b[j] && j < n; j++);
+	for (j = i + 1; a[j] == b[j] && j < n; j++) ;
 	if (j == n) {
 		cout << "Insertion Sort" << endl;
 		sort(a, a + i + 2);
@@ -21,7 +21,7 @@ int main() {
 					flag = 1;
 					break;
 				}
-			k = k * 2;
+			k *= 2;
 			for (i = 0; i < n / k; i++)
 				sort(a + i * k, a + (i + 1) * k);
 			sort(a + n / k * k, a + n);
