b033a53603901c47ddf7fbdbc56b3d6531859ad3
Vuzzway <13806680065@163.com>
2020-01-12 10:50:11 +0800

    Update 1032 挖掘机技术哪家强.cpp

--- Basic/1032 挖掘机技术哪家强.cpp
+++ Basic/1032 挖掘机技术哪家强.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
 const int maxn = 100010;
 int main() {
-	int n, id, score, tot[maxn] = {0};
+	int n, id, score;
+	vector<int> v(maxn);
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		cin >> id >> score;
-		tot[id] += score;
+		v[id] += score;
 	}
-	int p = max_element(tot, tot + maxn) - tot;
-	cout << p << ' ' << tot[p];
+	int p = max_element(v.begin(), v.end()) - v.begin();
+	cout << p << ' ' << v[p];
 	return 0;
 }
