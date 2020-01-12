5cba58d9d698a823a30ebb3daf433c465148af5a
Vuzzway <13806680065@163.com>
2020-01-12 19:40:41 +0800

    Update 1034 有理数四则运算.cpp

--- Basic/1034 有理数四则运算.cpp
+++ Basic/1034 有理数四则运算.cpp
@@ -4,25 +4,25 @@ long long a, b, c, d;
 long long gcd(long long a, long long b) {
 	return !b ? a : gcd(b, a % b);
 }
-void fun(long long m, long long n) {
-	if (m * n == 0) {
-		cout << (n == 0 ? "Inf" : "0");
+void fun(long long n, long long d) {
+	if (n * d == 0) {
+		cout << (d == 0 ? "Inf" : "0");
 		return ;
 	}
-	bool flag = (m < 0 && n > 0) || (m > 0 && n < 0);
-	m = abs(m); n = abs(n);
-	long long q = m / n;
+	bool flag = (n < 0 && d > 0) || (n > 0 && d < 0);
+	n = abs(n); d = abs(d);
+	long long q = n / d;
 	cout << (flag ? "(-" : "");
 	if (q != 0) cout << q;
-	if (m % n == 0) {
+	if (n % d == 0) {
 		if (flag) cout <<')';
 		return ;
 	}
 	if (q != 0) cout << ' ';
-	m -= q * n;
-	long long t = gcd(m, n);
-	m /= t; n /= t;
-	cout << m << '/' << n << (flag ? ")" : "");
+	n -= q * d;
+	long long t = gcd(n, d);
+	n /= t; d /= t;
+	cout << n << '/' << d << (flag ? ")" : "");
 }
 int main() {
 	scanf("%lld/%lld %lld/%lld", &a, &b, &c, &d);
