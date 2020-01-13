4509ee72f96160c88fc2bbdc17402c7a54810eac
Vuzzway <13806680065@163.com>
2020-01-13 21:23:48 +0800

    Update 1049 数列的片段和.cpp

--- Basic/1049 数列的片段和.cpp
+++ Basic/1049 数列的片段和.cpp
@@ -1,27 +1,13 @@
-/**
- * Sample input:
- * 4
- * 0.1 0.2 0.3 0.4
- * ----------------
- * Sample output:
- * 5.00
- */
-#include <cstdio>
-
-int main()
-{
-	// freopen("tst.txt", "r", stdin);
+#include <iostream>
+using namespace std;
+int main() {
 	int n;
-	double v, ans = 0;
-
-	scanf("%d", &n);
+	cin >> n;
+	double sum = 0, tmp;
 	for (int i = 1; i <= n; i++) {
-		scanf("%lf", &v);
-		ans += v * i * (n + 1 - i);
+		cin >> tmp;
+		sum += tmp * i * (n - i + 1);
 	}
-	printf("%.2f\n", ans);
-
+	printf("%.2f", sum);
 	return 0;
 }
-
-
