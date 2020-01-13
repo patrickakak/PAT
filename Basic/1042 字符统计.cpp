a8cf77a59696faefd5d0ec5af935c855f0500f9a
Vuzzway <13806680065@163.com>
2020-01-13 12:04:28 +0800

    Update 1042 字符统计.cpp

--- Basic/1042 字符统计.cpp
+++ Basic/1042 字符统计.cpp
@@ -1,44 +1,19 @@
-/**
- * Sample input:
- * This is a simple TEST.  There ARE numbers and other symbols 1&2&3...........
- * -----------------
- * Sample output:
- * e 7
- */
-#include <cstdio>
-#include <cstring>
 #include <iostream>
 using namespace std;
-
-#define maxn 1010
-int hashTbl[256] = {0};
-
-int main()
-{
-	// freopen("tst.txt", "r", stdin);
-
-	char str[maxn];
-	cin.get(str, maxn);
-	int len = strlen(str);
-	int maxTime = -1, t;
-	for (int i = 0; i < len; i++) {
-		if (isupper(str[i]))
-			str[i] = tolower(str[i]);
-		if (isalpha(str[i])) {
-			t = ++hashTbl[(int)str[i]];
-			maxTime = max(maxTime, t);
-		}
-	}
-
-	for (int i = 0 ; i < 256; i++) {
-		t = hashTbl[i];
-		if (t == maxTime) {
-			printf("%c %d\n", (char)i, maxTime);
-			break;
-		}
-	}
-
-	return 0;
+int main() {
+    string s;
+    getline(cin, s);
+    int len = s.length(), a[26] = {0};
+    for (int i = 0; i < len; i++)
+        s[i] = tolower(s[i]);
+    for (int i = 0; i < len; i++)
+        if (islower(s[i])) a[s[i] - 'a']++;
+    int max = a[0], t = 0;
+    for (int i = 1; i < 26; i++)
+        if (a[i] > max) {
+            max = a[i];
+            t = i;
+        }
+    printf("%c %d", t + 'a', max);
+    return 0;
 }
-
-
