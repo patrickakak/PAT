45d5f7ba4ec4c2ded4aee9923095a1cdc85215ac
Vuzzway <13806680065@163.com>
2020-01-13 10:10:36 +0800

    Update 1037 在霍格沃茨找零钱.cpp

--- Basic/1037 在霍格沃茨找零钱.cpp
+++ Basic/1037 在霍格沃茨找零钱.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int a, b, c, t, u, v, x, y, z;
-	scanf("%d.%d.%d %d.%d.%d", &a, &b, &c, &t, &u, &v);
-	if (a > t || (a == t && b > u) || (a == t && b == u && c > v)) {
-		swap(a, t); swap(b, u); swap(c, v);
+	int a, b, c, r, s, t, x, y, z;
+	scanf("%d.%d.%d %d.%d.%d", &a, &b, &c, &r, &s, &t);
+	if (a > r || (a == r && b > s) || (a == r && b == s && c > t)) {
+		swap(a, r); swap(b, s); swap(c, t);
 		cout << '-';
 	}
-	z = v < c ? v - c + 29 : v - c;
-	u = v < c ? u - 1 : u;
-	y = u < b ? u - b + 17 : u - b;
-	x = u < b ? t - a - 1 : t - a;
+	z = t < c ? t - c + 29 : t - c;
+	s = t < c ? s - 1 : s;
+	y = s < b ? s - b + 17 : s - b;
+	x = s < b ? r - a - 1 : r - a;
 	cout << x << '.' << y << '.' << z;
 	return 0;
 }
