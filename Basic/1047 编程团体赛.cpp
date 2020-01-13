aa7409409f2baf544ca93c49b5e464a4451e77d6
Vuzzway <13806680065@163.com>
2020-01-13 18:31:28 +0800

    Update 1047 编程团体赛.cpp

--- Basic/1047 编程团体赛.cpp
+++ Basic/1047 编程团体赛.cpp
@@ -1,41 +1,15 @@
-/**
- * Sample input:
- * 6
- * 3-10 99
- * 11-5 87
- * 102-1 0
- * 102-3 100
- * 11-9 89
- * 3-2 61
- * ------------
- * Sample output:
- * 11 176
- */
-#include <cstdio>
-
-#define maxn 10010
-int hashTbl[maxn] = {0};
-
-int main()
-{
-	// freopen("tst.txt", "r", stdin);
-
-	int n;
-	scanf("%d", &n);
-	int tid, mid, score;
-	for (int i = 0; i < n ;i++) {
-		scanf("%d-%d %d", &tid, &mid, &score);
-		hashTbl[tid] += score;
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int team[10010];
+int main() {
+	int n, tid, id, score;
+	cin >> n;
+	for (int i = 0; i<n; i++) {
+		scanf("%d-%d %d", &tid, &id, &score);
+		team[tid] += score;
 	}
-	int k, Max = -1;
-	for (int i = 0; i < maxn; i++)
-		if (hashTbl[i] > Max) {
-			k = i;
-			Max = hashTbl[i];
-		}
-	printf("%d %d\n", k, Max);
-
+	int p = max_element(team, team + 10010) - team;
+	cout << p << ' ' << team[p];
 	return 0;
 }
-
-
