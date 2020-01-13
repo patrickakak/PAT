3a421172130345e762a13b0f2f3b7f7de255f154
Vuzzway <13806680065@163.com>
2020-01-13 18:06:01 +0800

    Update 1045 快速排序.cpp

--- Basic/1045 快速排序.cpp
+++ Basic/1045 快速排序.cpp
@@ -1,46 +1,26 @@
-/**
- * Sample input:
- * 5
- * 1 3 2 4 5
- * -------------
- * Sample output:
- * 3
- * 1 4 5
- */
-#include <cstdio>
+#include <iostream>
 #include <algorithm>
+#include <vector>
+int v[100000];
 using namespace std;
-
-#define maxn 100010
-const int INF = 0x3fffffff;
-int a[maxn], leftMax[maxn], rightMin[maxn];
-int ans[maxn], num = 0;
-
-int main()
-{
-	// freopen("tst.txt", "r", stdin);
-	int n;
-
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
-		scanf("%d", &a[i]);
-	leftMax[0] = 0;
-	for (int i = 1; i < n; i++)
-		leftMax[i] = max(leftMax[i-1], a[i-1]);
-	rightMin[n-1] = INF;
-	for (int i = n-2; i >= 0; i--)
-		rightMin[i] = min(rightMin[i+1], a[i+1]);
-	for (int i = 0; i < n; i++)
-		if (leftMax[i] < a[i] && rightMin[i] > a[i])
-			ans[num++] = a[i];
-	printf("%d\n", num);
-	for (int i = 0; i < num; i++) {
-		printf("%d", ans[i]);
-		if (i < num-1) printf(" ");
+int main() {
+	int n, max = 0, cnt = 0;
+	cin >> n;
+	vector<int> a(n), b(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+		b[i] = a[i];
 	}
-	printf("\n");
-
+	sort(a.begin(), a.end());
+	for (int i = 0; i < n; i++) {
+		if (a[i] == b[i] && b[i] > max) v[cnt++] = b[i];
+		if (b[i] > max) max = b[i];
+	}
+	printf("%d\n", cnt);
+	for(int i = 0; i < cnt; i++) {
+		if (i != 0) cout << ' ';
+		cout << v[i];
+	}
+	cout << endl;
 	return 0;
 }
-
-
