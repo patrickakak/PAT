c5c49da3654df35c17e698130b51dbdd04f03502
Vuzzway <13806680065@163.com>
2020-01-13 10:21:45 +0800

    Update 1038 统计同成绩学生.cpp

--- Basic/1038 统计同成绩学生.cpp
+++ Basic/1038 统计同成绩学生.cpp
@@ -1,38 +1,19 @@
-/**
- * Sample input:
- * 10
- * 60 75 90 55 75 99 82 90 75 50
- * 3 75 90 88
- * ------------------
- * Sample output:
- * 3 2 0
- */
-#include <cstdio>
-
-#define maxn 110
-int hashTbl[maxn] = {0};
-
-int main()
-{
-	// freopen("tst.txt", "r", stdin);
-
-	int n, k, grade;
-
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+	int n, k, t;
 	scanf("%d", &n);
+	vector<int> v(110);
 	for (int i = 0; i < n; i++) {
-		scanf("%d", &grade);
-		hashTbl[grade]++;
+		scanf("%d", &t);
+		v[t]++;
 	}
 	scanf("%d", &k);
 	for (int i = 0; i < k; i++) {
-		scanf("%d", &grade);
-		if (i > 0)
-			printf(" ");
-		printf("%d", hashTbl[grade]);
+		scanf("%d", &t);
+		if (i != 0) printf(" ");
+		printf("%d", v[t]);
 	}
-	printf("\n");
-
 	return 0;
 }
-
-
