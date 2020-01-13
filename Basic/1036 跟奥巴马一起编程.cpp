d65690bcfa43eec07e28556267fc2580359bbaca
Vuzzway <13806680065@163.com>
2020-01-13 09:38:58 +0800

    Update 1036 跟奥巴马一起编程.cpp

--- Basic/1036 跟奥巴马一起编程.cpp
+++ Basic/1036 跟奥巴马一起编程.cpp
@@ -1,38 +1,18 @@
-/**
- * Sample Input:
- * 10 a
- * --------------
- * Sample Output:
- * aaaaaaaaaa
- * a        a
- * a        a
- * a        a
- * aaaaaaaaaa
- */
-#include <stdio.h>
-
-int main()
-{
-	int N, row, i, j;
-	char ch;
-
-	// freopen("test.txt", "r", stdin);
-	scanf("%d %c", &N, &ch);
-
-	row = (N%2 == 1 ? N/2+1 : N/2);
-	for (i = 0; i < row; i++) {
-		for (j = 0; j < N; j++)
-			if (i == 0 || i == row-1)
-				printf("%c", ch);
-			else {
-				if (j == 0 || j == N-1)
-					printf("%c", ch);
-				else
-					printf(" ");
-			}
-		printf("\n");
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	int n;
+	char c;
+	cin >> n >> c;
+	for (int i = 0; i < n; i++) cout << c;
+	cout << endl;
+	int row = round(1.0 * n / 2);
+	for (int i = 0; i < row-2; i++) {
+		cout << c;
+		for (int i = 0; i < n-2; i++) cout << ' ';
+		cout << c << endl;
 	}
+	for (int i = 0; i < n; i++) cout << c;
 	return 0;
 }
-
-
