1a80c377e468b5d84f5be8016757577bf9fd75e2
Vuzzway <13806680065@163.com>
2020-01-14 22:46:02 +0800

    Update 1057 数零壹 (20分).cpp

--- Basic/1057 数零壹 (20分).cpp
+++ Basic/1057 数零壹 (20分).cpp
@@ -11,10 +11,10 @@ int main() {
 		}
 	int cnt0 = 0, cnt1 = 0;
 	while (n != 0) {
-		if(n % 2 == 0) cnt0++;
+		if (n % 2 == 0) cnt0++;
 		else cnt1++;
 		n = n / 2;
 	}
-	printf("%d %d", cnt0, cnt1);
+	cout << cnt0 << ' ' << cnt1;
 	return 0;
 }
