31a45680735a17debf0f5bc50588c2bd2e12517b
Vuzzway <13806680065@163.com>
2020-01-14 17:57:41 +0800

    Update 1053 住房空置率 (20分).cpp

--- Basic/1053 住房空置率 (20分).cpp
+++ Basic/1053 住房空置率 (20分).cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int main() {
 	int n, d, k, maybe = 0, must = 0;
-	double e, temp;
+	double e, tmp;
 	cin >> n >> e >> d;
 	for (int i = 0; i < n; i++) {
 		cin >> k;
 		int sum = 0;
 		for (int j = 0; j < k; j++) {
-			cin >> temp;
-			if (temp < e) sum++;
+			cin >> tmp;
+			if (tmp < e) sum++;
 		}
 		if (sum > k / 2)
 			k > d ? must++ : maybe++;
