40f2c0ac254f1412c714979e723d4359ec2a4703
Vuzzway <13806680065@163.com>
2020-01-14 11:15:37 +0800

    Update 1050 螺旋矩阵 (25分).cpp

--- Basic/1050 螺旋矩阵 (25分).cpp
+++ Basic/1050 螺旋矩阵 (25分).cpp
@@ -5,7 +5,7 @@
 using namespace std;
 int main() {
 	int N, m, n, t = 0;
-	scanf("%d", &N);
+	cin >> N;
 	for (n = sqrt((double)N); n >= 1; n--)
 		if (N % n == 0) {
 			m = N / n;
@@ -13,7 +13,7 @@ int main() {
 		}
 	vector<int> a(N);
 	for (int i = 0; i < N; i++)
-		scanf("%d", &a[i]);
+		cin >> a[i];
 	sort(a.rbegin(), a.rend());
 	vector<vector<int>> b(m, vector<int>(n));
 	int level = m / 2 + m % 2;
@@ -29,10 +29,10 @@ int main() {
 	}
 	for (int i = 0; i < m; i++) {
 		for (int j = 0 ; j < n; j++) {
-			printf("%d", b[i][j]);
-			if (j != n - 1) printf(" ");
+			cout << b[i][j];
+			if (j != n - 1) cout << ' ';
 		}
-		printf("\n");
+		cout << endl;
 	}
 	return 0;
 }
