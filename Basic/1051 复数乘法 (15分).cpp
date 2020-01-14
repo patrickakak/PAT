b1bc712c10ac6ec46083d9a63a15cec5c019c972
Vuzzway <13806680065@163.com>
2020-01-14 12:47:55 +0800

    Update 1051 复数乘法 (15分).cpp

--- Basic/1051 复数乘法 (15分).cpp
+++ Basic/1051 复数乘法 (15分).cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main() {
 	double r1, p1, r2, p2, A, B;
 	cin >> r1 >> p1 >> r2 >> p2;
-	A = r1 * r2 * cos(p1) * cos(p2) - r1 * r2 * sin(p1) * sin(p2);
-	B = r1 * r2 * cos(p1) * sin(p2) + r1 * r2 * sin(p1) * cos(p2);
+	A = r1 * r2 * cos(p1 + p2);
+	B = r1 * r2 * sin(p1 + p2);
 	if (A + 0.005 >= 0 && A < 0)
 		printf("0.00");
 	else
