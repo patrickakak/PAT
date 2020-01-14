806ab16e8337c92773838c17a9fb0db4dd69e3e7
Vuzzway <13806680065@163.com>
2020-01-14 20:37:04 +0800

    Create 1055 集体照 (25分).cpp

---
+++ Basic/1055 集体照 (25分).cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+struct node {
+	string name;
+	int ht;
+};
+int cmp(struct node a, struct node b) {
+	return a.ht != b.ht ? a.ht > b.ht : a.name < b.name;
+}
+int main() {
+	int n, k, m;
+	cin >> n >> k;
+	vector<node> stu(n);
+	for (int i = 0; i < n; i++)
+		cin >> stu[i].name >> stu[i].ht;
+	sort(stu.begin(), stu.end(), cmp);
+	int t = 0, row = k;
+	while (row) {
+		if (row == k)
+			m = n - n / k * (k - 1);
+		else
+			m = n / k;
+		vector<string> ans(m);
+		ans[m / 2] = stu[t].name;
+		int j = m / 2 - 1;
+		for (int i = t + 1; i < t + m; i = i + 2)
+			ans[j--] = stu[i].name;
+		j = m / 2 + 1;
+		for (int i = t + 2; i < t + m; i = i + 2)
+			ans[j++] = stu[i].name;
+		cout << ans[0];
+		for (int i = 1; i < m; i++)
+			cout << " " << ans[i];
+		cout << endl;
+		t = t + m;
+		row--;
+	}
+	return 0;
+}
