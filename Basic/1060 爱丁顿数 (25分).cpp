f2e8f42461017e4faf8c648f5edf79f2c42c6e00
Vuzzway <13806680065@163.com>
2020-01-15 14:08:45 +0800

    Create 1060 爱丁顿数 (25分).cpp

---
+++ Basic/1060 爱丁顿数 (25分).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int a[1000000];
+bool cmp(int a, int b) {
+	return a > b;
+}
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) cin >> a[i];
+	sort(a + 1, a + n + 1, cmp);
+	int e = 0, p = 1;
+	while (e <= n && a[p] > p) {
+		e++;
+		p++;
+	}
+	cout << e;
+	return 0;
+}
