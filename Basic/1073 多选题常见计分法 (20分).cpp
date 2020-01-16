47549c97336f06c505946230bf90f95ba5926155
Vuzzway <13806680065@163.com>
2020-01-16 20:32:58 +0800

    Update 1073 多选题常见计分法 (20分).cpp

--- Basic/1073 多选题常见计分法 (20分).cpp
+++ Basic/1073 多选题常见计分法 (20分).cpp
@@ -2,14 +2,14 @@
 #include <vector>
 using namespace std;
 int main() {
-	int n, m, optnum, truenum, temp, maxcnt = 0;
+	int n, m, optnum, truenum, tmp, maxcnt = 0;
 	int hash[] = {1, 2, 4, 8, 16}, opt[1010][110] = {0};
 	char c;
 	scanf("%d %d", &n, &m);
-	vector<int> fullscore(m), trueopt(m);
+	vector<int> tot(m), trueopt(m);
 	vector<vector<int>> cnt(m, vector<int>(5));
 	for (int i = 0; i < m; i++) {
-		scanf("%d %d %d", &fullscore[i], &optnum, &truenum);
+		scanf("%d %d %d", &tot[i], &optnum, &truenum);
 		for (int j = 0; j < truenum; j++) {
 			scanf(" %c", &c);
 			trueopt[i] += hash[c-'a'];
@@ -19,26 +19,28 @@ int main() {
 		double grade = 0;
 		for (int j = 0; j < m; j++) {
 			getchar();
-			scanf("(%d", &temp);
-			for (int k = 0; k < temp; k++) {
+			scanf("(%d", &tmp);
+			for (int k = 0; k < tmp; k++) {
 				scanf(" %c)", &c);
 				opt[i][j] += hash[c-'a'];
 			}
 			int el = opt[i][j] ^ trueopt[j];
 			if (el) {
 				if ((opt[i][j] | trueopt[j]) == trueopt[j])
-					grade += fullscore[j] * 1.0 / 2;
+					grade += tot[j] * 1.0 / 2;
 				if (el)
 					for (int k = 0; k < 5; k++)
 						if (el & hash[k]) cnt[j][k]++;
-			} else grade += fullscore[j];
+			} else
+				grade += tot[j];
 		}
 		printf("%.1f\n", grade);
 	}
 	for (int i = 0; i < m; i++)
 		for (int j = 0; j < 5; j++)
 			maxcnt = maxcnt > cnt[i][j] ? maxcnt : cnt[i][j];
-	if (maxcnt == 0) printf("Too simple\n");
+	if (maxcnt == 0)
+		printf("Too simple\n");
 	else
 		for (int i = 0; i < m; i++)
 			for (int j = 0; j < cnt[i].size(); j++)
