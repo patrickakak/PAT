bc3f08a24cdedb0c04f415df702bea6011656b42
Vuzzway <13806680065@163.com>
2020-01-17 17:20:01 +0800

    Update 1083 是否存在相等的差 (20分).cpp

--- Basic/1083 是否存在相等的差 (20分).cpp
+++ Basic/1083 是否存在相等的差 (20分).cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int n, t, a[10010] = {0};
+	int n, t, a[10001] = {0};
 	cin >> n;
 	for (int i = 1; i <= n; i++) {
 		cin >> t;
 		a[abs(t-i)]++;
 	}
-	for (int i = 9999; i >= 0; i--)
+	for (int i = 10000; i >= 0; i--)
 		if (a[i] >= 2) cout << i << ' ' << a[i] << endl;
 	return 0;
 }
