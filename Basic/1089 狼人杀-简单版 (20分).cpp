f0bafa3a65fddcf068088923dce6d14f4058b6bd
Vuzzway <13806680065@163.com>
2020-01-18 15:30:04 +0800

    Update 1089 狼人杀-简单版 (20分).cpp

--- Basic/1089 狼人杀-简单版 (20分).cpp
+++ Basic/1089 狼人杀-简单版 (20分).cpp
@@ -8,11 +8,11 @@ int main() {
 	for (int i = 1; i <= n; i++) cin >> v[i];
 	for (int i = 1; i <= n; i++)
 		for (int j = i + 1; j <= n; j++) {
-			vector<int> lie, a(n + 1, 1);
+			vector<int> liar, a(n + 1, 1);
 			a[i] = a[j] = -1;
 			for (int k = 1; k <= n; k++)
-				if (v[k] * a[abs(v[k])] < 0) lie.push_back(k);
-			if (lie.size() == 2 && a[lie[0]] + a[lie[1]] == 0) {
+				if (v[k] * a[abs(v[k])] < 0) liar.push_back(k);
+			if (liar.size() == 2 && a[liar[0]] + a[liar[1]] == 0) {
 				cout << i << ' ' << j;
 				return 0;
 			}
