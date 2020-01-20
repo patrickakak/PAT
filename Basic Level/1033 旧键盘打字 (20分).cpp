e5dbb1a080a51a1c99c2e2e872eac6d61d9257cb
Vuzzway <13806680065@163.com>
2020-01-20 11:38:38 +0800

    Update 1033 旧键盘打字 (20分).cpp

--- Basic Level/1033 旧键盘打字 (20分).cpp
+++ Basic Level/1033 旧键盘打字 (20分).cpp
@@ -4,7 +4,7 @@ int main() {
 	string broken, text;
 	getline(cin, broken);
 	getline(cin, text);
-	for (int i = 0, len = text.length(); i < len; i++) {
+	for (int i = 0; i < text.length(); i++) {
 		if (broken.find(toupper(text[i])) != string::npos) continue;
 		if (isupper(text[i]) && broken.find('+') != string::npos) continue;
 		cout << text[i];
