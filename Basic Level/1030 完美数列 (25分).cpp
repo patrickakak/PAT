1d80b265842b45fbd30b943f5acf93faede1d218
Vuzzway <13806680065@163.com>
2020-01-20 11:27:24 +0800

    Update 1030 完美数列 (25分).cpp

--- Basic Level/1030 完美数列 (25分).cpp
+++ Basic Level/1030 完美数列 (25分).cpp
@@ -9,13 +9,11 @@ int main() {
 	for (int i = 0; i < n; i++)
 		cin >> v[i];
 	sort(v.begin(), v.end());
-	int len = 0, cnt = 0;
+	int len = 0;
 	for (int i = 0; i < n; i++)
 		for (int j = i + len; j < n; j++)
-			if (v[j] <= v[i] * p) {
-				cnt = j - i + 1;
-				if (cnt > len) len = cnt;
-			} else break;
+			if (v[j] <= v[i] * p) len++;
+			else break;
 	cout << len;
 	return 0;
 }
