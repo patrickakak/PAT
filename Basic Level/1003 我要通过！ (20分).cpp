0e834fea4768dd225b2a764900183a65d2ed3565
Vuzzway <13806680065@163.com>
2020-01-20 09:28:44 +0800

    Update 1003 我要通过！ (20分).cpp

--- Basic Level/1003 我要通过！ (20分).cpp
+++ Basic Level/1003 我要通过！ (20分).cpp
@@ -5,13 +5,13 @@ int main() {
 	int n, p, t;
 	cin >> n;
 	string s;
-	for (int i = 0; i < n; i++) {
+	while (n--) {
 		cin >> s;
 		unordered_map<char, int> m;
-		for (int j = 0; j < s.length(); j++) {
-			m[s[j]]++;
-			if (s[j] == 'P') p = j;
-			if (s[j] == 'T') t = j;
+		for (int i = 0; i < s.length(); i++) {
+			m[s[i]]++;
+			if (s[i] == 'P') p = i;
+			if (s[i] == 'T') t = i;
 		}
 		if (m['P'] == 1 && m['T'] == 1 && m.size() == 3 && t-p != 1 && p*(t-p-1) == s.length()-1-t)
 			cout << "YES" << endl;
