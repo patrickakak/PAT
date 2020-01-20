e48294479cf7719577eb2a3e0fb431daeac85c0f
Vuzzway <13806680065@163.com>
2020-01-20 17:02:53 +0800

    Update 1045 快速排序 (25分).cpp

--- Basic Level/1045 快速排序 (25分).cpp
+++ Basic Level/1045 快速排序 (25分).cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-int v[100000];
 using namespace std;
 int main() {
-	int n, max = 0, cnt = 0;
+	int n, max = 0;
 	cin >> n;
-	vector<int> a(n), b(n);
+	vector<int> a(n), b(n), v;
 	for (int i = 0; i < n; i++) {
 		cin >> a[i];
 		b[i] = a[i];
 	}
 	sort(a.begin(), a.end());
 	for (int i = 0; i < n; i++) {
-		if (a[i] == b[i] && b[i] > max) v[cnt++] = b[i];
+		if (a[i] == b[i] && b[i] > max) v.push_back(b[i]);
 		if (b[i] > max) max = b[i];
 	}
-	printf("%d\n", cnt);
-	for (int i = 0; i < cnt; i++) {
+	printf("%d\n", v.size());
+	for (int i = 0; i < v.size(); i++) {
 		if (i != 0) cout << ' ';
 		cout << v[i];
 	}
