5073b0c1347efc9020ca1153b9c1591e482bf859
Vuzzway <13806680065@163.com>
2020-01-21 14:13:03 +0800

    Update 1061 Dating.cpp

--- Advanced Level/1061 Dating.cpp
+++ Advanced Level/1061 Dating.cpp
@@ -1,60 +1,31 @@
-/**
- * Sample input:
- * 3485djDkxh4hhGE 
- * 2984akDfkkkkggEdsb 
- * s&hgsfdk 
- * d&Hyscvnm
- * -------------
- * Sample output:
- * THU 14:04
- */
-#include <stdio.h>
-#include <ctype.h>
-
-#define S_LEN 80
-// Letter to Day
-// A B C D E F G
-// 0 1 2 3 4 5 6
-char L2D[7][4] = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
-
-
-int main()
-{
-	// freopen("test.txt", "r", stdin);
-	char s1[S_LEN], s2[S_LEN], s3[S_LEN], s4[S_LEN];
-	int day, hr, min, i, j;
-
-	scanf("%s %s", s1, s2);
-	scanf("%s %s", s3, s4);
-	i = j = 0;
-	while (s1[i] && s2[j]) {
-		if (s1[i] >= 'A' && s1[i] <= 'G' && s1[i] == s2[j]) {
-			day = s1[i] - 'A';
+#include <iostream>
+using namespace std;
+string days[] = {"MON","TUE","WED","THU","FRI","SAT","SUN"};
+int main() {
+	string a, b, c, d;
+	cin >> a >> b >> c >> d;
+	int i = 0, j = 0;
+	while (i < a.length() && i < b.length()) {
+		if (a[i] == b[i] && a[i]>='A' && a[i]<='G') {
+			cout << days[a[i]-'A'] << ' ';
 			break;
 		}
-		i++; j++;
+		i++;
 	}
-	i++; j++;
-	while (s1[i] && s2[j]) {
-		if (s1[i] >= 'A' && s1[i] <= 'N' && s1[i] == s2[j]) {
-			hr = s1[i] - 'A' + 10;
-			break;
-		} else if (s1[i] >= '0' && s1[i] <= '9' && s1[i] == s2[j]) {
-			hr = s1[i] - '0';
+	i += 1;
+	while (i < a.length() && i < b.length()) {
+		if (a[i] == b[i] && (isdigit(a[i]) || (a[i] >= 'A' && a[i] <= 'N'))) {
+			printf("%02d:", isdigit(a[i]) ? a[i] - '0' : a[i] - 'A' + 10);
 			break;
 		}
-		i++; j++;
+		i++;
 	}
-	i = j = 0;
-	while (s3[i] && s4[j]) {
-		if (isalpha(s3[i]) && s3[i] == s4[j]) {
-			min = i;
+	while (j < c.length() && j < d.length()) {
+		if (c[j] == d[j] && isalpha(c[j])) {
+			printf("%02d", j);
 			break;
 		}
-		i++; j++;
+		j++;
 	}
-	printf("%s %02d:%02d\n", L2D[day], hr, min);
-
 	return 0;
 }
-
