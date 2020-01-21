5707a44a12cb8c9a3122a890b31f084b030b854c
Vuzzway <13806680065@163.com>
2020-01-21 20:18:39 +0800

    Update 1061 Dating (20).cpp

--- Advanced Level/1061 Dating (20).cpp
+++ Advanced Level/1061 Dating (20).cpp
@@ -5,27 +5,21 @@ int main() {
 	string a, b, c, d;
 	cin >> a >> b >> c >> d;
 	int i = 0, j = 0;
-	while (i < a.length() && i < b.length()) {
+	for ( ; i < min(a.length(), b.length()); i++)
 		if (a[i] == b[i] && a[i]>='A' && a[i]<='G') {
 			cout << days[a[i]-'A'] << ' ';
 			break;
 		}
-		i++;
-	}
 	i += 1;
-	while (i < a.length() && i < b.length()) {
+	for ( ; i < min(a.length(), b.length()); i++)
 		if (a[i] == b[i] && (isdigit(a[i]) || (a[i] >= 'A' && a[i] <= 'N'))) {
 			printf("%02d:", isdigit(a[i]) ? a[i] - '0' : a[i] - 'A' + 10);
 			break;
 		}
-		i++;
-	}
-	while (j < c.length() && j < d.length()) {
+	for ( ; j < min(c.length(), d.length()); j++)
 		if (c[j] == d[j] && isalpha(c[j])) {
 			printf("%02d", j);
 			break;
 		}
-		j++;
-	}
 	return 0;
 }
