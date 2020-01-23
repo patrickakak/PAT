acfc8ad44a7451170f6c1e7b91329541e2e40af0
Vuzzway <13806680065@163.com>
2020-01-23 21:05:19 +0800

    Update 1065 A+B and C (64bit) (20).cpp

--- Advanced Level/1065 A+B and C (64bit) (20).cpp
+++ Advanced Level/1065 A+B and C (64bit) (20).cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main() {
 	int n;
 	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
+	for (int i = 1; i <= n; i++) {
 		long long a, b, c;
 		scanf("%lld %lld %lld", &a, &b, &c);
 		long long sum = a + b;
-		printf("Case #%d: ", i + 1);
+		printf("Case #%d: ", i);
 		if (a > 0 && b > 0 && sum < 0) printf("true\n");
 		else if (a < 0 && b < 0 && sum >= 0) printf("false\n");
 		else if (sum > c) printf("true\n");
