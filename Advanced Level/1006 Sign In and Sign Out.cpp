1d407b7dff859dbac629a91f5ef5acedf939c04e
Vuzzway <13806680065@163.com>
2020-01-24 10:23:25 +0800

    Update 1006 Sign In and Sign Out.cpp

--- Advanced Level/1006 Sign In and Sign Out.cpp
+++ Advanced Level/1006 Sign In and Sign Out.cpp
@@ -1,71 +1,21 @@
-/**
- * Sample input:
- * 3
- * CS301111 15:30:28 17:00:10
- * SC3021234 08:00:00 11:25:25
- * CS301133 21:45:00 21:58:40
- * --------------------------
- * Sample output:
- * SC3021234 CS301133
- */
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-
-#define ID_L 16
-
-typedef struct {
-	char id[ID_L];
-	int hh, mm, ss;
-} time;
-
-time t1, t2, earliest, latest;
-
-void init()
-{
-	earliest.hh = 24;
-	earliest.mm = earliest.ss = 60;
-	latest.hh = latest.mm = latest.ss = 0;
-}
-
-bool less(time t1, time t2)
-{
-	if (t1.hh != t2.hh)
-		return t1.hh < t2.hh;
-	else if (t1.mm != t2.mm)
-		return t1.mm < t2.mm;
-	else
-		return t1.ss < t2.ss;
-}
-
-bool more(time t1, time t2)
-{
-	if (t1.hh != t2.hh)
-		return t1.hh > t2.hh;
-	else if (t1.mm != t2.mm)
-		return t1.mm > t2.mm;
-	else
-		return t1.ss > t2.ss;
-}
-
-int main()
-{
-	// freopen("test.txt", "r", stdin);
-	int M, i;
-
-	init();
-	scanf("%d", &M);
-	for (i = 0; i < M; i++) {
-		scanf("%s %d:%d:%d %d:%d:%d",
-				t1.id, &t1.hh, &t1.mm, &t1.ss, &t2.hh, &t2.mm, &t2.ss);
-		strcpy(t2.id, t1.id);
-		if (more(earliest, t1))
-			earliest = t1;
-		if (less(latest, t2))
-			latest = t2;
+#include <iostream>
+using namespace std;
+int main() {
+	int m;
+	cin >> m;
+	string id, in, out, unlock, lock;
+	string early = "24:00:00", late = "00:00:00";
+	for (int i = 0; i < m; i++) {
+		cin >> id >> in >> out;
+		if (early > in) {
+			early = in;
+			unlock = id;
+		}
+		if (late < out) {
+			late = out;
+			lock = id;
+		}
 	}
-	printf("%s %s\n", earliest.id, latest.id);
-
+	cout << unlock << ' ' << lock;
 	return 0;
 }
-
