778f0b57a81822066fade06959ee3b0cc0785ebf
Vuzzway <13806680065@163.com>
2020-01-24 10:42:01 +0800

    Update 1011 World Cup Betting.cpp

--- Advanced Level/1011 World Cup Betting.cpp
+++ Advanced Level/1011 World Cup Betting.cpp
@@ -1,51 +1,16 @@
-/**
- * Sample input:
- * 1.1 2.5 1.7
- * 1.2 3.1 1.6
- * 4.1 1.2 1.1
- * -------------
- * Sample output:
- * T T W 39.31
- */
-#include <stdio.h>
-
-int max(double W, double T, double L)
-{
-	if (W > T)
-		return W > L ? 0 : 2;
-	else
-		return T > L ? 1 : 2;
-}
-
-int main()
-{
-	// freopen("test.txt", "r", stdin);
-	double W, T, L, r[3] = {};
-	char chs[3] = {};
-	int i, ret;
-
-	for (i = 0; i < 3; i++) {
-		scanf("%lf %lf %lf", &W, &T, &L);
-		ret = max(W, T, L);
-		switch (ret) {
-		case 0:
-			r[i] = W;
-			chs[i] = 'W';
-			break;
-		case 1:
-			r[i] = T;
-			chs[i] = 'T';
-			break;
-		case 2:
-			r[i] = L;
-			chs[i] = 'L';
-			break;
-		}
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+	string s = "WTL";
+	double profit = 1, a[3] = {0};
+	for (int i = 0; i < 3; i++) {
+		cin >> a[0] >> a[1] >> a[2];
+		int p = max_element(a, a + 3) - a;
+		if (i != 0) cout << ' ';
+		cout << s[p];
+		profit *= a[p];
 	}
-	for (i = 0; i < 3; i++)
-		printf("%c ", chs[i]);
-	printf("%.2f\n", (r[0]*r[1]*r[2]*0.65-1)*2);
-
+	printf(" %.2f", 2 * (profit * .65 - 1));
 	return 0;
 }
-
