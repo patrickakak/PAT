969aeaf8c0f2c6753cebf390555dbdc3c807d885
Vuzzway <13806680065@163.com>
2020-01-24 15:22:16 +0800

    Update 1134 Vertex Cover (25).cpp

--- Advanced Level/1134 Vertex Cover (25).cpp
+++ Advanced Level/1134 Vertex Cover (25).cpp
@@ -2,31 +2,27 @@
 #include <vector>
 using namespace std;
 int main() {
-	int n, m, k, nv, a, b, num;
+	int n, m, k, nv, a, b, t, j;
 	scanf("%d%d", &n, &m);
 	vector<int> v[n];
-	for (int i = 0;i < m; i++) {
+	for (int i = 0; i < m; i++) {
 		scanf("%d%d", &a, &b);
 		v[a].push_back(i);
 		v[b].push_back(i);
 	}
 	scanf("%d", &k);
 	for (int i = 0; i < k; i++) {
+		vector<int> h(m);
 		scanf("%d", &nv);
-		int flag = 0;
-		vector<int> hash(m, 0);
 		for (int j = 0; j < nv; j++) {
-			scanf("%d", &num);
-			for (int t = 0; t < v[num].size(); t++)
-				hash[v[num][t]] = 1;
+			scanf("%d", &t);
+			for (int l = 0; l < v[t].size(); l++)
+				h[v[t][l]] = 1;
 		}
-		for (int j = 0; j < m; j++)
-			if (hash[j] == 0) {
-				printf("No\n");
-				flag = 1;
-				break;
-			}
-		if (flag == 0) printf("Yes\n");
+		for (j = 0; j < m; j++)
+			if (h[j] == 0) break;
+		if (j < m) printf("No\n");
+		else printf("Yes\n");
 	}
 	return 0;
 }
