9018e65a650d64dd5701ed7de7b23f301dc5a289
Vuzzway <13806680065@163.com>
2020-01-24 15:52:12 +0800

    Update 1033 To Fill or Not to Fill.cpp

--- Advanced Level/1033 To Fill or Not to Fill.cpp
+++ Advanced Level/1033 To Fill or Not to Fill.cpp
@@ -1,88 +1,61 @@
-/**
- * Sample input1:
- * 50 1300 12 8
- * 6.00 1250
- * 7.00 600
- * 7.00 150
- * 7.10 0
- * 7.20 200
- * 7.50 400
- * 7.30 1000
- * 6.85 300
- * Sample output1:
- * 749.17
- * ---------------
- * Sample input2:
- * 50 1300 12 2
- * 7.10 0
- * 7.00 600
- * Sample output2:
- * The maximum travel distance = 1200.00
- */
-#include <cstdio>
+#include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
-
-const int INF = (1 << 30);
-#define maxn 510
-struct Station {
-	double price;	// unit liter price
-	double dis;
-} st[maxn];
-
-bool cmp(Station a, Station b)
-{
+const int inf = 99999999;
+struct station {
+	double price, dis;
+};
+bool cmp1(station a, station b) {
 	return a.dis < b.dis;
 }
-
-int main()
-{
-	// freopen("tst.txt", "r", stdin);
+int main() {
+	double cmax, d, davg;
 	int n;
-	double Cmax, D, Davg;
-
-	scanf("%lf%lf%lf%d", &Cmax, &D, &Davg, &n);
-	for (int i = 0; i < n; i++)
-		scanf("%lf%lf", &st[i].price, &st[i].dis);
-	st[n].price = 0;
-	st[n].dis = D;
-	sort(st, st + n, cmp);
-	if (st[0].dis != 0)
-		printf("The maximum travel distance = 0.00\n");
-	else {
-		int now = 0;	// current station num
-		double ans = 0, nowTank = 0, MAX = Cmax * Davg;
-		while (now < n) {
-			int k = -1;
-			double priceMin = INF;
-			for (int i = now+1; i<=n && st[i].dis-st[now].dis<=MAX; i++)
-				if (st[i].price < priceMin) {
-					priceMin = st[i].price;
-					k = i;
-					if (priceMin < st[now].price)
-						break;
-				}
-			if (k == -1) break;
-			double need = (st[k].dis - st[now].dis) / Davg;
-			if (priceMin < st[now].price) {
-				if (nowTank < need) {
-					ans += (need - nowTank) * st[now].price;
-					nowTank = 0;
-				} else
-					nowTank -= need;
-			} else {
-				ans += (Cmax - nowTank) * st[now].price;
-				nowTank = Cmax - need;
-			}
-			now = k;
-		}
-		if (now == n)
-			printf("%.2f\n", ans);
-		else
-			printf("The maximum travel distance = %.2f\n", st[now].dis + MAX);
+	scanf("%lf%lf%lf%d", &cmax, &d, &davg, &n);
+	vector<station> sta(n + 1);
+	sta[0] = {0.0, d};
+	for(int i = 1; i <= n; i++)
+		scanf("%lf%lf", &sta[i].price, &sta[i].dis);
+	sort(sta.begin(), sta.end(), cmp1);
+	double nowdis = 0.0, maxdis = 0.0, nowprice = 0.0, totalPrice = 0.0, leftdis = 0.0;
+	if(sta[0].dis != 0) {
+		printf("The maximum travel distance = 0.00");
+		return 0;
+	} else {
+		nowprice = sta[0].price;
 	}
-
+	while(nowdis < d) {
+		maxdis = nowdis + cmax * davg;
+		double minPriceDis = 0, minPrice = inf;
+		int flag = 0;
+		for(int i = 1; i <= n && sta[i].dis <= maxdis; i++) {
+			if(sta[i].dis <= nowdis) continue;
+			if(sta[i].price < nowprice) {
+				totalPrice += (sta[i].dis - nowdis - leftdis) * nowprice / davg;
+				leftdis = 0.0;
+				nowprice = sta[i].price;
+				nowdis = sta[i].dis;
+				flag = 1;
+				break;
+			}
+			if(sta[i].price < minPrice) {
+				minPrice = sta[i].price;
+				minPriceDis = sta[i].dis;
+			}
+		}
+		if(flag == 0 && minPrice != inf) {
+			totalPrice += (nowprice * (cmax - leftdis / davg));
+			leftdis = cmax * davg - (minPriceDis - nowdis);
+			nowprice = minPrice;
+			nowdis = minPriceDis;     
+		}
+		if(flag == 0 && minPrice == inf) {
+			nowdis += cmax * davg;
+			printf("The maximum travel distance = %.2f", nowdis);
+			return 0;
+		}
+	}
+	printf("%.2f", totalPrice);
 	return 0;
 }
-
-
