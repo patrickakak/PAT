251e2dcbb6327a4cb5eb8cf3a2c1443fbbbfb220
Vuzzway <13806680065@163.com>
2020-01-25 09:29:18 +0800

    Update 1012 The Best Rank.cpp

--- Advanced Level/1012 The Best Rank.cpp
+++ Advanced Level/1012 The Best Rank.cpp
@@ -1,78 +1,44 @@
-/**
- * Sample input:
- * 5 6
- * 310101 98 85 88
- * 310102 70 95 88
- * 310103 82 87 94
- * 310104 91 91 91
- * 310105 85 90 90
- * 310101
- * 310102
- * 310103
- * 310104
- * 310105
- * 999999
- * --------------
- * Sample output:
- * 1 C
- * 1 M
- * 1 E
- * 1 A
- * 3 A
- * N/A
- */
-#include <cstdio>
+#include <iostream>
 #include <algorithm>
+#include <vector>
+#include <unordered_set>
 using namespace std;
-
-struct Student {
-	int id;
-	int grade[4];
-} stu[2010];
-
-char course[4] = {'A', 'C', 'M', 'E'};
-int Rank[1000000][4];
-int now;
-
-bool cmp(Student a, Student b)
-{
-	return a.grade[now] > b.grade[now];
-}
-
-int main()
-{
-	// freopen("tst.txt", "r", stdin);
-	int N, M;
-
-	scanf("%d %d", &N, &M);
-	for (int i = 0; i < N; i++) {
-		scanf("%d%d%d%d", &stu[i].id, 
-				&stu[i].grade[1], &stu[i].grade[2], &stu[i].grade[3]);
-		stu[i].grade[0] = (stu[i].grade[1]+stu[i].grade[2]+stu[i].grade[3])/3;
+struct node {
+	int id, r[4];
+	double g[4];
+};
+int flag;
+bool cmp(node a, node b) { return a.g[flag] > b.g[flag]; }
+int main() {
+	int n, m, id;
+	cin >> n >> m;
+	vector<node> v(n);
+	unordered_set<int> s;
+	for (int i = 0; i < n; i++) {
+		cin >> v[i].id >> v[i].g[1] >> v[i].g[2] >> v[i].g[3];
+		v[i].g[0] = (v[i].g[1] + v[i].g[2] + v[i].g[3]) / 3;
+		s.insert(v[i].id);
 	}
-	for (now = 0; now < 4; now++) {
-		sort(stu, stu+N, cmp);
-		Rank[stu[0].id][now] = 1;
-		for (int i = 1; i < N; i++)
-			if (stu[i].grade[now] == stu[i-1].grade[now])
-				Rank[stu[i].id][now] = Rank[stu[i-1].id][now];
-			else
-				Rank[stu[i].id][now] = i+1;
-	}
-	int query;
-	for (int i = 0; i < M; i++) {
-		scanf("%d", &query);
-		if (Rank[query][0] == 0)
-			printf("N/A\n");
-		else {
-			int k = 0;
-			for (int j = 1; j < 4; j++)
-				if (Rank[query][j] < Rank[query][k])
-					k = j;
-			printf("%d %c\n", Rank[query][k], course[k]);
+	string str = "ACME";
+	for (int i = 0; i < m; i++) {
+		cin >> id;
+		if (s.find(id) == s.end()) {
+			printf("N/A\n"); continue;
 		}
+		for (flag = 0; flag < 4; flag++) {
+			sort(v.begin(), v.end(), cmp);
+			v[0].r[flag] = 1;
+			for (int j = 1; j < n; j++) {
+				v[j].r[flag] = j + 1;
+				if (v[j].g[flag] == v[j-1].g[flag]) v[j].r[flag] = v[j-1].r[flag];
+			}
+		}
+		for (int j = 0; j < n; j++)
+			if (v[j].id == id) {
+				int p = min_element(v[j].r, v[j].r + 4) - v[j].r;
+				printf("%d %c\n", v[j].r[p], str[p]);
+				break;
+			}
 	}
-
 	return 0;
 }
-
