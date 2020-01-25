85d8e491d7abb76e858ae00086baefa73db654b6
Vuzzway <13806680065@163.com>
2020-01-25 10:26:27 +0800

    Update 1025 PAT Ranking.cpp

--- Advanced Level/1025 PAT Ranking.cpp
+++ Advanced Level/1025 PAT Ranking.cpp
@@ -1,79 +1,38 @@
-/**
- * Sample input:
- * 2
- * 5
- * 1234567890001 95
- * 1234567890005 100
- * 1234567890003 95
- * 1234567890002 77
- * 1234567890004 85
- * 4
- * 1234567890013 65
- * 1234567890011 25
- * 1234567890014 100
- * 1234567890012 85
- * ----------------
- * Sample output:
- * 9
- * 1234567890005 1 1 1
- * 1234567890014 1 2 1
- * 1234567890001 3 1 2
- * 1234567890003 3 1 2
- * 1234567890004 5 1 4
- * 1234567890012 5 2 2
- * 1234567890002 7 1 5
- * 1234567890013 8 2 3
- * 1234567890011 9 2 4
- */
-#include <cstdio>
-#include <cstring>
+#include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
-
-struct Stu {
-	char id[20];
-	int grade;
-	int loc;
-	int r;
-} stu[30010];
-
-bool cmp(Stu a, Stu b)
-{
-	if (a.grade != b.grade) return a.grade > b.grade;
-	else return strcmp(a.id, b.id) < 0;
-}
-
-int main()
-{
-	// freopen("tst.txt", "r", stdin);
-	int N, K, num = 0;
-
-	scanf("%d", &N);
-	for (int i = 0; i < N; i++){
-		scanf("%d", &K);
-		for (int j = 0 + num; j < K + num; j++) {
-			scanf("%s%d", stu[j].id, &stu[j].grade);
-			stu[j].loc = i + 1;
+struct node {
+	string id;
+	int loc, g, r;
+};
+bool cmp(node a, node b) { return a.g != b.g ? a.g > b.g : a.id < b.id; }
+int main() {
+	int n, k, g, sum = 0;
+	string id;
+	cin >> n;
+	vector<node> v;
+	for (int i = 0; i < n; i++) {
+		cin >> k;
+		for (int j = 0; j < k; j++) {
+			cin >> id >> g;
+			v.push_back(node{id, i+1, g});
 		}
-		sort(stu+num, stu+num+K, cmp);
-		stu[num].r = 1;
-		for (int j = 1 + num; j < K + num; j++)
-			if (stu[j].grade == stu[j-1].grade)
-				stu[j].r = stu[j-1].r;
-			else
-				stu[j].r = j + 1 - num;
-		num += K;
+		sort(v.begin() + sum, v.begin() + sum + k, cmp);
+		v[sum].r = 1;
+		for (int j = sum + 1; j < sum + k; j++) {
+			v[j].r = j - sum + 1;
+			if (v[j].g == v[j-1].g) v[j].r = v[j-1].r;
+		}
+		sum += k;
 	}
-	sort(stu, stu+num, cmp);
-	printf("%d\n", num);
+	printf("%d\n", sum);
+	sort(v.begin(), v.end(), cmp);
 	int r = 1;
-	for (int i = 0; i < num; i++) {
-		if (i > 0 && stu[i].grade != stu[i-1].grade)
-			r = i + 1;
-		printf("%s %d %d %d\n", stu[i].id, r, stu[i].loc, stu[i].r);
+	printf("%s %d %d %d\n", v[0].id.c_str(), r, v[0].loc, v[0].r);
+	for (int i = 1; i < sum; i++) {
+		if (v[i].g != v[i-1].g) r = i + 1;
+		printf("%s %d %d %d\n", v[i].id.c_str(), r, v[i].loc, v[i].r);
 	}
-
 	return 0;
 }
-
-
