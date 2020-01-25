f92c424129868548e298b927d74d06f5dbae0946
Vuzzway <13806680065@163.com>
2020-01-25 15:25:57 +0800

    Update 1075 PAT Judge (25).cpp

--- Advanced Level/1075 PAT Judge (25).cpp
+++ Advanced Level/1075 PAT Judge (25).cpp
@@ -1,57 +1,49 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
-#define maxn 10010
-struct User {
-	int id, total, solve, score[10];
-	bool pass;
-} user[maxn];
-bool cmp(User a, User b) {
-	if (a.pass != b.pass) return a.pass;
-	else if (a.total != b.total) return a.total > b.total;
-	else if (a.solve != b.solve) return a.solve > b.solve;
-	else return a.id < b.id;
+struct node {
+	int uid, perfect, r, tot, print;
+	vector<int> p;
+};
+bool cmp(node a, node b) {
+	if (a.tot!=b.tot) return a.tot>b.tot;
+	else if (a.perfect!=b.perfect) return a.perfect>b.perfect;
+	else return a.uid<b.uid;
 }
 int main() {
-	int N, K, M;
-	int p[10];
-	scanf("%d%d%d", &N, &K, &M);
-	for (int i = 1; i <= K; i++)
-		scanf("%d", &p[i]);
-	int uid, pid, score;
-	for (int i = 1; i <= N; i++)
-		for (int j = 1; j <= K; j++)
-			user[i].score[j] = -1;
-	for (int i = 0; i < M; i++) {
-		scanf("%d%d%d", &uid, &pid, &score);
-		user[uid].id = uid;
-		if (score > user[uid].score[pid]) {
-			if (user[uid].score[pid] == -1)
-				user[uid].total += score;
-			else
-				user[uid].total += score - user[uid].score[pid];
-			user[uid].score[pid] = score;
-			if (score == p[pid])
-				user[uid].solve++;
-		}
-		if (score == -1 && user[uid].score[pid]==-1)
-			user[uid].score[pid] = 0;
-		if (score > -1)
-			user[uid].pass = true;
+	int n, k, m, uid, pid, g;
+	cin >> n >> k >> m;
+	vector<int> p(k, -1);
+	vector<node> v(n);
+	for (int i = 0; i < n; i++) v[i].p = p;
+	for (int i = 0; i < k; i++) cin >> p[i];
+	for (int i = 0; i < m; i++) {
+		cin >> uid >> pid >> g;
+		v[uid-1].uid = uid;
+		int t = max(g, v[uid-1].p[pid-1]);
+		if (t != -1) {
+			v[uid-1].print = 1;
+			v[uid-1].p[pid-1] = t;
+		} else v[uid-1].p[pid-1] = 0;
 	}
-	sort(user+1, user+1+N, cmp);
-	int r = 1;
-	for (int i = 1; i <= N; i++) {
-		if (user[i].pass == false)
-			continue;
-		if (i > 1 && user[i].total != user[i-1].total)
-			r = i;
-		printf("%d %05d %d", r, user[i].id, user[i].total);
-		for (int j = 1; j <= K; j++)
-			if (user[i].score[j] == -1)
-				printf(" -");
-			else
-				printf(" %d", user[i].score[j]);
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < k; j++) {
+			if (v[i].p[j] != -1) v[i].tot += v[i].p[j];
+			if (v[i].p[j] == p[j]) v[i].perfect++;
+		}
+	sort(v.begin(), v.end(), cmp);
+	v[0].r = 1;
+	for (int i = 1; i < n; i++) {
+		v[i].r = i + 1;
+		if (v[i].tot == v[i-1].tot) v[i].r = v[i-1].r;
+	}
+	for (int i = 0; i < n; i++) {
+		if (!v[i].print) continue;
+		printf("%d %05d %d", v[i].r, v[i].uid, v[i].tot);
+		for (int j = 0; j < k; j++)
+			if (v[i].p[j] != -1) printf(" %d", v[i].p[j]);
+			else printf(" -");
 		printf("\n");
 	}
 	return 0;
