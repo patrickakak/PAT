be59f96f73450a7d9e152d0dc6cdd65b3050dd9a
Vuzzway <13806680065@163.com>
2020-01-26 08:21:26 +0800

    -

--- Advanced Level/1015 Reversible Primes (20).cpp
+++ Advanced Level/1015 Reversible Primes (20).cpp
@@ -2,7 +2,7 @@
 using namespace std;
 bool isprime(int n) {
 	if (n < 2) return false;
-	for(int i = 2; i * i <= n; i++)
+	for (int i = 2; i * i <= n; i++)
 		if (n % i == 0) return false;
 	return true;
 }
