c425ed4a88168f6469dc104092e59c09233a6deb
Vuzzway <13806680065@163.com>
2020-01-26 08:20:58 +0800

    Update 1015 Reversible Primes (20).cpp

--- Advanced Level/1015 Reversible Primes (20).cpp
+++ Advanced Level/1015 Reversible Primes (20).cpp
@@ -1,52 +1,28 @@
-/**
- * Sample input:
- * 73 10
- * 23 2
- * 23 10
- * -2
- * -------------
- * Sample output:
- * Yes
- * Yes
- * No
- */
-#include <cstdio>
-#include <cmath>
-
-bool isPrime(int n)
-{
-	if (n <= 1) return false;
-	int sqr = (int)sqrt(1.0 * n);
-	for (int i = 2; i <= sqr; i++)
+#include <iostream>
+using namespace std;
+bool isprime(int n) {
+	if (n < 2) return false;
+	for(int i = 2; i * i <= n; i++)
 		if (n % i == 0) return false;
 	return true;
 }
-
-int main()
-{
-	// freopen("tst.txt", "r", stdin);
-
-	int n, D, r[110];
-
+int main() {
+	int n, d;
 	while (scanf("%d", &n) != EOF) {
 		if (n < 0) break;
-		scanf("%d", &D);
-		if (!isPrime(n))
+		scanf("%d", &d);
+		if (!isprime(n)) {
 			printf("No\n");
-		else {
-			int len = 0;
-			do {
-				r[len++] = n % D;
-				n /= D;
-			} while (n != 0);
-			for (int i = 0; i < len; i++)
-				n = n * D + r[i];
-			if (isPrime(n)) printf("Yes\n");
-			else printf("No\n");
+			continue;
 		}
+		int len = 0, arr[100];
+		do {
+			arr[len++] = n % d;
+			n = n / d;
+		} while (n != 0);
+		for (int i = 0; i < len; i++)
+			n = n * d + arr[i];
+		printf("%s\n", isprime(n) ? "Yes" : "No");
 	}
-
 	return 0;
 }
-
-
