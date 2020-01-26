e136b172cb2b1b6da1efd6ba4e46930e2bebcc56
Vuzzway <13806680065@163.com>
2020-01-26 14:45:11 +0800

    Update and rename 1019 General Palindromic Number.cpp to 1019 General Palindromic Number (20).cpp

---
+++ Advanced Level/1019 General Palindromic Number (20).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+bool ispalindromic(int a[], int len) {
+	for (int i=0, j=len-1; i<j; i++, j--)
+		if (a[i] != a[j]) return false;
+	return true;
+}
+int main() {
+	int n, b, a[100000], len = 0;
+	cin >> n >> b;
+	do {
+		a[len++] = n % b;
+		n /= b;
+	} while (n);
+	if (ispalindromic(a, len)) printf("Yes\n");
+	else printf("No\n");
+	for (int i = len - 1; i >= 0; i--) {
+		if (i != len - 1) printf(" ");
+		printf("%d", a[i]);
+	}
+	return 0;
+}
