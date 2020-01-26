c76d97bfd764bd75dd906817a2cc76e0e3c0743f
Vuzzway <13806680065@163.com>
2020-01-26 15:04:19 +0800

    Update 1054 The Dominant Color.cpp

--- Advanced Level/1054 The Dominant Color.cpp
+++ Advanced Level/1054 The Dominant Color.cpp
@@ -1,30 +1,19 @@
-
-#include <cstdio>
+#include <iostream>
 #include <map>
 using namespace std;
-
-int main()
-{
-	// freopen("tst.txt", "r", stdin);
+int main() {
 	int m, n, tmp;
-	map<int, int> mp;
-
-	scanf("%d%d", &m, &n);
+	scanf("%d %d", &m, &n);
+	map<int, int> a;
+	int half = m * n / 2;
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++) {
 			scanf("%d", &tmp);
-			if (mp.find(tmp) != mp.end()) mp[tmp]++;
-			else mp[tmp] = 1;
+			a[tmp]++;
+			if (a[tmp] > half) {
+				printf("%d", tmp);
+				return 0;
+			}
 		}
-	int k, Max = 0;
-	for (map<int,int>::iterator it=mp.begin(); it != mp.end(); it++)
-		if (Max < it->second) {
-			Max = it->second;
-			k = it->first;
-		}
-	printf("%d", k);
-
 	return 0;
 }
-
-
