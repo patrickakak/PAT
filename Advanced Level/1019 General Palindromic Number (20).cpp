b35151d4bee25f54faea1d1dddc7bfcd5b1244ef
Vuzzway <13806680065@163.com>
2020-01-26 14:46:12 +0800

    Update 1019 General Palindromic Number (20).cpp

--- Advanced Level/1019 General Palindromic Number (20).cpp
+++ Advanced Level/1019 General Palindromic Number (20).cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 bool ispalindromic(int a[], int len) {
-	for (int i=0, j=len-1; i<j; i++, j--)
+	for (int i = 0, j = len - 1; i < j; i++, j--)
 		if (a[i] != a[j]) return false;
 	return true;
 }
