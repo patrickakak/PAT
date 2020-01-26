00c20a2eec3c1015ffe3c156714557bbeec4e46c
Vuzzway <13806680065@163.com>
2020-01-26 11:16:06 +0800

    Update 1145 Hashing - Average Search Time (25).cpp

--- Advanced Level/1145 Hashing - Average Search Time (25).cpp
+++ Advanced Level/1145 Hashing - Average Search Time (25).cpp
@@ -7,29 +7,29 @@ bool isprime(int n) {
 	return true;
 }
 int main() {
-	int sz, n, m, a, ans = 0;
+	int sz, n, m, key, ans = 0;
 	scanf("%d %d %d", &sz, &n, &m);
 	while (!isprime(sz)) sz++;
 	vector<int> v(sz);
 	for (int i = 0; i < n; i++) {
-		scanf("%d", &a);
+		scanf("%d", &key);
 		int flag = 0;
-		for (int j = 0; j < sz; j++) {
-			int pos = (a + j * j) % sz;
+		for (int step = 0; step < sz; step++) {
+			int pos = (key + step * step) % sz;
 			if (v[pos] == 0) {
-				v[pos] = a;
+				v[pos] = key;
 				flag = 1;
 				break;
 			}
 		}
-		if (!flag) printf("%d cannot be inserted.\n", a);
+		if (!flag) printf("%d cannot be inserted.\n", key);
 	}
 	for (int i = 0; i < m; i++) {
-		scanf("%d", &a);
-		for (int j = 0; j <= sz; j++) {
+		scanf("%d", &key);
+		for (int step = 0; step <= sz; step++) {
 			ans++;
-			int pos = (a + j * j) % sz;
-			if (v[pos] == a || v[pos] == 0) break;
+			int pos = (key + step * step) % sz;
+			if (v[pos] == key || v[pos] == 0) break;
 		}
 	}
 	printf("%.1lf\n", 1.0 * ans / m);
