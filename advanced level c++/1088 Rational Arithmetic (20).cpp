f53cd3077fa728bf9b7a582b15cd4f7349d8721e
vuzway <13806680065@163.com>
2020-01-27 21:06:52 +0800

    -

---
+++ advanced level c++/1088 Rational Arithmetic (20).cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+long gcd(long a, long b) {
+	return !b ? a : gcd(b, a % b);
+}
+void f(long n, long d) {
+	if (n * d == 0) {
+		cout << (d == 0 ? "Inf" : "0");
+		return ;
+	}
+	bool flag = (n < 0 && d > 0) || (n > 0 && d < 0);
+	n = abs(n); d = abs(d);
+	long q = n / d;
+	cout << (flag ? "(-" : "");
+	if (q != 0) cout << q;
+	if (n % d == 0) {
+		if (flag) cout <<')';
+		return ;
+	}
+	if (q != 0) cout << ' ';
+	n -= q * d;
+	long t = gcd(n, d);
+	n /= t; d /= t;
+	cout << n << '/' << d << (flag ? ")" : "");
+}
+int main() {
+	long a, b, c, d;
+	scanf("%ld/%ld %ld/%ld", &a, &b, &c, &d);
+	f(a, b); cout << " + "; f(c, d); cout << " = "; f(a*d + b*c, b*d); cout << endl;
+	f(a, b); cout << " - "; f(c, d); cout << " = "; f(a*d - b*c, b*d); cout << endl;
+	f(a, b); cout << " * "; f(c, d); cout << " = "; f(a*c, b*d); cout << endl;
+	f(a, b); cout << " / "; f(c, d); cout << " = "; f(a*d, b*c);
+	return 0;
+}
