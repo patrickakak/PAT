8732c991b3765e9904d5a178aa700ef62bbbdd0f
vuzway <13806680065@163.com>
2020-01-27 15:06:41 +0800

    -

--- advanced level c++/1096 Consecutive Factors (20).cpp
+++ advanced level c++/1096 Consecutive Factors (20).cpp
@@ -6,18 +6,17 @@ int main(){
 	cin >> n;
 	int first = 0, len = 0, maxn = sqrt(n) + 1;
 	for (int i = 2; i <= maxn; i++) {
-		int j, tmp = 1;
+		int j, fac = 1;
 		for (j = i; j <= maxn; j++) {
-			tmp *= j;
-			if (n % tmp != 0) break;
+			fac *= j;
+			if (n % fac != 0) break;
 		}
 		if (j - i > len) {
 			len = j - i;
 			first = i;
 		}
 	}
-	if (first == 0)
-		cout << 1 << endl << n;
+	if (first == 0) cout << 1 << endl << n;
 	else {
 		cout << len << endl;
 		for (int i = 0; i < len; i++) {
