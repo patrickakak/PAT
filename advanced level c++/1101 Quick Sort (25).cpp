0add1b3b0e8e2b420b21215fdf1efaa2d7368e54
vuzway <13806680065@163.com>
2020-01-27 20:28:37 +0800

    -

---
+++ advanced level c++/1101 Quick Sort (25).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main() {
+	int n, max = 0;
+	cin >> n;
+	vector<int> a(n), b(n), v;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+		b[i] = a[i];
+	}
+	sort(a.begin(), a.end());
+	for (int i = 0; i < n; i++) {
+		if (a[i] == b[i] && b[i] > max) v.push_back(b[i]);
+		if (b[i] > max) max = b[i];
+	}
+	printf("%d\n", v.size());
+	for (int i = 0; i < v.size(); i++) {
+		if (i != 0) cout << ' ';
+		cout << v[i];
+	}
+	cout << endl;
+	return 0;
+}
