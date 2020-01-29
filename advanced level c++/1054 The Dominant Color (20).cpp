3b2bf15542db03e4eeb351d79f5fdd3ea518ec34
vuzway9132 <13806680065@163.com>
2020-01-29 12:41:10 +0800

    Update and rename 1054 The Dominant Color.cpp to 1054 The Dominant Color (20).cpp

--- advanced level c++/1054 The Dominant Color (20).cpp
+++ advanced level c++/1054 The Dominant Color (20).cpp
@@ -1,16 +1,16 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
 using namespace std;
 int main() {
 	int m, n, tmp;
 	scanf("%d %d", &m, &n);
-	map<int, int> a;
+	unordered_map<int, int> mp;
 	int half = m * n / 2;
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++) {
 			scanf("%d", &tmp);
-			a[tmp]++;
-			if (a[tmp] > half) {
+			mp[tmp]++;
+			if (mp[tmp] > half) {
 				printf("%d", tmp);
 				return 0;
 			}
