bddb4168ec4ff3b1671be6e61727ecaee43cb8d3
vuzway9132 <13806680065@163.com>
2020-01-29 20:18:07 +0800

    -

--- advanced level c++/1095 Cars on Campus (30).cpp
+++ advanced level c++/1095 Cars on Campus (30).cpp
@@ -16,25 +16,25 @@ bool cmp2(node a, node b) {
 	return a.time < b.time;
 }
 int main() {
-	int n, k, maxtime = -1, tempindex = 0;
+	int n, k, maxtime = -1, tmpindex = 0;
 	scanf("%d%d\n", &n, &k);
-	vector<node> record(n), car;
+	vector<node> rec(n), car;
 	for (int i = 0; i < n; i++) {
-		char temp[5];
+		char tmp[5];
 		int h, m, s;
-		scanf("%s %d:%d:%d %s\n", record[i].id, &h, &m, &s, temp);
-		int temptime = h * 3600 + m * 60 + s;
-		record[i].time = temptime;
-		record[i].flag = strcmp(temp, "in") == 0 ? 1 : -1;
+		scanf("%s %d:%d:%d %s\n", rec[i].id, &h, &m, &s, tmp);
+		int tmptime = h * 3600 + m * 60 + s;
+		rec[i].time = tmptime;
+		rec[i].flag = strcmp(tmp, "in") == 0 ? 1 : -1;
 	}
-	sort(record.begin(), record.end(), cmp1);
-	map<string, int> mapp;
+	sort(rec.begin(), rec.end(), cmp1);
+	map<string, int> mp;
 	for (int i = 0; i < n - 1; i++)
-		if (strcmp(record[i].id, record[i+1].id) == 0 && record[i].flag == 1 && record[i+1].flag == -1) {
-			car.push_back(record[i]);
-			car.push_back(record[i+1]);
-			mapp[record[i].id] += (record[i+1].time - record[i].time);
-			if (maxtime < mapp[record[i].id]) maxtime = mapp[record[i].id];
+		if (strcmp(rec[i].id, rec[i+1].id) == 0 && rec[i].flag == 1 && rec[i+1].flag == -1) {
+			car.push_back(rec[i]);
+			car.push_back(rec[i+1]);
+			mp[rec[i].id] += (rec[i+1].time - rec[i].time);
+			if (maxtime < mp[rec[i].id]) maxtime = mp[rec[i].id];
 		}
 	sort(car.begin(), car.end(), cmp2);
 	vector<int> cnt(n);
@@ -44,16 +44,16 @@ int main() {
 	for (int i = 0; i < k; i++) {
 		int h, m, s;
 		scanf("%d:%d:%d", &h, &m, &s);
-		int j, temptime = h * 3600 + m * 60 + s;
-		for (j = tempindex; j < car.size(); j++)
-			if (car[j].time > temptime) {
+		int j, tmptime = h * 3600 + m * 60 + s;
+		for (j = tmpindex; j < car.size(); j++)
+			if (car[j].time > tmptime) {
 				printf("%d\n", cnt[j - 1]);
 				break;
 			} else if(j == car.size() - 1)
 				printf("%d\n", cnt[j]);
-		tempindex = j;
+		tmpindex = j;
 	}
-	for (map<string, int>::iterator it = mapp.begin(); it != mapp.end(); it++)
+	for (map<string, int>::iterator it = mp.begin(); it != mp.end(); it++)
 		if (it->second == maxtime) printf("%s ", it->first.c_str());
 	printf("%02d:%02d:%02d", maxtime / 3600, (maxtime % 3600) / 60, maxtime % 60);
 	return 0;
