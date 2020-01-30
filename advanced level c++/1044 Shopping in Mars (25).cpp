bc6273a667b1472742e6d334d04441955802467f
vuzway9132 <13806680065@163.com>
2020-01-30 20:31:21 +0800

    -

--- advanced level c++/1044 Shopping in Mars (25).cpp
+++ advanced level c++/1044 Shopping in Mars (25).cpp
@@ -5,24 +5,23 @@ using namespace std;
 int main() {
 	int n, m, d = 0x3fff;
 	scanf("%d%d", &n, &m);
-	vector<int> sum(n+1);
-	sum[0] = 0;
+	vector<int> sum(n + 1, 0);
 	for (int i = 1; i <= n; i++) {
 		scanf("%d", &sum[i]);
 		sum[i] += sum[i-1];
 	}
 	for (int i = 1; i <= n; i++) {
-		int j = upper_bound(sum.begin()+i, sum.end(), sum[i-1]+m) - sum.begin();
+		int j = upper_bound(sum.begin() + i, sum.end(), sum[i-1] + m) - sum.begin();
 		if (sum[j-1] - sum[i-1] == m) {
 			d = m;
 			break;
-		} else if (j <= n && sum[j]-sum[i-1] < d)
+		} else if (j <= n && sum[j] - sum[i-1] < d)
 			d = sum[j] - sum[i-1];
 	}
 	for (int i = 1; i <= n; i++) {
-		int j = upper_bound(sum.begin()+i, sum.end(), sum[i-1]+d) - sum.begin();
+		int j = upper_bound(sum.begin() + i, sum.end(), sum[i-1] + d) - sum.begin();
 		if (sum[j-1] - sum[i-1] == d)
-			printf("%d-%d\n", i, j-1);
+			printf("%d-%d\n", i, j - 1);
 	}
 	return 0;
 }
