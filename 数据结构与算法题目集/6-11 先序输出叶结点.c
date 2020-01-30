81aff707d4eb2933079bc06a4168c40c0ae17be3
vuzway9132 <13806680065@163.com>
2020-01-30 23:08:01 +0800

    -

--- 数据结构与算法题目集/6-11 先序输出叶结点.c
+++ 数据结构与算法题目集/6-11 先序输出叶结点.c
@@ -1,23 +1,7 @@
-/**
- * 6-11: Print leaves node in preorder traversal
- *               A
- *              / \
- *             B   C
- *            / \  / \
- *           D  F  G  I
- *             /    \
- *            E      H
- * --------------------------------------
- * Sample Output:
- * Leaf nodes are: D E H I
- */
-void PreorderPrintLeaves(BinTree BT)
-{
+void PreorderPrintLeaves(BinTree BT) {
 	if (BT) {
-		if (!BT->Left && !BT->Right)
-			printf(" %c", BT->Data);
+		if (!BT->Left && !BT->Right) printf(" %c", BT->Data);
 		PreorderPrintLeaves(BT->Left);
 		PreorderPrintLeaves(BT->Right);
 	}
 }
-
