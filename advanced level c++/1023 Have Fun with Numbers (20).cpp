46efe81ed50496da4b0909238337c8fe80d8cfcd
vuzway9132 <13806680065@163.com>
2020-01-30 10:39:23 +0800

    -

---
+++ advanced level c++/1023 Have Fun with Numbers (20).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+	string s, ans;
+	cin >> s;
+	int flag = 0, carry = 0;
+	for (int i = s.length() - 1; i >= 0; i--) {
+		int sum = carry + 2 * (s[i] - '0');
+		carry = sum / 10;
+		int r = sum % 10;
+		ans += ('0' + r);
+	}
+	if (carry != 0) ans += '1';
+	else if (is_permutation(s.begin(), s.end(), ans.begin())) flag = 1;
+	reverse(ans.begin(), ans.end());
+	printf("%s\n%s", flag ? "Yes" : "No", ans.c_str());
+	return 0;
+}
