2439f5c6fc8339444065c1eef918212a4c348717
vuzway9132 <13806680065@163.com>
2020-01-30 17:58:36 +0800

    -

--- advanced level c++/1044 Shopping in Mars (25).cpp
+++ advanced level c++/1044 Shopping in Mars (25).cpp
@@ -1,32 +1,10 @@
-/**
- * Sample input1:
- * 16 15
- * 3 2 1 5 4 6 8 7 16 10 15 11 9 12 14 13
- * Sample output1:
- * 1-5
- * 4-6
- * 7-8
- * 11-11
- * -----------------
- * Sample input2:
- * 5 13
- * 2 4 5 7 9
- * Sample output2:
- * 2-4
- * 4-5
- */
 #include <cstdio>
 #include <algorithm>
 using namespace std;
-
 #define maxn 100010
-int sum[maxn] = {0};	// sum[n]: A[1]~A[n]
-
-int main()
-{
-	// freopen("tst.txt", "r", stdin);
+int sum[maxn] = {0};
+int main() {
 	int n, S, nearS = 100000010;
-
 	scanf("%d%d", &n, &S);
 	sum[0] = 0;
 	for (int i = 1; i <= n; i++) {
@@ -46,8 +24,5 @@ int main()
 		if (sum[j-1] - sum[i-1] == nearS)
 			printf("%d-%d\n", i, j-1);
 	}
-
 	return 0;
 }
-
-
