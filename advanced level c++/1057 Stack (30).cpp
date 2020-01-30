c0043ed2b4f0a16178aa72fc99f8a5d5908293dd
vuzway9132 <13806680065@163.com>
2020-01-30 17:47:21 +0800

    -

--- advanced level c++/1057 Stack (30).cpp
+++ advanced level c++/1057 Stack (30).cpp
@@ -6,23 +6,21 @@ using namespace std;
 int c[maxn];
 stack<int> s;
 void update(int x, int v) {
-	for(int i = x; i < maxn; i += lowbit(i))
+	for (int i = x; i < maxn; i += lowbit(i))
 		c[i] += v;
 }
 int getsum(int x) {
 	int sum = 0;
-	for(int i = x; i >= 1; i -= lowbit(i))
+	for (int i = x; i >= 1; i -= lowbit(i))
 		sum += c[i];
 	return sum;
 }
 void PeekMedian() {
 	int left = 1, right = maxn, mid, k = (s.size() + 1) / 2;
-	while(left < right) {
+	while (left < right) {
 		mid = (left + right) / 2;
-		if(getsum(mid) >= k)
-			right = mid;
-		else
-			left = mid + 1;
+		if (getsum(mid) >= k) right = mid;
+		else left = mid + 1;
 	}
 	printf("%d\n", left);
 }
@@ -30,25 +28,21 @@ int main() {
 	int n, temp;
 	scanf("%d", &n);
 	char str[15];
-	for(int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) {
 		scanf("%s", str);
-		if(str[1] == 'u') {
+		if (str[1] == 'u') {
 			scanf("%d", &temp);
 			s.push(temp);
 			update(temp, 1);
-		} else if(str[1] == 'o') {
-			if(!s.empty()) {
+		} else if (str[1] == 'o') {
+			if (!s.empty()) {
 				update(s.top(), -1);
 				printf("%d\n", s.top());
 				s.pop();
-			} else {
-				printf("Invalid\n");
-			}
+			} else printf("Invalid\n");
 		} else {
-			if(!s.empty())
-				PeekMedian();
-			else
-				printf("Invalid\n");
+			if (!s.empty()) PeekMedian();
+			else printf("Invalid\n");
 		}
 	}
 	return 0;
