4370dcd8bea4f18da05e03281efd7b7efebbb3bd
vuzway9132 <13806680065@163.com>
2020-01-31 14:09:23 +0800

    -

--- advanced level c++/1038 Recover the Smallest Number (30).cpp
+++ advanced level c++/1038 Recover the Smallest Number (30).cpp
@@ -6,16 +6,13 @@ bool cmp(string a, string b) { return a + b < b + a; }
 int main() {
 	int n;
 	cin >> n;
-	vector<string> ss(n);
-	for (int i = 0; i < n; i++)
-		cin >> ss[i];
-	sort(ss.begin(), ss.end(), cmp);
-	string ans;
-	for (int i = 0; i < n; i++)
-		ans += ss[i];
-	while (ans.size() != 0 && ans[0] == '0')
-		ans.erase(ans.begin());
-	if (ans.size() == 0) cout << 0;
-	else cout << ans;
+	vector<string> v(n);
+	for (int i = 0; i < n; i++) cin >> v[i];
+	sort(v.begin(), v.end(), cmp);
+	string s;
+	for (int i = 0; i < n; i++) s += v[i];
+	while (s.length() != 0 && s[0] == '0') s.erase(s.begin());
+	if (s.size() == 0) cout << 0;
+	else cout << s;
 	return 0;
 }
