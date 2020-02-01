cc55283299dbac5a18c70cb1c253a94fb166d1b1
vuzway9132 <13806680065@163.com>
2020-02-01 15:34:28 +0800

    -

--- advanced level c++/1079 Total Sales of Supply Chain (25).cpp
+++ advanced level c++/1079 Total Sales of Supply Chain (25).cpp
@@ -1,37 +1,36 @@
-#include <cstdio>
+#include <iostream>
 #include <vector>
 #include <cmath>
 using namespace std;
-const int maxn = 100010;
-struct Node {
+struct node {
 	double data;
 	vector<int> child;
-} node[maxn];
-int n;
-double p, r, ans = 0;
-void DFS(int index, int depth) {
-	if (node[index].child.size() == 0) {
-		ans += node[index].data * pow(1+r, depth);
-		return;
+};
+vector<node> v;
+double ans = 0.0, p, r;
+void dfs(int index, int depth) {
+	if (v[index].child.size() == 0) {
+		ans += v[index].data * pow(1 + r, depth);
+		return ;
 	}
-	for (int i = 0; i < (int)node[index].child.size(); i++)
-		DFS(node[index].child[i], depth+1);
+	for (int i = 0; i < v[index].child.size(); i++)
+		dfs(v[index].child[i], depth + 1);
 }
 int main() {
-	int k, child;
-	scanf("%d%lf%lf", &n, &p, &r);
-	r /= 100;
+	int n, k, c;
+	scanf("%d %lf %lf", &n, &p, &r);
+	r = r / 100;
+	v.resize(n);
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &k);
-		if (k == 0) 	// leaf
-			scanf("%lf", &node[i].data);
+		if (k == 0) scanf("%lf", &v[i].data);
 		else
 			for (int j = 0; j < k; j++) {
-				scanf("%d", &child);
-				node[i].child.push_back(child);
+				scanf("%d", &c);
+				v[i].child.push_back(c);
 			}
 	}
-	DFS(0, 0);
-	printf("%.1f\n", p * ans);
+	dfs(0, 0);
+	printf("%.1f", ans * p);
 	return 0;
 }
