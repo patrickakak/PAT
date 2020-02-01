d27a2b61823b80558949875fd3ad87855c2b94cb
vuzway9132 <13806680065@163.com>
2020-02-01 15:31:48 +0800

    -

--- advanced level c++/1079 Total Sales of Supply Chain (25).cpp
+++ advanced level c++/1079 Total Sales of Supply Chain (25).cpp
@@ -1,25 +1,7 @@
-/**
- * Sample input:
- * 10 1.80 1.00
- * 3 2 3 5
- * 1 9
- * 1 4
- * 1 7
- * 0 7
- * 2 6 1
- * 1 8
- * 0 9
- * 0 4
- * 0 3
- * --------------
- * Sample output:
- * 42.4
- */
 #include <cstdio>
 #include <vector>
 #include <cmath>
 using namespace std;
-
 const int maxn = 100010;
 struct Node {
 	double data;
@@ -27,9 +9,7 @@ struct Node {
 } node[maxn];
 int n;
 double p, r, ans = 0;
-
-void DFS(int index, int depth)
-{
+void DFS(int index, int depth) {
 	if (node[index].child.size() == 0) {
 		ans += node[index].data * pow(1+r, depth);
 		return;
@@ -37,12 +17,8 @@ void DFS(int index, int depth)
 	for (int i = 0; i < (int)node[index].child.size(); i++)
 		DFS(node[index].child[i], depth+1);
 }
-
-int main()
-{
-	// freopen("tst.txt", "r", stdin);
+int main() {
 	int k, child;
-
 	scanf("%d%lf%lf", &n, &p, &r);
 	r /= 100;
 	for (int i = 0; i < n; i++) {
@@ -57,9 +33,5 @@ int main()
 	}
 	DFS(0, 0);
 	printf("%.1f\n", p * ans);
-
 	return 0;
 }
-
-
-
