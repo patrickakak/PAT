4f55a51dc4349217ec078237b9086a9fd998ec50
vuzway9132 <13806680065@163.com>
2020-02-01 19:35:05 +0800

    -

---
+++ advanced level c++/1090 Highest Price in Supply Chain (25).cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+struct node {
+	vector<int> child;
+};
+int dmax = 0;
+double p, r;
+vector<int> d(100000);
+vector<node> v;
+void dfs(int index, int depth) {
+	if (v[index].child.size() == 0) {
+		if (depth > dmax) dmax = depth;
+		d[depth]++;
+		return ;
+	}
+	for (int i = 0; i < v[index].child.size(); i++)
+		dfs(v[index].child[i], depth + 1);
+}
+int main() {
+	int n, t, root;
+	scanf("%d %lf %lf", &n, &p, &r);
+	v.resize(n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &t);
+		if (t == -1) root = i;
+		else v[t].child.push_back(i);
+	}
+	dfs(root, 0);
+	printf("%.2f %d", p * pow(1 + r/100, dmax), d[dmax]);
+	return 0;
+}
