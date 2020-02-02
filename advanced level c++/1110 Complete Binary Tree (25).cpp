9ddf36ccf50465f435b9f8a1b6a57809e8867770
vuzway9132 <13806680065@163.com>
2020-02-02 15:47:38 +0800

    -

--- advanced level c++/1110 Complete Binary Tree (25).cpp
+++ advanced level c++/1110 Complete Binary Tree (25).cpp
@@ -2,36 +2,34 @@
 using namespace std;
 struct node{
 	int l, r;
-} a[100];
+} tree[100];
 int maxn = -1, ans;
 void dfs(int root, int index) {
 	if (index > maxn) {
 		maxn = index;
 		ans = root;
 	}
-	if (a[root].l != -1) dfs(a[root].l, index * 2);
-	if (a[root].r != -1) dfs(a[root].r, index * 2 + 1);
+	if (tree[root].l != -1) dfs(tree[root].l, index * 2);
+	if (tree[root].r != -1) dfs(tree[root].r, index * 2 + 1);
 }
 int main() {
-	int n, root = 0, have[100] = {0};
+	int n, root = 0, h[100] = {0};
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		string l, r;
 		cin >> l >> r;
-		if (l == "-")
-			a[i].l = -1;
+		if (l == "-") tree[i].l = -1;
 		else {
-			a[i].l = stoi(l);
-			have[stoi(l)] = 1;
+			tree[i].l = stoi(l);
+			h[stoi(l)] = 1;
 		}
-		if (r == "-")
-			a[i].r = -1;
+		if (r == "-") tree[i].r = -1;
 		else {
-			a[i].r = stoi(r);
-			have[stoi(r)] = 1;
+			tree[i].r = stoi(r);
+			h[stoi(r)] = 1;
 		}
 	}
-	while (have[root] != 0) root++;
+	while (h[root] != 0) root++;
 	dfs(root, 1);
 	if (maxn == n) cout << "YES " << ans;
 	else cout << "NO " << root;
