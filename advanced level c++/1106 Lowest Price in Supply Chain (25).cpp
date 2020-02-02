4c6d603764bca47368338d7f9a2b66ececeff19f
vuzway9132 <13806680065@163.com>
2020-02-02 09:04:41 +0800

    -

--- advanced level c++/1106 Lowest Price in Supply Chain (25).cpp
+++ advanced level c++/1106 Lowest Price in Supply Chain (25).cpp
@@ -1,33 +1,13 @@
-/**
- * Sample input:
- * 10 1.80 1.00
- * 3 2 3 5
- * 1 9
- * 1 4
- * 1 7
- * 0
- * 2 6 1
- * 1 8
- * 0
- * 0
- * 0
- * --------------
- * Sample output:
- * 1.8362 2
- */
 #include <cstdio>
 #include <vector>
 #include <cmath>
 using namespace std;
-
 const int maxn = 100010;
 const double INF = 1e12;
 vector<int> node[maxn];
 int n, num = 0;
 double p, r, ans = INF;
-
-void DFS(int index, int depth)
-{
+void DFS(int index, int depth) {
 	if (node[index].size() == 0) {
 		double price = p * pow(1+r, depth);
 		if (price < ans) {
@@ -40,12 +20,8 @@ void DFS(int index, int depth)
 	for (int i = 0; i < (int)node[index].size(); i++)
 		DFS(node[index][i], depth+1);
 }
-
-int main()
-{
-	// freopen("tst.txt", "r", stdin);
+int main() {
 	int k, child;
-
 	scanf("%d%lf%lf", &n, &p, &r);
 	r /= 100;
 	for (int i = 0; i < n; i++) {
@@ -58,9 +34,5 @@ int main()
 	}
 	DFS(0, 0);
 	printf("%.4f %d\n", ans, num);
-
 	return 0;
 }
-
-
-
