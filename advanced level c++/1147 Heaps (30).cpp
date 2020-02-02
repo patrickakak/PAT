ff737830524af0f85ada4f45630c0433d5124b3f
vuzway9132 <13806680065@163.com>
2020-02-02 13:32:18 +0800

    -

---
+++ advanced level c++/1147 Heaps (30).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int a[1005], m, n;
+void postOrder(int index) {
+	if (index > n) return;
+	postOrder(index * 2);
+	postOrder(index * 2 + 1);
+	printf("%d%s", a[index], index == 1 ? "\n" : " ");
+}
+int main() {
+	scanf("%d %d", &m, &n);
+	while (m--) {
+		int minn = 1, maxn = 1;
+		for (int i = 1; i <= n; i++) scanf("%d", &a[i]);
+		for (int i = 2; i <= n; i++) {
+			if (a[i] > a[i / 2]) maxn = 0;
+			if (a[i] < a[i / 2]) minn = 0;
+		}
+		if (maxn == 1) printf("Max Heap\n");
+		else if (minn == 1) printf("Min Heap\n");
+		else printf("Not Heap\n");
+		postOrder(1);
+	}
+	return 0;
+}
