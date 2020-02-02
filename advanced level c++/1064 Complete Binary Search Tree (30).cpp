5313f607910003bc989e426cdd933ff50171e87d
vuzway9132 <13806680065@163.com>
2020-02-02 20:35:38 +0800

    -

--- advanced level c++/1064 Complete Binary Search Tree (30).cpp
+++ advanced level c++/1064 Complete Binary Search Tree (30).cpp
@@ -1,23 +1,24 @@
-#include <cstdio>
+#include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
-const int maxn = 1010;
-int n, number[maxn], CBT[maxn], index = 0;
-void inOrder(int root) {
-	if (root > n) return;
-	inOrder(root * 2);
-	CBT[root] = number[index++];
-	inOrder(root*2 + 1);
+int n, cnt = 0;
+vector<int> in, cbt;
+void inOrder(int index) {
+	if (index >= n) return;
+	inOrder(2 * index + 1);
+	cbt[index] = in[cnt++];
+	inOrder(2 * index + 2);
 }
 int main() {
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
-		scanf("%d", &number[i]);
-	sort(number, number+n);
-	inOrder(1);
-	for (int i = 1; i <= n; i++) {
-		printf("%d", CBT[i]);
-		if (i < n) printf(" ");
+	cin >> n;
+	in.resize(n), cbt.resize(n);
+	for (int i = 0; i < n; i++) scanf("%d", &in[i]);
+	sort(in.begin(), in.end());
+	inOrder(0);
+	for (int i = 0; i < n; i++) {
+		if (i != 0) printf(" ");
+		printf("%d", cbt[i]);
 	}
 	return 0;
 }
