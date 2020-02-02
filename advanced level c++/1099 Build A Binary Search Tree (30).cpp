8db6dbba2ce0ce3c8f3a9494a1964551bed374f9
vuzway9132 <13806680065@163.com>
2020-02-02 21:30:32 +0800

    -

--- advanced level c++/1099 Build A Binary Search Tree (30).cpp
+++ advanced level c++/1099 Build A Binary Search Tree (30).cpp
@@ -1,44 +1,40 @@
-#include <cstdio>
-#include <queue>
+#include <iostream>
 #include <algorithm>
+#include <vector>
+#include <queue>
 using namespace std;
-const int maxn = 110;
-struct Node {
-	int data, left, right;
-} node[maxn];
-int n, in[maxn], num = 0;
-void inOrder(int root) {
-	if (root == -1) return;
-	inOrder(node[root].left);
-	node[root].data = in[num++];
-	inOrder(node[root].right);
+struct node { int data, l, r; };
+int n, cnt = 0;
+vector<node> bst;
+vector<int> in, level;
+void inOrder(int index) {
+	if (index == -1) return;
+	inOrder(bst[index].l);
+	bst[index].data = in[cnt++];
+	inOrder(bst[index].r);
 }
-void BFS(int root) {
+void bfs(int root) {
 	queue<int> q;
 	q.push(root);
-	num = 0;
 	while (!q.empty()) {
-		int now = q.front();
+		int t = q.front();
+		level.push_back(bst[t].data);
 		q.pop();
-		printf("%d", node[now].data);
-		num++;
-		if (num < n) printf(" ");
-		if (node[now].left != -1) q.push(node[now].left);
-		if (node[now].right != -1) q.push(node[now].right);
+		if (bst[t].l != -1) q.push(bst[t].l);
+		if (bst[t].r != -1) q.push(bst[t].r);
 	}
 }
 int main() {
-	int left, right;
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
-		scanf("%d%d", &left, &right);
-		node[i].left = left;
-		node[i].right = right;
-	}
-	for (int i = 0; i < n; i++)
-		scanf("%d", &in[i]);
-	sort(in, in + n);
+	cin >> n;
+	bst.resize(n), in.resize(n);
+	for (int i = 0; i < n; i++) cin >> bst[i].l >> bst[i].r;
+	for (int i = 0; i < n; i++) scanf("%d", &in[i]);
+	sort(in.begin(), in.end());
 	inOrder(0);
-	BFS(0);
+	bfs(0);
+	for (int i = 0; i < n; i++) {
+		if (i != 0) printf(" ");
+		printf("%d", level[i]);
+	}
 	return 0;
 }
