1d15a7ef370acac22ccdd3ece7a54a39c66ddd8c
vuzway9132 <13806680065@163.com>
2020-02-02 13:01:55 +0800

    -

--- advanced level c++/1138 Postorder Traversal (25).cpp
+++ advanced level c++/1138 Postorder Traversal (25).cpp
@@ -3,12 +3,12 @@
 using namespace std;
 vector<int> pre, in;
 bool flag = false;
-void postorder(int root, int start, int end) {
+void postOrder(int root, int start, int end) {
 	if (start > end || flag == true) return;
 	int i = start;
 	while (i <= end && in[i] != pre[root]) i++;
-	postorder(root + 1, start, i - 1);
-	postorder(root + i - start + 1, i + 1, end);
+	postOrder(root + 1, start, i - 1);
+	postOrder(root + i - start + 1, i + 1, end);
 	if (flag == false) {
 		printf("%d", pre[root]);
 		flag = true;
@@ -20,6 +20,6 @@ int main() {
 	pre.resize(n), in.resize(n);
 	for (int i = 0; i < n; i++) scanf("%d", &pre[i]);
 	for (int i = 0; i < n; i++) scanf("%d", &in[i]);
-	postorder(0, 0, n - 1);
+	postOrder(0, 0, n - 1);
 	return 0;
 }
