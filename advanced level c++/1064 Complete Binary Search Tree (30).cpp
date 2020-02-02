ffdec1aca48779280b2418a6b8725fc83b63bc9a
vuzway9132 <13806680065@163.com>
2020-02-02 20:07:35 +0800

    -

--- advanced level c++/1064 Complete Binary Search Tree (30).cpp
+++ advanced level c++/1064 Complete Binary Search Tree (30).cpp
@@ -1,28 +1,15 @@
-/**
- * Sample input:
- * 10
- * 1 2 3 4 5 6 7 8 9 0
- * --------------------
- * Sample output:
- * 6 3 8 1 5 7 9 0 2 4
- */
 #include <cstdio>
 #include <algorithm>
 using namespace std;
-
 const int maxn = 1010;
 int n, number[maxn], CBT[maxn], index = 0;
-void inOrder(int root)
-{
+void inOrder(int root) {
 	if (root > n) return;
 	inOrder(root * 2);
 	CBT[root] = number[index++];
 	inOrder(root*2 + 1);
 }
-
-int main()
-{
-	// freopen("tst.txt", "r", stdin);
+int main() {
 	scanf("%d", &n);
 	for (int i = 0; i < n; i++)
 		scanf("%d", &number[i]);
@@ -32,9 +19,5 @@ int main()
 		printf("%d", CBT[i]);
 		if (i < n) printf(" ");
 	}
-
 	return 0;
 }
-
-
-
