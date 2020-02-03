c1e97deb6f9369d468cea73008443ab2e623a92f
vuzway9132 <13806680065@163.com>
2020-02-03 10:08:56 +0800

    -

--- advanced level c++/1127 ZigZagging on a Tree  (30).cpp
+++ advanced level c++/1127 ZigZagging on a Tree  (30).cpp
@@ -1,45 +1,42 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 using namespace std;
-vector<int> in, post, res[35];
-int n, tree[35][2], root;
 struct node {
-	int index, depth;
-};
-void dfs(int &index, int inl, int inr, int postl, int postr) {
-	if (inl > inr) return;
-	index = postr;
-	int i = 0;
-	while (in[i] != post[postr]) i++;
-	dfs(tree[index][0], inl, i - 1, postl, postl + (i - inl) - 1);
-	dfs(tree[index][1], i + 1, inr, postl + (i - inl), postr - 1);
+	int data;
+	node *l, *r;
+} *root;
+vector<int> in, post, d[30];
+int n, dmax = 0;
+void dfs(node *r, int level) {
+	if (r == NULL) return;
+	if (dmax < level) dmax = level;
+	d[level].push_back(r->data);
+	if (r->l) dfs(r->l, level + 1);
+	if (r->r) dfs(r->r, level + 1);
 }
-void bfs() {
-	queue<node> q;
-	q.push(node{root, 0});
-	while (!q.empty()) {
-		node tmp = q.front();
-		q.pop();
-		res[tmp.depth].push_back(post[tmp.index]);
-		if (tree[tmp.index][0] != 0) q.push(node{tree[tmp.index][0], tmp.depth + 1});
-		if (tree[tmp.index][1] != 0) q.push(node{tree[tmp.index][1], tmp.depth + 1});
-	}
+node *build(int inl, int inr, int postl, int postr) {
+	if (inl > inr || postl > postr) return NULL;
+	node *t = new(node);
+	t->data = post[postr];
+	t->l = t->r = NULL;
+	int i = inl;
+	while (i <= inr && in[i] != post[postr]) i++;
+	t->l = build(inl, i - 1, postl, postl + i - inl - 1);
+	t->r = build(i + 1, inr, postl + i - inl, postr - 1);
+	return t;
 }
 int main() {
 	cin >> n;
-	in.resize(n + 1), post.resize(n + 1);
-	for (int i = 1; i <= n; i++) cin >> in[i];
-	for (int i = 1; i <= n; i++) cin >> post[i];
-	dfs(root, 1, n, 1, n);
-	bfs();
-	printf("%d", res[0][0]);
-	for (int i = 1; i < 35; i++)
-		if (i % 2 == 1)
-			for (int j = 0; j < res[i].size(); j++)
-				printf(" %d", res[i][j]);
+	in.resize(n), post.resize(n);
+	for (int i = 0; i < n; i++) cin >> in[i];
+	for (int i = 0; i < n; i++) cin >> post[i];
+	root = build(0, n - 1, 0, n - 1);
+	dfs(root, 0);
+	printf("%d", d[0][0]);
+	for (int i = 1; i <= dmax; i++)
+		if (i % 2 == 0)
+			for (int j = d[i].size() - 1; j >= 0; j--) printf(" %d", d[i][j]);
 		else
-			for (int j = res[i].size() - 1; j >= 0; j--)
-				printf(" %d", res[i][j]);
+			for (int j = 0; j < d[i].size(); j++) printf(" %d", d[i][j]);
 	return 0;
 }
