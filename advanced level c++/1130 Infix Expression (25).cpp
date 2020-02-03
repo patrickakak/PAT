4dfbe6f8ed834c1e2c74c25e664af7b4e199e992
vuzway9132 <13806680065@163.com>
2020-02-03 18:47:00 +0800

    -

--- advanced level c++/1130 Infix Expression (25).cpp
+++ advanced level c++/1130 Infix Expression (25).cpp
@@ -4,21 +4,21 @@ struct node {
 	string data;
 	int l, r;
 } a[100];
-string dfs(int root) {
-	if (a[root].l == -1 && a[root].r == -1) return a[root].data;
-	if (a[root].l == -1 && a[root].r != -1) return "(" +  a[root].data + dfs(a[root].r) + ")";
-	if (a[root].l != -1 && a[root].r != -1) return "(" +  dfs(a[root].l) + a[root].data + dfs(a[root].r) + ")";
+string dfs(int rt) {
+	if (a[rt].l == -1 && a[rt].r == -1) return a[rt].data;
+	if (a[rt].l == -1 && a[rt].r != -1) return "(" +  a[rt].data + dfs(a[rt].r) + ")";
+	if (a[rt].l != -1 && a[rt].r != -1) return "(" +  dfs(a[rt].l) + a[rt].data + dfs(a[rt].r) + ")";
 }
 int main() {
-	int h[100] = {0}, n, root = 1;
+	int h[100] = {0}, n, rt = 1;
 	cin >> n;
 	for (int i  = 1; i <= n; i++) {
 		cin >> a[i].data >> a[i].l >> a[i].r;
 		if (a[i].l != -1) h[a[i].l] = 1;
 		if (a[i].r != -1) h[a[i].r] = 1;
 	}
-	while (h[root] == 1) root++;
-	string ans = dfs(root);
+	while (h[rt] == 1) rt++;
+	string ans = dfs(rt);
 	if (ans[0] == '(') ans = ans.substr(1, ans.size()-2);
 	cout << ans;
 	return 0;
