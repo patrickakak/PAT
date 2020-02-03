7f2718730590e71d94d8a97296a2e9e54bba8c17
vuzway9132 <13806680065@163.com>
2020-02-03 20:22:35 +0800

    -

--- advanced level c++/1131 Subway Map (30).cpp
+++ advanced level c++/1131 Subway Map (30).cpp
@@ -3,21 +3,21 @@
 #include <unordered_map>
 using namespace std;
 vector<vector<int>> v(10000);
-int visit[10000], minCnt, minTransfer, src, dest;
+int visit[10000], minCnt, minTrans, src, dest;
 unordered_map<int, int> line;
 vector<int> path, tmpPath;
-int transferCnt(vector<int> a) {
+int transCnt(vector<int> a) {
 	int cnt = -1, preLine = 0;
 	for (int i = 1; i < a.size(); i++) {
-		if (line[a[i-1]*10000+a[i]] != preLine) cnt++;
-		preLine = line[a[i-1]*10000+a[i]];
+		if (line[a[i-1] * 10000 + a[i]] != preLine) cnt++;
+		preLine = line[a[i-1] * 10000 + a[i]];
 	}
 	return cnt;
 }
 void dfs(int node, int cnt) {
-	if (node == dest && (cnt < minCnt || (cnt == minCnt && transferCnt(tmpPath) < minTransfer))) {
+	if (node == dest && (cnt < minCnt || (cnt == minCnt && transCnt(tmpPath) < minTrans))) {
 		minCnt = cnt;
-		minTransfer = transferCnt(tmpPath);
+		minTrans = transCnt(tmpPath);
 		path = tmpPath;
 	}
 	if (node == dest) return;
@@ -39,28 +39,28 @@ int main() {
 			scanf("%d", &tmp);
 			v[pre].push_back(tmp);
 			v[tmp].push_back(pre);
-			line[pre*10000+tmp] = line[tmp*10000+pre] = i + 1;
+			line[pre * 10000 + tmp] = line[tmp * 10000 + pre] = i + 1;
 			pre = tmp;
 		}
 	}
 	scanf("%d", &k);
 	for (int i = 0; i < k; i++) {
 		scanf("%d%d", &src, &dest);
-		minCnt = 99999, minTransfer = 99999;
+		minCnt = 99999, minTrans = 99999;
 		tmpPath.clear();
 		tmpPath.push_back(src);
 		visit[src] = 1;
 		dfs(src, 0);
 		visit[src] = 0;
 		printf("%d\n", minCnt);
-		int preLine = 0, preTransfer = src;
+		int preLine = 0, preTrans = src;
 		for (int j = 1; j < path.size(); j++)
-			if (line[path[j-1]*10000+path[j]] != preLine) {
-				if (preLine != 0) printf("Take Line#%d from %04d to %04d.\n", preLine, preTransfer, path[j-1]);
-				preLine = line[path[j-1]*10000+path[j]];
-				preTransfer = path[j-1];
+			if (line[path[j-1] * 10000 + path[j]] != preLine) {
+				if (preLine != 0) printf("Take Line#%d from %04d to %04d.\n", preLine, preTrans, path[j-1]);
+				preLine = line[path[j-1] * 10000 + path[j]];
+				preTrans = path[j-1];
 			}
-		printf("Take Line#%d from %04d to %04d.\n", preLine, preTransfer, dest);
+		printf("Take Line#%d from %04d to %04d.\n", preLine, preTrans, dest);
 	}
 	return 0;
 }
