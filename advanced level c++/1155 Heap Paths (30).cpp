1492035929777652a8e125496cb97a5bf80de2ad
vuzway9132 <13806680065@163.com>
2020-02-04 10:13:02 +0800

    -

--- advanced level c++/1155 Heap Paths (30).cpp
+++ advanced level c++/1155 Heap Paths (30).cpp
@@ -2,26 +2,24 @@
 #include <vector>
 using namespace std;
 vector<int> v;
-int a[1009], n, isMin = 1, isMax = 1;
+int a[1010], n, isMin = 1, isMax = 1;
 void dfs(int index) {
-	if (index * 2 > n && index * 2 + 1 > n) {
-		if (index <= n)
-			for (int i = 0; i < v.size(); i++)
-				printf("%d%s", v[i], i != v.size() - 1 ? " " : "\n");
-	} else {
-		v.push_back(a[index * 2 + 1]);
-		dfs(index * 2 + 1);
+	if (index * 2 > n) {
+		v.push_back(a[index]);
+		for (int i = 0; i < v.size(); i++)
+			printf("%d%s", v[i], i != v.size() - 1 ? " " : "\n");
 		v.pop_back();
-		v.push_back(a[index * 2]);
-		dfs(index * 2);
-		v.pop_back();
+		return;
 	}
+	v.push_back(a[index]);
+	if (index * 2 + 1 <= n) dfs(index * 2 + 1);
+	if (index * 2 <= n) dfs(index * 2);
+	v.pop_back();
 }
 int main() {
 	cin >> n;
 	for (int i = 1; i <= n; i++)
 		scanf("%d", &a[i]);
-	v.push_back(a[1]);
 	dfs(1);
 	for (int i = 2; i <= n; i++) {
 		if (a[i/2] > a[i]) isMin = 0;
