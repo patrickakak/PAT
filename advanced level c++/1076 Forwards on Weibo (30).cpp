cdc7e7a7b46fed0f7b0aad387f65d202c7268b64
vuzway9132 <13806680065@163.com>
2020-02-04 20:40:52 +0800

    -

--- advanced level c++/1076 Forwards on Weibo (30).cpp
+++ advanced level c++/1076 Forwards on Weibo (30).cpp
@@ -19,10 +19,9 @@ int bfs(node tnode) {
 		int topid = top.id;
 		for (int i = 0; i < v[topid].size(); i++) {
 			int nextid = v[topid][i];
-			if (inq[nextid] == false && top.layer < l) {
-				node next = {nextid, top.layer + 1};
-				q.push(next);
-				inq[next.id] = true;
+			if (!inq[nextid] && top.layer < l) {
+				q.push({nextid, top.layer + 1});
+				inq[nextid] = true;
 				cnt++;
 			}
 		}
@@ -44,8 +43,7 @@ int main() {
 	int tid;
 	for (int i = 0; i < k; i++) {
 		scanf("%d", &tid);
-		node tnode = {tid, 0};
-		printf("%d\n", bfs(tnode));
+		printf("%d\n", bfs({tid, 0}));
 	}
 	return 0;
 }
