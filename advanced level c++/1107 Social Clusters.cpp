d4d92118e046f59bd1587b545bd785f00a188dbd
vuzway9132 <13806680065@163.com>
2020-02-05 13:40:58 +0800

    -

--- advanced level c++/1107 Social Clusters.cpp
+++ advanced level c++/1107 Social Clusters.cpp
@@ -1,77 +1,46 @@
-/**
- * Sample input:
- * 8
- * 3: 2 7 10
- * 1: 4
- * 2: 5 3
- * 1: 4
- * 1: 3
- * 1: 4
- * 4: 6 8 1 5
- * 1: 4
- * i--------------
- * Sample output:
- * 3
- * 4 3 1
- */
-#include <cstdio>
+#include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
-
-const int maxn = 1010;
-int S[maxn], hobby[maxn];
-
-void init()
-{
-	for (int i = 0; i < maxn; i++) {
-		hobby[i] = -1;
-		S[i] = -1;
+vector<int> father, isRoot;
+int cmp1(int a, int b){return a > b;}
+int findFather(int x) {
+	int a = x;
+	while (x != father[x]) x = father[x];
+	while (a != father[a]) {
+		int z = a;
+		a = father[a];
+		father[z] = x;
 	}
+	return x;
 }
-
-int Find(int x)
-{
-	if (S[x] < 0) return x;
-	else return S[x] = Find(S[x]);
+void Union(int a, int b) {
+	int faA = findFather(a);
+	int faB = findFather(b);
+	if (faA != faB) father[faA] = faB;
 }
-
-void Union(int Root1, int Root2)
-{
-	if (S[Root2] < S[Root1]) {
-		S[Root2] += S[Root1];
-		S[Root1] = Root2;
-	} else {
-		S[Root1] += S[Root2];
-		S[Root2] = Root1;
-	}
-}
-
-int main()
-{
-	// freopen("tst.txt", "r", stdin);
-	int n, k, h, num = 0;
-
-	init();
+int main() {
+	int n, k, t, cnt = 0;
+	int course[1001] = {0};
 	scanf("%d", &n);
+	father.resize(n + 1), isRoot.resize(n + 1);
+	for (int i = 1; i <= n; i++) father[i] = i;
 	for (int i = 1; i <= n; i++) {
 		scanf("%d:", &k);
 		for (int j = 0; j < k; j++) {
-			scanf("%d", &h);
-			if (hobby[h] == -1) hobby[h] = i;
-			else Union(Find(i), Find(hobby[h]));
+			scanf("%d", &t);
+			if (course[t] == 0) course[t] = i;
+			Union(i, findFather(course[t]));
 		}
 	}
+	for (int i = 1; i <= n; i++) isRoot[findFather(i)]++;
 	for (int i = 1; i <= n; i++)
-		if (S[i] < 0) num++;
-	sort(S + 1, S + n + 1);
-	printf("%d\n", num);
-	for (int i = 1; i <= num; i++) {
-		printf("%d", -S[i]);
-		if (i < num) printf(" ");
+		if (isRoot[i] != 0) cnt++;
+	printf("%d\n", cnt);
+	sort(isRoot.begin(), isRoot.end(), cmp1);
+	for (int i = 0; i < cnt; i++) {
+		printf("%d", isRoot[i]);
+		if (i != cnt - 1) printf(" ");
 	}
-
 	return 0;
 }
-
-
-
