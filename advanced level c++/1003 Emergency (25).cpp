e36c5ccc8ab2fa89badc9726bbca60c6a83a8be7
vuzway9132 <13806680065@163.com>
2020-02-06 17:38:16 +0800

    -

--- advanced level c++/1003 Emergency (25).cpp
+++ advanced level c++/1003 Emergency (25).cpp
@@ -1,28 +1,27 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int n, m, c1, c2;
+int n, m, s, d;
 int e[510][510], weight[510], dis[510], num[510], w[510];
 bool visit[510];
 const int inf = 0x3fffffff;
 int main() {
-	scanf("%d%d%d%d", &n, &m, &c1, &c2);
-	for (int i = 0; i < n; i++)
-		scanf("%d", &weight[i]);
+	scanf("%d%d%d%d", &n, &m, &s, &d);
+	for (int i = 0; i < n; i++) scanf("%d", &weight[i]);
 	fill(e[0], e[0] + 510 * 510, inf);
 	fill(dis, dis + 510, inf);
-	int a, b, c;
+	int c1, c2, l;
 	for (int i = 0; i < m; i++) {
-		scanf("%d%d%d", &a, &b, &c);
-		e[a][b] = e[b][a] = c;
+		scanf("%d%d%d", &c1, &c2, &l);
+		e[c1][c2] = e[c2][c1] = l;
 	}
-	dis[c1] = 0;
-	w[c1] = weight[c1];
-	num[c1] = 1;
+	dis[s] = 0;
+	w[s] = weight[s];
+	num[s] = 1;
 	for (int i = 0; i < n; i++) {
 		int u = -1, minn = inf;
 		for (int j = 0; j < n; j++)
-			if (visit[j] == false && dis[j] < minn) {
+			if (!visit[j] && dis[j] < minn) {
 				u = j;
 				minn = dis[j];
 			}
@@ -35,11 +34,11 @@ int main() {
 					num[v] = num[u];
 					w[v] = w[u] + weight[v];
 				} else if (dis[u] + e[u][v] == dis[v]) {
-					num[v] = num[v] + num[u];
+					num[v] += num[u];
 					if (w[u] + weight[v] > w[v]) w[v] = w[u] + weight[v];
 				}
 			}
 	}
-	printf("%d %d", num[c2], w[c2]);
+	printf("%d %d", num[d], w[d]);
 	return 0;
 }
