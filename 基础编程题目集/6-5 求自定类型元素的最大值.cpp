7bc53b88e2fea9984f2c94039361e9bc3bf0ec32
ziwei woo <13806680065@163.com>
2020-02-09 21:07:46 +0800

    -

--- 基础编程题目集/6-5 求自定类型元素的最大值.cpp
+++ 基础编程题目集/6-5 求自定类型元素的最大值.cpp
@@ -1,11 +1,6 @@
-ElementType Max(ElementType S[], int N)
-{
-	int i, MaxIndex = 0;
-	
-	for (i = 0; i < N; i++)
-		if (S[i] > S[MaxIndex])
-			MaxIndex = i;
-			
+ElementType Max(ElementType S[], int N) {
+	int MaxIndex = 0;
+	for (int i = 0; i < N; i++)
+		if (S[i] > S[MaxIndex]) MaxIndex = i;
 	return S[MaxIndex];
 }
-
