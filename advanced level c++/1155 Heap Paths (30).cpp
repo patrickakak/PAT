d927a66e88913aa8701b61494879496850b25649
vuzway9132 <13806680065@163.com>
2020-02-10 10:14:27 +0800

    -

--- advanced level c++/1155 Heap Paths (30).cpp
+++ advanced level c++/1155 Heap Paths (30).cpp
@@ -4,22 +4,20 @@ using namespace std;
 vector<int> v;
 int a[1010], n, isMin = 1, isMax = 1;
 void dfs(int index) {
-	if (index * 2 > n) {
-		v.push_back(a[index]);
+	v.push_back(a[index]);
+	if (index * 2 > n) {	
 		for (int i = 0; i < v.size(); i++)
 			printf("%d%s", v[i], i != v.size() - 1 ? " " : "\n");
 		v.pop_back();
 		return;
 	}
-	v.push_back(a[index]);
 	if (index * 2 + 1 <= n) dfs(index * 2 + 1);
 	if (index * 2 <= n) dfs(index * 2);
 	v.pop_back();
 }
 int main() {
 	cin >> n;
-	for (int i = 1; i <= n; i++)
-		scanf("%d", &a[i]);
+	for (int i = 1; i <= n; i++) scanf("%d", &a[i]);
 	dfs(1);
 	for (int i = 2; i <= n; i++) {
 		if (a[i/2] > a[i]) isMin = 0;
