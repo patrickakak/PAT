b8141e0a8ff94ae309b744ca8afc9ba0134ca3af
vuzway9132 <13806680065@163.com>
2020-02-10 21:40:52 +0800

    -

--- 基础编程题目集/7-21 求特殊方程的正整数解.cpp
+++ 基础编程题目集/7-21 求特殊方程的正整数解.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int n;
+	int n, cnt = 0;
 	scanf("%d", &n);
-	int cnt = 0;
 	for (int x = 1; 2*x*x <= n; x++)
 		for (int y = x; x*x + y*y <= n; y++)
 			if (x*x + y*y == n) {
 				printf("%d %d\n", x, y);
 				cnt++;
 			}
-	if (cnt==0) printf("No Solution");
+	if (cnt == 0) printf("No Solution");
 	return 0;
 }
