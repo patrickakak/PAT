d276d16cccfa82793bde849eb6d0cb609b2c3a18
vuzway9132 <13806680065@163.com>
2020-02-10 21:22:49 +0800

    -

--- 基础编程题目集/7-12 两个数的简单计算器.cpp
+++ 基础编程题目集/7-12 两个数的简单计算器.cpp
@@ -10,7 +10,7 @@ int main() {
 	case '*': printf("%d", a*b); break;
 	case '/': printf("%d", a/b); break;
 	case '%': printf("%d", a%b); break;
-	default: printf("ERROR");
+	default : printf("ERROR");
 	}
 	return 0;
 }
