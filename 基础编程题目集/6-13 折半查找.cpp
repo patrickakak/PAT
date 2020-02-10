524ecdc711e188192bd132bd01f4c5851d0f90d5
ziwei woo <13806680065@163.com>
2020-02-10 11:51:02 +0800

    -

--- 基础编程题目集/6-13 折半查找.cpp
+++ 基础编程题目集/6-13 折半查找.cpp
@@ -1,10 +1,10 @@
-int binSearch(SeqList T, KeyType k) {
-	int mid, low = 1, high = T.len;
+int Search_Bin(SSTable T, KeyType k) {
+	int mid, low = 1, high = T.length;
 	while (low <= high) {
 		mid = (low + high) / 2;
-		if (T.data[mid].key == k) return mid;
-		else if (T.data[mid].key > k) high = mid - 1;
+		if (T.R[mid].key == k) return mid;
+		else if (T.R[mid].key > k) high = mid - 1;
 		else low = mid + 1;
 	}
-	return index;
+	return 0;
 }
