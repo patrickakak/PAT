e1ca8670e70c206e01e7d4e67afb18412b208911
ziwei woo <13806680065@163.com>
2020-02-10 13:25:19 +0800

    -

--- 基础编程题目集/7-14 求整数段和.cpp
+++ 基础编程题目集/7-14 求整数段和.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int a, b, sum = 0;
-	scanf("%d%d", &a, &b);
-	for (int i = a, j = 1; i <= b; i++, j++) {
+	int a, b, i, cot = 0, sum = 0;
+	scanf("%d %d", &a, &b);
+	for (i = a; i <= b; i++) {
 		printf("%5d", i);
-		sum += i;
-		if (j % 5 == 0) printf("\n");
+		cot++;
+		if (cot % 5 == 0 && i != b) printf("\n");
 	}
-	j--;
-	if (j % 5 != 0) printf("\n");
+	printf("\n");
+	for(i = a; i <= b; i++) sum += i;
 	printf("Sum = %d", sum);
 	return 0;
 }
