2ed3982d398fadc272f633bda6336346247b16b4
vuzway9132 <13806680065@163.com>
2020-02-10 20:29:51 +0800

    -

--- 基础编程题目集/6-9 统计个位数字.c
+++ 基础编程题目集/6-9 统计个位数字.c
@@ -1,11 +1,10 @@
 int Count_Digit(const int N, const int D) {
-	int temp, count;
-	temp = N < 0 ? -N : N;
-	if (N == 0 && D == 0)  return 1;
-	count = 0;
-	while (temp) {
-		if (temp%10 == D) count++;
-		temp /= 10;
+	int tmp = N < 0 ? -N : N;
+	if (N == 0 && D == 0) return 1;
+	int count = 0;
+	while (tmp) {
+		if (tmp % 10 == D) count++;
+		tmp /= 10;
 	}
 	return count;
 }
