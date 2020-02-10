b1c3b9222ca5285b8b3d0199fcb517011f5a1248
vuzway9132 <13806680065@163.com>
2020-02-10 21:45:10 +0800

    -

--- 基础编程题目集/7-23 币值转换.cpp
+++ 基础编程题目集/7-23 币值转换.cpp
@@ -1,33 +1,31 @@
 #include <iostream>
 using namespace std;
-char num[10] = {'a','b','c','d','e','f','g','h','i','j'};
-char unit[3] = {'S','B','Q'};
-char wei[2] = {'W','Y'};
+char a[] = {'a','b','c','d','e','f','g','h','i','j'};
+char b[] = {'S','B','Q'};
+char c[] = {'W','Y'};
 int main() {
 	string s;
 	cin >> s;
-	int len = s.length();
-	int l = 0, r = len-1;
+	int len = s.length(), l = 0, r = len - 1, zcnt = 0;
 	while (r - 4 >= 0) r -= 4;
 	bool flag = false;
-	int zcnt = 0;
 	while (l < len) {
 		while (l <= r) {
-			int tmp = s[l]-'0';
+			int tmp = s[l] - '0';
 			if (tmp != 0 && flag == true) {
-				cout << num[0];
+				cout << a[0];
 				flag = false;
 			}
 			if (l > 0 && tmp == 0) {
 				zcnt++;
 				flag = true;
-			} else cout << num[tmp];
-			if (l != r && tmp != 0) cout << unit[r-l-1];
+			} else cout << a[tmp];
+			if (l != r && tmp != 0) cout << b[r-l-1];
 			l++;
 		}
 		if (r != len - 1) {
-			if (zcnt==4 && r==len-5) ;
-			else cout << wei[(len-r)/4-1];
+			if (zcnt == 4 && r == len - 5) ;
+			else cout << c[(len-r)/4 - 1];
 		}
 		r += 4;
 	}
