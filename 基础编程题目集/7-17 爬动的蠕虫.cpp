0691033174ca7c42f6b576082e9a3820ffbd8428
vuzway9132 <13806680065@163.com>
2020-02-10 21:35:42 +0800

    -

--- 基础编程题目集/7-17 爬动的蠕虫.cpp
+++ 基础编程题目集/7-17 爬动的蠕虫.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int n, u, d;
+	int n, u, d, min = 0, flag = 0, ht = 0;
 	scanf("%d%d%d", &n, &u, &d);
-	int min = 0;
-	int flag = 0;
-	int ht = 0;
 	while (ht < n) {
 		if (flag == 0) {
 			ht += u;
