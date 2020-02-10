2cfb90efa1b0b7878b510daa2d67d3ccaa3b0342
vuzway9132 <13806680065@163.com>
2020-02-10 20:58:49 +0800

    -

--- 基础编程题目集/6-10 阶乘计算升级版.c
+++ 基础编程题目集/6-10 阶乘计算升级版.c
@@ -1,20 +1,20 @@
 #include <string.h>
 typedef struct LargeNum {
 	int len, d[10010];
-} largeNum;
-void print(largeNum n) {
+} large;
+void print(large n) {
 	for (int i = n.len - 1; i >= 0; i--)
 		printf("%d", n.d[i]);
 }
-largeNum change(char s[]) {
-	largeNum r;
+large change(char s[]) {
+	large r;
 	r.len = strlen(s);
 	for (int i = r.len-1, j = 0; i >= 0; i--, j++)
 		r.d[j] = s[i] - '0';
 	return r;
 }
-largeNum multi(largeNum a, int b) {
-	largeNum r;
+large mult(large a, int b) {
+	large r;
 	r.len = 0;
 	int carry = 0;
 	for (int i = 0; i < a.len; i++) {
@@ -31,8 +31,8 @@ largeNum multi(largeNum a, int b) {
 void Print_Factorial(const int N) {
 	if (N < 0) printf("Invalid input\n");
 	else {
-		largeNum c = change("1");
-		for (int i = 1; i <= N; i++) c = multi(c, i);
+		large c = change("1");
+		for (int i = 1; i <= N; i++) c = mult(c, i);
 		print(c);
 	}
 }
