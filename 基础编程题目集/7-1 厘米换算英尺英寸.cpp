bc64c8b29ddf28e1ba7e6dfab1c04b7275d59f30
vuzway9132 <13806680065@163.com>
2020-02-10 21:13:27 +0800

    -

--- 基础编程题目集/7-1 厘米换算英尺英寸.cpp
+++ 基础编程题目集/7-1 厘米换算英尺英寸.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 using namespace std;
 int main() {
-	double centimeters, inches;
-	int foot;
-	scanf("%lf", &centimeters);
-	foot = (int)centimeters / 30.48;
-	inches = (centimeters/30.48 - foot)*12;
-	printf("%d %d\n", foot, (int)inches);
+	double cm;
+	scanf("%lf", &cm);
+	int foot = (int)cm / 30.48;
+	double inch = (cm / 30.48 - foot) * 12;
+	printf("%d %d\n", foot, (int)inch);
 	return 0;
 }
