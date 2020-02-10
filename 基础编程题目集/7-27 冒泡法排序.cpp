e7b0dabe1af7a734ef8c58d451c77c30aff58f48
vuzway9132 <13806680065@163.com>
2020-02-10 21:50:44 +0800

    -

--- 基础编程题目集/7-27 冒泡法排序.cpp
+++ 基础编程题目集/7-27 冒泡法排序.cpp
@@ -4,12 +4,10 @@ using namespace std;
 int main() {
 	int n, k, a[105];
 	scanf("%d%d", &n, &k);
-	for (int i = 0; i < n; i++)
-		scanf("%d", &a[i]);
+	for (int i = 0; i < n; i++) scanf("%d", &a[i]);
 	for (int i = 0; i < k; i++)
 		for (int j = 0; j < n-i-1; j++)
-			if (a[j+1] < a[j])
-				swap(a[j], a[j+1]);
+			if (a[j+1] < a[j]) swap(a[j], a[j+1]);
 	for (int i = 0; i < n; i++) {
 		if (i > 0) printf(" ");
 		printf("%d", a[i]);
