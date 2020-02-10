55c432917c963601dfe35da732aad3f8697c4ce2
vuzway9132 <13806680065@163.com>
2020-02-10 21:52:17 +0800

    -

--- 基础编程题目集/7-28 猴子选大王.cpp
+++ 基础编程题目集/7-28 猴子选大王.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 using namespace std;
-bool hashTbl[1005] = {false};
+bool hit[1005];
 int main() {
 	int n;
 	scanf("%d", &n);
 	int left = n, cnt = 0, flag = 0;
 	while (1) {
 		for (int i = 1; i <= n; i++) {
-			if (hashTbl[i] == true) continue;
-			cnt++;
-			if (cnt == 3) {
+			if (hit[i] == true) continue;
+			if (++cnt == 3) {
 				left--;
 				cnt = 0;
-				hashTbl[i] = true;
+				hit[i] = true;
 			}
 			if (left == 1) {
 				flag = 1;
@@ -22,7 +21,7 @@ int main() {
 		if (flag == 1) break;
 	}
 	for (int i = 1; i <= n; i++)
-		if (hashTbl[i] == false) {
+		if (hit[i] == false) {
 			printf("%d", i);
 			break;
 		}
