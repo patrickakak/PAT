4c31ec36e6d3e25e1e09fe7a9b0d2b01600a42db
vuzway9132 <13806680065@163.com>
2020-02-10 20:22:41 +0800

    -

--- 基础编程题目集/6-7 统计某类完全平方数.c
+++ 基础编程题目集/6-7 统计某类完全平方数.c
@@ -1,13 +1,10 @@
-#include <math.h>
 int IsTheNumber(const int N) {
-	int yes = 0, temp, i;
-	int arr[10] = {0};
-	temp = N;
-	while (temp) {
-		arr[temp%10]++;
-		temp /= 10;
+	int yes = 0, tmp = N, arr[10] = {0};
+	while (tmp) {
+		arr[tmp % 10]++;
+		tmp /= 10;
 	}
-	for (i = 0; i < 10; i++)
+	for (int i = 0; i < 10; i++)
 		if (arr[i] > 1 && sqrt(N) == (int)sqrt(N)) {
 			yes = 1;
 			break;
