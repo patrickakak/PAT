6e59a8e7248472eece7c6e6b7e0853102c99e0e0
vuzway9132 <13806680065@163.com>
2020-02-10 22:00:18 +0800

    -

--- 基础编程题目集/7-34 通讯录的录入与显示.cpp
+++ 基础编程题目集/7-34 通讯录的录入与显示.cpp
@@ -2,23 +2,20 @@
 #include <algorithm>
 using namespace std;
 struct contacts {
-	string name, birth, gender, fixed, mobile;
+	string nm, bir, sex, fix, mbl;
 } c[15];
 int main() {
 	int n, k, id;
 	cin >> n;
-	for (int i=0; i<n; i++) {
-		cin >> c[i].name >> c[i].birth >> c[i].gender;
-		cin >> c[i].fixed >> c[i].mobile;
+	for (int i = 0; i < n; i++) {
+		cin >> c[i].nm >> c[i].bir >> c[i].sex;
+		cin >> c[i].fix >> c[i].mbl;
 	}
 	cin >> k;
 	for (int i = 0; i < k; i++) {
 		cin >> id;
 		if (id >= n || id < 0) cout << "Not Found" << endl;
-		else {
-			cout << c[id].name << " " << c[id].fixed << " " << c[id].mobile;
-			cout << " " << c[id].gender << " " << c[id].birth << endl;
-		}
+		else cout << c[id].nm << " " << c[id].fix << " " << c[id].mbl << " " << c[id].sex << " " << c[id].bir << endl;
 	}
 	return 0;
 }
