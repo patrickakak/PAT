07fcaf1e7ae6cbac0502e5da107251b53ecc139e
vuzway9132 <13806680065@163.com>
2020-02-11 17:23:21 +0800

    -

--- 数据结构与算法题目集/7-2 一元多项式的乘法与加法运算 (20分).cpp
+++ 数据结构与算法题目集/7-2 一元多项式的乘法与加法运算 (20分).cpp
@@ -1,13 +1,5 @@
-/* Sample input:
- * 4 3 4 -5 2  6 1  -2 0
- * 3 5 20  -7 4  3 1
- * Sample output:
- * 15 24 -25 22 30 21 -10 20 -21 8 35 6 -33 5 14 4 -15 3 18 2 -6 1
- * 5 20 -4 4 -5 2 9 1 -2 0
- */
 #include <cstdio>
 #include <cstdlib>
-
 typedef struct Node *PtrToNode;
 struct Node {
 	int coef;
@@ -15,63 +7,24 @@ struct Node {
 	PtrToNode link;
 };
 typedef PtrToNode Polynomial;
-
-/* Assistant functions */
-int Compare(int a, int b);
-PtrToNode MakeNode(int c, int e);
-void Attach(int c, int e, PtrToNode *pRear);
-
-/* Main feature functions */
-Polynomial Add(Polynomial P1, Polynomial P2);
-Polynomial Mult(Polynomial P1, Polynomial P2);
-void PrintPoly(Polynomial P);
-Polynomial ReadPoly();
-
-int main()
-{
-	Polynomial P1, P2, PP, PS; 	/* Product and sum of P1 and P2 */
-
-	P1 = ReadPoly();
-	P2 = ReadPoly();
-	PP = Mult(P1, P2);
-	PS = Add(P1, P2);
-	PrintPoly(PP);
-	PrintPoly(PS);
-
-	return 0;
-}
-
-int Compare(int a, int b)
-{
+int Compare(int a, int b) {
 	return a > b ? 1 : a == b ? 0 : -1;
 }
-
-PtrToNode MakeNode(int c, int e)
-{
-	PtrToNode P;
-	
-	P = (PtrToNode) malloc(sizeof(struct Node));
+PtrToNode MakeNode(int c, int e) {
+	PtrToNode P = (PtrToNode) malloc(sizeof(struct Node));
 	P->coef = c;
 	P->exp = e;
 	P->link = NULL;
 	return P;
 }
-
-void Attach(int c, int e, PtrToNode *pRear)
-{
-	PtrToNode P;
-
-	P = MakeNode(c, e);
+void Attach(int c, int e, PtrToNode *pRear) {
+	PtrToNode P = MakeNode(c, e);
 	(*pRear)->link = P;
 	*pRear = P;
 }
-
-Polynomial Add(Polynomial P1, Polynomial P2)
-{
+Polynomial Add(Polynomial P1, Polynomial P2) {
 	Polynomial P, Rear, Tmp;
 	int sum;
-	
-	/* Using a dummy header for convenience */
 	P = (Polynomial) malloc(sizeof(struct Node)); P->link = NULL;
 	Rear = P;
 	while (P1 && P2)
@@ -91,46 +44,32 @@ Polynomial Add(Polynomial P1, Polynomial P2)
 			P2 = P2->link;
 			break;
 		}
-
-	/* Handle the rest of the list which is not empty */
 	for (; P1; P1 = P1->link) Attach(P1->coef, P1->exp, &Rear);
 	for (; P2; P2 = P2->link) Attach(P2->coef, P2->exp, &Rear);
-
 	Tmp = P; P = P->link; free(Tmp);
 	return P;
 }
-
-Polynomial Mult(Polynomial P1, Polynomial P2)
-{
+Polynomial Mult(Polynomial P1, Polynomial P2) {
 	Polynomial P;
 	PtrToNode Rear, t1, t2, Tmp;
 	int c, e;
-
-	/* If one of the polynomial list is empty */
 	if (!P1 || !P2) return NULL;
-
 	t1 = P1; t2 = P2;
 	P = (Polynomial) malloc(sizeof(struct Node)); P->link = NULL;
 	Rear = P;
-	/* Create a polynomial list firstly */
 	while (t2) {
 		Attach(t1->coef*t2->coef, t1->exp+t2->exp, &Rear);
 		t2 = t2->link;
 	}
 	t1 = t1->link;
-
-	/* Add new items into polynomial P created beforehand */
 	while (t1) {
 		t2 = P2; Rear = P;
 		while (t2) {
 			c = t1->coef * t2->coef;
 			e = t1->exp + t2->exp;
-
-			/* Find the position to insert into */
 			while (Rear->link && Rear->link->exp > e)
 				Rear = Rear->link;
 			if (Rear->link && Rear->link->exp == e) {
-				/* Coefficient sum is not 0 */
 				if (Rear->link->coef + c)
 					Rear->link->coef += c;
 				else {
@@ -144,33 +83,24 @@ Polynomial Mult(Polynomial P1, Polynomial P2)
 		}
 		t1 = t1->link;
 	}
-
 	Tmp = P; P = P->link; free(Tmp);
 	return P;
 }
-
-void PrintPoly(Polynomial P)
-{
+void PrintPoly(Polynomial P) {
 	PtrToNode T = P;
 	int flag = 0;
-
 	if (!P) { printf("0 0\n"); return; }
-
 	while (T) {
 		if (!flag) flag = 1;
 		else printf(" ");
-
 		printf("%d %d", T->coef, T->exp);
 		T = T->link;
 	}
 	putchar('\n');
 }
-
-Polynomial ReadPoly()
-{
+Polynomial ReadPoly() {
 	int N, c, e;
 	Polynomial P, Rear, t;
-
 	scanf("%d", &N);
 	P = (Polynomial) malloc(sizeof(struct Node)); P->link = NULL;
 	Rear = P;
@@ -178,8 +108,16 @@ Polynomial ReadPoly()
 		scanf("%d %d", &c, &e);
 		Attach(c, e, &Rear);
 	}
-
 	t = P; P = P->link; free(t);
 	return P;
 }
-
+int main() {
+	Polynomial P1, P2, PP, PS;
+	P1 = ReadPoly();
+	P2 = ReadPoly();
+	PP = Mult(P1, P2);
+	PS = Add(P1, P2);
+	PrintPoly(PP);
+	PrintPoly(PS);
+	return 0;
+}
