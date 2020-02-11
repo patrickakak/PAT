ce2998e5a7c317508ddb4431615643eb3163d315
vuzway9132 <13806680065@163.com>
2020-02-11 12:49:06 +0800

    -

--- 基础编程题目集/7-37 整数分解为若干项之和 (20分).cpp
+++ 基础编程题目集/7-37 整数分解为若干项之和 (20分).cpp
@@ -2,30 +2,29 @@
 #include <vector>
 using namespace std;
 vector<int> v;
-int sum = 0, cnt = 0;
-void factorization(int i, int n) {
+int n, sum = 0, cnt = 0;
+void factorization(int i) {
 	if (sum > n) return;
 	if (sum == n) {
-		cout << n << "=";
+		printf("%d=", n);
 		for (int i = 0; i < v.size(); i++) {
 			if (i != 0) printf("+");
 			printf("%d", v[i]);
 		}
-		if (++cnt % 4 == 0 || v[v.size()-1] == n) cout << endl;
-		else cout << ";";
+		if (++cnt % 4 == 0 || v[v.size()-1] == n) printf("\n");
+		else printf(";");
 		return;
 	}
 	for (int j = i; j <= n; j++) {
 		sum += j;
 		v.push_back(j);
-		factorization(j, n);
+		factorization(j);
 		sum -= j;
 		v.pop_back();
 	}
 }
 int main() {
-	int n;
-	cin >> n;
-	factorization(1, n);
+	scanf("%d", &n);
+	factorization(1);
 	return 0;
 }
