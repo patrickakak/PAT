bb72563890df0d38e757cf12fda600e125cef137
vuzway9132 <13806680065@163.com>
2020-02-11 14:39:27 +0800

    -

--- data structure and algorithms/7-1 Maximum Subsequence Sum (25).cpp
+++ data structure and algorithms/7-1 Maximum Subsequence Sum (25).cpp
@@ -1,35 +1,23 @@
 #include <iostream>
 using namespace std;
-typedef struct OutNode *PtrToOutNode;
-struct OutNode {
-	int first, last, MaxSum;
-};
 int a[100000];
-PtrToOutNode MaxSubseqSum4(int A[], int N) {
-	int ThisSum = 0, MaxSum = -1, Diff = 0;
-	PtrToOutNode out = new struct OutNode;
-	out->first = out->last = out->MaxSum = 0;
-	for (int i = 0; i < N; i++) {
-		ThisSum += A[i];
-		if (ThisSum > MaxSum) {
-			MaxSum = ThisSum;
-			out->last = i;
-			out->first = out->last - Diff;
-			Diff++;
-		} else if (ThisSum < 0) {
-			ThisSum = 0, Diff = 0;
-		} else Diff++;
-	}
-	out->MaxSum = MaxSum;
-	return out;
-}
 int main() {
-	int K;
-	PtrToOutNode out;
-	scanf("%d", &K);
-	for (int i = 0; i < K; i++) scanf("%d", &a[i]);
-	out = MaxSubseqSum4(a, K);
-	if (out->MaxSum >= 0) printf("%d %d %d\n", out->MaxSum, a[out->first], a[out->last]);
-	else printf("%d %d %d\n", 0, a[0], a[K-1]);
+	int k, tmpSum = 0, maxSum = -1, first, last, tmp = 0;
+	cin >> k;
+	for (int i = 0; i < k; i++) {
+		scanf("%d", &a[i]);
+		tmpSum += a[i];
+		if (tmpSum > maxSum) {
+			maxSum = tmpSum;
+			last = i;
+		} else if (tmpSum < 0)
+			tmpSum = 0;
+	}
+	for (int i = last; i >= 0; i--) {
+		tmp += a[i];
+		if (tmp == maxSum) first = i;
+	}
+	if (maxSum == -1) printf("0 %d %d", a[0], a[k-1]);
+	else printf("%d %d %d", maxSum, a[first], a[last]);
 	return 0;
 }
