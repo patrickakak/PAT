a4d5e6992ac0c15e4ac9254f9c3712a14657f934
vuzway9132 <13806680065@163.com>
2020-02-11 12:47:15 +0800

    -

--- 基础编程题目集/7-37 整数分解为若干项之和 (20分).cpp
+++ 基础编程题目集/7-37 整数分解为若干项之和 (20分).cpp
@@ -6,13 +6,12 @@ int sum = 0, cnt = 0;
 void factorization(int i, int n) {
 	if (sum > n) return;
 	if (sum == n) {
-		cnt++;
 		cout << n << "=";
 		for (int i = 0; i < v.size(); i++) {
 			if (i != 0) printf("+");
 			printf("%d", v[i]);
 		}
-		if (cnt % 4 == 0 || v[v.size()-1] == n) cout << endl;
+		if (++cnt % 4 == 0 || v[v.size()-1] == n) cout << endl;
 		else cout << ";";
 		return;
 	}
