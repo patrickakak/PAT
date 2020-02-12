4f61ac3feca2a0c323d4c9238aa5041709d392e9
vuzway9132 <13806680065@163.com>
2020-02-12 18:55:27 +0800

    -

---
+++ data structure and algorithms/7-7 Complete Binary Search Tree (30).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int n, cnt = 0;
+vector<int> in, cbt;
+void inOrder(int index) {
+	if (index >= n) return;
+	inOrder(2 * index + 1);
+	cbt[index] = in[cnt++];
+	inOrder(2 * index + 2);
+}
+int main() {
+	cin >> n;
+	in.resize(n), cbt.resize(n);
+	for (int i = 0; i < n; i++) scanf("%d", &in[i]);
+	sort(in.begin(), in.end());
+	inOrder(0);
+	for (int i = 0; i < n; i++) {
+		if (i != 0) printf(" ");
+		printf("%d", cbt[i]);
+	}
+	return 0;
+}
