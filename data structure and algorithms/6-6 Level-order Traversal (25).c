151a9ca90a09249e86b75b5e9a0c49b937aeeb99
vuzway9132 <13806680065@163.com>
2020-02-12 15:16:17 +0800

    -

--- data structure and algorithms/6-6 Level-order Traversal (25).c
+++ data structure and algorithms/6-6 Level-order Traversal (25).c
@@ -1,15 +1,3 @@
-/**
- *       3
- *      / \
- *     5   6
- *    / \   \
- *   1   8  10
- *      /
- *     9
- * ---------------------------
- * Sample Output:
- * Level-order: 3 5 6 1 8 10 9
- */
 #include <stdbool.h>
 #define ERROR NULL
 typedef Tree DataType;
@@ -21,66 +9,43 @@ struct QNode {
 	Position Front, Rear;
 };
 typedef PtrToQNode Queue;
-
-Queue CreateQueue(int QSize)
-{
-	Queue Q;
-
-	Q = (Queue) malloc(sizeof(*Q));
+Queue CreateQueue(int QSize) {
+	Queue Q = (Queue) malloc(sizeof(*Q));
 	Q->Data = (DataType *) malloc(QSize * sizeof(DataType));
 	Q->Front = Q->Rear = 0;
 	Q->Capacity = QSize;
 	return Q;
 }
-
-void DestroyQueue(Queue Q)
-{
+void DestroyQueue(Queue Q) {
 	free(Q->Data);
 	free(Q);
 }
-
-bool IsFullQ(Queue Q)
-{
+bool IsFullQ(Queue Q) {
 	return (Q->Rear+1)%Q->Capacity == Q->Front;
 }
-
-bool IsEmptyQ(Queue Q)
-{
+bool IsEmptyQ(Queue Q) {
 	return Q->Front == Q->Rear;
 }
-
-bool Enqueue(Queue Q, DataType X)
-{
+bool Enqueue(Queue Q, DataType X) {
 	if (IsFullQ(Q)) return false;
-
 	Q->Rear = (Q->Rear+1)%Q->Capacity;
 	Q->Data[Q->Rear] = X;
 	return true;
 }
-
-DataType Dequeue(Queue Q)
-{
+DataType Dequeue(Queue Q) {
 	if (IsEmptyQ(Q)) return ERROR;
 	Q->Front = (Q->Front+1)%Q->Capacity;
 	return Q->Data[Q->Front];
 }
-
-void Level_order(Tree T, void (*visit)(Tree ThisNode))
-{
-	Queue Q;
-	DataType X;
-
+void Level_order(Tree T, void (*visit)(Tree ThisNode)) {
 	if (!T) return;
-	Q = CreateQueue(MaxTree);
+	Queue Q = CreateQueue(MaxTree);
 	Enqueue(Q, T);
 	while (!IsEmptyQ(Q)) {
-		X = Dequeue(Q);
+		DataType X = Dequeue(Q);
 		(*visit)(X);
-		if (X->Left)
-			Enqueue(Q, X->Left);
-		if (X->Right)
-			Enqueue(Q, X->Right);
+		if (X->Left) Enqueue(Q, X->Left);
+		if (X->Right) Enqueue(Q, X->Right);
 	}
 	DestroyQueue(Q);
 }
-
