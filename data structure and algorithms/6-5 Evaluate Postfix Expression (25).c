36be334f8de2b2852c57b84e1c2cf3bb7bfe0e5a
vuzway9132 <13806680065@163.com>
2020-02-12 15:14:04 +0800

    -

--- data structure and algorithms/6-5 Evaluate Postfix Expression (25).c
+++ data structure and algorithms/6-5 Evaluate Postfix Expression (25).c
@@ -1,19 +1,3 @@
-/**
- * Sample Input1:
- * 11 -2 5.5 * + 23 7 / -
- * Sample Output1:
- * -3.285714
- * -----------------------
- * Sample Input2:
- * 11 -2 5.5 * + 23 0 / -
- * Sample Output2:
- * ERROR
- * -----------------------
- * Sample Input3:
- * 11 -2 5.5 * + 23 7 / - *
- * Sample Output3:
- * ERROR
- */
 #include <stdbool.h>
 #include <ctype.h>
 #include <string.h>
@@ -26,87 +10,55 @@ struct StackRecord {
 	ElementType Data[Max_Expr];
 	int Top;
 };
-
-Stack CreateStack()
-{
-	Stack S;
-
-	S = (Stack) malloc(sizeof(struct StackRecord));
+Stack CreateStack() {
+	Stack S = (Stack) malloc(sizeof(struct StackRecord));
 	if (!S) return ERROR;
 	S->Top = -1;
 	return S;
 }
-
-void DestroyStack(Stack S)
-{
+void DestroyStack(Stack S) {
 	free(S);
 }
-
-bool IsFullS(Stack S)
-{
+bool IsFullS(Stack S) {
 	return S->Top == Max_Expr-1;
 }
-
-bool Push(Stack S, ElementType X)
-{
+bool Push(Stack S, ElementType X) {
 	if (IsFullS(S)) return false;
 	S->Data[++S->Top] = X;
 	return true;
 }
-
-bool IsEmptyS(Stack S)
-{
+bool IsEmptyS(Stack S) {
 	return S->Top == -1;
 }
-
-ElementType Pop(Stack S)
-{
+ElementType Pop(Stack S) {
 	if (IsEmptyS(S)) return err;
 	return S->Data[S->Top--];
 }
-
-type GetOperandOrOperator(char *expr, char s[Max_Expr])
-{
+type GetOperandOrOperator(char *expr, char s[Max_Expr]) {
 	int i;
 	type typ;
-
-	for (i = 0; *expr && !isspace(*expr); expr++, i++)
-        s[i] = *expr;
+	for (i = 0; *expr && !isspace(*expr); expr++, i++) s[i] = *expr;
 	s[i] = '\0';
-
-	if (s[0] == '-' && (isdigit(s[1]) || s[1] == '.'))
-		typ = operand;
-	else if (s[0] == '+' && (isdigit(s[1]) || s[1] == '.'))
-		typ = operand;
-	else if (s[0] == '.')
-		typ = operand;
-	else if (isdigit(s[0]))
-		typ = operand;
-	else
-		typ = operator;
+	if (s[0] == '-' && (isdigit(s[1]) || s[1] == '.')) typ = operand;
+	else if (s[0] == '+' && (isdigit(s[1]) || s[1] == '.')) typ = operand;
+	else if (s[0] == '.') typ = operand;
+	else if (isdigit(s[0])) typ = operand;
+	else typ = operator;
 	return typ;
 }
-
-ElementType EvalPostfix(char *expr)
-{
-	Stack S;
+ElementType EvalPostfix(char *expr) {
 	char s[Max_Expr];
 	type tp;
 	ElementType v1, v2, v;
-	
-	S = CreateStack();
+	Stack S = CreateStack();
 	while (*expr) {
 		tp = GetOperandOrOperator(expr, s);
-		/* Special handling of *expr equals to null character */
 		expr += strlen(s);
 		if (*expr) expr += 1;
-		
-		if (tp == operand)
-			Push(S, atof(s));
+		if (tp == operand) Push(S, atof(s));
 		else {
 			v2 = Pop(S); v1 = Pop(S);
 			if (v2 == err || v1 == err) return Infinity;
-
 			switch (s[0]) {
 			case '+': v = v1 + v2; break;
 			case '-': v = v1 - v2; break;
@@ -123,4 +75,3 @@ ElementType EvalPostfix(char *expr)
 	if (S->Top != -1) return Infinity;
 	else return v;
 }
-
