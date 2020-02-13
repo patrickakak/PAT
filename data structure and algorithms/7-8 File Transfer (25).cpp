e23305da27bec919d393ecd99eafcc7c4f3df8b9
vuzway9132 <13806680065@163.com>
2020-02-13 11:53:30 +0800

    -

--- data structure and algorithms/7-8 File Transfer (25).cpp
+++ data structure and algorithms/7-8 File Transfer (25).cpp
@@ -3,32 +3,6 @@
 typedef int ElementType;
 typedef int SetName;
 typedef ElementType SetType[MaxSize];
-/* T = O(N^2) */
-/*
-void Find(SetType S[], ElementType X)
-{
-	int i;
-
-	for (i = 0; i < MaxSize && S[i].Data != X; i++) ;
-	if (i >= MaxSize) return -1;
-	for (; S[i].Parent >= 0; i = S[i].Parent) ;
-	return i;
-}
-*/
-
-/* TSSN implimentation: two simple sometimes naive 
- * if Union(Find(2), Find(1)), ... Union(Find(n), Find(1)), then T = O(N^2))
- *
- * By default, Root1 and Root2 are different root of set respectively */
-/*
-void Union(SetType S, SetName Root1, SetName Root2)
-{
-	S[Root2] = Root1;
-}
-*/
-
-/* Merge trees according to rank, 
- * in worst case, the height of tree will be H = O(logN) */
 void Union(SetType S, SetName Root1, SetName Root2) {
 	if (S[Root2] < S[Root1]) {
 		S[Root2] += S[Root1];
@@ -38,26 +12,6 @@ void Union(SetType S, SetName Root1, SetName Root2) {
 		S[Root2] = Root1;
 	}
 }
-
-/* Initialize the elements in a set as -1 */
-/*
-SetName Find(SetType S, ElementType X)
-{
-	for (; S[X] >= 0; X = S[X]) ;
-	return X;
-}
-*/
-
-/* Path compression (refer to Ackermann Function)
- * 
- * k1Ma(M, N) <= T(M, N) <= k2Ma(M, N)
- *
- *            /  2^j                (i=1 and j>=1)
- * A(i, j) =     A(i-1, 2)          (i>=2 and j=1)
- *            \  A(i-1, A(i, j-1))  (i>=2 and j>=2)
- *
- * a(M, N) = min{ i>=1 | A(i, [M/N]) > logN } <= O(log*N) <= 4 (normally) 
- */
 SetName Find(SetType S, ElementType X) {
 	if (S[X] < 0) return X;
 	else return S[X] = Find(S, S[X]);
