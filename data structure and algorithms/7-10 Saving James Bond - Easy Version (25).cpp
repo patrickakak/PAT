730c88a93f8edfe9a74e7c3295c32840ae1c8c8c
vuzway9132 <13806680065@163.com>
2020-02-14 11:41:44 +0800

    -

--- data structure and algorithms/7-10 Saving James Bond - Easy Version (25).cpp
+++ data structure and algorithms/7-10 Saving James Bond - Easy Version (25).cpp
@@ -1,72 +1,40 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#define Diameter 15
-#define O2B 50
-#define MAXN 100
-#define YES 1
-#define NO 0
-typedef struct {
-	int x, y;
-} Coordinates;
-typedef int Vertex;
-void InitVisited(int N);
-void Save007(int N, int D);
-bool FirstJump(int D, Vertex V);
-int DFS(Vertex V, int N, int D);
-bool IsSafe(Vertex V, int D);
-bool Jump(Vertex V, Vertex W, int D);
-
-Coordinates Crocs[MAXN];
-bool visited[MAXN];
+#include <iostream>
+#include <cmath>
+using namespace std;
+struct node {
+	double x, y;
+} c[110];
+int n;
+double d;
+bool visit[110];
+bool firJump(int v) {
+	return (d+7.5) * (d+7.5) >= c[v].x * c[v].x + c[v].y * c[v].y;
+}
+bool jump(int a, int b) {
+	return d * d >= (c[a].x-c[b].x)*(c[a].x-c[b].x) + (c[a].y-c[b].y)*(c[a].y-c[b].y);
+}
+bool isSafe(int v) {
+	return (fabs(c[v].x) + d >= 50) || (fabs(c[v].y) + d >= 50);
+}
+bool dfs(int v) {
+	visit[v] = true;
+	if (isSafe(v)) return true;
+	for (int i = 0; i < n; i++)
+		if (!visit[i] && jump(v, i) && dfs(i) == true)
+			return true;
+	return false;
+}
 int main() {
-	int N, D, i;	/* D: One step wide of James */
-	scanf("%d %d", &N, &D);
-	for (i = 0; i < N; i++)
-		scanf("%d %d", &Crocs[i].x, &Crocs[i].y);
-	Save007(N, D);
-	return 0;
-}
-void InitVisited(int N) {
-	for (int i = 0; i < N; i++)
-		visited[i] = false;
-}
-void Save007(int N, int D) {
-	int answer = NO;
-	Vertex V;
-	InitVisited(N);
-	for (V = 0; V < N; V++)
-		if (FirstJump(D, V)) {
-			answer = DFS(V, N, D);
-			if (answer == YES) break;
+	cin >> n >> d;
+	for (int i = 0; i < n; i++)
+		cin >> c[i].x >> c[i].y;
+	bool ans = false;
+	for (int i = 0; i < n; i++)
+		if (!visit[i] && firJump(i)) {
+			ans = dfs(i);
+			if (ans == true) break;
 		}
-	if (answer == YES) puts("Yes");
-	else puts("No");
-}
-bool FirstJump(int D, Vertex V) {
-	return ((float)D + Diameter/2) * ((float)D + Diameter/2) >= Crocs[V].x*Crocs[V].x + Crocs[V].y*Crocs[V].y;
-}
-int DFS(Vertex V, int N, int D) {
-	int answer = NO;
-	Vertex W;
-	visited[V] = true;
-	if (IsSafe(V, D))
-		answer = YES;
-	else
-		for (W = 0; W < N; W++)
-			if (!visited[W] && Jump(V, W, D)) {
-				answer = DFS(W, N, D);
-				if (answer == YES) break;
-			}
-	return answer;
-}
-bool IsSafe(Vertex V, int D) {
-	int AbsX, AbsY;
-	AbsX = Crocs[V].x > 0 ? Crocs[V].x : -Crocs[V].x;
-	AbsY = Crocs[V].y > 0 ? Crocs[V].y : -Crocs[V].y;
-	return AbsX+D >= O2B || AbsY+D >= O2B;
-}
-bool Jump(Vertex V, Vertex W, int D) {
-	return D*D >= (Crocs[V].x - Crocs[W].x)*(Crocs[V].x - Crocs[W].x) + (Crocs[V].y - Crocs[W].y)*(Crocs[V].y - Crocs[W].y);
+	if (ans == true) printf("Yes");
+	else printf("No");
+	return 0;
 }
