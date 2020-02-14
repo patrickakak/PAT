516c1f57fe340bddf20325869910422fd8c56b65
vuzway9132 <13806680065@163.com>
2020-02-14 20:33:54 +0800

    -

--- 数据结构与算法题目集/7-9 旅游规划 (25).cpp
+++ 数据结构与算法题目集/7-9 旅游规划 (25).cpp
@@ -1,142 +1,60 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
-#define MaxVertexNum 500
-#define INFINITY 65535
-#define ERROR -1
-struct DNode {
-	int dist, cost;
-};
-typedef struct DNode WeightType;
-
-typedef struct GNode *PtrToGNode;
-struct GNode {
-	int Nv, Ne;
-	WeightType G[MaxVertexNum][MaxVertexNum];
-};
-typedef PtrToGNode MGraph;
-
-typedef int Vertex;
-typedef struct ENode *PtrToENode;
-struct ENode {
-	Vertex V1, V2;
-	WeightType Weight;
-};
-typedef PtrToENode Edge;
-
-void InsertEdge(MGraph Graph, Edge E);
-MGraph CreatGraph(int VertexNum);
-MGraph BuildGrpah(Vertex *AddrS, Vertex *AddrD);
-void TravelPlan(MGraph Graph, Vertex S, Vertex D);
-void Dijkstra(MGraph Graph, int dist[], int cost[], Vertex S);
-
-int main()
-{
-	MGraph Graph;
-	Vertex S, D;
-	
-	Graph = BuildGrpah(&S, &D);
-	TravelPlan(Graph, S, D);
-
-	return 0;
-}
-
-MGraph CreatGraph(int VertexNum)
-{
-	MGraph Graph;
-	Vertex V, W;
-	
-	Graph = (MGraph) malloc(sizeof(struct GNode));
-	Graph->Nv = VertexNum;
-	Graph->Ne = 0;
-	
-	for (V = 0 ; V < Graph->Nv; V++)
-		for (W = 0; W < Graph->Nv; W++) {
-			Graph->G[V][W].dist = INFINITY;
-			Graph->G[V][W].cost = INFINITY;
+#include <iostream>
+#include <vector>
+using namespace std;
+const int inf = 0x2fffffff;
+int n, m, s, d, e[510][510], dis[510], c[510][510], mincost = inf;
+bool visit[510];
+vector<int> pre[510], tmppath, path;
+void dfs(int v) {
+	tmppath.push_back(v);
+	if (v == s) {
+		int cost = 0;
+		for (int i = tmppath.size()-1; i > 0; i--) {
+			int c1 = tmppath[i], c2 = tmppath[i-1];
+			cost += c[c1][c2];
 		}
-	return Graph;
-}
-
-MGraph BuildGrpah(Vertex *AddrS, Vertex *AddrD)
-{
-	int N, i;
-	MGraph Graph;
-	Edge E;
-	
-	scanf("%d", &N);
-	Graph = CreatGraph(N);
-	scanf("%d", &Graph->Ne);
-	scanf("%d %d", AddrS, AddrD);
-	if (Graph->Ne) {
-		E = (Edge) malloc(sizeof(struct ENode));
-		for (i = 0; i < Graph->Ne; i++) {
-			scanf("%d %d %d %d", &E->V1, &E->V2, 
-					&E->Weight.dist, &E->Weight.cost);
-			InsertEdge(Graph, E);
+		if (cost < mincost) {
+			mincost = cost;
+			path = tmppath;
 		}
+		tmppath.pop_back();
+		return;
 	}
-	return Graph;
+	for (int i = 0; i < pre[v].size(); i++) dfs(pre[v][i]);
+	tmppath.pop_back();
 }
-
-void InsertEdge(MGraph Graph, Edge E)
-{
-	Graph->G[E->V1][E->V2].dist = E->Weight.dist;
-	Graph->G[E->V1][E->V2].cost = E->Weight.cost;
-
-	Graph->G[E->V2][E->V1].dist = E->Weight.dist;
-	Graph->G[E->V2][E->V1].cost = E->Weight.cost;
-}
-
-/* Single-source shortest problem with a little modification */
-void TravelPlan(MGraph Graph, Vertex S, Vertex D)
-{
-	int cost[MaxVertexNum], dist[MaxVertexNum];
-
-	Dijkstra(Graph, dist, cost, S);
-	printf("%d %d\n", dist[D], cost[D]);
-}
-
-Vertex FindMinDist(MGraph Graph, int dist[], bool collected[])
-{
-	Vertex MinV, V;
-	int MinDist = INFINITY;
-
-	for (V = 0; V < Graph->Nv; V++)
-		if (collected[V] == false && dist[V] < MinDist) {
-			MinDist = dist[V];
-			MinV = V;
-		}
-	if (MinDist < INFINITY) return MinV;
-	else return ERROR;
-}
-
-void Dijkstra(MGraph Graph, int dist[], int cost[], Vertex S)
-{
-	bool collected[MaxVertexNum];
-	Vertex V, W;
-
-	for (V = 0; V < Graph->Nv; V++) {
-		dist[V] = Graph->G[S][V].dist;
-		cost[V] = Graph->G[S][V].cost;
-		collected[V] = false;
+int main() {
+	fill(e[0], e[0] + 510 * 510, inf);
+	fill(dis, dis + 510, inf);
+	fill(c[0], c[0] + 510 * 510, inf);
+	cin >> n >> m >> s >> d;
+	int c1, c2, l, cost;
+	for (int i = 0; i < m; i++) {
+		scanf("%d%d%d%d", &c1, &c2, &l, &cost);
+		e[c1][c2] = e[c2][c1]=l;
+		c[c1][c2] = c[c2][c1]=cost;
 	}
-	dist[S] = 0;
-	collected[S] = true;
-	while (1) {
-		V = FindMinDist(Graph, dist, collected);
-		if (V == ERROR)
-			break;
-		collected[V] = true;
-		for (W = 0; W < Graph->Nv; W++)
-			if (collected[W] == false && Graph->G[V][W].dist < INFINITY) {
-				if (dist[V] + Graph->G[V][W].dist < dist[W]) {
-					dist[W] = dist[V] + Graph->G[V][W].dist;
-					cost[W] = cost[V] + Graph->G[V][W].cost;
-				} else if ((dist[V] + Graph->G[V][W].dist == dist[W]) 
-						&& (cost[V] + Graph->G[V][W].cost < cost[W]))
-					cost[W] = cost[V] + Graph->G[V][W].cost;
+	dis[s] = 0;
+	for (int i = 0; i < n; i++) {
+		int u = -1, minn = inf;
+		for (int j = 0; j < n; j++)
+			if (!visit[j] && dis[j] < minn) {
+				u = j;
+				minn = dis[j];
+			}
+		if (u == -1) break;
+		visit[u] = true;
+		for (int v = 0; v < n; v++)
+			if (!visit[v] && e[u][v] != inf) {
+				if (dis[v] > dis[u] + e[u][v]) {
+					dis[v] = dis[u] + e[u][v];
+					pre[v].clear();
+					pre[v].push_back(u);
+				} else if (dis[v] == dis[u] + e[u][v])
+					pre[v].push_back(u);
 			}
 	}
+	dfs(d);
+	printf("%d %d", dis[d], mincost);
+	return 0;
 }
