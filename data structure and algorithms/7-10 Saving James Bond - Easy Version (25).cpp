bc3247b258db916dc23769f0820d5cfd899bda91
vuzway9132 <13806680065@163.com>
2020-02-15 20:10:19 +0800

    -

--- data structure and algorithms/7-10 Saving James Bond - Easy Version (25).cpp
+++ data structure and algorithms/7-10 Saving James Bond - Easy Version (25).cpp
@@ -7,15 +7,9 @@ struct node {
 int n;
 double d;
 bool visit[110];
-bool firJump(int v) {
-	return (d + 7.5) * (d + 7.5) >= c[v].x * c[v].x + c[v].y * c[v].y;
-}
-bool jump(int a, int b) {
-	return d * d >= pow(c[a].x-c[b].x, 2) + pow(c[a].y-c[b].y, 2);
-}
-bool isSafe(int v) {
-	return (fabs(c[v].x) + d >= 50) || (fabs(c[v].y) + d >= 50);
-}
+bool firJump(int v) { return (d + 7.5) * (d + 7.5) >= c[v].x * c[v].x + c[v].y * c[v].y; }
+bool jump(int a, int b) { return d * d >= pow(c[a].x-c[b].x, 2) + pow(c[a].y-c[b].y, 2); }
+bool isSafe(int v) { return (fabs(c[v].x) + d >= 50) || (fabs(c[v].y) + d >= 50); }
 bool dfs(int v) {
 	visit[v] = true;
 	if (isSafe(v)) return true;
