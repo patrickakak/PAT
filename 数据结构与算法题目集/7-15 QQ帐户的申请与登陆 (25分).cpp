ffdef747be70148cd90b0f6d49006fd79cd28a60
vuzway9132 <13806680065@163.com>
2020-02-15 20:25:59 +0800

    -

--- 数据结构与算法题目集/7-15 QQ帐户的申请与登陆 (25分).cpp
+++ 数据结构与算法题目集/7-15 QQ帐户的申请与登陆 (25分).cpp
@@ -1,27 +1,9 @@
-/**
- * Sample Input:
- * 5
- * L 1234567890 myQQ@qq.com
- * N 1234567890 myQQ@qq.com
- * N 1234567890 myQQ@qq.com
- * L 1234567890 myQQ@qq
- * L 1234567890 myQQ@qq.com
- * -------------------------
- * Smaple Output:
- * ERROR: Not Exist
- * New: OK
- * ERROR: Exist
- * ERROR: Wrong PW
- * Login: OK
- */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #define MAXN 100000
 #define KEYLENGTH 10
 #define PWLENGTH 16
-
 typedef unsigned int Index;
 typedef char KeyType[KEYLENGTH+1];
 typedef char PWType[PWLENGTH+1];
@@ -33,13 +15,11 @@ struct LNode {
 };
 typedef PtrToLNode Position;
 typedef PtrToLNode List;
-
 typedef struct TblNode *HashTable;
 struct TblNode {
 	int TableSize;
 	List Heads;
 };
-
 int NextPrime(int N);
 HashTable CreateTable(int TblSize);
 void DestroyTable(HashTable H);
@@ -48,18 +28,14 @@ Index Hash(KeyType Key, int Size);
 Position Find(HashTable H, KeyType Key);
 void Insert(HashTable H, KeyType Key, PWType PW);
 void Apply(HashTable H, KeyType Key, PWType PW);
-
-int main()
-{
+int main() {
 	int N, i;
 	char Op;
 	KeyType Key;
 	PWType PW;
 	HashTable H;
-
 	scanf("%d\n", &N);
 	H = CreateTable(N);
-	
 	for (i = 0; i < N; i++) {
 		scanf("%c %s %s\n", &Op, Key, PW);
 		switch (Op) {
@@ -74,11 +50,8 @@ int main()
 	DestroyTable(H);
 	return 0;
 }
-
-void Apply(HashTable H, KeyType Key, PWType PW)
-{
+void Apply(HashTable H, KeyType Key, PWType PW) {
 	Position P;
-
 	P = Find(H, Key);
 	if (P)
 		puts("ERROR: Exist");
@@ -87,25 +60,18 @@ void Apply(HashTable H, KeyType Key, PWType PW)
 		puts("New: OK");
 	}
 }
-
-void Insert(HashTable H, KeyType Key, PWType PW)
-{
+void Insert(HashTable H, KeyType Key, PWType PW) {
 	Position NewCell;
 	Index Pos;
-
 	NewCell = (Position) malloc(sizeof(struct LNode));
 	strcpy(NewCell->Key, Key);
 	strcpy(NewCell->PW, PW);
-
 	Pos = Hash(Key, H->TableSize);
 	NewCell->Next = H->Heads[Pos].Next;
 	H->Heads[Pos].Next = NewCell;
 }
-
-void Login(HashTable H, KeyType Key, PWType PW)
-{
+void Login(HashTable H, KeyType Key, PWType PW) {
 	Position P;
-
 	P = Find(H, Key);
 	if (!P)
 		puts("ERROR: Not Exist");
@@ -116,34 +82,24 @@ void Login(HashTable H, KeyType Key, PWType PW)
 			puts("Login: OK");
 	}
 }
-
-Index Hash(KeyType Key, int Size)
-{
+Index Hash(KeyType Key, int Size) {
 	int i;
 	Index Pos = 0;
-
 	for (i = 0; Key[i]; i++)
 		Pos = (Pos<<4) + Key[i];
 	return Pos % Size;
 }
-
-Position Find(HashTable H, KeyType Key)
-{
+Position Find(HashTable H, KeyType Key) {
 	Position P;
 	Index Pos;
-
 	Pos = Hash(Key, H->TableSize);
-
 	P = H->Heads[Pos].Next;
 	while (P && strcmp(P->Key, Key))
 		P = P->Next;
 	return P;
 }
-
-int NextPrime(int N)
-{
+int NextPrime(int N) {
 	int i, p;
-
 	if (N == 1 || N == 2) return 2;
 	p = (N%2) ? N : N+1;
 	while (1) {
@@ -154,16 +110,12 @@ int NextPrime(int N)
 	}
 	return p;
 }
-
-HashTable CreateTable(int TblSize)
-{
+HashTable CreateTable(int TblSize) {
 	HashTable H;
 	int i;
-
 	H = (HashTable) malloc(sizeof(struct TblNode));
 	H->TableSize = NextPrime(TblSize);
 	H->Heads = (List) malloc(H->TableSize * sizeof(struct LNode));
-
 	for (i = 0; i < H->TableSize; i++) {
 		H->Heads[i].Key[0] = '\0';
 		H->Heads[i].PW[0] = '\0';
@@ -171,12 +123,9 @@ HashTable CreateTable(int TblSize)
 	}
 	return H;
 }
-
-void DestroyTable(HashTable H)
-{
+void DestroyTable(HashTable H) {
 	int i;
 	Position P, Tmp;
-
 	for (i = 0; i < H->TableSize; i++) {
 		P = H->Heads[i].Next;
 		while (P) {
@@ -188,4 +137,3 @@ void DestroyTable(HashTable H)
 	free(H->Heads);
 	free(H);
 }
-
