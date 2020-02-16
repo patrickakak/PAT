94d92969ff182b4d5f2f957f2c7aecd3422bdf43
vuzway9132 <13806680065@163.com>
2020-02-16 16:39:59 +0800

    -

---
+++ 数据结构与算法题目集/7-41 PAT排名汇总 (25分).cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+struct node {
+	string id;
+	int loc, g, r;
+};
+bool cmp(node a, node b) { return a.g != b.g ? a.g > b.g : a.id < b.id; }
+int main() {
+	int n, k, g, sum = 0;
+	string id;
+	cin >> n;
+	vector<node> v;
+	for (int i = 0; i < n; i++) {
+		cin >> k;
+		for (int j = 0; j < k; j++) {
+			cin >> id >> g;
+			v.push_back(node{id, i+1, g});
+		}
+		sort(v.begin() + sum, v.begin() + sum + k, cmp);
+		v[sum].r = 1;
+		for (int j = sum + 1; j < sum + k; j++) {
+			v[j].r = j - sum + 1;
+			if (v[j].g == v[j-1].g) v[j].r = v[j-1].r;
+		}
+		sum += k;
+	}
+	printf("%d\n", sum);
+	sort(v.begin(), v.end(), cmp);
+	int r = 1;
+	printf("%s %d %d %d\n", v[0].id.c_str(), r, v[0].loc, v[0].r);
+	for (int i = 1; i < sum; i++) {
+		if (v[i].g != v[i-1].g) r = i + 1;
+		printf("%s %d %d %d\n", v[i].id.c_str(), r, v[i].loc, v[i].r);
+	}
+	return 0;
+}
