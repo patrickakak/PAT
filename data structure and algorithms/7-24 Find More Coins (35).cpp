af1b2e75da58a15fb11f0f458506a269a7fc60b3
vuzway9132 <13806680065@163.com>
2020-02-16 21:20:11 +0800

    -

--- data structure and algorithms/7-24 Find More Coins (35).cpp
+++ data structure and algorithms/7-24 Find More Coins (35).cpp
@@ -4,13 +4,12 @@
 using namespace std;
 int dp[10010], w[10010];
 bool choice[10010][110];
-int cmp1(int a, int b) { return a > b; }
+int cmp(int a, int b) { return a > b; }
 int main() {
 	int n, m;
 	scanf("%d%d", &n, &m);
-	for (int i = 1; i <= n; i++)
-		scanf("%d", &w[i]);
-	sort(w + 1, w + n + 1, cmp1);
+	for (int i = 1; i <= n; i++) scanf("%d", &w[i]);
+	sort(w + 1, w + n + 1, cmp);
 	for (int i = 1; i <= n; i++)
 		for (int j = m; j >= w[i]; j--)
 			if (dp[j] <= dp[j-w[i]] + w[i]) {
