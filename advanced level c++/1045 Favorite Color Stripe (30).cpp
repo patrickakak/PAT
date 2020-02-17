8f873c0169b239d24010e6933e63f4960a8b4c9b
vuzway9132 <13806680065@163.com>
2020-02-17 16:28:41 +0800

    -

--- advanced level c++/1045 Favorite Color Stripe (30).cpp
+++ advanced level c++/1045 Favorite Color Stripe (30).cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 int book[201], a[10001], dp[10001];
 int main() {
@@ -12,14 +11,12 @@ int main() {
 	scanf("%d", &l);
 	for (int i = 0; i < l; i++) {
 		scanf("%d", &x);
-		if (book[x] >= 1)
-			a[num++] = book[x];
+		if (book[x] >= 1) a[num++] = book[x];
 	}
 	for (int i = 0; i < num; i++) {
 		dp[i] = 1;
 		for (int j = 0; j < i; j++)
-			if (a[i] >= a[j])
-				dp[i] = max(dp[i], dp[j] + 1);
+			if (a[i] >= a[j]) dp[i] = max(dp[i], dp[j] + 1);
 		maxn = max(dp[i], maxn);
 	}
 	printf("%d", maxn);
