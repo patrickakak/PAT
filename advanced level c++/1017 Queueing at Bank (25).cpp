fa021706af427c046bb4783edf2bf31b174bfcbd
vuzway9132 <13806680065@163.com>
2020-02-19 12:53:57 +0800

    -

--- advanced level c++/1017 Queueing at Bank (25).cpp
+++ advanced level c++/1017 Queueing at Bank (25).cpp
@@ -4,43 +4,38 @@
 using namespace std;
 struct node {
 	int come, time;
-} tempcustomer;
-bool cmp1(node a, node b) {
-	return a.come < b.come;
-}
+} tmpcustomer;
+bool cmp(node a, node b) { return a.come < b.come; }
 int main() {
 	int n, k;
 	scanf("%d%d", &n, &k);
 	vector<node> custom;
-	for(int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) {
 		int hh, mm, ss, time;
 		scanf("%d:%d:%d %d", &hh, &mm, &ss, &time);
 		int cometime = hh * 3600 + mm * 60 + ss;
-		if(cometime > 61200) continue;
-		tempcustomer = {cometime, time * 60};
-		custom.push_back(tempcustomer);
+		if (cometime > 61200) continue;
+		tmpcustomer = {cometime, time * 60};
+		custom.push_back(tmpcustomer);
 	}
-	sort(custom.begin(), custom.end(), cmp1);
+	sort(custom.begin(), custom.end(), cmp);
 	vector<int> window(k, 28800);
 	double result = 0.0;
-	for(int i = 0; i < custom.size(); i++) {
-		int tempindex = 0, minfinish = window[0];
-		for(int j = 1; j < k; j++) {
-			if(minfinish > window[j]) {
+	for (int i = 0; i < custom.size(); i++) {
+		int tmpindex = 0, minfinish = window[0];
+		for (int j = 1; j < k; j++)
+			if (minfinish > window[j]) {
 				minfinish = window[j];
-				tempindex = j;
+				tmpindex = j;
 			}
-		}
-		if(window[tempindex] <= custom[i].come) {
-			window[tempindex] = custom[i].come + custom[i].time;
-		} else {
-			result += (window[tempindex] - custom[i].come);
-			window[tempindex] += custom[i].time;
+		if (window[tmpindex] <= custom[i].come)
+			window[tmpindex] = custom[i].come + custom[i].time;
+		else {
+			result += (window[tmpindex] - custom[i].come);
+			window[tmpindex] += custom[i].time;
 		}
 	}
-	if(custom.size() == 0)
-		printf("0.0");
-	else
-		printf("%.1f", result / 60.0 / custom.size());
+	if (custom.size() == 0) printf("0.0");
+	else printf("%.1f", result / 60.0 / custom.size());
 	return 0;
 }
