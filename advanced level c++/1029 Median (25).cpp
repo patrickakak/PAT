a497a5bbe7897c78e0f5b3236a7c9796ea05b998
vuzway9132 <13806680065@163.com>
2020-02-19 13:04:34 +0800

    -

--- advanced level c++/1029 Median (25).cpp
+++ advanced level c++/1029 Median (25).cpp
@@ -6,11 +6,9 @@ vector<long long> a(400010);
 int main() {
 	int n, m, i;
 	scanf("%d", &n);
-	for (i = 0; i < n; i++)
-		scanf("%lld", &a[i]);
+	for (i = 0; i < n; i++) scanf("%lld", &a[i]);
 	scanf("%d", &m);
-	for (int j = i; j < m + i; j++)
-		scanf("%lld", &a[j]);
+	for (int j = i; j < m + i; j++) scanf("%lld", &a[j]);
 	sort(a.begin(), a.begin() + m + n);
 	printf("%lld\n", a[(m + n - 1) / 2]);
 	return 0;
