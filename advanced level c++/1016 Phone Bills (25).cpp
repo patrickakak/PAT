dfe34c8d2b6f0742d0419b7580134869fbedb57f
vuzway9132 <13806680065@163.com>
2020-02-22 22:18:29 +0800

    -

--- advanced level c++/1016 Phone Bills (25).cpp
+++ advanced level c++/1016 Phone Bills (25).cpp
@@ -5,15 +5,12 @@
 using namespace std;
 struct node {
 	string name;
-	int status, month, time, day, hour, minute;
+	int status, month, time, day, hr, mm;
 };
-bool cmp(node a, node b) {
-	return a.name != b.name ? a.name < b.name : a.time < b.time;
-}
+bool cmp(node a, node b) { return a.name != b.name ? a.name < b.name : a.time < b.time; }
 double billFromZero(node call, int *rate) {
-	double total = rate[call.hour] * call.minute + rate[24] * 60 * call.day;
-	for (int i = 0; i < call.hour; i++)
-		total += rate[i] * 60;
+	double total = rate[call.hr] * call.mm + rate[24] * 60 * call.day;
+	for (int i = 0; i < call.hr; i++) total += rate[i] * 60;
 	return total / 100.0;
 }
 int main() {
@@ -26,28 +23,27 @@ int main() {
 	vector<node> data(n);
 	for (int i = 0; i < n; i++) {
 		cin >> data[i].name;
-		scanf("%d:%d:%d:%d", &data[i].month, &data[i].day, &data[i].hour, &data[i].minute);
-		string temp;
-		cin >> temp;
-		data[i].status = (temp == "on-line") ? 1 : 0;
-		data[i].time = data[i].day * 24 * 60 + data[i].hour * 60 + data[i].minute;
+		scanf("%d:%d:%d:%d", &data[i].month, &data[i].day, &data[i].hr, &data[i].mm);
+		string tmp;
+		cin >> tmp;
+		data[i].status = (tmp == "on-line") ? 1 : 0;
+		data[i].time = data[i].day * 24 * 60 + data[i].hr * 60 + data[i].mm;
 	}
 	sort(data.begin(), data.end(), cmp);
-	map<string, vector<node> > custom;
-	for (int i = 1; i < n; i++) {
+	map<string, vector<node>> custom;
+	for (int i = 1; i < n; i++)
 		if (data[i].name == data[i - 1].name && data[i - 1].status == 1 && data[i].status == 0) {
 			custom[data[i - 1].name].push_back(data[i - 1]);
 			custom[data[i].name].push_back(data[i]);
 		}
-	}
 	for (auto it : custom) {
-		vector<node> temp = it.second;
+		vector<node> tmp = it.second;
 		cout << it.first;
-		printf(" %02d\n", temp[0].month);
+		printf(" %02d\n", tmp[0].month);
 		double total = 0.0;
-		for (int i = 1; i < temp.size(); i += 2) {
-			double t = billFromZero(temp[i], rate) - billFromZero(temp[i - 1], rate);
-			printf("%02d:%02d:%02d %02d:%02d:%02d %d $%.2f\n", temp[i - 1].day, temp[i - 1].hour, temp[i - 1].minute, temp[i].day, temp[i].hour, temp[i].minute, temp[i].time - temp[i - 1].time, t);
+		for (int i = 1; i < tmp.size(); i += 2) {
+			double t = billFromZero(tmp[i], rate) - billFromZero(tmp[i - 1], rate);
+			printf("%02d:%02d:%02d %02d:%02d:%02d %d $%.2f\n", tmp[i-1].day, tmp[i-1].hr, tmp[i-1].mm, tmp[i].day, tmp[i].hr, tmp[i].mm, tmp[i].time - tmp[i-1].time, t);
 			total += t;
 		}
 		printf("Total amount: $%.2f\n", total);
