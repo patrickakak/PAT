5e8536b90bb1121586f200119d115961a3ea2ba1
vuzway9132 <13806680065@163.com>
2020-02-24 00:07:47 +0800

    -

--- basic level c++/1068 万绿丛中一点红 (20分).cpp
+++ basic level c++/1068 万绿丛中一点红 (20分).cpp
@@ -2,34 +2,31 @@
 #include <vector>
 #include <unordered_map>
 using namespace std;
-int m, n, tol, dir[8][2] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}};
-vector<vector<int>> v;
+int m, n, tol, v[1010][1010], dir[8][2] = {{-1,-1}, {-1,0}, {-1,1}, {0,1}, {1,1}, {1,0}, {1,-1}, {0,-1}};
 bool judge(int i, int j) {
 	for (int k = 0; k < 8; k++) {
 		int tx = i + dir[k][0];
 		int ty = j + dir[k][1];
-		if (tx >= 0 && tx < n && ty >= 0 && ty < m && abs(v[i][j]-v[tx][ty]) <= tol) return false;
+		if (tx >= 1 && tx <= n && ty >= 1 && ty <= m && abs(v[i][j]-v[tx][ty]) <= tol) return false;
 	}
 	return true;
 }
 int main() {
 	int cnt = 0, x = 0, y = 0;
 	scanf("%d%d%d", &m, &n, &tol);
-	v.resize(n, vector<int>(m));
 	unordered_map<int, int> mp;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++) {
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= m; j++) {
 			scanf("%d", &v[i][j]);
 			mp[v[i][j]]++;
 		}
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= m; j++)
 			if (mp[v[i][j]] == 1 && judge(i, j) == true) {
 				cnt++;
-				x = i + 1;
-				y = j + 1;
+				x = i, y = j;
 			}
-	if (cnt == 1) printf("(%d, %d): %d", y, x, v[x-1][y-1]);
+	if (cnt == 1) printf("(%d, %d): %d", y, x, v[x][y]);
 	else if (cnt == 0) printf("Not Exist");
 	else printf("Not Unique");
 	return 0;
