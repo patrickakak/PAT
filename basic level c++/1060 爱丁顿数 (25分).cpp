b0893aaef031fffb4a41e10d04cbb09388b6aab9
vuzway9132 <13806680065@163.com>
2020-02-23 22:11:53 +0800

    -

--- basic level c++/1060 爱丁顿数 (25分).cpp
+++ basic level c++/1060 爱丁顿数 (25分).cpp
@@ -6,7 +6,7 @@ int main() {
 	int n;
 	scanf("%d", &n);
 	vector<int> a(n+1);
-	for (int i = 1; i <= n; i++) cin >> a[i];
+	for (int i = 1; i <= n; i++) scanf("%d", &a[i]);
 	sort(a.rbegin(), a.rend()-1);
 	int e = 0, p = 1;
 	while (e <= n && a[p] > p) {
