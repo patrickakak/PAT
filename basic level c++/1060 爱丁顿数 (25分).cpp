8bb81cb6ac730d322a3266e66a90237dedeeabbf
vuzway9132 <13806680065@163.com>
2020-02-23 22:16:43 +0800

    -

--- basic level c++/1060 爱丁顿数 (25分).cpp
+++ basic level c++/1060 爱丁顿数 (25分).cpp
@@ -1,18 +1,17 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 using namespace std;
 int main() {
 	int n;
-	scanf("%d", &n);
-	vector<int> a(n+1);
-	for (int i = 1; i <= n; i++) scanf("%d", &a[i]);
-	sort(a.rbegin(), a.rend()-1);
-	int e = 0, p = 1;
-	while (e <= n && a[p] > p) {
-		e++;
-		p++;
-	}
-	cout << e;
+	cin >> n;
+	vector<int> v(n+1);
+	for (int i = 1; i <= n; i++) scanf("%d", &v[i]);
+	sort(v.rbegin(), v.rend()-1);
+	int e = 0;
+	for (int i = 1; i <= n; i++)
+		if (v[i] > i) e++;
+		else break;
+	printf("%d", e);
 	return 0;
 }
