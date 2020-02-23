62bcb3b0a0746072a7c4ce90984aa105a37b4283
vuzway9132 <13806680065@163.com>
2020-02-23 21:19:57 +0800

    -

--- basic level c++/1048 数字加密 (20分).cpp
+++ basic level c++/1048 数字加密 (20分).cpp
@@ -4,21 +4,18 @@ using namespace std;
 int main() {
 	string a, b, c;
 	cin >> a >> b;
-	int lena = a.length(), lenb = b.length();
+	int len = max(a.length(), b.length());
 	reverse(a.begin(), a.end());
 	reverse(b.begin(), b.end());
-	if (lena > lenb) b.append(lena - lenb, '0');
-	else a.append(lenb - lena, '0');
-	char str[14] = {"0123456789JQK"};
+	b.append(len - b.length(), '0');
+	a.append(len - a.length(), '0');
+	string ss = "0123456789JQK";
 	for (int i = 0; i < a.length(); i++)
-		if (i % 2 == 0)
-			c += str[(a[i] - '0' + b[i] - '0') % 13];
+		if (i % 2 == 0) c += ss[(a[i] - '0' + b[i] - '0') % 13];
 		else {
 			int tmp = b[i] - a[i];
-			if (tmp < 0) tmp += 10;
-			c += str[tmp];
+			c += ss[tmp < 0 ? tmp+=10 : tmp];
 		}
-	for (int i = c.length() - 1; i >= 0; i--)
-		cout << c[i];
+	for (int i = c.length() - 1; i >= 0; i--) printf("%c", c[i]);
 	return 0;
 }
