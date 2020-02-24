f9d57b435736f45be0ab70dbf349632aa90cb5d2
vuzway9132 <13806680065@163.com>
2020-02-24 11:27:50 +0800

    -

--- basic level c++/1027 打印沙漏 (20分).cpp
+++ basic level c++/1027 打印沙漏 (20分).cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int n, row = 0;
+	int n, row = 1, cnt = 1;
 	char c;
 	cin >> n >> c;
-	for (int i = 0; i < n; i++)
-		if ((2 * i * (i + 2) + 1) > n) {
-			row = i - 1;
-			break;
-		}
+	while (cnt + 2 * (2 * row + 1) <= n) {
+		cnt += 2 * (2 * row + 1);
+		row++;
+	}
+	row -= 1;
 	for (int i = row; i >= 1; i--) {
-		for (int j = row - i; j >= 1; j--) cout << ' ';
-		for (int k = i * 2 + 1; k >= 1; k--) cout << c;
-		cout << endl;
+		for (int j = 0; j < row - i; j++) printf(" ");
+		for (int j = 0; j < i * 2 + 1; j++) printf("%c", c);
+		printf("\n");
 	}
-	for (int i = 0; i < row; i++) cout << ' ';
-	cout << c << endl;
+	for (int i = 0; i < row; i++) printf(" ");
+	printf("%c\n", c);
 	for (int i = 1; i <= row; i++) {
-		for (int j = row - i; j >= 1; j--) cout << ' ';
-		for (int k = i * 2 + 1; k >= 1; k--) cout << c;
-		cout << endl;
+		for (int j = 0; j < row - i; j++) printf(" ");
+		for (int j = 0; j < i * 2 + 1; j++) printf("%c", c);
+		printf("\n");
 	}
-	cout << (n - (2 * row * (row + 2) + 1));
+	printf("%d", n - cnt);
 	return 0;
 }
