040c554bbcdca40add61bd3fbc7a8c74f7be7a81
vuzway9132 <13806680065@163.com>
2020-02-24 17:33:28 +0800

    -

--- basic level c++/1035 插入与归并 (25分).cpp
+++ basic level c++/1035 插入与归并 (25分).cpp
@@ -1,35 +1,33 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
 int main() {
-	int n, a[100], b[100], i, j;
+	int n, i, j, k = 1, flag = 0;
 	cin >> n;
-	for (int i = 0; i < n; i++) cin >> a[i];
-	for (int i = 0; i < n; i++) cin >> b[i];
-	for (i = 0; i < n - 1 && b[i] <= b[i + 1]; i++) ;
-	for (j = i + 1; a[j] == b[j] && j < n; j++) ;
+	vector<int> a(n), b(n);
+	for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+	for (int i = 0; i < n; i++) scanf("%d", &b[i]);
+	for (i = 1; i < n && b[i-1] <= b[i]; i++) ;
+	for (j = i; j < n && a[j] == b[j]; j++) ;
 	if (j == n) {
-		cout << "Insertion Sort" << endl;
-		sort(a, a + i + 2);
+		printf("Insertion Sort\n");
+		sort(a.begin(), a.begin() + i + 1);
 	} else {
-		cout << "Merge Sort" << endl;
-		int k = 1, flag = 1;
-		while (flag) {
-			flag = 0;
-			for (int i = 0; i < n; i++)
-				if (a[i] != b[i]) {
-					flag = 1;
-					break;
-				}
+		printf("Merge Sort\n");
+		while (1) {
+			int i;
+			for (i = 0; i < n && a[i] == b[i]; i++) ;
+			if (i == n) flag = 1;
 			k *= 2;
-			for (int i = 0; i < n / k; i++)
-				sort(a + i * k, a + (i + 1) * k);
-			sort(a + n / k * k, a + n);
+			for (int j = 0; j < n; j += k)
+				sort(a.begin() + j, a.begin() + min(n, j+k));
+			if (flag == 1) break;
 		}
 	}
-	for (int j = 0; j < n; j++) {
-		if (j != 0) cout << ' ';
-		cout << a[j];
+	for (int i = 0; i < n; i++) {
+		if (i != 0) printf(" ");
+		printf("%d", a[i]);
 	}
 	return 0;
 }
