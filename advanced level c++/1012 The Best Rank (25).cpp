3747c37eda9de5b5ca6200077fc66aad7d412508
vuzway9132 <13806680065@163.com>
2020-02-26 14:44:34 +0800

    -

--- advanced level c++/1012 The Best Rank (25).cpp
+++ advanced level c++/1012 The Best Rank (25).cpp
@@ -18,19 +18,19 @@ int main() {
 		v[i].g[0] = v[i].g[1] + v[i].g[2] + v[i].g[3];
 		s.insert(v[i].id);
 	}
+	for (flag = 0; flag < 4; flag++) {
+		sort(v.begin(), v.end(), cmp);
+		v[0].r[flag] = 1;
+		for (int j = 1; j < n; j++) {
+			v[j].r[flag] = j + 1;
+			if (v[j].g[flag] == v[j-1].g[flag]) v[j].r[flag] = v[j-1].r[flag];
+		}
+	}
 	string str = "ACME";
 	for (int i = 0; i < m; i++) {
 		scanf("%d", &id);
 		if (s.find(id) == s.end()) {
 			printf("N/A\n"); continue;
-		}
-		for (flag = 0; flag < 4; flag++) {
-			sort(v.begin(), v.end(), cmp);
-			v[0].r[flag] = 1;
-			for (int j = 1; j < n; j++) {
-				v[j].r[flag] = j + 1;
-				if (v[j].g[flag] == v[j-1].g[flag]) v[j].r[flag] = v[j-1].r[flag];
-			}
 		}
 		for (int j = 0; j < n; j++)
 			if (v[j].id == id) {
