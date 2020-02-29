1ad4dfd24a8e1960a87d0d54dac4630684e7e0ca
vuzway9132 <13806680065@163.com>
2020-02-29 15:32:20 +0800

    -

--- advanced level c++/1151 LCA in a Binary Tree (30).cpp
+++ advanced level c++/1151 LCA in a Binary Tree (30).cpp
@@ -4,19 +4,17 @@
 using namespace std;
 unordered_map<int, int> pos;
 vector<int> in, pre;
-void lca(int inl, int inr, int preRoot, int u, int v) {
+void lca(int inl, int inr, int prel, int prer, int u, int v) {
 	if (inl > inr) return;
-	int inRoot = pos[pre[preRoot]], uIn = pos[u], vIn = pos[v];
-	if (uIn < inRoot && vIn < inRoot)
-		lca(inl, inRoot - 1, preRoot + 1, u, v);
-	else if ((uIn < inRoot && vIn > inRoot) || (uIn > inRoot && vIn < inRoot))
-		printf("LCA of %d and %d is %d.\n", u, v, in[inRoot]);
-	else if (uIn > inRoot && vIn > inRoot)
-		lca(inRoot + 1, inr, preRoot + 1 + (inRoot - inl), u, v);
-	else if (uIn == inRoot)
-		printf("%d is an ancestor of %d.\n", u, v);
-	else if (vIn == inRoot)
-		printf("%d is an ancestor of %d.\n", v, u);
+	int rin = pos[pre[prel]], uin = pos[u], vin = pos[v];
+	if (uin < rin && vin < rin)
+		lca(inl, rin-1, prel+1, prel+(rin-inl)-1, u, v);
+	else if ((uin < rin && vin > rin) || (uin > rin && vin < rin))
+		printf("LCA of %d and %d is %d.\n", u, v, in[rin]);
+	else if (uin > rin && vin > rin)
+		lca(rin+1, inr, prel+1+(rin-inl), prer, u, v);
+	else
+		printf("%d is an ancestor of %d.\n", in[rin], uin == rin ? v : u);
 }
 int main() {
 	int m, n, u, v;
@@ -33,7 +31,7 @@ int main() {
 			printf("ERROR: %d and %d are not found.\n", u, v);
 		else if (pos[u] == 0 || pos[v] == 0)
 			printf("ERROR: %d is not found.\n", pos[u] == 0 ? u : v);
-		else lca(1, n, 1, u, v);
+		else lca(1, n, 1, n, u, v);
 	}
 	return 0;
 }
