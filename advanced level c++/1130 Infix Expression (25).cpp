353ea999ff7014b8ae48d563eabec6b02c6d698f
vuzway9132 <13806680065@163.com>
2020-02-29 18:40:20 +0800

    -

--- advanced level c++/1130 Infix Expression (25).cpp
+++ advanced level c++/1130 Infix Expression (25).cpp
@@ -5,9 +5,9 @@ struct node {
 	int l, r;
 } a[100];
 string dfs(int root) {
+	if (root == -1) return "";
 	if (a[root].l == -1 && a[root].r == -1) return a[root].s;
-	if (a[root].l == -1 && a[root].r != -1) return "(" + a[root].s + dfs(a[root].r) + ")";
-	if (a[root].l != -1 && a[root].r != -1) return "(" + dfs(a[root].l) + a[root].s + dfs(a[root].r) + ")";
+	else return "(" + dfs(a[root].l) + a[root].s + dfs(a[root].r) + ")";
 }
 int main() {
 	int h[100] = {0}, n, root = 1;
@@ -17,7 +17,7 @@ int main() {
 		if (a[i].l != -1) h[a[i].l] = 1;
 		if (a[i].r != -1) h[a[i].r] = 1;
 	}
-	while (h[root] == 1) root++;
+	while (root <= n && h[root] == 1) root++;
 	string ans = dfs(root);
 	if (ans[0] == '(') ans = ans.substr(1, ans.size()-2);
 	cout << ans;
