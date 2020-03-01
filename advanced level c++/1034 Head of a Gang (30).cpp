2889a25f1cf658307f891d29711aca2ee1da2936
vuzway9132 <13806680065@163.com>
2020-03-01 23:16:07 +0800

    -

--- advanced level c++/1034 Head of a Gang (30).cpp
+++ advanced level c++/1034 Head of a Gang (30).cpp
@@ -4,7 +4,7 @@ using namespace std;
 map<string, int> str2int, ans;
 map<int, string> int2str;
 int index = 1, k;
-int stoifunc(string s) {
+int getid(string s) {
 	if (str2int[s] == 0) {
 		str2int[s] = index;
 		int2str[index] = s;
@@ -13,21 +13,21 @@ int stoifunc(string s) {
 }
 int G[2010][2010], weight[2010];
 bool vis[2010];
-void dfs(int u, int &head, int &numMbr, int &totalweight) {
+void dfs(int u, int &head, int &numMbr, int &totalwt) {
 	vis[u] = true;
 	numMbr++;
 	if (weight[u] > weight[head]) head = u;
-	totalweight += weight[u];
+	totalwt += weight[u];
 	for (int v = 1; v < index; v++)
 		if (!vis[v] && G[u][v] > 0)
-			dfs(v, head, numMbr, totalweight);
+			dfs(v, head, numMbr, totalwt);
 }
 void dfsTrave() {
 	for (int i = 1; i < index; i++)
 		if (!vis[i]) {
-			int head = i, numMbr = 0, totalweight = 0;
-			dfs(i, head, numMbr, totalweight);
-			if (numMbr > 2 && totalweight/2 > k) ans[int2str[head]] = numMbr;
+			int head = i, numMbr = 0, totalwt = 0;
+			dfs(i, head, numMbr, totalwt);
+			if (numMbr > 2 && totalwt/2 > k) ans[int2str[head]] = numMbr;
 		}
 }
 int main() {
@@ -36,8 +36,8 @@ int main() {
 	string s1, s2;
 	for (int i = 0; i < n; i++) {
 		cin >> s1 >> s2 >> w;
-		int id1 = stoifunc(s1);
-		int id2 = stoifunc(s2);
+		int id1 = getid(s1);
+		int id2 = getid(s2);
 		weight[id1] += w;
 		weight[id2] += w;
 		G[id1][id2] += w;
