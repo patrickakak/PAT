0568279ff99830c4b3d0fe68e2433eea9f98a875
vuzway9132 <13806680065@163.com>
2020-03-01 20:38:55 +0800

    -

--- advanced level c++/1013 Battle Over Cities (25).cpp
+++ advanced level c++/1013 Battle Over Cities (25).cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int n, v[1010][1010];
+int n, e[1010][1010];
 bool visit[1010];
-void dfs(int node) {
-	visit[node] = true;
+void dfs(int cid) {
+	visit[cid] = true;
 	for (int i = 1; i <= n; i++)
-		if (visit[i] == false && v[node][i] == 1)
-			dfs(i);
+		if (!visit[i] && e[cid][i] == 1) dfs(i);
 }
 int main() {
 	int m, k, a, b;
 	scanf("%d%d%d", &n, &m, &k);
 	for (int i = 0; i < m; i++) {
 		scanf("%d%d", &a, &b);
-		v[a][b] = v[b][a] = 1;
+		e[a][b] = e[b][a] = 1;
 	}
 	for (int i = 0; i < k; i++) {
 		fill(visit, visit + 1010, false);
