8542b2c6eb0b774745dcdedc18ad067c33addf3d
vuzway9132 <13806680065@163.com>
2020-03-04 22:31:42 +0800

    -

--- advanced level c++/1163 Dijkstra Sequence (30).cpp
+++ advanced level c++/1163 Dijkstra Sequence (30).cpp
@@ -1,48 +1,44 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-const int inf=0x2fffffff;
-const int maxn=1005;
-int d[maxn],s[maxn],G[maxn][maxn];
-bool visit[maxn];
+const int inf = 0x2fffffff;
+int e[1010][1010], dis[1010];
+bool vis[1010];
 int main() {
-	int n,m,a,b,c,k;
-	scanf("%d %d\n", &n, &m);
-	fill(G[0], G[0]+maxn*maxn, inf);
-	while (m--) {
-		scanf("%d %d %d\n",&a,&b,&c);
-		G[a][b]=G[b][a]=c;
+	fill(e[0], e[0]+1010*1010, inf);
+	int nv, ne, k, a, b, w;
+	cin >> nv >> ne;
+	for (int i = 0; i < ne; i++) {
+		scanf("%d%d%d", &a, &b, &w);
+		e[a][b] = e[b][a] = w;
 	}
-	scanf("%d\n",&k);
+	cin >> k;
+	vector<int> seq(nv);
 	while (k--) {
-		m=n;
-		for (int i=1;i<=n;i++)
-			scanf("%d",&s[i]);
-		bool f=true;
-		int start=s[1];
-		fill(visit,visit+maxn,false);
-		fill(d,d+maxn,inf);
-		d[start]=0;
-		for(int i=1;i<=n;i++){
-			int u = -1, minn = inf;
-			for (int j = 1; j <= n; j++) {
-				if (!visit[j] && d[j] < minn) {
+		fill(dis, dis+1010, inf);
+		fill(vis, vis+1010, false);
+		for (int i = 0; i < nv; i++) scanf("%d", &seq[i]);
+		dis[seq[0]] = 0;
+		for (int i = 1; i <= nv; i++) {
+			int minn = inf, u = -1;
+			for (int j = 1; j <= nv; j++)
+				if (!vis[j] && dis[j] < minn) {
+					minn = dis[j];
 					u = j;
-					minn = d[j];
 				}
-			}
-			if (u == -1 || d[u] != d[s[i]]){
-				f=false;
-				continue;
-			}
-			u = s[i];
-			visit[u]=true;
-			for (int v=1;v<=n;v++){
-				if(!visit[v] && G[u][v]!=inf && G[u][v]+d[u]<d[v]){
-					d[v]=G[u][v]+d[u];
-				}
-			}
+			if (u == -1) break;
+			vis[u] = true;
+			for (int v = 1; v <= nv; v++)
+				if (!vis[v] && e[u][v] != inf && dis[v] > dis[u] + e[u][v])
+					dis[v] = dis[u] + e[u][v];
 		}
-		if(!f)printf("No\n");
+		int flag = 0;
+		for (int i = 1; i < nv; i++)
+			if (dis[seq[i]] < dis[seq[i-1]]) {
+				flag = 1;
+				break;
+			}
+		if (flag == 1) printf("No\n");
 		else printf("Yes\n");
 	}
 	return 0;
