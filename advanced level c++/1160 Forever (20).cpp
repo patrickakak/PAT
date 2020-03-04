1cb53a2b052b4f2b9e8ae6d1dd96dca4c032b563
vuzway9132 <13806680065@163.com>
2020-03-04 19:27:59 +0800

    -

--- advanced level c++/1160 Forever (20).cpp
+++ advanced level c++/1160 Forever (20).cpp
@@ -17,28 +17,30 @@ bool isprime(int n) {
 		if (n % i == 0) return false;
 	return true;
 }
-int gcd(int a, int b) { return !b ? a: gcd(b, a % b); }
+int gcd(int a, int b) {
+	return !b ? a: gcd(b, a % b);
+}
 int main() {
-	int t;
-	cin >> t;
-	for (int i = 1; i <= t; i++) {
+	int N;
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
 		int k, m;
-		cin >> k >> m;
+		scanf("%d%d", &k, &m);
 		vector<pair<int, int>> ans;
-		int begin = pow(10, k - 1), end = pow(10, k);
+		int begin = pow(10, k-1), end = pow(10, k);
 		for (int i = begin + 9; i < end; i += 10) {
 			if (getSum(i) != m) continue;
-			int n = getSum(i + 1), d = gcd(m, n);
-			if (isprime(d) && d > 2)
-				ans.push_back({n, i});
+			int n = getSum(i + 1);
+			int d = gcd(m, n);
+			if (d > 2 && isprime(d)) ans.push_back({n, i});
 		}
 		sort(ans.begin(), ans.end(), [](pair<int, int> a, pair<int, int> b) {
 				return a.first != b.first ? a.first < b.first : a.second < b.second;
 				});
 		printf("Case %d\n", i);
 		if (ans.empty()) printf("No Solution\n");
-		for (auto entry : ans)
-			printf("%d %d\n", entry.first, entry.second);
+		for (auto it : ans)
+			printf("%d %d\n", it.first, it.second);
 	}
 	return 0;
 }
