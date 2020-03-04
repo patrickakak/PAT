3a38238a411df325ce6bb1842d5b1747b8f82edd
vuzway9132 <13806680065@163.com>
2020-03-04 20:40:52 +0800

    -

--- advanced level c++/1163 Dijkstra Sequence (30).cpp
+++ advanced level c++/1163 Dijkstra Sequence (30).cpp
@@ -1,66 +1,49 @@
 #include <iostream>
-#include <vector>
-#include <set>
-#include <algorithm>
-#include <string>
-#include <unordered_map>
-#include <math.h>
-#define maxsize 1002
-#define INF 99999999
 using namespace std;
-int n, m, matrx[maxsize][maxsize] = {0}, d[maxsize];
-bool vis[maxsize] = {false};
-vector<int> now;
-void check() {
-	fill(d + 1, d + 1 + n, INF);
-	fill(vis + 1, vis + 1 + n, false);
-	d[now[0]] = 0;
-	set<int> tmp;
-	int index = 0;
-	for (int i = 0; i < n; i++) {
-		int u, mini = INF;
-		for (int j = 1; j <= n; j++)
-			if (vis[j] == false) {
-				if (d[j] < mini) {
-					mini = d[j];
-					tmp.clear();
-					tmp.insert(j);
-				}
-				else if (d[j] == mini)
-					tmp.insert(j);
-			}
-		if (tmp.find(now[index]) != tmp.end()) {
-			vis[now[index]] = true;
-			u = now[index];
-			tmp.clear();
-		} else {
-			cout << "No" << endl;
-			return;
-		}
-		for (int j = 1; j <= n; j++)
-			if (vis[j] == false && matrx[u][j] != 0 && d[j] > d[u] + matrx[u][j])
-				d[j] = d[u] + matrx[u][j];
-		index++;
-	}
-	cout << "Yes" << endl;
-}
+const int inf=0x2fffffff;
+const int maxn=1005;
+int d[maxn],s[maxn],G[maxn][maxn];
+bool visit[maxn];
 int main() {
-	std::iostream::sync_with_stdio(false);
-	std::cin.tie(0);
-	cin >> n >> m;
-	int a, b, c;
-	for (int i = 0; i < m; i++) {
-		cin >> a >> b >> c;
-		matrx[a][b] = matrx[b][a] = c;
+	int n,m,a,b,c,k;
+	scanf("%d %d\n", &n, &m);
+	fill(G[0], G[0]+maxn*maxn, inf);
+	while (m--) {
+		scanf("%d %d %d\n",&a,&b,&c);
+		G[a][b]=G[b][a]=c;
 	}
-	cin >> m;
-	for (int i = 0; i < m; i++) {
-		now.clear();
-		for (int j = 0; j < n; j++) {
-			cin >> a;
-			now.push_back(a);
+	scanf("%d\n",&k);
+	while (k--) {
+		m=n;
+		for (int i=1;i<=n;i++)
+			scanf("%d",&s[i]);
+		bool f=true;
+		int start=s[1];
+		fill(visit,visit+maxn,false);
+		fill(d,d+maxn,inf);
+		d[start]=0;
+		for(int i=1;i<=n;i++){
+			int u = -1, minn = inf;
+			for (int j = 1; j <= n; j++) {
+				if (!visit[j] && d[j] < minn) {
+					u = j;
+					minn = d[j];
+				}
+			}
+			if (u == -1 || d[u] != d[s[i]]){
+				f=false;
+				continue;
+			}
+			u = s[i];
+			visit[u]=true;
+			for (int v=1;v<=n;v++){
+				if(!visit[v] && G[u][v]!=inf && G[u][v]+d[u]<d[v]){
+					d[v]=G[u][v]+d[u];
+				}
+			}
 		}
-		check();
+		if(!f)printf("No\n");
+		else printf("Yes\n");
 	}
 	return 0;
 }
