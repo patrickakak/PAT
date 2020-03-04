07cd2c5137d207d26feae6e07773b66913fb6de5
vuzway9132 <13806680065@163.com>
2020-03-04 19:22:57 +0800

    -

--- advanced level c++/1160 Forever (20).cpp
+++ advanced level c++/1160 Forever (20).cpp
@@ -3,22 +3,21 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int digit_sum(int num) {
+int getSum(int num) {
 	int sum = 0;
-	while (num) {
+	do {
 		sum += num % 10;
 		num /= 10;
-	}
+	} while (num);
 	return sum;
 }
-bool is_prime(int num) {
-	if (num < 3) return num == 2;
-	if (num % 2 == 0) return false;
-	for (int i = 3; i <= num / i; i += 2)
-		if (num % i == 0) return false;
+bool isprime(int n) {
+	if (n < 2) return false;
+	for (int i = 2; i * i <= n; i++)
+		if (n % i == 0) return false;
 	return true;
 }
-int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+int gcd(int a, int b) { return !b ? a: gcd(b, a % b); }
 int main() {
 	int t;
 	cin >> t;
@@ -28,9 +27,9 @@ int main() {
 		vector<pair<int, int>> ans;
 		int begin = pow(10, k - 1), end = pow(10, k);
 		for (int i = begin + 9; i < end; i += 10) {
-			if (digit_sum(i) != m) continue;
-			int n = digit_sum(i + 1), d = gcd(m, n);
-			if (is_prime(d) && d > 2)
+			if (getSum(i) != m) continue;
+			int n = getSum(i + 1), d = gcd(m, n);
+			if (isprime(d) && d > 2)
 				ans.push_back({n, i});
 		}
 		sort(ans.begin(), ans.end(), [](pair<int, int> a, pair<int, int> b) {
