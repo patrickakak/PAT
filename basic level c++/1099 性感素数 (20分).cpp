73cdd4ec50ca652b8ee12d74cc3ea491a905a753
vuzway9132 <13806680065@163.com>
2020-03-05 19:17:45 +0800

    -

---
+++ basic level c++/1099 性感素数 (20分).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+bool isprime(int n) {
+	if (n < 2) return false;
+	for (int i = 2; i*i <= n; i++)
+		if (n % i == 0) return false;
+	return true;
+}
+int main() {
+	int n;
+	cin >> n;
+	if (isprime(n)) {
+		if (isprime(n - 6)) {
+			printf("Yes\n%d", n - 6);
+			return 0;
+		} else if (isprime(n + 6)) {
+			printf("Yes\n%d", n + 6);
+			return 0;
+		}
+	}
+	while (!(isprime(n) && (isprime(n - 6) || isprime(n + 6)))) n++;
+	printf("No\n%d", n);
+	return 0;
+}
