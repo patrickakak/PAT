dc390e51e13c2dafaa559d32d7747648768114bb
vuzway9132 <13806680065@163.com>
2020-03-06 22:26:07 +0800

    -

--- advanced level c++/1082 Read Number in Chinese (25).cpp
+++ advanced level c++/1082 Read Number in Chinese (25).cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 string a[] = {"ling", "yi", "er", "san", "si", "wu", "liu", "qi", "ba", "jiu"};
 string b[] = {"", "Shi", "Bai", "Qian", "Wan", "Yi"};
+vector<string> v;
 int main() {
 	long long n;
 	cin >> n;
 	string s = to_string(n);
 	int l = 0, r = s.length() - 1;
 	if (s[0] == '-') {
-		printf("Fu");
+		v.push_back("Fu");
 		l = 1;
 	}
 	while (r - 4 >= l) r -= 4;
@@ -18,17 +20,20 @@ int main() {
 			if (l > 0 && s[l] == '0') zflag = true;
 			else {
 				if (zflag == true) {
-					printf(" ling");
+					v.push_back("ling");
 					zflag = false;
 				}
-				if (l > 0) printf(" ");
-				cout << a[s[l]-'0'];
+				v.push_back(a[s[l]-'0']);
 				isPrint = true;
-				if (l != r) cout << ' ' << b[r-l];
+				if (l != r) v.push_back(b[r-l]);
 			}
 		if (isPrint == true && r != s.length()-1)
-			cout << ' ' << b[(s.length() - r) / 4 + 3];
+			v.push_back(b[(s.length() - r) / 4 + 3]);
 		r += 4;
+	}
+	for (int i = 0; i < v.size(); i++) {
+		if (i != 0) printf(" ");
+		printf("%s", v[i].c_str());
 	}
 	return 0;
 }
