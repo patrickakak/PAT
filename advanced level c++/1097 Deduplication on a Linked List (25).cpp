2926c6d7b386e01011a412df7c58de7700465abe
vuzway9132 <13806680065@163.com>
2020-03-07 20:28:03 +0800

    -

--- advanced level c++/1097 Deduplication on a Linked List (25).cpp
+++ advanced level c++/1097 Deduplication on a Linked List (25).cpp
@@ -8,19 +8,17 @@ struct node {
 unordered_map<int, bool> m;
 vector<node> v(100000), a, b;
 int main() {
-	int fir, n, addr;
+	int fir, n, addr, key, next;
 	cin >> fir >> n;
 	for (int i = 0; i < n; i++) {
-		cin >> addr;
-		v[addr].addr = addr;
-		cin >> v[addr].key >> v[addr].next;
+		scanf("%d%d%d", &addr, &key, &next);
+		v[addr] = node{addr, key, next};
 	}
-	for (int p = fir; p != -1; p=v[p].next)
+	for (int p = fir; p != -1; p = v[p].next)
 		if (m[abs(v[p].key)] == false) {
 			m[abs(v[p].key)] = true;
 			a.push_back(v[p]);
-		} else
-			b.push_back(v[p]);
+		} else b.push_back(v[p]);
 	for (int i = 0; i < a.size(); i++)
 		if (i != a.size()-1) printf("%05d %d %05d\n", a[i].addr, a[i].key, a[i+1].addr);
 		else printf("%05d %d -1\n", a[i].addr, a[i].key);
