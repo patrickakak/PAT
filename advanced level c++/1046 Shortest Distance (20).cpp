f3bfb2570e11e93657bf88ca6c9459c30363e4ae
vuzway9132 <13806680065@163.com>
2020-03-07 14:57:47 +0800

    -

--- advanced level c++/1046 Shortest Distance (20).cpp
+++ advanced level c++/1046 Shortest Distance (20).cpp
@@ -1,21 +1,20 @@
 #include <iostream>
-#include <vector>
 using namespace std;
+int d[100010];
 int main() {
-	int n, sum = 0, left, right, cnt, tmp;
-	scanf("%d", &n);
-	vector<int> dis(n + 1);
-	for (int i = 1; i <= n; i++) {
+	int n, tmp;
+	cin >> n;
+	d[1] = 0;
+	for (int i = 2; i < n+2; i++) {
 		scanf("%d", &tmp);
-		sum += tmp;
-		dis[i] = sum;
+		d[i] = d[i-1] + tmp;
 	}
-	scanf("%d", &cnt);
-	for (int i = 0; i < cnt; i++) {
-		scanf("%d %d", &left, &right);
-		if (left > right) swap(left, right);
-		tmp = dis[right - 1] - dis[left - 1];
-		printf("%d\n", min(tmp, sum - tmp));
+	int m, a, b, sum = d[n+1];
+	cin >> m;
+	for (int i = 0; i < m; i++) {
+		scanf("%d%d", &a, &b);
+		tmp = abs(d[a]-d[b]);
+		printf("%d\n", min(tmp, sum-tmp));
 	}
 	return 0;
 }
