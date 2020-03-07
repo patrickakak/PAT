71870b3529170b739a5f533475e2a0f110308908
vuzway9132 <13806680065@163.com>
2020-03-08 00:23:31 +0800

    -

--- advanced level c++/1167 Cartesian Tree (30).cpp
+++ advanced level c++/1167 Cartesian Tree (30).cpp
@@ -5,13 +5,13 @@
 using namespace std;
 map<int, int> mp;
 vector<int> in;
-void inorder(int inl, int inr, int index) {
+void preorder(int inl, int inr, int index) {
 	if (inl > inr) return;
 	int p = min_element(in.begin()+inl, in.begin()+inr+1) - (in.begin()+inl);
 	int minn = in[inl+p];
 	mp[minn] = index;
-	inorder(inl, inl+p-1, 2*index);
-	inorder(inl+p+1, inr, 2*index+1);
+	preorder(inl, inl+p-1, 2*index);
+	preorder(inl+p+1, inr, 2*index+1);
 }
 bool cmp(int a, int b) { return mp[a] < mp[b]; }
 int main() {
@@ -19,7 +19,7 @@ int main() {
 	cin >> n;
 	in.resize(n);
 	for (int i = 0; i < n; i++) scanf("%d", &in[i]);
-	inorder(0, n-1, 1);
+	preorder(0, n-1, 1);
 	sort(in.begin(), in.end(), cmp);
 	for (int i = 0; i < n; i++) {
 		if (i != 0) printf(" ");
