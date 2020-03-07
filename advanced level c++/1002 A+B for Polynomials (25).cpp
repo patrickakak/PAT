6fd362c51e1194db6f7cb9eb3e2638a3761ba2ce
vuzway9132 <13806680065@163.com>
2020-03-07 14:05:28 +0800

    -

--- advanced level c++/1002 A+B for Polynomials (25).cpp
+++ advanced level c++/1002 A+B for Polynomials (25).cpp
@@ -1,23 +1,28 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+double a[1010];
 int main() {
-	double num, c[1001] = {0};
-	int m, n, t, cnt = 0;
-	scanf("%d", &m);
-	for (int i = 0; i < m; i++) {
-		scanf("%d%lf", &t, &num);
-		c[t] += num;
+	int k1, k2, e, cnt = 0;
+	double c;
+	cin >> k1;
+	for (int i=0; i<k1; i++) {
+		scanf("%d%lf", &e, &c);
+		a[e] += c;
 	}
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
-		scanf("%d%lf", &t, &num);
-		c[t] += num;
+	cin >> k2;
+	for (int i = 0; i < k2; i++) {
+		scanf("%d%lf", &e, &c);
+		a[e] += c;
 	}
-	for (int i = 0; i < 1001; i++)
-		if (c[i] != 0) cnt++;
+	vector<pair<int, double>> v;
+	for (int i = 1009; i >= 0; i--)
+		if (a[i] != 0) {
+			cnt++;
+			v.push_back({i, a[i]});
+		}
 	printf("%d", cnt);
-	for (int i = 1000; i >= 0; i--)
-		if (c[i] != 0.0)
-			printf(" %d %.1f", i, c[i]);
+	for (int i = 0; i < v.size(); i++)
+		printf(" %d %.1f", v[i].first, v[i].second);
 	return 0;
 }
