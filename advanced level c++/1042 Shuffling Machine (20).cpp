e88ac089932ba7d6305884af0db605655660b202
vuzway9132 <13806680065@163.com>
2020-03-07 14:35:50 +0800

    -

--- advanced level c++/1042 Shuffling Machine (20).cpp
+++ advanced level c++/1042 Shuffling Machine (20).cpp
@@ -1,23 +1,18 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int cnt, start[55], end[55], scan[55];
-	scanf("%d", &cnt);
-	for (int i = 1; i <= 54; i++) {
-		scanf("%d", &scan[i]);
-		end[i] = i;
+	int k, a[54], b[54], seq[54];
+	cin >> k;
+	for (int i = 0; i < 54; i++) a[i] = i;
+	for (int i = 0; i < 54; i++) scanf("%d", &seq[i]);
+	while (k--) {
+		for (int i = 0; i < 54; i++) b[seq[i]-1] = a[i];
+		for (int i = 0; i < 54; i++) a[i] = b[i];
 	}
-	for (int i = 0; i < cnt; i++) {
-		for (int j = 1; j <= 54; j++)
-			start[j] = end[j];
-		for (int k = 1; k <= 54; k++)
-			end[scan[k]] = start[k];
-	}
-	string c = "SHCDJ";
-	for (int i = 1; i <= 54; i++) {
-		end[i] = end[i] - 1;
-		printf("%c%d", c[end[i] / 13], end[i] % 13 + 1);
-		if (i != 54) printf(" ");
+	string s = "SHCDJ";
+	for (int i = 0; i < 54; i++) {
+		if (i != 0) printf(" ");
+		printf("%c%d", s[a[i]/13], a[i]%13+1);
 	}
 	return 0;
 }
