a6d6bc289d4b7c1670b9126cd8479e44d7d879ce
vuzway9132 <13806680065@163.com>
2020-03-08 14:35:13 +0800

    -

--- advanced level c++/1093 Count PAT's (25).cpp
+++ advanced level c++/1093 Count PAT's (25).cpp
@@ -3,15 +3,14 @@ using namespace std;
 int main() {
 	string s;
 	cin >> s;
-	int len = s.length(), res = 0, pcnt = 0, tcnt = 0;
-	for (int i = 0; i < len; i++)
-		if (s[i] == 'T')
-			tcnt++;
-	for (int i = 0; i < len; i++) {
+	int ans = 0, pcnt = 0, tcnt = 0;
+	for (int i = 0; i < s.length(); i++)
+		if (s[i] == 'T') tcnt++;
+	for (int i = 0; i < s.length(); i++) {
 		if (s[i] == 'P') pcnt++;
 		if (s[i] == 'T') tcnt--;
-		if (s[i] == 'A') res = (res + (pcnt * tcnt) % 1000000007) % 1000000007;
+		if (s[i] == 'A') ans = (ans + (pcnt * tcnt) % 1000000007) % 1000000007;
 	}
-	cout << res;
+	printf("%d", ans);
 	return 0;
 }
