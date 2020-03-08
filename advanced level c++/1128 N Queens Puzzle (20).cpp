8e06c077de877e2aa049c9319912eb32fa862477
vuzway9132 <13806680065@163.com>
2020-03-08 17:15:12 +0800

    -

--- advanced level c++/1128 N Queens Puzzle (20).cpp
+++ advanced level c++/1128 N Queens Puzzle (20).cpp
@@ -7,16 +7,16 @@ int main() {
 	while (k--) {
 		scanf("%d", &n);
 		vector<int> v(n);
-		bool res = true;
+		bool flag = true;
 		for (int i = 0; i < n; i++) {
 			scanf("%d", &v[i]);
 			for (int j = 0; j < i; j++)
 				if (v[i] == v[j] || abs(v[i]-v[j]) == abs(i-j)) {
-					res = false;
+					flag = false;
 					break;
 				}
 		}
-		printf("%s\n", res == true ? "YES" : "NO");
+		printf("%s\n", flag == true ? "YES" : "NO");
 	}
 	return 0;
 }
