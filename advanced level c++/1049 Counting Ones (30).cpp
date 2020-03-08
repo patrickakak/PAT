e94e1b96067aa24d87dc904f7c2dd8ccb98e6b20
vuzway9132 <13806680065@163.com>
2020-03-08 11:11:05 +0800

    -

--- advanced level c++/1049 Counting Ones (30).cpp
+++ advanced level c++/1049 Counting Ones (30).cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int n, left, right, a = 1, now = 1, ans = 0;
+	int n, left, right, a = 1, now = 1, cnt = 0;
 	scanf("%d", &n);
 	while (n / a) {
 		left = n / (a * 10), now = n / a % 10, right = n % a;
-		if (now == 0) ans += left * a;
-		else if (now == 1) ans += left * a + right + 1;
-		else ans += (left + 1) * a;
+		if (now == 0) cnt += left * a;
+		else if (now == 1) cnt += left * a + right + 1;
+		else cnt += (left + 1) * a;
 		a *= 10;
 	}
-	printf("%d", ans);
+	printf("%d", cnt);
 	return 0;
 }
