1ba08b0955be3e004a22364f76b4ef140183409f
vuzway9132 <13806680065@163.com>
2020-03-08 16:17:12 +0800

    -

--- advanced level c++/1109 Group Photo (25).cpp
+++ advanced level c++/1109 Group Photo (25).cpp
@@ -1,39 +1,35 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 using namespace std;
 struct node {
 	string nm;
 	int ht;
 };
-int cmp(node &a, node &b) {
+bool cmp(node &a, node &b) {
 	return a.ht != b.ht ? a.ht > b.ht : a.nm < b.nm;
 }
 int main() {
-	int n, k, m;
+	int n, k, m, cnt = 0;
 	cin >> n >> k;
 	vector<node> v(n);
-	for (int i = 0; i < n; i++)
-		cin >> v[i].nm >> v[i].ht;
+	for (int i = 0; i < n; i++) cin >> v[i].nm >> v[i].ht;
 	sort(v.begin(), v.end(), cmp);
-	int t = 0, row = k;
-	while (row) {
-		m = (row == k ? n % k + n / k : n / k);
-		vector<string> ans(m);
-		ans[m / 2] = v[t].nm;
-		int j = m / 2 - 1;
-		for (int i = t + 1; i < t + m; i += 2)
-			ans[j--] = v[i].nm;
-		j = m / 2 + 1;
-		for (int i = t + 2; i < t + m; i += 2)
-			ans[j++] = v[i].nm;
-		for (int i = 0; i < m; i++) {
-			if (i != 0) printf(" ");
-			printf("%s", ans[i].c_str());
+	for (int row = k; row > 0; row--) {
+		m = row == k ? n/k + n%k : n/k;
+		vector<node> t(m);
+		t[m/2] = v[cnt++];
+		for (int j = 1; j < m; j++) {
+			int left = m/2 - j;
+			if (left >= 0) t[left] = v[cnt++];
+			int right = m/2 + j;
+			if (right < m) t[right] = v[cnt++];
+		}
+		for (int j = 0; j < m; j++) {
+			if (j != 0) printf(" ");
+			printf("%s", t[j].nm.c_str());
 		}
 		printf("\n");
-		t += m;
-		row--;
 	}
 	return 0;
 }
