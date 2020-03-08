cedf1b2e6915d7fbf596e511771dfaec16adfa0b
vuzway9132 <13806680065@163.com>
2020-03-08 16:35:30 +0800

    -

--- advanced level c++/1116 Come on! Let's C (20).cpp
+++ advanced level c++/1116 Come on! Let's C (20).cpp
@@ -1,37 +1,34 @@
 #include <iostream>
 #include <unordered_map>
 using namespace std;
-bool isPrime(int n) {
-	for (int i = 2; i * i <= n; i++)
-		if (n % i == 0) return false;
+bool isprime(int n) {
+	if (n < 2) return false;
+	for (int i = 2; i*i <= n; i++)
+		if (n%i == 0) return false;
 	return true;
 }
 int main() {
-	int n, k, id, rnk;
+	int n, k, id;
 	cin >> n;
-	unordered_map<int, int> m;
+	unordered_map<int, int> exist, checked;
 	for (int i = 1; i <= n; i++) {
-		cin >> id;
-		m[id] = i;
+		scanf("%d", &id);
+		exist[id] = i;
 	}
 	cin >> k;
 	for (int i = 0; i < k; i++) {
-		cin >> id;
-		rnk = m[id];
-		if (rnk == 0)
-			printf("%04d: Are you kidding?\n", id);
-		else if (rnk == -1)
-			printf("%04d: Checked\n", id);
-		else if (rnk == 1) {
-			printf("%04d: Mystery Award\n", id);
-			m[id] = -1;
-		} else if (isPrime(rnk)) {
-			printf("%04d: Minion\n", id);
-			m[id] = -1;
-		} else {
-			printf("%04d: Chocolate\n", id);
-			m[id] = -1;
-		}
+		scanf("%d", &id);
+		if (exist[id]) {
+			if (checked[id]) {
+				printf("%04d: Checked\n", id);
+				continue;
+			} else {
+				if (exist[id] == 1) printf("%04d: Mystery Award\n", id);
+				else if (isprime(exist[id])) printf("%04d: Minion\n", id);
+				else printf("%04d: Chocolate\n", id);
+				checked[id] = 1;
+			}
+		} else printf("%04d: Are you kidding?\n", id);
 	}
 	return 0;
 }
