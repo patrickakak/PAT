d8a8122a6b3c2103bf8696b6c8f1c0728f8bbdb1
vuzway9132 <13806680065@163.com>
2020-03-08 14:58:19 +0800

    -

--- advanced level c++/1096 Consecutive Factors (20).cpp
+++ advanced level c++/1096 Consecutive Factors (20).cpp
@@ -4,25 +4,26 @@ using namespace std;
 int main() {
 	int n;
 	cin >> n;
-	int first = 0, len = 0, maxn = sqrt(n) + 1;
-	for (int i = 2; i <= maxn; i++) {
-		int j = i, fac = 1;
-		for ( ; j <= maxn; j++) {
-			fac *= j;
-			if (n % fac != 0) break;
+	int maxlen = 0, fir = -1, sqr = sqrt(n);
+	for (int i = 2; i <= sqr+1; i++) {
+		int fac = i, len = 0, j = i;
+		while (n%fac == 0) {
+			len++;
+			fac *= ++j;
 		}
-		if (j - i > len) {
-			len = j - i;
-			first = i;
+		if (len > maxlen) {
+			maxlen = len;
+			fir = i;
 		}
 	}
-	if (first == 0) printf("1\n%d", n);
-	else {
-		printf("%d\n", len);
-		for (int i = 0; i < len; i++) {
-			printf("%d", first+i);
-			if (i != len - 1) printf("*");
-		}
+	if (fir == -1) {
+		printf("1\n%d", n);
+		return 0;
+	}
+	printf("%d\n", maxlen);
+	for (int i = 0; i < maxlen; i++) {
+		if (i != 0) printf("*");
+		printf("%d", fir+i);
 	}
 	return 0;
 }
