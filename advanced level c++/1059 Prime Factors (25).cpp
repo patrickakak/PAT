1a532b5c203a97fa0f3fb5d8d3adb5fe9e6f1817
vuzway9132 <13806680065@163.com>
2020-03-08 22:53:00 +0800

    -

--- advanced level c++/1059 Prime Factors (25).cpp
+++ advanced level c++/1059 Prime Factors (25).cpp
@@ -1,30 +1,37 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-vector<int> prime(50000, 1);
+struct node {
+	long long p, k;
+};
+vector<node> v;
+vector<bool> prime(50000, true);
 int main() {
-	for (int i = 2; i * i < 50000; i++)
-		for (int j = 2; j * i < 50000; j++)
-			prime[j * i] = 0;
-	long int a;
-	scanf("%ld", &a);
-	printf("%ld=", a);
-	if (a == 1) printf("1");
-	bool state = false;
-	for (int i = 2; i < 50000 && a > 1; i++) {
-		int cnt = 0, flag = 0;
-		while (prime[i] == 1 && a % i == 0) {
-			cnt++;
-			a /= i;
-			flag = 1;
-		}
-		if (flag) {
-			if (state) printf("*");
-			state = true;
-			printf("%d", i);
-		}
-		if (cnt > 1) printf("^%d", cnt);
+	for (int i = 2; i*i < 50000; i++)
+		for (int j = 2; j*i < 50000; j++)
+			prime[j*i] = false;
+	long long n, t;
+	cin >> n;
+	printf("%lld=", n);
+	if (n == 1) {
+		printf("1");
+		return 0;
 	}
-	if (a > 1) printf("%s%ld", state ? "*" : "", a);
+	t = n;
+	for (int i = 2; i*i <= n; i++)
+		if (prime[i] == true && t%i == 0) {
+			int cnt = 0;
+			while (t%i == 0) {
+				cnt++;
+				t /= i;
+			}
+			v.push_back(node{i, cnt});
+		}
+	if (t != 1) v.push_back(node{t, 1});
+	for (int i = 0; i < v.size(); i++) {
+		if (i != 0) printf("*");
+		printf("%lld", v[i].p);
+		if (v[i].k > 1) printf("^%lld", v[i].k);
+	}
 	return 0;
 }
