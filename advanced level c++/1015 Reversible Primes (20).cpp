ff6e79480c26bf7172c584ea285dd1ee60bbb248
vuzway9132 <13806680065@163.com>
2020-03-08 22:16:19 +0800

    -

--- advanced level c++/1015 Reversible Primes (20).cpp
+++ advanced level c++/1015 Reversible Primes (20).cpp
@@ -2,27 +2,30 @@
 using namespace std;
 bool isprime(int n) {
 	if (n < 2) return false;
-	for (int i = 2; i * i <= n; i++)
-		if (n % i == 0) return false;
+	for (int i = 2; i*i <= n; i++)
+		if (n%i == 0) return false;
 	return true;
 }
 int main() {
 	int n, d;
-	while (scanf("%d", &n) != EOF) {
+	while (scanf("%d", &n)) {
 		if (n < 0) break;
 		scanf("%d", &d);
 		if (!isprime(n)) {
 			printf("No\n");
 			continue;
 		}
-		int len = 0, arr[100];
+		string s;
 		do {
-			arr[len++] = n % d;
-			n = n / d;
+			int r = n%d;
+			s += r + '0';
+			n /= d;
 		} while (n != 0);
-		for (int i = 0; i < len; i++)
-			n = n * d + arr[i];
-		printf("%s\n", isprime(n) ? "Yes" : "No");
+		int rev = 0;
+		for (int i = 0; i < s.length(); i++)
+			rev = rev * d + (s[i] - '0');
+		if (isprime(rev)) printf("Yes\n");
+		else printf("No\n");
 	}
 	return 0;
 }
