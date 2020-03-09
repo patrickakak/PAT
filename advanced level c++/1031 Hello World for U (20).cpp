c0ff8e1b957f4d319970a05819b56659aaf1ba48
vuzway9132 <13806680065@163.com>
2020-03-09 17:18:45 +0800

    -

--- advanced level c++/1031 Hello World for U (20).cpp
+++ advanced level c++/1031 Hello World for U (20).cpp
@@ -1,18 +1,19 @@
 #include <iostream>
-#include <cstring>
+#include <vector>
 using namespace std;
 int main() {
-	char c[81], u[30][30];
-	memset(u, ' ', sizeof(u));
-	scanf("%s", c);
-	int n = strlen(c) + 2;
-	int n1 = n / 3, n2 = n / 3 + n % 3, index = 0;
-	for (int i = 0; i < n1; i++) u[i][0] = c[index++];
-	for (int i = 1; i <= n2 - 2; i++) u[n1-1][i] = c[index++];
-	for (int i = n1 - 1; i >= 0; i--) u[i][n2-1] = c[index++];
+	string s;
+	cin >> s;
+	int n1 = 1, n2 = s.length(), cnt = 0;
+	for (; n2 >= n1; n2 -= 2, n1 += 1) ;
+	n2 += 2, n1 -= 1;
+	vector<vector<char>> v(n1, vector<char>(n2, ' '));
+	for (int j = 0; j < n1 && cnt < s.length(); j++) v[j][0] = s[cnt++];
+	for (int j = 1; j < n2 && cnt < s.length(); j++) v[n1-1][j] = s[cnt++];
+	for (int j = n1-2; j >= 0 && cnt < s.length(); j--) v[j][n2-1] = s[cnt++];
 	for (int i = 0; i < n1; i++) {
-		for (int j = 0; j < n2; j++) 
-			printf("%c", u[i][j]);
+		for (int j = 0; j < n2; j++)
+			printf("%c", v[i][j]);
 		printf("\n");
 	}
 	return 0;
