11d1b5b3f6a73d42769c243d759110b6d8253195
vuzway9132 <13806680065@163.com>
2020-03-10 20:06:04 +0800

    -

--- advanced level c++/1141 PAT Ranking of Institutions (25).cpp
+++ advanced level c++/1141 PAT Ranking of Institutions (25).cpp
@@ -7,7 +7,7 @@ struct node {
 	string uni;
 	int tws, ns;
 };
-bool cmp(node a, node b) {
+bool cmp(node &a, node &b) {
 	if (a.tws != b.tws) return a.tws > b.tws;
 	else if (a.ns != b.ns) return a.ns < b.ns;
 	else return a.uni < b.uni;
@@ -23,23 +23,22 @@ int main() {
 		double score;
 		scanf("%lf", &score);
 		cin >> uni;
-		for (int j = 0; j < uni.length(); j++)
-			uni[j] = tolower(uni[j]);
+		for (int j = 0; j < uni.length(); j++) uni[j] = tolower(uni[j]);
 		if (id[0] == 'B') score /= 1.5;
 		else if (id[0] == 'T') score *= 1.5;
 		sum[uni] += score;
 		cnt[uni]++;
 	}
-	vector<node> ans;
-	for (auto it = cnt.begin(); it != cnt.end(); it++)
-		ans.push_back(node{it->first, (int)sum[it->first], cnt[it->first]});
-	sort(ans.begin(), ans.end(), cmp);
+	vector<node> v;
+	for (auto it : cnt)
+		v.push_back(node{it.first, (int)sum[it.first], cnt[it.first]});
+	sort(v.begin(), v.end(), cmp);
 	int r = 0, pres = -1;
-	printf("%d\n", ans.size());
-	for (int i = 0; i < ans.size(); i++) {
-		if (pres != ans[i].tws) r = i + 1;
-		pres = ans[i].tws;
-		printf("%d %s %d %d\n", r, ans[i].uni.c_str(), ans[i].tws, ans[i].ns);
+	printf("%d\n", v.size());
+	for (int i = 0; i < v.size(); i++) {
+		if (pres != v[i].tws) r = i + 1;
+		pres = v[i].tws;
+		printf("%d %s %d %d\n", r, v[i].uni.c_str(), v[i].tws, v[i].ns);
 	}
 	return 0;
 }
