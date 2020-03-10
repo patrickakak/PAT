60e1bb2c0cb0c69f8119b6747e663907eadc5b7b
vuzway9132 <13806680065@163.com>
2020-03-10 11:37:06 +0800

    -

--- advanced level c++/1148 Werewolf - Simple Version (20).cpp
+++ advanced level c++/1148 Werewolf - Simple Version (20).cpp
@@ -5,7 +5,7 @@ int main() {
 	int n;
 	cin >> n;
 	vector<int> v(n+1);
-	for (int i = 1; i <= n; i++) cin >> v[i];
+	for (int i = 1; i <= n; i++) scanf("%d", &v[i]);
 	for (int i = 1; i <= n; i++)
 		for (int j = i + 1; j <= n; j++) {
 			vector<int> liar, a(n + 1, 1);
@@ -13,10 +13,10 @@ int main() {
 			for (int k = 1; k <= n; k++)
 				if (v[k] * a[abs(v[k])] < 0) liar.push_back(k);
 			if (liar.size() == 2 && a[liar[0]] + a[liar[1]] == 0) {
-				cout << i << ' ' << j;
+				printf("%d %d", i, j);
 				return 0;
 			}
 		}
-	cout << "No Solution";
+	printf("No Solution");
 	return 0;
 }
