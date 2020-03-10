d268ed8c5bfd4e5f9e7a0d7abeb692f4f0cb0cf8
vuzway9132 <13806680065@163.com>
2020-03-10 16:12:36 +0800

    -

--- advanced level c++/1100 Mars Numbers (20).cpp
+++ advanced level c++/1100 Mars Numbers (20).cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
-string a[13] = {"tret", "jan", "feb", "mar", "apr", "may", "jun", "jly", "aug", "sep", "oct", "nov", "dec"};
-string b[13] = {"####", "tam", "hel", "maa", "huh", "tou", "kes", "hei", "elo", "syy", "lok", "mer", "jou"};
+string a[13] = {"tret","jan","feb","mar","apr","may","jun","jly","aug","sep","oct","nov","dec"};
+string b[13] = {"####","tam","hel","maa","huh","tou","kes","hei","elo","syy","lok","mer","jou"};
 string s;
 int len;
-void func1(int t) {
+void f1(int t) {
 	if (t / 13) cout << b[t / 13];
 	if ((t / 13) && (t % 13)) cout << " ";
 	if (t % 13 || t == 0) cout << a[t % 13];
 }
-void func2() {
+void f2() {
 	int t1 = 0, t2 = 0;
 	string s1 = s.substr(0, 3), s2;
 	if (len > 4) s2 = s.substr(4);
@@ -26,8 +26,8 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		getline(cin, s);
 		len = s.length();
-		if (isdigit(s[0])) func1(stoi(s));
-		else func2();
+		if (isdigit(s[0])) f1(stoi(s));
+		else f2();
 		cout << endl;
 	}
 	return 0;
