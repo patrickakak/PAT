25014a47a89cda00e795ef2b61f9baeb7ab67df7
vuzway9132 <13806680065@163.com>
2020-03-12 17:24:08 +0800

    -

--- advanced level c++/1075 PAT Judge (25).cpp
+++ advanced level c++/1075 PAT Judge (25).cpp
@@ -14,35 +14,35 @@ bool cmp(node &a, node &b) {
 int main() {
 	int n, k, m, uid, pid, g;
 	scanf("%d%d%d", &n, &k, &m);
-	vector<int> p(k, -1);
-	vector<node> v(n);
-	for (int i = 0; i < n; i++) v[i].p = p;
-	for (int i = 0; i < k; i++) scanf("%d", &p[i]);
-	for (int i = 0; i < m; i++) {
+	vector<int> p(k+1, -1);
+	vector<node> v(n+1);
+	for (int i = 1; i <= n; i++) v[i].p = p;
+	for (int i = 1; i <= k; i++) scanf("%d", &p[i]);
+	while (m--) {
 		scanf("%d%d%d", &uid, &pid, &g);
-		v[uid-1].uid = uid;
-		int t = max(g, v[uid-1].p[pid-1]);
-		if (t == -1) v[uid-1].p[pid-1] = 0;
+		v[uid].uid = uid;
+		int t = max(g, v[uid].p[pid]);
+		if (t == -1) v[uid].p[pid] = 0;
 		else {
-			v[uid-1].print = 1;
-			v[uid-1].p[pid-1] = t;
+			v[uid].print = 1;
+			v[uid].p[pid] = t;
 		}
 	}
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < k; j++) {
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= k; j++) {
 			if (v[i].p[j] != -1) v[i].tot += v[i].p[j];
 			if (v[i].p[j] == p[j]) v[i].perf++;
 		}
-	sort(v.begin(), v.end(), cmp);
-	v[0].r = 1;
-	for (int i = 1; i < n; i++) {
-		v[i].r = i + 1;
+	sort(v.begin()+1, v.end(), cmp);
+	v[1].r = 1;
+	for (int i = 2; i <= n; i++) {
+		v[i].r = i;
 		if (v[i].tot == v[i-1].tot) v[i].r = v[i-1].r;
 	}
-	for (int i = 0; i < n; i++) {
+	for (int i = 1; i <= n; i++) {
 		if (!v[i].print) continue;
 		printf("%d %05d %d", v[i].r, v[i].uid, v[i].tot);
-		for (int j = 0; j < k; j++)
+		for (int j = 1; j <= k; j++)
 			if (v[i].p[j] != -1) printf(" %d", v[i].p[j]);
 			else printf(" -");
 		printf("\n");
