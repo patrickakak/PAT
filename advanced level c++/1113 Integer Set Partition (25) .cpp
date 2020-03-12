ae8964c0f953f0516d5d657e47d6c672058f62e3
vuzway9132 <13806680065@163.com>
2020-03-12 20:48:19 +0800

    -

--- advanced level c++/1113 Integer Set Partition (25) .cpp
+++ advanced level c++/1113 Integer Set Partition (25) .cpp
@@ -11,8 +11,7 @@ int main() {
 		sum += v[i];
 	}
 	sort(v.begin(), v.end());
-	for (int i = 0; i < n / 2; i++)
-		halfsum += v[i];
+	for (int i = 0; i < n / 2; i++) halfsum += v[i];
 	printf("%d %d", n % 2, sum - 2 * halfsum);
 	return 0;
 }
