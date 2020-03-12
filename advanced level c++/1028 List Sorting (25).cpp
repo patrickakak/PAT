1ba2044f564127ca95cc1b9e6a4fe0ea07293609
vuzway9132 <13806680065@163.com>
2020-03-12 14:57:20 +0800

    -

--- advanced level c++/1028 List Sorting (25).cpp
+++ advanced level c++/1028 List Sorting (25).cpp
@@ -4,21 +4,21 @@
 using namespace std;
 struct node {
 	int id, g;
-	string name;
+	string nm;
 };
 int n, c;
-bool cmp(node a, node b) {
+bool cmp(node &a, node &b) {
 	if (c == 1) return a.id < b.id;
-	else if (c == 2) return a.name != b.name ? a.name < b.name : a.id < b.id;
+	else if (c == 2) return a.nm != b.nm ? a.nm < b.nm : a.id < b.id;
 	else return a.g != b.g ? a.g < b.g : a.id < b.id;
 }
 int main() {
 	cin >> n >> c;
 	vector<node> v(n);
 	for (int i = 0; i < n; i++)
-		cin >> v[i].id >> v[i].name >> v[i].g;
+		cin >> v[i].id >> v[i].nm >> v[i].g;
 	sort(v.begin(), v.end(), cmp);
 	for (int i = 0; i < n; i++)
-		printf("%06d %s %d\n", v[i].id, v[i].name.c_str(), v[i].g);
+		printf("%06d %s %d\n", v[i].id, v[i].nm.c_str(), v[i].g);
 	return 0;
 }
