c312f0dee758b1f84c04467fbf55ba034f462481
vuzway9132 <13806680065@163.com>
2020-03-13 10:40:38 +0800

    -

--- advanced level c++/1036 Boys vs Girls (25).cpp
+++ advanced level c++/1036 Boys vs Girls (25).cpp
@@ -18,11 +18,11 @@ int main() {
 		}
 	}
 	if (m == "" || f == "") flag = 1;
-	if (f == "") cout << "Absent" << endl;
-	else cout << f << ' ' << fid << endl;
-	if (m == "") cout << "Absent" << endl;
-	else cout << m << ' ' << mid << endl;
-	if (flag == 1) cout << "NA";
-	else cout << hf - lm;
+	if (f == "") printf("Absent\n");
+	else printf("%s %s\n", f.c_str(), fid.c_str());
+	if (m == "") printf("Absent\n");
+	else printf("%s %s\n", m.c_str(), mid.c_str());
+	if (flag == 1) printf("NA");
+	else printf("%d", hf-lm);
 	return 0;
 }
