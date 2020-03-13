53c305781651a3e82f74550a526764f47468da6b
vuzway9132 <13806680065@163.com>
2020-03-13 16:26:43 +0800

    -

--- advanced level c++/1044 Shopping in Mars (25).cpp
+++ advanced level c++/1044 Shopping in Mars (25).cpp
@@ -11,12 +11,12 @@ int main() {
 		sum[i] = sum[i-1] + tmp;
 	}
 	for (int i = 1; i <= n; i++) {
-		int p = lower_bound(sum.begin()+i, sum.end(), sum[i-1]+m) - (sum.begin()+i);
-		if (i+p <= n && minn > sum[i+p]-sum[i-1]) minn = sum[i+p]-sum[i-1];
+		int p = lower_bound(sum.begin()+i, sum.end(), sum[i-1]+m) - sum.begin();
+		if (p <= n && minn > sum[p]-sum[i-1]) minn = sum[p]-sum[i-1];
 	}
 	for (int i = 1; i <= n; i++) {
-		int p = lower_bound(sum.begin()+i, sum.end(), sum[i-1]+m)-(sum.begin()+i);
-		if (p <= n && sum[i+p]-sum[i-1] == minn) printf("%d-%d\n", i, i+p);
+		int p = lower_bound(sum.begin()+i, sum.end(), sum[i-1]+m) - sum.begin();
+		if (p <= n && sum[p]-sum[i-1] == minn) printf("%d-%d\n", i, p);
 	}
 	return 0;
 }
