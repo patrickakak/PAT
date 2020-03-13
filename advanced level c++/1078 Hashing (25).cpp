e175a9a978a0d712188ee84370baf21aa5df3b9f
vuzway9132 <13806680065@163.com>
2020-03-13 14:12:14 +0800

    -

--- advanced level c++/1078 Hashing (25).cpp
+++ advanced level c++/1078 Hashing (25).cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int sz, n, h[10100];
 bool isprime(int n) {
-	if (n == 1) return false;
+	if (n < 2) return false;
 	for (int i = 2; i * i <= n; i++)
 		if (n % i == 0) return false;
 	return true;
@@ -12,19 +12,19 @@ void insert(int key) {
 		int index = (key + step * step) % sz;
 		if (h[index] == 0) {
 			h[index] = 1;
-			cout << index;
+			printf("%d", index);
 			return ;
 		}
 	}
-	cout << '-';
+	printf("-");
 }
 int main() {
 	cin >> sz >> n;
 	while (!isprime(sz)) sz++;
 	for (int i = 0; i < n; i++) {
 		int key;
-		cin >> key;
-		if (i != 0) cout << ' ';
+		scanf("%d", &key);
+		if (i != 0) printf(" ");
 		insert(key);
 	}
 	return 0;
