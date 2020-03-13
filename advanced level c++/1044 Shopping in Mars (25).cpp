0bf1eda13aa44799766b1cc98cd43566455a5447
vuzway9132 <13806680065@163.com>
2020-03-13 16:25:36 +0800

    -

--- advanced level c++/1044 Shopping in Mars (25).cpp
+++ advanced level c++/1044 Shopping in Mars (25).cpp
@@ -3,25 +3,20 @@
 #include <algorithm>
 using namespace std;
 int main() {
-	int n, m, d = 0x2fffffff;
-	scanf("%d%d", &n, &m);
-	vector<int> sum(n + 1, 0);
+	int n, m, tmp, minn = 0x2fffffff;
+	cin >> n >> m;
+	vector<int> sum(n+1);
 	for (int i = 1; i <= n; i++) {
-		scanf("%d", &sum[i]);
-		sum[i] += sum[i-1];
+		scanf("%d", &tmp);
+		sum[i] = sum[i-1] + tmp;
 	}
 	for (int i = 1; i <= n; i++) {
-		int j = lower_bound(sum.begin() + i, sum.end(), sum[i-1] + m) - sum.begin();
-		if (j <= n && sum[j] - sum[i-1] == m) {
-			d = m;
-			break;
-		} else if (j <= n && sum[j] - sum[i-1] < d)
-			d = sum[j] - sum[i-1];
+		int p = lower_bound(sum.begin()+i, sum.end(), sum[i-1]+m) - (sum.begin()+i);
+		if (i+p <= n && minn > sum[i+p]-sum[i-1]) minn = sum[i+p]-sum[i-1];
 	}
 	for (int i = 1; i <= n; i++) {
-		int j = lower_bound(sum.begin() + i, sum.end(), sum[i-1] + d) - sum.begin();
-		if (j <= n && sum[j] - sum[i-1] == d)
-			printf("%d-%d\n", i, j);
+		int p = lower_bound(sum.begin()+i, sum.end(), sum[i-1]+m)-(sum.begin()+i);
+		if (p <= n && sum[i+p]-sum[i-1] == minn) printf("%d-%d\n", i, i+p);
 	}
 	return 0;
 }
