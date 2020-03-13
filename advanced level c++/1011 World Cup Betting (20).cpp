83fe7b9f285089b6eba1270df3bec05c789ef880
vuzway9132 <13806680065@163.com>
2020-03-13 10:37:36 +0800

    -

--- advanced level c++/1011 World Cup Betting (20).cpp
+++ advanced level c++/1011 World Cup Betting (20).cpp
@@ -5,10 +5,10 @@ int main() {
 	string s = "WTL";
 	double profit = 1, a[3] = {0};
 	for (int i = 0; i < 3; i++) {
-		cin >> a[0] >> a[1] >> a[2];
+		scanf("%lf%lf%lf", &a[0], &a[1], &a[2]);
 		int p = max_element(a, a + 3) - a;
-		if (i != 0) cout << ' ';
-		cout << s[p];
+		if (i != 0) printf(" ");
+		printf("%c", s[p]);
 		profit *= a[p];
 	}
 	printf(" %.2f", 2 * (profit * .65 - 1));
