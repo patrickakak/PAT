137f947884ac84fb674ccb73cc6924a8888f2655
vuzway9132 <13806680065@163.com>
2020-03-14 21:38:03 +0800

    -

--- advanced level c++/1151 LCA in a Binary Tree (30).cpp
+++ advanced level c++/1151 LCA in a Binary Tree (30).cpp
@@ -4,15 +4,15 @@
 using namespace std;
 unordered_map<int, int> pos;
 vector<int> in, pre;
-void lca(int inl, int inr, int prel, int prer, int u, int v) {
+void lca(int prel, int prer, int inl, int inr, int u, int v) {
 	if (inl > inr) return;
 	int rin = pos[pre[prel]], uin = pos[u], vin = pos[v];
 	if (uin < rin && vin < rin)
-		lca(inl, rin-1, prel+1, prel+(rin-inl)-1, u, v);
+		lca(prel+1, prel+(rin-inl)-1, inl, rin-1, u, v);
 	else if ((uin < rin && vin > rin) || (uin > rin && vin < rin))
 		printf("LCA of %d and %d is %d.\n", u, v, in[rin]);
 	else if (uin > rin && vin > rin)
-		lca(rin+1, inr, prel+1+(rin-inl), prer, u, v);
+		lca(prel+1+(rin-inl), prer, rin+1, inr, u, v);
 	else printf("%d is an ancestor of %d.\n", in[rin], uin == rin ? v : u);
 }
 int main() {
