2e4765c745a71d61183e9f708e28d102d344f1b9
vuzway9132 <13806680065@163.com>
2020-03-14 10:08:29 +0800

    -

--- advanced level c++/1037 Magic Coupon (25).cpp
+++ advanced level c++/1037 Magic Coupon (25).cpp
@@ -6,16 +6,16 @@ int main() {
 	int nc, np, ans = 0;
 	cin >> nc;
 	vector<int> c(nc);
-	for (int i = 0; i < nc; i++) cin >> c[i];
+	for (int i = 0; i < nc; i++) scanf("%d", &c[i]);
 	cin >> np;
 	vector<int> p(np);
-	for (int i = 0; i < np; i++) cin >> p[i];
+	for (int i = 0; i < np; i++) scanf("%d", &p[i]);
 	sort(c.begin(), c.end());
 	sort(p.begin(), p.end());
 	for (int i = 0, j = 0; i < nc && j < np && c[i] < 0 && p[j] < 0; i++, j++)
 		ans += c[i] * p[j];
 	for (int i = nc - 1, j = np - 1; i >= 0 && j >= 0 && c[i] > 0 && p[j] > 0; i--, j--)
 		ans += c[i] * p[j];
-	cout << ans;
+	printf("%d", ans);
 	return 0;
 }
