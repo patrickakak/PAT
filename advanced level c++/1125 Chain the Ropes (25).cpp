2862375312526afe5065d0113ef8fc33187980ed
vuzway9132 <13806680065@163.com>
2020-03-14 11:00:23 +0800

    -

--- advanced level c++/1125 Chain the Ropes (25).cpp
+++ advanced level c++/1125 Chain the Ropes (25).cpp
@@ -6,12 +6,10 @@ int main() {
 	int n;
 	cin >> n;
 	vector<int> v(n);
-	for (int i = 0; i < n; i++)
-		cin >> v[i];
+	for (int i = 0; i < n; i++) scanf("%d", &v[i]);
 	sort(v.begin(), v.end());
 	int ans = v[0];
-	for (int i = 1; i < n; i++)
-		ans = (ans + v[i]) / 2;
-	cout << ans;
+	for (int i = 1; i < n; i++) ans = (ans + v[i]) / 2;
+	printf("%d", ans);
 	return 0;
 }
