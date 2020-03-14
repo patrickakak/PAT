7a3cca455d95dc7d1abf2b7a2d3b6e5571150e35
vuzway9132 <13806680065@163.com>
2020-03-14 10:59:04 +0800

    -

--- advanced level c++/1070 Mooncake (25).cpp
+++ advanced level c++/1070 Mooncake (25).cpp
@@ -5,13 +5,15 @@ using namespace std;
 struct mooncake {
 	double amt, per, tot;
 };
-bool cmp(mooncake a, mooncake b) { return a.per > b.per; }
+bool cmp(mooncake &a, mooncake &b) {
+	return a.per > b.per;
+}
 int main() {
 	int n, d;
 	cin >> n >> d;
 	vector<mooncake> a(n);
-	for (int i = 0; i < n; i++) cin >> a[i].amt;
-	for (int i = 0; i < n; i++) cin >> a[i].tot;
+	for (int i = 0; i < n; i++) scanf("%lf", &a[i].amt);
+	for (int i = 0; i < n; i++) scanf("%lf", &a[i].tot);
 	for (int i = 0; i < n; i++) a[i].per = a[i].tot / a[i].amt;
 	sort(a.begin(), a.end(), cmp);
 	double res = 0;
