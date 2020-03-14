2e5bb5a37a57b7840993423edca98d10f77fbe7f
vuzway9132 <13806680065@163.com>
2020-03-14 10:56:34 +0800

    -

--- advanced level c++/1067 Sort with Swap(0, i) (25).cpp
+++ advanced level c++/1067 Sort with Swap(0, i) (25).cpp
@@ -1,23 +1,24 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int main() {
-	int n, t, cnt = 0, a[100000];
-	scanf("%d", &n);
+	int n, cnt = 0;
+	cin >> n;
+	vector<int> a(n), p(n);
 	for (int i = 0; i < n; i++) {
-		scanf("%d", &t);
-		a[t] = i;
+		scanf("%d", &a[i]);
+		p[a[i]] = i;
 	}
-	for (int i = 1; i < n; i++)
-		if (i != a[i]) {
-			while (a[0] != 0) {
-				swap(a[0], a[a[0]]);
-				cnt++;
-			}
-			if (i != a[i]) {
-				swap(a[0], a[i]);
-				cnt++;
-			}
+	for (int i = 1; i < n; i++) {
+		while (p[0] != 0) {
+			swap(p[0], p[p[0]]);
+			cnt++;
 		}
+		if (p[i] != i) {
+			swap(p[i], p[0]);
+			cnt++;
+		}
+	}
 	printf("%d", cnt);
 	return 0;
 }
