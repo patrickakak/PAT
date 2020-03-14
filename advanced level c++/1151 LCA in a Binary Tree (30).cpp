ce4e4422439d016ef536e3620a563854b7e59968
vuzway9132 <13806680065@163.com>
2020-03-14 22:01:48 +0800

    -

--- advanced level c++/1151 LCA in a Binary Tree (30).cpp
+++ advanced level c++/1151 LCA in a Binary Tree (30).cpp
@@ -4,19 +4,19 @@
 using namespace std;
 unordered_map<int, int> pos;
 vector<int> pre, in;
-void lca(int prel, int prer, int inl, int inr, int u, int v) {
+int m, n, u, v;
+void lca(int prel, int prer, int inl, int inr) {
 	if (inl > inr) return;
 	int ain = pos[pre[prel]], uin = pos[u], vin = pos[v];
 	if (uin < ain && vin < ain)
-		lca(prel+1, prel+(ain-inl)-1, inl, ain-1, u, v);
+		lca(prel+1, prel+(ain-inl)-1, inl, ain-1);
+	else if (uin > ain && vin > ain)
+		lca(prel+1+(ain-inl), prer, ain+1, inr);
 	else if ((uin < ain && vin > ain) || (uin > ain && vin < ain))
 		printf("LCA of %d and %d is %d.\n", u, v, in[ain]);
-	else if (uin > ain && vin > ain)
-		lca(prel+1+(ain-inl), prer, ain+1, inr, u, v);
 	else printf("%d is an ancestor of %d.\n", in[ain], uin == ain ? v : u);
 }
 int main() {
-	int m, n, u, v;
 	scanf("%d %d", &m, &n);
 	pre.resize(n + 1), in.resize(n + 1);
 	for (int i = 1; i <= n; i++) {
@@ -30,7 +30,7 @@ int main() {
 			printf("ERROR: %d and %d are not found.\n", u, v);
 		else if (pos[u] == 0 || pos[v] == 0)
 			printf("ERROR: %d is not found.\n", pos[u] == 0 ? u : v);
-		else lca(1, n, 1, n, u, v);
+		else lca(1, n, 1, n);
 	}
 	return 0;
 }
