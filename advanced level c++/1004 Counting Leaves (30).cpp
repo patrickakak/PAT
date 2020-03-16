06be2f2169d1f065ac3d289c4ceb7c695eae095a
vuzway9132 <13806680065@163.com>
2020-03-16 15:15:58 +0800

    -

--- advanced level c++/1004 Counting Leaves (30).cpp
+++ advanced level c++/1004 Counting Leaves (30).cpp
@@ -4,14 +4,14 @@
 using namespace std;
 vector<int> v[100];
 int book[100], maxdepth = -1;
-void dfs(int index, int depth) {
-	if (v[index].size() == 0) {
+void dfs(int idx, int depth) {
+	if (v[idx].size() == 0) {
 		book[depth]++;
 		maxdepth = max(maxdepth, depth);
 		return ;
 	}
-	for (int i = 0; i < v[index].size(); i++)
-		dfs(v[index][i], depth + 1);
+	for (int i = 0; i < v[idx].size(); i++)
+		dfs(v[idx][i], depth + 1);
 }
 int main() {
 	int n, m, k, node, c;
