4e5e4cdc8c4643978d8add5f6afe61c5a59056cc
vuzway9132 <13806680065@163.com>
2020-03-18 16:20:46 +0800

    -

--- advanced level c++/1091 Acute Stroke (30).cpp
+++ advanced level c++/1091 Acute Stroke (30).cpp
@@ -8,17 +8,17 @@ int X[] = {1, -1, 0, 0, 0, 0};
 int Y[] = {0, 0, 1, -1, 0, 0};
 int Z[] = {0, 0, 0, 0, 1, -1};
 int m, n, l, t, a[1300][130][80];
-bool vis[1300][130][80];
+bool inq[1300][130][80];
 bool judge(int x, int y, int z) {
 	if (x < 0 || x >= m || y < 0 || y >= n || z < 0 || z >= l) return false;
-	if (a[x][y][z] == 0 || vis[x][y][z] == true) return false;
+	if (a[x][y][z] == 0 || inq[x][y][z] == true) return false;
 	return true;
 }
 int bfs(int x, int y, int z) {
 	int cnt = 0;
 	queue<node> q;
 	q.push(node{x, y, z});
-	vis[x][y][z] = true;
+	inq[x][y][z] = true;
 	while (!q.empty()) {
 		node top = q.front();
 		q.pop();
@@ -26,7 +26,7 @@ int bfs(int x, int y, int z) {
 		for (int i = 0; i < 6; i++) {
 			int tx = top.x + X[i], ty = top.y + Y[i], tz = top.z + Z[i];
 			if (judge(tx, ty, tz)) {
-				vis[tx][ty][tz] = true;
+				inq[tx][ty][tz] = true;
 				q.push(node{tx, ty, tz});
 			}
 		}
@@ -43,7 +43,7 @@ int main() {
 	for (int i = 0; i < l; i++)
 		for (int j = 0; j < m; j++)
 			for (int k = 0; k < n; k++)
-				if (a[j][k][i] == 1 && vis[j][k][i] == false)
+				if (a[j][k][i] == 1 && inq[j][k][i] == false)
 					ans += bfs(j, k, i);
 	printf("%d", ans);
 	return 0;
