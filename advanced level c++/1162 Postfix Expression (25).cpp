5f6c2c9702fd8965ce30714047026608cc6697ed
vuzway9132 <13806680065@163.com>
2020-03-18 17:32:08 +0800

    -

--- advanced level c++/1162 Postfix Expression (25).cpp
+++ advanced level c++/1162 Postfix Expression (25).cpp
@@ -1,29 +1,27 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 struct node {
 	string s;
 	int l, r;
-};
-vector<node> v(25);
+} a[25];
 int h[25];
 void dfs(int root) {
 	if (root == -1) return;
-	int cnt = (v[root].l == -1) + (v[root].r == -1);
+	int cnt = (a[root].l == -1) + (a[root].r == -1);
 	printf("(");
-	if (cnt == 1) cout << v[root].s;
-	dfs(v[root].l);
-	dfs(v[root].r);
-	if (cnt != 1) cout << v[root].s;
+	if (cnt == 1) cout << a[root].s;
+	dfs(a[root].l);
+	dfs(a[root].r);
+	if (cnt != 1) cout << a[root].s;
 	printf(")");
 }
 int main() {
 	int n, root = 1;
 	cin >> n;
 	for (int i = 1; i <= n; i++) {
-		cin >> v[i].s >> v[i].l >> v[i].r;
-		if (v[i].l != -1) h[v[i].l] = 1;
-		if (v[i].r != -1) h[v[i].r] = 1;
+		cin >> a[i].s >> a[i].l >> a[i].r;
+		if (a[i].l != -1) h[a[i].l] = 1;
+		if (a[i].r != -1) h[a[i].r] = 1;
 	}
 	while (root <= n && h[root] == 1) root++;
 	dfs(root);
