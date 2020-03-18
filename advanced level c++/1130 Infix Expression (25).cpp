e46f542af589882f4b5d39ebcefb5411bf077347
vuzway9132 <13806680065@163.com>
2020-03-18 17:30:30 +0800

    -

--- advanced level c++/1130 Infix Expression (25).cpp
+++ advanced level c++/1130 Infix Expression (25).cpp
@@ -3,14 +3,14 @@ using namespace std;
 struct node {
 	string s;
 	int l, r;
-} a[100];
+} a[25];
 string inOrder(int root) {
 	if (root == -1) return "";
 	if (a[root].l == -1 && a[root].r == -1) return a[root].s;
 	else return "(" + inOrder(a[root].l) + a[root].s + inOrder(a[root].r) + ")";
 }
 int main() {
-	int h[100] = {0}, n, root = 1;
+	int h[25] = {0}, n, root = 1;
 	cin >> n;
 	for (int i  = 1; i <= n; i++) {
 		cin >> a[i].s >> a[i].l >> a[i].r;
