eb4b60205820147ce7b0d48040bf6a72c8ac70ff
vuzway9132 <13806680065@163.com>
2020-03-18 22:22:52 +0800

    -

--- advanced level c++/1131 Subway Map (30).cpp
+++ advanced level c++/1131 Subway Map (30).cpp
@@ -3,34 +3,34 @@
 #include <unordered_map>
 using namespace std;
 vector<vector<int>> v(10000);
-int visit[10000], minCnt, minTrans, src, dest;
+int vis[10000], minCnt, minTrans, src, dest;
 unordered_map<int, int> line;
 vector<int> path, tmpPath;
 int transCnt(vector<int> &a) {
-	int transCnt = -1, preLine = 0;
+	int cnt = -1, preLine = 0;
 	for (int i = 1; i < a.size(); i++) {
-		if (line[a[i-1] * 10000 + a[i]] != preLine) transCnt++;
-		preLine = line[a[i-1] * 10000 + a[i]];
+		if (line[a[i-1]*10000 + a[i]] != preLine) cnt++;
+		preLine = line[a[i-1]*10000 + a[i]];
 	}
-	return transCnt;
+	return cnt;
 }
-void dfs(int node, int cnt) {
-	tmpPath.push_back(node);
-	visit[node] = 1;
-	if (node == dest) {
+void dfs(int idx, int cnt) {
+	tmpPath.push_back(idx);
+	vis[idx] = 1;
+	if (idx == dest) {
 		if (cnt < minCnt || (cnt == minCnt && transCnt(tmpPath) < minTrans)) {
 			minCnt = cnt;
 			minTrans = transCnt(tmpPath);
 			path = tmpPath;
 		}
 		tmpPath.pop_back();
-		visit[node] = 0;
+		vis[idx] = 0;
 		return;
 	}
-	for (int i = 0; i < v[node].size(); i++)
-		if (!visit[v[node][i]]) dfs(v[node][i], cnt + 1);
+	for (int i = 0; i < v[idx].size(); i++)
+		if (!vis[v[idx][i]]) dfs(v[idx][i], cnt+1);
 	tmpPath.pop_back();
-	visit[node] = 0;
+	vis[idx] = 0;
 }
 int main() {
 	int n, m, k, pre, sid;
@@ -41,7 +41,7 @@ int main() {
 			scanf("%d", &sid);
 			v[pre].push_back(sid);
 			v[sid].push_back(pre);
-			line[pre * 10000 + sid] = line[sid * 10000 + pre] = i + 1;
+			line[pre*10000 + sid] = line[sid*10000 + pre] = i + 1;
 			pre = sid;
 		}
 	}
@@ -54,9 +54,9 @@ int main() {
 		printf("%d\n", minCnt);
 		int preLine = 0, preTrans = src;
 		for (int i = 1; i < path.size(); i++)
-			if (line[path[i-1] * 10000 + path[i]] != preLine) {
+			if (line[path[i-1]*10000 + path[i]] != preLine) {
 				if (preLine != 0) printf("Take Line#%d from %04d to %04d.\n", preLine, preTrans, path[i-1]);
-				preLine = line[path[i-1] * 10000 + path[i]];
+				preLine = line[path[i-1]*10000 + path[i]];
 				preTrans = path[i-1];
 			}
 		printf("Take Line#%d from %04d to %04d.\n", preLine, preTrans, dest);
