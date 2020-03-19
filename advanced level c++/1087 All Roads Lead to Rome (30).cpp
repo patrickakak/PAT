d72b9a91fca946b5cbb24ecff9cdba218411d298
vuzway9132 <13806680065@163.com>
2020-03-19 16:35:13 +0800

    -

--- advanced level c++/1087 All Roads Lead to Rome (30).cpp
+++ advanced level c++/1087 All Roads Lead to Rome (30).cpp
@@ -3,21 +3,20 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-const int inf = 0x3fffffff;
-int e[205][205], weight[205], dis[205];
-bool visit[205];
+const int inf = 0x2fffffff;
+int n, k, maxvalue = 0, mindepth = inf, cntpath = 0, e[205][205], weight[205], dis[205];
+double maxavg;
+bool vis[205];
 vector<int> pre[205], tmppath, path;
 map<string, int> s2i;
 map<int, string> i2s;
-int n, k, maxvalue = 0, mindepth = inf, cntpath = 0;
-double maxavg;
 void dfs(int v) {
 	tmppath.push_back(v);
 	if (v == 1) {
 		int value = 0;
 		for (int i = 0; i < tmppath.size(); i++)
 			value += weight[tmppath[i]];
-		double tmpavg = 1.0 * value / (tmppath.size() - 1);
+		double tmpavg = 1.0 * value / (tmppath.size()-1);
 		if (value > maxvalue) {
 			maxvalue = value;
 			maxavg = tmpavg;
@@ -36,7 +35,8 @@ void dfs(int v) {
 int main() {
 	fill(e[0], e[0] + 205 * 205, inf);
 	fill(dis, dis + 205, inf);
-	string s;
+	string s, sa, sb;
+	int hap;
 	cin >> n >> k >> s;
 	s2i[s] = 1;
 	i2s[1] = s;
@@ -45,8 +45,6 @@ int main() {
 		s2i[s] = i + 1;
 		i2s[i + 1] = s;
 	}
-	string sa, sb;
-	int hap;
 	for (int i = 0; i < k; i++) {
 		cin >> sa >> sb >> hap;
 		e[s2i[sa]][s2i[sb]] = e[s2i[sb]][s2i[sa]] = hap;
@@ -55,14 +53,14 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		int u = -1, minn = inf;
 		for (int j = 1; j <= n; j++)
-			if (!visit[j] && dis[j] < minn) {
+			if (!vis[j] && dis[j] < minn) {
 				u = j;
 				minn = dis[j];
 			}
 		if (u == -1) break;
-		visit[u] = true;
+		vis[u] = true;
 		for (int v = 1; v <= n; v++)
-			if (!visit[v] && e[u][v] != inf) {
+			if (!vis[v] && e[u][v] != inf) {
 				if (dis[u] + e[u][v] < dis[v]) {
 					dis[v] = dis[u] + e[u][v];
 					pre[v].clear();
@@ -74,7 +72,7 @@ int main() {
 	int rom = s2i["ROM"];
 	dfs(rom);
 	printf("%d %d %d %d\n", cntpath, dis[rom], maxvalue, (int)maxavg);
-	for (int i = path.size() - 1; i >= 1; i--) cout << i2s[path[i]] << "->";
-	cout << "ROM";
+	for (int i = path.size()-1; i >= 1; i--) printf("%s->", i2s[path[i]].c_str());
+	printf("ROM");
 	return 0;
 }
