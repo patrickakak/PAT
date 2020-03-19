a74b15f99084c64b558f0caf61059a1062c557a5
vuzway9132 <13806680065@163.com>
2020-03-19 10:03:09 +0800

    -

--- advanced level c++/1021 Deepest Root (25).cpp
+++ advanced level c++/1021 Deepest Root (25).cpp
@@ -3,22 +3,22 @@
 #include <set>
 #include <algorithm>
 using namespace std;
-int n, maxheight = 0;
+int n, maxht = 0;
 vector<vector<int>> v;
-bool visit[10010];
+bool vis[10010];
 set<int> s;
 vector<int> tmp;
-void dfs(int node, int height) {
-	if (height > maxheight) {
+void dfs(int node, int ht) {
+	if (ht > maxht) {
 		tmp.clear();
 		tmp.push_back(node);
-		maxheight = height;
-	} else if (height == maxheight)
+		maxht = ht;
+	} else if (ht == maxht)
 		tmp.push_back(node);
-	visit[node] = true;
+	vis[node] = true;
 	for (int i = 0; i < v[node].size(); i++)
-		if (!visit[v[node][i]])
-			dfs(v[node][i], height + 1);
+		if (!vis[v[node][i]])
+			dfs(v[node][i], ht + 1);
 }
 int main() {
 	scanf("%d", &n);
@@ -30,19 +30,19 @@ int main() {
 		v[b].push_back(a);
 	}
 	for (int i = 1; i <= n; i++)
-		if (!visit[i]) {
+		if (!vis[i]) {
 			dfs(i, 1);
 			if (i == 1) {
 				if (tmp.size() != 0) src = tmp[0];
-				for (auto j : tmp) s.insert(j);
+				for (auto it : tmp) s.insert(it);
 			}
 			cnt++;
 		}
 	if (cnt > 1) printf("Error: %d components", cnt);
 	else {
 		tmp.clear();
-		maxheight = 0;
-		fill(visit, visit + 10010, false);
+		maxht = 0;
+		fill(vis, vis + 10010, false);
 		dfs(src, 1);
 		for (auto i : tmp) s.insert(i);
 		for (auto it : s) printf("%d\n", it);
