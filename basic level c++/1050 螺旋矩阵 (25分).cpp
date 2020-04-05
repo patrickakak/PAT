d8e8b845f78fa591ad931f210db86eb3f2793083
vuzway9132 <13806680065@163.com>
2020-04-05 20:27:05 +0800

    -

--- basic level c++/1050 螺旋矩阵 (25分).cpp
+++ basic level c++/1050 螺旋矩阵 (25分).cpp
@@ -15,21 +15,17 @@ int main() {
 	for (int i = 0; i < N; i++) scanf("%d", &a[i]);
 	sort(a.rbegin(), a.rend());
 	vector<vector<int>> b(m, vector<int>(n));
-	int level = m / 2 + m % 2;
+	int level = m/2 + m%2;
 	for (int i = 0; i < level; i++) {
-		for (int j = i; j <= n - 1 - i && t < N; j++)
-			b[i][j] = a[t++];
-		for (int j = i + 1; j <= m - 2 - i && t < N; j++)
-			b[j][n - 1 - i] = a[t++];
-		for (int j = n - i - 1; j >= i && t < N; j--)
-			b[m - 1 - i][j] = a[t++];
-		for (int j = m - 2 - i; j >= i + 1 && t < N; j--)
-			b[j][i] = a[t++];
+		for (int j = i; j <= n-1-i && t < N; j++) b[i][j] = a[t++];
+		for (int j = i+1; j <= m-2-i && t < N; j++) b[j][n-1-i] = a[t++];
+		for (int j = n-i-1; j >= i && t < N; j--) b[m-1-i][j] = a[t++];
+		for (int j = m-2-i; j >= i+1 && t < N; j--) b[j][i] = a[t++];
 	}
 	for (int i = 0; i < m; i++) {
 		for (int j = 0 ; j < n; j++) {
 			printf("%d", b[i][j]);
-			if (j != n - 1) printf(" ");
+			if (j != n-1) printf(" ");
 		}
 		printf("\n");
 	}
