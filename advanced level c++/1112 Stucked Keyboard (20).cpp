bb62576043911998cbfad9598dd098fb9a190859
vuzway9132 <13806680065@163.com>
2020-04-11 23:38:43 +0800

    -

--- advanced level c++/1112 Stucked Keyboard (20).cpp
+++ advanced level c++/1112 Stucked Keyboard (20).cpp
@@ -2,7 +2,7 @@
 #include <map>
 #include <set>
 using namespace std;
-bool sureOK[256];
+bool ok[256];
 int main() {
 	int k, cnt = 1;
 	scanf("%d", &k);
@@ -15,14 +15,14 @@ int main() {
 	for (int i = 0; i < s.length(); i++) {
 		if (s[i] == pre) cnt++;
 		else {
-			if (cnt % k != 0) sureOK[pre] = true;
+			if (cnt % k != 0) ok[pre] = true;
 			cnt = 1;
 		}
 		if (cnt % k == 0) m[s[i]] = true;
 		pre = s[i];
 	}
 	for (int i = 0; i < s.length()-1; i++)
-		if (sureOK[s[i]]) m[s[i]] = false;
+		if (ok[s[i]]) m[s[i]] = false;
 	for (int i = 0; i < s.length()-1; i++)
 		if (m[s[i]] == true && printed.find(s[i]) == printed.end()) {
 			printf("%c", s[i]);
