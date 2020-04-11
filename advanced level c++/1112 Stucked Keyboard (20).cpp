4f9f5c96dd27b7d42d579c16b644607ca379fde2
vuzway9132 <13806680065@163.com>
2020-04-11 23:46:22 +0800

    -

--- advanced level c++/1112 Stucked Keyboard (20).cpp
+++ advanced level c++/1112 Stucked Keyboard (20).cpp
@@ -1,37 +1,29 @@
 #include <iostream>
-#include <map>
-#include <set>
+#include <vector>
 using namespace std;
-bool ok[256];
 int main() {
-	int k, cnt = 1;
-	scanf("%d", &k);
-	string s;
-	cin >> s;
-	map<char, bool> m;
-	set<char> printed;
+	int k, cnt = 1, ok[256];
+	string s, suspect;
+	cin >> k >> s;
 	char pre = '#';
 	s += '#';
 	for (int i = 0; i < s.length(); i++) {
 		if (s[i] == pre) cnt++;
 		else {
-			if (cnt % k != 0) ok[pre] = true;
+			if (cnt % k == 0) {
+				if (suspect.find(pre) == string::npos)
+					suspect += pre;
+			} else ok[pre] = 1;
+			pre = s[i];
 			cnt = 1;
 		}
-		if (cnt % k == 0) m[s[i]] = true;
-		pre = s[i];
 	}
-	for (int i = 0; i < s.length()-1; i++)
-		if (ok[s[i]]) m[s[i]] = false;
-	for (int i = 0; i < s.length()-1; i++)
-		if (m[s[i]] == true && printed.find(s[i]) == printed.end()) {
-			printf("%c", s[i]);
-			printed.insert(s[i]);
-		}
-	printf("\n");
+	for (int i = 0; i < suspect.length(); i++)
+		if (ok[suspect[i]] == 1) suspect.erase(i);
+	cout << suspect << endl;
 	for (int i = 0; i < s.length()-1; i++) {
 		printf("%c", s[i]);
-		if (m[s[i]]) i += k - 1;
+		if (suspect.find(s[i]) != string::npos) i += k-1;
 	}
 	return 0;
 }
