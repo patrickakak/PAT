09f86ed5d3415ecf3b8063a5d6c0f309e34b1e37
vuzway9132 <13806680065@163.com>
2020-04-13 14:13:55 +0800

    -

--- advanced level c++/1135 Is It A Red-Black Tree (30).cpp
+++ advanced level c++/1135 Is It A Red-Black Tree (30).cpp
@@ -14,24 +14,24 @@ node *build(node *rt, int v) {
 	else rt->r = build(rt->r, v);
 	return rt;
 }
-bool judge1(node *rt) {
+bool f1(node *rt) {
 	if (rt == NULL) return true;
 	if (rt->key < 0) {
 		if (rt->l && rt->l->key < 0) return false;
 		if (rt->r && rt->r->key < 0) return false;
 	}
-	return judge1(rt->l) && judge1(rt->r);
+	return f1(rt->l) && f1(rt->r);
 }
-int getNum(node *rt) {
+int cnt(node *rt) {
 	if (rt == NULL) return 1;
-	int l = getNum(rt->l);
-	int r = getNum(rt->r);
+	int l = cnt(rt->l);
+	int r = cnt(rt->r);
 	return rt->key > 0 ? max(l, r) + 1 : max(l, r);
 }
-bool judge2(node *rt) {
+bool f2(node *rt) {
 	if (rt == NULL) return true;
-	if (getNum(rt->l) != getNum(rt->r)) return false;
-	return judge2(rt->l) && judge2(rt->r);
+	if (cnt(rt->l) != cnt(rt->r)) return false;
+	return f2(rt->l) && f2(rt->r);
 }
 int main() {
 	int k, n;
@@ -44,7 +44,7 @@ int main() {
 			scanf("%d", &a[j]);
 			root = build(root, a[j]);
 		}
-		if (a[0] < 0 || judge1(root) == false || judge2(root) == false) printf("No\n");
+		if (a[0] < 0 || f1(root) == false || f2(root) == false) printf("No\n");
 		else printf("Yes\n");
 	}
 	return 0;
