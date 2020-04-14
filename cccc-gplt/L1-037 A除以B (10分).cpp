de26d934c39528943b97874a216caa3b1147ba5c
vuzway9132 <13806680065@163.com>
2020-04-14 11:26:04 +0800

    -

---
+++ cccc-gplt/L1-037 A除以B (10分).cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a, b;
+	scanf("%d%d", &a, &b);
+	if (b > 0) printf("%d/%d=%.2f", a, b, (double)a/b);
+	else if (b < 0) printf("%d/(%d)=%.2f", a, b, (double)a/b);
+	else printf("%d/%d=Error", a, b);
+	return 0;
+}
