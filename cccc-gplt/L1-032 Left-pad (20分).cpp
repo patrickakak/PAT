9fc1a1b3856aa563bda585ff0357ec32d77d33f9
vuzway9132 <13806680065@163.com>
2020-04-14 18:36:46 +0800

    -

---
+++ cccc-gplt/L1-032 Left-pad (20分).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+	int n;
+	char c;
+	string s;
+	scanf("%d %c\n", &n, &c);
+	getline(cin, s);
+	reverse(s.begin(), s.end());
+	if (s.length() < n)
+    for (int i=0, d=n-s.length(); i < d; i++)
+      s += c;
+	else
+    s = s.substr(0, n);
+	reverse(s.begin(), s.end());
+	printf("%s\n", s.c_str());
+	return 0;
+}
