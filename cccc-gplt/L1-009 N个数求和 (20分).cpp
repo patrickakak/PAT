29a8076413e17b8bf8b14f4e7b84fc88fc644012
vuzway9132 <13806680065@163.com>
2020-04-14 14:12:54 +0800

    -

---
+++ cccc-gplt/L1-009 N个数求和 (20分).cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+long long gcd(long long a, long long b) {
+	return !b ? abs(a) : gcd(b, a % b);
+}
+int main() {
+	long long n, a, b, suma = 0, sumb = 1, d;
+	scanf("%lld", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%lld/%lld", &a, &b);
+		d = gcd(a, b);
+		a /= d;
+		b /= d;
+		suma = a * sumb + suma * b;
+		sumb = b * sumb;
+		d = gcd(suma, sumb);
+		sumb /= d;
+		suma /= d;
+	}
+	long long integer = suma / sumb;
+	suma -= (sumb * integer);
+	if (integer != 0) {
+		printf("%lld", integer);
+		if (suma != 0) printf(" ");
+	}
+	if (suma != 0) printf("%lld/%lld", suma, sumb);
+	if (integer == 0 && suma == 0) printf("0");
+	return 0;
+}
