1f633383a89d0a04b7f006d2e46c07ff412e1ebb
vuzway9132 <13806680065@163.com>
2020-04-15 14:40:16 +0800

    -

--- cccc-gplt/L1-050 倒数第N个字符串 (15分).cpp
+++ cccc-gplt/L1-050 倒数第N个字符串 (15分).cpp
@@ -5,12 +5,11 @@ using namespace std;
 int main() {
 	int l, n;
 	cin >> l >> n;
-	int maxn = pow(26, l);
-	int x = maxn - n;
+	int pos = pow(26, l) - n;
 	string s;
 	for (int i = 0; i < l; i++) {
-		s += x%26 + 'a';
-		x /= 26;
+		s += pos%26 + 'a';
+		pos /= 26;
 	}
 	reverse(s.begin(), s.end());
 	cout << s << endl;
