2a9a715871a90f043c48a09c2c51741fa29c2e7f
vuzway9132 <13806680065@163.com>
2020-04-17 19:10:57 +0800

    -

--- cccc-gplt/L2-018 多项式A除以B (25分).cpp
+++ cccc-gplt/L2-018 多项式A除以B (25分).cpp
@@ -17,7 +17,7 @@ int main() {
 		if (i == 0) maxe2 = e;
 	}
 	for (int i = maxe1; i >= 0; i--) {
-		int d = i-maxe2;
+		int d = i - maxe2;
 		if (d < 0) break;
 		q[d] = a[i] / b[maxe2];
 		for (int j = maxe2; j >= 0; j--) a[j+d] -= b[j] * q[d];
