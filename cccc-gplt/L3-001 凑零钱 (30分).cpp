3039d8abcc0055a19b11672bc0f564aca66591c3
vuzway9132 <13806680065@163.com>
2020-04-19 22:54:14 +0800

    -

---
+++ cccc-gplt/L3-001 凑零钱 (30分).cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int n, m, flag = 0;
+vector<int> v, path, rest;
+void dfs(int idx, int sum) {
+	if (flag == 1 || sum+rest[idx] < m) return;
+	if (idx == n || sum >= m) {
+		if (sum == m) {
+			flag = 1;
+			for (int i = 0; i < path.size(); i++) {
+				if (i != 0) printf(" ");
+				printf("%d", path[i]);
+			}
+		}
+		return;
+	}
+	path.push_back(v[idx]);
+	dfs(idx + 1, sum + v[idx]);
+	path.pop_back();
+	dfs(idx + 1, sum);
+}
+int main() {
+	cin >> n >> m;
+	v.resize(n), rest.resize(n);
+	for (int i = 0; i < n; i++) scanf("%d", &v[i]);
+	sort(v.begin(), v.end());
+	for (int i = n-1; i >= 0; i--)
+		if (i == n-1) rest[i] = v[i];
+		else rest[i] = rest[i+1] + v[i];
+	dfs(0, 0);
+	if (flag == 0) printf("No Solution");
+	return 0;
+}
