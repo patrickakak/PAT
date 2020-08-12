bd93d3b9e939824f2d03115fefa367b98a62e037
vuzway9132 <13806680065@163.com>
2020-08-12 22:04:54 +0800

    -

--- basic level c++/1002 写出这个数 (20分).cpp
+++ basic level c++/1002 写出这个数 (20分).cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-string ss[10] = {"ling","yi","er","san","si","wu","liu","qi","ba","jiu"};
 int main() {
 	string s;
 	cin >> s;
@@ -8,6 +7,7 @@ int main() {
 	for (int i = 0; i < s.length(); i++)
 		sum += s[i] - '0';
 	string num = to_string(sum);
+	string ss[] = {"ling","yi","er","san","si","wu","liu","qi","ba","jiu"};
 	for (int i = 0; i < num.length(); i++) {
 		cout << ss[num[i] - '0'];
 		if (i != num.length()-1) cout << " ";
