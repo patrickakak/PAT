56f3536ecaa9e1f5816dda6d45931fb09aa451dd
vuzway9132 <13806680065@163.com>
2020-08-12 15:44:16 +0800

    -

--- basic level c++/1049 数列的片段和 (20分).cpp
+++ basic level c++/1049 数列的片段和 (20分).cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main() {
 	int n;
 	cin >> n;
-	double sum = 0, tmp;
+	long double sum = 0, tmp;
 	for (int i = 1; i <= n; i++) {
-		scanf("%lf", &tmp);
+		scanf("%Lf", &tmp);
 		sum += tmp * i * (n - i + 1);
 	}
-	printf("%.2f", sum);
+	printf("%.2Lf", sum);
 	return 0;
 }
