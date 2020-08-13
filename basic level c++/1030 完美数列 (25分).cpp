f74336d225fa433a9e95ca9491589021911d1b5b
vuzway9132 <13806680065@163.com>
2020-08-13 16:41:12 +0800

    -

--- basic level c++/1030 完美数列 (25分).cpp
+++ basic level c++/1030 完美数列 (25分).cpp
@@ -1,19 +1,22 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
 using namespace std;
 int main() {
-	int n, p, len = 0;
+	int n, p, d = 0;
 	cin >> n >> p;
 	vector<long long> v(n);
-	for (int i = 0; i < n; i++) scanf("%lld", &v[i]);
+	for (int i = 0; i < n; i++)
+		scanf("%lld", &v[i]);
 	sort(v.begin(), v.end());
-	for (int i = 0; i + len < n; i++) {
-		int j;
-		for (j = i + len; j < n; j++)
-			if (v[j] > p * v[i]) break;
-		if (j - i > len) len = j - i;
+	for (int i = 0; i < n; i++)
+		if (v[i] > v[0]*p) break;
+		else d++;
+	for (int i = 1; i < v.size(); ) {
+		if (i+d >= v.size()) break;
+		if (v[i+d] <= v[i]*p) d++;
+		else i++;
 	}
-	printf("%d", len);
+	printf("%d\n", d);
 	return 0;
 }
