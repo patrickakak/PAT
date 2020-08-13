20619c5aab935b38800db87b49115b5d7484b481
vuzway9132 <13806680065@163.com>
2020-08-13 10:40:04 +0800

    -

--- advanced level c++/1062 Talent and Virtue (25).cpp
+++ advanced level c++/1062 Talent and Virtue (25).cpp
@@ -2,33 +2,31 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
-struct node {
-	int num, de, cai;
+struct stu {
+	int id, d, c, r;
 };
-int cmp(struct node a, struct node b) {
-	if ((a.de + a.cai) != (b.de + b.cai)) return (a.de + a.cai) > (b.de + b.cai);
-	else if (a.de != b.de) return a.de > b.de;
-	else return a.num < b.num;
+vector<stu> v;
+bool cmp(stu &a, stu &b) {
+	if (a.r != b.r) return a.r < b.r;
+	else if (a.d+a.c != b.d+b.c) return a.d+a.c > b.d+b.c;
+	else if (a.d != b.d) return a.d > b.d;
+	else return a.id < b.id;
 }
 int main() {
-	int n, low, high;
-	scanf("%d %d %d", &n, &low, &high);
-	vector<node> v[4];
-	node temp;
-	int total = n;
+	int n, l, h, id, d, c;
+	cin >> n >> l >> h;
 	for (int i = 0; i < n; i++) {
-		scanf("%d %d %d", &temp.num, &temp.de, &temp.cai);
-		if (temp.de < low || temp.cai < low) total--;
-		else if (temp.de >= high && temp.cai >= high) v[0].push_back(temp);
-		else if (temp.de >= high && temp.cai < high) v[1].push_back(temp);
-		else if (temp.de < high && temp.cai < high && temp.de >= temp.cai) v[2].push_back(temp);
-		else v[3].push_back(temp);
+		scanf("%d %d %d", &id, &d, &c);
+		if (d>=l && c>=l) {
+			if (d>=h && c>=h) v.push_back({id, d, c, 1});
+			else if (d >= h) v.push_back({id, d, c, 2});
+			else if (d<h && c<h && d>=c) v.push_back({id, d, c, 3});
+			else v.push_back({id, d, c, 4});
+		}
 	}
-	printf("%d\n", total);
-	for (int i = 0; i < 4; i++) {
-		sort(v[i].begin(), v[i].end(), cmp);
-		for (int j = 0; j < v[i].size(); j++)
-			printf("%d %d %d\n", v[i][j].num, v[i][j].de, v[i][j].cai);
-	}
+	sort(v.begin(), v.end(), cmp);
+	printf("%d\n", (int)v.size());
+	for (int i = 0; i < v.size(); i++)
+		printf("%08d %d %d\n", v[i].id, v[i].d, v[i].c);
 	return 0;
 }
