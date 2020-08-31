33d5cf5774f022f517004ae1b16a558395c849d0
vuzway9132 <13806680065@163.com>
2020-08-31 18:27:07 +0800

    -

--- data structures and algorithms/7-23 Document Distance (35).py
+++ data structures and algorithms/7-23 Document Distance (35).py
@@ -1,7 +1,6 @@
 import math, string, sys
 
-translation_table = string.maketrans(string.punctuation+string.uppercase,
-                                     " "*len(string.punctuation)+string.lowercase)
+translation_table = string.maketrans(string.punctuation+string.uppercase, " "*len(string.punctuation)+string.lowercase)
 
 def get_words_from_line_list(text):
     text = text.translate(translation_table)
@@ -19,6 +18,17 @@ def count_frequency(word_list):
 
 def word_frequencies_for_file(text):
     word_list = get_words_from_line_list(text)
+    for i in range(len(word_list)):
+        w=word_list[i]
+        l=len(w)
+        if l>3 and w[l-3:]=='ing':
+            word_list[i]=w[:l-3]
+        elif l>3 and w[l-3:]=='ies':
+            word_list[i]=w[:l-3]+'y'
+        elif l>2 and w[l-2:]=='es':
+            word_list[i]=w[:l-2]
+        elif l>2 and w[l-2:]=='ed':
+            word_list[i]=w[:l-2]  
     freq_mapping = count_frequency(word_list)
     return freq_mapping
 
@@ -35,26 +45,24 @@ def vector_angle(D1,D2):
     return math.acos(numerator/denominator)
 
 def main():
-    n=input()
-    d={}
+    n = input()
+    d = {}
     for i in range(n):
-        title=raw_input()
-        s=' '
-        text=' '
-        while s!='#':
-            s=raw_input()
-            if s=='#':
-                break
-            else:
-                text+=' '+s
-        sorted_word_list=word_frequencies_for_file(text)
-        d[title]=sorted_word_list
-    m=input()
+        title = raw_input()
+        s, text = ' ', ' '
+        while s != '#':
+            s = raw_input()
+            if s == '#': break
+            else: text += ' '+s
+        sorted_word_list = word_frequencies_for_file(text)
+        d[title] = sorted_word_list
+    m = input()
     for i in range(m):
-        s=raw_input().split()
-        a, b=s[0], s[1]
+        s = raw_input().split()
+        a, b = s[0], s[1]
         distance = vector_angle(d[a], d[b])
         print "Case %d: %0.3f"  %(i+1, distance)
 
 if __name__ == "__main__":
     main()
+
